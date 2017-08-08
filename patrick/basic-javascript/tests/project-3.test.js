480843adf9755a39bc064175a4ac86f761824620
mixelpixel <mixelpix@mac.com>
2017-08-08 15:18:24 -0400

    basicJS project-3 BNF setup

--- patrick/basic-javascript/tests/project-3.test.js
+++ patrick/basic-javascript/tests/project-3.test.js
@@ -2,3 +2,38 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-3');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+// makeCat,
+// addProperty,
+// invokeMethod,
+// multiplyMysteryNumberByFive,
+// deleteProperty,
+// newUser,
+// hasEmail,
+// hasProperty,
+// verifyPassword,
+// updatePassword,
+// addFriend,
+// setUsersToPremium,
+// sumUserPostLikes,
+// addCalculateDiscountPriceMethod
+
+describe('Project-3 Functions', () => {
+
+  describe('NAME', () => {
+    it.skip('should be a function', () => {
+      const NAME = funcs.NAME;
+      assert.typeOf(NAME, 'function');
+      assert.isFunction(NAME);
+    });
+    it.skip('should return a TYPE', () => {
+      const NAME = funcs.NAME;
+      asset.isTYPE(NAME(PARAMETERS), TYPE)
+    });
+    it.skip('should return a VALUE', () => {
+      const NAME = funcs.NAME;
+      asset.isVALUE(NAME(PARAMETERS), VALUE)
+    });
+  });
+
+});
