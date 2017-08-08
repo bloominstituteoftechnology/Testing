53f63a3d76624b647a5a1cc08e63e0f2f058ded1
mixelpixel <mixelpix@mac.com>
2017-08-08 17:33:54 -0400

    basicJS 3 addProperty √

--- patrick/basic-javascript/tests/project-3.test.js
+++ patrick/basic-javascript/tests/project-3.test.js
@@ -6,258 +6,210 @@ const funcs = require('../src/project-3');
 describe('Project-3 Functions', () => {
 
   describe('makeCat', () => {
+    const makeCat = funcs.makeCat;
     it('should be a function', () => {
-      const makeCat = funcs.makeCat;
-      assert.typeOf(makeCat, 'function');
       assert.isFunction(makeCat);
     });
-    it('should return a `object`', () => {
-      const makeCat = funcs.makeCat;
+    it('should return an `Object`', () => {
       assert.isObject(makeCat('Eddie Spagetti', 4))
     });
     it('new cat Object includes `{name: \'Eddie Spagetti\'}`', () => {
-      const makeCat = funcs.makeCat;
       assert.include(makeCat('Eddie Spagetti', 4),
-        {name: 'Eddie Spagetti'})
+        {name: 'Eddie Spagetti'});
     });
     it('new cat Object includes `{age: 4}`', () => {
-      const makeCat = funcs.makeCat;
-      assert.include(makeCat('Eddie Spagetti', 4),
-        {age: 4})
+      assert.include(makeCat('Eddie Spagetti', 4), {age: 4});
+      assert.propertyVal(makeCat('Eddie Spagetti', 4), 'age', 4);
     });
-    it('new cat Object includes `{meow}`', () => {
-      const makeCat = funcs.makeCat;
-      assert.property(makeCat('nyan', 52), 'meow')
-      assert.isFunction(makeCat('nyan', 52).meow)
+    it('new cat Object includes `[Function: meow]]`', () => {
+      assert.property(makeCat('nyan', 2), 'meow');
+      assert.isFunction(makeCat('nyan', 2).meow);
+      // assert.propertyVal(makeCat('nyan', 2), 'meow', Function); // <~~~~ ????????????????????/
+      // assert.propertyVal(makeCat('nyan', 2), 'meow', [Function: meow]); // <~~~~ ????????????????????/
+      // assert.propertyVal(makeCat('nyan', 2), 'meow', 'Function'); // <~~~~ ????????????????????/
+      // assert.propertyVal(makeCat('nyan', 2), 'meow', [Function: Function]); // <~~~~ ????????????????????/
+
+    });
+  });
+
+  describe('addProperty', () => {
+    const addProperty = funcs.addProperty;
+    it('should be a function', () => {
+      assert.isFunction(addProperty);
+    });
+    it('should return an `Object`', () => {
+      assert.isObject(addProperty({}, 1));
+    });
+    it('should return the added proprty', () => {
+      assert.property(addProperty({}, 1), 1);
+      assert.propertyVal(addProperty({}, 1), 1, null);
+      // console.log(addProperty({}, 1));
+      // console.log('\n\n\n');
     });
   });
 
   describe('NAME', () => {
+    const NAME = funcs.NAME;
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
+      assert.isVALUE(NAME(PARAMETERS), VALUE)
+    });
+  });
+
+  describe('NAME', () => {
+    const NAME = funcs.NAME;
+    it.skip('should be a function', () => {
+      assert.isFunction(NAME);
+    });
+    it.skip('should return a `TYPE`', () => {
+      assert.isTYPE(NAME(PARAMETERS), TYPE)
+    });
+    it.skip('should return a `VALUE`', () => {
+      assert.isVALUE(NAME(PARAMETERS), VALUE)
+    });
+  });
+
+  describe('NAME', () => {
+    const NAME = funcs.NAME;
+    it.skip('should be a function', () => {
+      assert.isFunction(NAME);
+    });
+    it.skip('should return a `TYPE`', () => {
+      assert.isTYPE(NAME(PARAMETERS), TYPE)
+    });
+    it.skip('should return a `VALUE`', () => {
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
 
   describe('NAME', () => {
     it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
       assert.isFunction(NAME);
     });
     it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
       assert.isTYPE(NAME(PARAMETERS), TYPE)
     });
     it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
       assert.isVALUE(NAME(PARAMETERS), VALUE)
     });
   });
-
-  describe('NAME', () => {
-    it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
-      assert.isFunction(NAME);
-    });
-    it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
-      assert.isTYPE(NAME(PARAMETERS), TYPE)
-    });
-    it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
-      assert.isVALUE(NAME(PARAMETERS), VALUE)
-    });
-  });
-
-  describe('NAME', () => {
-    it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
-      assert.isFunction(NAME);
-    });
-    it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
-      assert.isTYPE(NAME(PARAMETERS), TYPE)
-    });
-    it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
-      assert.isVALUE(NAME(PARAMETERS), VALUE)
-    });
-  });
-
-  describe('NAME', () => {
-    it.skip('should be a function', () => {
-      const NAME = funcs.NAME;
-      assert.typeOf(NAME, 'function');
-      assert.isFunction(NAME);
-    });
-    it.skip('should return a `TYPE`', () => {
-      const NAME = funcs.NAME;
-      assert.isTYPE(NAME(PARAMETERS), TYPE)
-    });
-    it.skip('should return a `VALUE`', () => {
-      const NAME = funcs.NAME;
-      assert.isVALUE(NAME(PARAMETERS), VALUE)
-    });
-  });
-
 
 });
+
 
 // // TEMPLATE
 // describe('Project-# Functions', () => {
 //
 //   describe('NAME', () => {
+//     const NAME = funcs.NAME;
 //     it.skip('should be a function', () => {
-//       const NAME = funcs.NAME;
-//       assert.typeOf(NAME, 'function');
 //       assert.isFunction(NAME);
 //     });
 //     it.skip('should return a `TYPE`', () => {
-//       const NAME = funcs.NAME;
 //       assert.isTYPE(NAME(PARAMETERS), TYPE)
 //     });
 //     it.skip('should return a `VALUE`', () => {
-//       const NAME = funcs.NAME;
 //       assert.isVALUE(NAME(PARAMETERS), VALUE)
 //     });
 //   });
