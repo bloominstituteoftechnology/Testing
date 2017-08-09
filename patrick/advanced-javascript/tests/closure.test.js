10e85fd99a37e6924a4261d2261f281302ce80c8
mixelpixel <mixelpix@mac.com>
2017-08-09 14:49:02 -0400

    was `const assert = require('chai').expect;` intentional?

--- patrick/advanced-javascript/tests/closure.test.js
+++ patrick/advanced-javascript/tests/closure.test.js
@@ -2,3 +2,35 @@ const assert = require('chai').expect;
 const funcs = require('../src/closure');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('Closure', () => {
+
+  describe('`counter`', () => {
+    it('should be a function', () => {
+      const counter = funcs.counter;
+      expect(counter).to.be.a('function');
+    });
+  });
+
+  describe('`counterFactory`', () => {
+    it('should be a function', () => {
+      const counterFactory = funcs.counterFactory;
+      expect(counterFactory).to.be.a('function');
+    });
+  });
+
+  describe('`cacheFunction`', () => {
+    it('should be a function', () => {
+      const cacheFunction = funcs.cacheFunction;
+      expect(cacheFunction).to.be.a('function');
+    });
+  });
+
+  describe('`limitFunctionCallCount`', () => {
+    it('should be a function', () => {
+      const limitFunctionCallCount = funcs.limitFunctionCallCount;
+      expect(limitFunctionCallCount).to.be.a('function');
+    });
+  });
+
+});
