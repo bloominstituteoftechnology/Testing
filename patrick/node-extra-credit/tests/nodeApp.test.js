55d7b2acbbd139c661c01b601e2856f980006073
mixelpixel <mixelpix@mac.com>
2017-08-09 15:24:22 -0400

    nodeEC - function check √

--- patrick/node-extra-credit/tests/nodeApp.test.js
+++ patrick/node-extra-credit/tests/nodeApp.test.js
@@ -3,10 +3,31 @@ const expect = chai.expect;
 const nodeFuncs = require('../src/nodeApp.js');
 
 
-// for the extra credit you have two functions to provide test coverage for.
-// you'll check to see what the return data is coming back from your getter fn. eg. type, array of objects and length of the array etc.
-// you'll then check the people setter to see if it actually does what it's supposed to with the people passed in.
+// for the extra credit you have two functions to provide test coverage for. - OKAY √
+// you'll check to see what the return data is coming back from your getter fn.
+  // eg. type, array of objects and length of the array etc. - OKAY √
+// you'll then check the people setter to see if it actually does
+// what it's supposed to with the people passed in. - OKAY √
 
-// this isn't a real live api, you'd normally have to deal with some async actions etc. for this, you'll simply be updating a static array of users.
+// this isn't a real live api, you'd normally have to deal with some async
+// actions etc. for this, you'll simply be updating a static array of users. - OKAY √
 
-// have fun!
+// have fun! - YaY √
+
+describe('Node Extra Credit Tests', () => {
+
+  describe('`peopleGetter`', () => {
+    it('should be an `Object`', () => {
+      const peopleGetter = nodeFuncs.peopleGetter;
+      expect(peopleGetter).to.be.an('function');
+    });
+  });
+
+  describe('`peopleSetter`', () => {
+    it('should be an `Object`', () => {
+      const peopleSetter = nodeFuncs.peopleSetter;
+      expect(peopleSetter).to.be.an('function');
+    });
+  });
+
+});
