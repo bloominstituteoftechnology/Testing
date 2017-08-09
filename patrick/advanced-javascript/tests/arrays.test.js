e888b7fccb2bef193d8f4c8bacc8740711ba1bd3
mixelpixel <mixelpix@mac.com>
2017-08-08 21:38:16 -0400

    advJS-arrays all functions √

--- patrick/advanced-javascript/tests/arrays.test.js
+++ patrick/advanced-javascript/tests/arrays.test.js
@@ -5,9 +5,9 @@ const arrayFunctions = require('../src/arrays');
 const expect = chai.expect;
 chai.use(sinonChai);
 
-// we've gone ahead and gotten a start here for you, 
+// we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
-// first step is to make sure all these assertions work. 
+// first step is to make sure all these assertions work.
 // then make sure you have at least 2-3 more assertions for every function.
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
@@ -18,7 +18,7 @@ describe('Arrays', () => {
   describe('`each`', () => {
     it('should be a function', () => {
       const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+      expect(each).to.be.a('function');
     });
     // begin here
   });
@@ -26,36 +26,36 @@ describe('Arrays', () => {
   describe('`map`', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.an('function');
     });
   });
 
   describe('`reduce`', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
     });
   });
 
   describe('`find`', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
-      expect(find).to.be.an('array');
+      expect(find).to.be.an('function');
     });
   });
 
   describe('`filter`', () => {
     it('should be a function', () => {
       const filter = arrayFunctions.filter;
-      expect(filter).to.be.a('null');
+      expect(filter).to.be.a('function');
     });
   });
 
   describe('`flatten`', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
-      expect(flatten).to.be.a('promise');
+      expect(flatten).to.be.a('function');
     });
   });
-  
+
 });
