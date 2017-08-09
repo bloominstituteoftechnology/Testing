e520363473aedbe1445912518fb62d503725b4e5
Lois Truby <loyslane@gmail.com>
2017-08-09 16:49:04 -0400

    deleted original files; all completed files are in Lois and patrick folders

--- advanced-javascript/tests/objects.test.js
+++
@@ -1,4 +0,0 @@
-const assert = require('chai').expect;
-const objectFunctions = require('../src/objects');
-
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
