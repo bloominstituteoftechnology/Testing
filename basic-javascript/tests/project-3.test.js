1ab8c89b7ab1586f04d7fb230a8d079986695894
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2017-10-11 15:55:48 -0400

    Testing

--- basic-javascript/tests/project-3.test.js
+++ basic-javascript/tests/project-3.test.js
@@ -1,4 +1,22 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const funcs = require('../src/project-3');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project 3 Functions', () => {
+  describe('makeCat', () => {
+    it('should be a funtion', () => {
+      const makeCat = funcs.makeCat;
+      expect(makeCat).to.be.a('function');
+    });
+    it('should return an object', () => {
+      const makeCat = funcs.makeCat;
+      expect(makeCat('Chris', 21)).to.be.an('object');
+    });
+    it('should return Meow when meow is invoked', () => {
+      const makeCat = funcs.makeCat;
+      const newCat = makeCat('Chris', 21);
+      expect(newCat.meow()).to.equal('Meow!');
+    });
+  });
+});
--- basic-javascript/tests/project-4.test.js
+++ basic-javascript/tests/project-4.test.js
@@ -1,4 +1,10 @@
-const assert = require('chai').assert;
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const funcs = require('../src/project-4.js');
+
+const expect = chai.expect;
+const assert = chai.assert;
+chai.use(sinonChai);
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
