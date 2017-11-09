264258da839701b37e60a1fa5f75697182948e9c
Perry Ahern <pahern@gmail.com>
2017-11-08 20:05:24 -0500

    Some test results: all of 1, part of 2, makeCat in 3. Waaaaay too many problems for 4 hours.

--- basic-javascript/tests/project-3.test.js
+++ basic-javascript/tests/project-3.test.js
@@ -1,4 +1,34 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const funcs = require('../src/project-3');
+const cases = require('../src/project-3.js');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-3 Functions', () => {
+  describe('`makeCat`', () => {
+    it('should be a function', () => {
+      const makeCat = funcs.makeCat;
+      assert.typeOf(makeCat, 'function');
+    })
+  });
+  it('should have name Foofie', () => {
+    const makeCat = cases.makeCat;
+    const newCat = makeCat('Foofie', 3);
+    expect(newCat.name).to.equal('Foofie');
+  });
+  it('should have age 3', () => {
+    const makeCat = cases.makeCat;
+    const newCat = makeCat('Foofie', 3);
+    expect(newCat.age).to.equal(3);
+  });
+  it('should say meow is a function', () => {
+    const makeCat = cases.makeCat;
+    const newCat = makeCat('Foofie', 3);
+    assert.typeOf(newCat.meow, 'function');
+  });
+  it('should return Meow! from meow()', () => {
+    const makeCat = cases.makeCat;
+    const newCat = makeCat('Foofie', 3);
+    expect(newCat.meow()).to.equal('Meow!');
+  });
+
+});
