61a37483f615ed17d16aae1a9e5ab28045d8c8a0
invegat <mark.c.oliver@gmail.com>
2017-11-09 18:13:54 -0500

    done

--- node-extra-credit/tests/nodeApp.test.js
+++ node-extra-credit/tests/nodeApp.test.js
@@ -1,7 +1,11 @@
 const chai = require('chai');
-const nodeFuncs = require('../src/nodeApp.js');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
 const expect = chai.expect;
+chai.use(sinonChai);
+
+const nodeFuncs = require('../src/nodeApp.js');
 
 // for the extra credit you have two functions to provide test coverage for.
 // you'll check to see what the return data is coming back from your getter fn. eg. type, array of objects and length of the array etc.
@@ -10,3 +14,29 @@ const expect = chai.expect;
 // this isn't a real live api, you'd normally have to deal with some async actions etc. for this, you'll simply be updating a static array of users.
 
 // have fun!
+describe('nodeApp', () => {
+  describe('peopleGetter', () => {
+    const peopleGetter = nodeFuncs.peopleGetter;
+    it('should be a function', () => {
+      expect(peopleGetter).to.be.a('function');
+    });
+    it('should return the users array', () => {
+      expect(peopleGetter().length).to.be.eq(10);
+    });
+  });
+  describe('peopleSetter', () => {
+    const peopleSetter = nodeFuncs.peopleSetter;
+    it('should be a function', () => {
+      expect(peopleSetter).to.be.a('function');
+    });
+    it('should return the users array', () => {
+      const user = {
+        id: 11,
+        first_name: 'Mark',
+        last_name: 'Oliver',
+        email: 'mark.c.oliver@gmail.com'
+      };
+      expect(peopleSetter(user).length).to.be.eq(11);
+    });
+  });
+});
