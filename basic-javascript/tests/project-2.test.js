619f1db27ec89a882ae0e21b6b601c1807a91e19
Cole Phillips <nphillips78@gmail.com>
2018-01-29 18:20:54 -0500

    initial commit

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -2,3 +2,24 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('PROJECT 2', () => {
+  describe('getBiggest', () => {});
+  describe('greeting', () => {});
+  isTenOrFive,
+    isInRange,
+    isInteger,
+    fizzBuzz,
+    isPrime,
+    returnFirst,
+    returnLast,
+    getArrayLength,
+    incrementByOne,
+    addItemToArray,
+    addItemToFront,
+    wordsToSentence,
+    contains,
+    addNumbers,
+    averageTestScore,
+    largestNumber;
+});
