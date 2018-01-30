9c454a0a5b444fce793bd83217a3d3272d5c38fe
atiffany <abby.tiffany@gmail.com>
2018-01-29 19:53:02 -0800

    completed 3/4s

--- advanced-javascript/tests/arrays.test.js
+++ advanced-javascript/tests/arrays.test.js
@@ -18,43 +18,108 @@ describe('Arrays', () => {
   describe('`each`', () => {
     it('should be a function', () => {
       const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+      expect(each).to.be.a('function');
     });
     // begin here
+    it('should execute call back at least once', () => {
+      const callBack = sinon.spy();
+      const each = arrayFunctions.each;
+      each([1], callBack);
+      expect(callBack).to.have.callCount(1);
+    });
   });
 
   describe('`map`', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.an('function');
+    });
+    it('should execute the callback at least once', () => {
+      const callBack = sinon.spy();
+      const map = arrayFunctions.map;
+      map([1], callBack);
+      expect(callBack).to.have.callCount(1);
+    });
+    it('should return a type of array', () => {
+      const callBack = sinon.spy();
+      const map = arrayFunctions.map;
+      expect(map([1], callBack)).to.be.an('array');
     });
   });
 
   describe('`reduce`', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
+    });
+    it('should execute the callback at least once', () => {
+      const callBack = sinon.spy();
+      const reduce = arrayFunctions.reduce;
+      reduce([1, 2], callBack, 0);
+      expect(callBack).to.have.callCount(2);
+    });
+    it('should return a number', () => {
+      const reduce = arrayFunctions.reduce;
+      const reducer = (acc, curr) => acc + curr;
+      expect(reduce([12, 12, 11], reducer, 0)).to.be.a('number');
+    });
+    it('should perform correct calculations using the callback provided', () => {
+      const reduce = arrayFunctions.reduce;
+      const reducer = (acc, curr) => acc * curr;
+      expect(reduce([1, 2, 3, 4, 5], reducer, 1)).to.equal(120);
     });
   });
 
   describe('`find`', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
-      expect(find).to.be.an('array');
+      expect(find).to.be.an('function');
+    });
+    it('should find the item we are searching for', () => {
+      const find = arrayFunctions.find;
+      const callBack = (element) => {
+        return element === 'turtle' ? element : undefined;
+      };
+      expect(find([1, 2, 'turtle', 'blue'], callBack)).to.equal('turtle');
+    });
+    it('should return an item with the same type as what we are seaching for', () => {
+      const find = arrayFunctions.find;
+      const callBack = (element) => {
+        return element === 3 ? element : undefined;
+      };
+      expect(find([1, 3, 'orange', 'banana'], callBack)).to.be.a('number');
     });
   });
 
   describe('`filter`', () => {
     it('should be a function', () => {
       const filter = arrayFunctions.filter;
-      expect(filter).to.be.a('null');
+      expect(filter).to.be.a('function');
+    });
+    it('should return items that meet the filter', () => {
+      const filter = arrayFunctions.filter;
+      const callBack = (element) => { return element > 10; };
+      expect(filter([1, 133, 244, 32, 3], callBack)).to.eql([133, 244, 32]);
+    });
+    it('should return an array', () => {
+      const filter = arrayFunctions.filter;
+      const callBack = (element) => { return element > 1; };
+      expect(Array.isArray(filter([1, 2, 3], callBack))); // this gave me hell, tried to make it work with typeof for a long time
     });
   });
 
   describe('`flatten`', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
-      expect(flatten).to.be.a('promise');
+      expect(flatten).to.be.a('function');
+    });
+    it('should flatten nested arrays', () => {
+      const flatten = arrayFunctions.flatten;
+      expect(flatten([1, 2, [3, [4]]])).to.eql([1, 2, 3, 4]);
+    });
+    it('should return an array', () => {
+      const flatten = arrayFunctions.flatten;
+      expect(Array.isArray(flatten([1, 2, [3, [4, [5]]]])));
     });
   });
 });
--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -3,5 +3,29 @@ const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('PROJECT 2', () => {
-    
+  let standardMockArray = [];
+  beforeEach(() => {
+    standardMockArray = [1, 2, 3, 4];
+  });
+  afterEach(() => {
+    standardMockArray = [];
+  });
+  describe('getBiggest', () => {});
+  describe('greeting', () => {});
+  describe('isTenOrFive', () => {});
+  describe('isInRange', () => {});
+  describe('isInteger', () => {});
+  describe('fizzBuzz', () => {});
+  describe('isPrime', () => {});
+  describe('returnFirst', () => {});
+  describe('returnLast', () => {});
+  describe('getArrayLength', () => {});
+  describe('incrementByOne', () => {});
+  describe('addItemToArray', () => {});
+  describe('addItemToFront', () => {});
+  describe('wordsToSentence', () => {});
+  describe('contains', () => {});
+  describe('addNumbers', () => {});
+  describe('averageTestScore', () => {});
+  describe('largestNumber', () => {});
 });
