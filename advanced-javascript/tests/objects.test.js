e137f4768913b55b9bce476d3d650178aebf5246
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-03-13 18:32:47 -0400

    Finished all tests

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 const objectFunctions = require('../src/objects');
@@ -42,4 +44,40 @@ describe('objects', () => {
   		assert.typeOf(funcs.mapObject({a: 1, b: 2}, double), 'object');
   	});
   });
+
+  describe('pairs', () => {
+    it('should convert an object into a nested array of key-value pairs', () => {
+      assert(funcs.pairs({a: 1, b: 2}), [['a', 1], ['b', 2]]);
+    });
+    it('should be a function', () => {
+      assert.typeOf(funcs.pairs, 'function');
+    });
+    it('should return an array', () => {
+      assert.typeOf(funcs.pairs({a: 1, b: 2}), 'array');
+    });
+  })
+
+  describe('invert', () => {
+    it('should be a function', () => {
+      assert.typeOf(funcs.invert, 'function');
+    });
+    it('should return an object', () => {
+      assert.typeOf(funcs.invert({a: 1, b: 2}), 'object');
+    });
+    it('should return a new object where the keys becomes the values and the values become the keys', () => {
+      assert(funcs.invert({a: 1, b: 2}), {1: 'a', 2: 'b'});
+    });
+  })
+
+  describe('defaults', () => {
+    it('should be a function', () => {
+      assert.typeOf(funcs.defaults, 'function');
+    });
+    it('should return an object', () => {
+      assert.typeOf(funcs.defaults({a: 1, b: 2}, {default: 'default'}), 'object');
+    });
+    it('should update default properties on first object passed as a parameter', () => {
+      assert(funcs.defaults({a: 1, b: 2}, {default: 'default'}), {a: 1, b: 2, default: 'default'});
+    });
+  })
 });
