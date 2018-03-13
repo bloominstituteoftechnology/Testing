2296279e6f0289def2f6c030233f7f6bc8a4ad3e
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-03-13 17:36:03 -0500

    objects.test.functioning

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -3,7 +3,40 @@ const objectFunctions = require('../src/objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {
-
+  describe('`keys`', () => {
+    it('should be a function', () => {
+      const { keys } = objectFunctions;
+      assert(keys).to.be.a('function');
+    });
+  });
+  describe('`values`', () => {
+    it('should be a function', () => {
+      const { values } = objectFunctions;
+      assert(values).to.be.a('function');
+    });
+  });
+  describe('`mapObject`', () => {
+    it('should be a function', () => {
+      const { mapObject } = objectFunctions;
+      assert(mapObject).to.be.a('function');
+    });
+  });
+  describe('`pairs`', () => {
+    it('should be a function', () => {
+      const { pairs } = objectFunctions;
+      assert(pairs).to.be.a('function');
+    });
+  });
+  describe('`invert`', () => {
+    it('should be a function', () => {
+      const { invert } = objectFunctions;
+      assert(invert).to.be.a('function');
+    });
+  });
+  describe('`defaults`', () => {
+    it('should be a function', () => {
+      const { defaults } = objectFunctions;
+      assert(defaults).to.be.a('function');
+    });
   });
 });
