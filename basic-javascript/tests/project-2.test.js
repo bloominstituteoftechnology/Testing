bdc2f9141f7b0c721030e82b39646246a3ed78f6
itsdevinci <devinbaldwin315@gmail.com>
2018-03-26 14:15:39 -0700

    Making progress with the basic-javascript test suite -finished project-1 -working on project-2

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -3,4 +3,56 @@
 const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-2 Functions', () => {
+  describe('getBiggest', () => {
+    it('should be a function', () => {
+      const getBiggest = funcs.getBiggest;
+      assert.typeOf(getBiggest, 'function');
+    });
+  });
+
+  describe('greeting', () => {
+    it('should be a function', () => {
+      const greeting = funcs.greeting;
+      assert.typeOf(greeting, 'function');
+    });
+    it('should return a string', () => {
+      const languange = funcs.greeting('German');
+      assert.typeOf(languange, 'string');
+    });
+  });
+  describe('isTenOrFive', () => {
+    it('should be a function', () => {
+      const isTenOrFive = funcs.isTenOrFive;
+      assert.typeOf(isTenOrFive, 'function');
+    });
+    it('should be a boolean', () => {
+      const num = funcs.isTenOrFive(10);
+      assert.typeOf(num, 'boolean');
+    });
+  });
+  describe('isInRange', () => {
+    it('should be a function', () => {
+      const isInRange = funcs.isInRange;
+      assert.typeOf(isInRange, 'function');
+    });
+    it('should be a boolean', () => {
+      const num = funcs.isInRange(50);
+      assert.typeOf(num, 'boolean');
+    });
+  });
+  describe('isInteger', () => {
+    it('should be a function', () => {
+      const isInteger = funcs.isInteger;
+      assert.typeOf(isInteger, 'function');
+    });
+    it('should be a boolean', () => {
+      const num = funcs.isInteger();
+      assert.typeOf(num, 'boolean');
+    });
+    it('should return true on integers', () => {
+      const results = funcs.isInteger(5);
+      assert.equal(results, true);
+    });
+  });
+});
