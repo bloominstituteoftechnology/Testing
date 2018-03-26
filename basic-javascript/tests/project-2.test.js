78627b99d4157d85eae5455f4bc2891cc2f499aa
Peter Gray <peter@petergraycreative.com>
2018-03-26 14:31:13 -0500

    Add test for getBiggest in basic js project 2
    Added 4 assertions to test function

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -4,3 +4,23 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-2 Functions', () => {
+  describe('`getBiggest`', () => {
+    it('should be a function', () => {
+      const getBiggest = funcs.getBiggest;
+      assert.typeOf(getBiggest, 'function');
+    });
+    it('should return a number', () => {
+      const getBiggest = funcs.getBiggest(5, 4);
+      assert.typeOf(getBiggest, 'number');
+    });
+    it('should return the biggest number', () => {
+      const getBiggest = funcs.getBiggest(5, 4);
+      assert.equal(getBiggest, 5, 'getBiggest(5, 4) should return 5.');
+    });
+    it('should return y if the numbers are equal', () => {
+      const getBiggest = funcs.getBiggest(5, 5);
+      assert.equal(getBiggest, 5, 'getBiggest(5, 5) should return 5.');
+    });
+  });
+});
