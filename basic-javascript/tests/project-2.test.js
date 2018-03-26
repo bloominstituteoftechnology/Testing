57d91f9fdcbd366169213707f1c099b597baf748
Peter Gray <peter@petergraycreative.com>
2018-03-26 15:38:07 -0500

    Add tests for incrementByOne, isPrime, greeting
    Also Corrected Array syntax errors

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -23,4 +23,50 @@ describe('Project-2 Functions', () => {
       assert.equal(getBiggest, 5, 'getBiggest(5, 5) should return 5.');
     });
   });
+
+  describe('`greeting`', () => {
+    it('should be a function', () => {
+      const greeting = funcs.greeting;
+      assert.typeOf(greeting, 'function');
+    });
+    it('should return a string', () => {
+      const greeting = funcs.greeting('German');
+      assert.typeOf(greeting, 'string');
+    });
+    it('should return Guten Tag', () => {
+      const greeting = funcs.greeting('German');
+      assert.equal(greeting, 'Guten Tag!', 'greeting should return Guten Tag!');
+    });
+  });
+
+  describe('`isPrime`', () => {
+    it('should be a function', () => {
+      const isPrime = funcs.isPrime;
+      assert.typeOf(isPrime, 'function');
+    });
+    it('should return a boolean', () => {
+      const isPrime = funcs.isPrime(5);
+      assert.isBoolean(isPrime, 'isPrime returns a boolean');
+    });
+    it('should return true', () => {
+      const isPrime = funcs.isPrime(5);
+      assert.isTrue(isPrime, 'isPrime returned true');
+    });
+  });
+
+  describe('incrementByOne', () => {
+    it('should be a function', () => {
+      const incrementByOne = funcs.incrementByOne;
+      assert.typeOf(incrementByOne, 'function');
+    });
+    it('should return an array', () => {
+      const incrementByOne = funcs.incrementByOne([1, 2, 3]);
+      assert.isArray(incrementByOne, 'returned an array');
+    });
+    it('should return an array with each element incremented by 1', () => {
+      const incrementByOne = funcs.incrementByOne([43, 53, 36]);
+      // const areEqual = [2, 3, 4].every((num, i) => num === incrementByOne[i]);
+      assert.sameOrderedMembers(incrementByOne, [44, 54, 37], 'same ordered members');
+    });
+  });
 });
