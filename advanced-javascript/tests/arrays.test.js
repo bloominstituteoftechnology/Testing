8c8c1ba19281b310f0c5f1935d3f34c83ac0e9f7
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-26 16:31:02 -0400

    Some Testers Functioning in Arrays
    array.js --- fixed linter errors.
    
    arrays.test.js --- midway through reduce.

--- advanced-javascript/tests/arrays.test.js
+++ advanced-javascript/tests/arrays.test.js
@@ -20,43 +20,76 @@ describe('Arrays', () => {
   describe('`each`', () => {
     it('should be a function', () => {
       const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+      expect(each).to.be.a('function');
     });
-    // begin here
+    it('should perform the callback', () => {
+      const each = arrayFunctions.each;
+      const callBack = sinon.spy();
+      each([1], callBack);
+      // Linter complains, this works too:
+      // expect(callBack).to.have.calledOnce;
+      expect(callBack).to.have.callCount(1);
+    });
+    it('should perform the callback 3 times', () => {
+      const each = arrayFunctions.each;
+      const callBack = sinon.spy();
+      each([1, 2, 3], callBack);
+      expect(callBack).to.have.callCount(3);
+    });
   });
 
   describe('`map`', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.a('function');
+    });
+    it('should perform the callback 4 times', () => {
+      const map = arrayFunctions.map;
+      const callBack = sinon.spy();
+      map([1, 2, 3, 4], callBack);
+      expect(callBack).to.have.callCount(4);
+    });
+    it('should return [1,4,9,16]', () => {
+      const map = arrayFunctions.map;
+      const callBack = element => {
+        return (element *= element);
+      };
+      const array = [1, 2, 3, 4];
+      expect(map(array, callBack)).to.deep.equal([1, 4, 9, 16]);
     });
   });
 
   describe('`reduce`', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
+    });
+    it('should perform the callback twice', () => {
+      const reduce = arrayFunctions.reduce;
+      const callBack = sinon.spy();
+      const array = [1, 2];
+      expect(callBack).to.have.calledTwice();
     });
   });
 
   describe('`find`', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
-      expect(find).to.be.an('array');
+      expect(find).to.be.a('function');
     });
   });
 
   describe('`filter`', () => {
     it('should be a function', () => {
       const filter = arrayFunctions.filter;
-      expect(filter).to.be.a('null');
+      expect(filter).to.be.a('function');
     });
   });
 
   describe('`flatten`', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
-      expect(flatten).to.be.a('promise');
+      expect(flatten).to.be.a('function');
     });
   });
 });
