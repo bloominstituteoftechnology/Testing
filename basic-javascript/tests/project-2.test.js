4954047c147ca3b5679348b5a5b61636c5f14888
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-26 15:10:16 -0500

    Begin Project-2 Tests
    in project-2.test.js:
    add assertions for getBiggest

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -2,5 +2,19 @@
 
 const assert = require('chai').assert;
 const funcs = require('../src/project-2');
-
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('Project-2 Functions', () => {
+  describe('`getBiggest`', () => {
+    const getBiggest = funcs.getBiggest;
+    it('should be a function', () => {
+      assert.typeOf(getBiggest, 'function');
+    });
+    it('should return a number', () => {
+      assert.isFinite(getBiggest(10, 5), true);
+    });
+    it('should return 10', () => {
+      assert.equal(getBiggest(10, 5), 10);
+    });
+  });
+});
