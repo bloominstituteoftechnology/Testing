049aa0f2801e8c5eb0fb74c1d59a7242fed118ec
johnjohnx4 <johnjohnx4@gmmail.com>
2018-03-26 15:50:19 -0400

    Started on project 2

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -1,6 +1,40 @@
 /* eslint-disable prefer-destructuring */
 
 const assert = require('chai').assert;
-const funcs = require('../src/project-2');
+const funcs = require ('../src/project-2');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-2 Functions', () => {
+    describe('getBiggest', () => {
+        it('Should be a function', () => {
+            assert.typeOf(funcs.getBiggest, 'function');
+        });
+        it('Should return 5', () => {
+            assert(funcs.getBiggest(3,5), 5);
+        });
+        it('Should return 5', () => {
+            assert(funcs.getBiggest(5,5), 5);
+        });
+        it('Should return 5', () => {
+            assert(funcs.getBiggest(5, 3), 5);
+        });
+    });
+
+
+    describe('greeting', () => {
+        it('Should be a function', () => {
+            assert.typeOf(funcs.greeting, 'function');
+        });
+        it('Should return Guten Tag!', () => {
+            assert(funcs.greeting('German', 'Guten Tag!'));
+        });
+        it('Should return Hola!', () => {
+            assert(funcs.greeting('Spanish', 'Hola!'));
+        });
+        it('Should return Guten Tag!', () => {
+            assert(funcs.greeting('Random', 'Hello!'));
+        });
+    })
+
+
+
+});
