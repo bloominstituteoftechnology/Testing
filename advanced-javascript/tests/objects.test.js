9530fbbf1900ab89e0ae19a4a5e813759043ac85
Res Yeg <shaqk702@gmail.com>
2018-03-26 19:46:43 -0700

    tests for pairs, invert and defaults in objects.test.js completed

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -4,7 +4,7 @@ const objectFunctions = require('../src/objects');
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
 	describe('`keys`', () => {
-    	it('should be an function', () => {
+    	it('should be a function', () => {
   			const keys = objectFunctions.keys;
   			expect(keys).to.be.an('function');
   		});
@@ -26,16 +26,58 @@ describe('objects', () => {
   		});
   	});
   	describe('`mapObject`', () => {
-    	it('should be an function', () => {
+    	it('should be a function', () => {
   			const mapObject = objectFunctions.mapObject;
   			expect(mapObject).to.be.an('function');
   		});
-  		it('should return', () => {
+  		it('should return two', () => {
   			const mapObject = objectFunctions.mapObject;
   			const result = mapObject({key1: 'one', key2: 'two'}, (value)=>{
   				return value;
   			});
   			expect(result['key2']).to.equal('two');
+  		});
+    });
+    describe('`pairs`', () => {
+    	it('should be a function', () => {
+  			const pairs = objectFunctions.pairs;
+  			expect(pairs).to.be.an('function');
+  		});
+  		it('should return key2', () => {
+  			const pairs = objectFunctions.pairs;
+        const result = pairs({key1: 'one', key2: 'two'});
+        // console.log(result);
+  			expect(result[1][0]).to.equal('key2');
+  		});
+    });
+    describe('`invert`', () => {
+    	it('should be a function', () => {
+  			const invert = objectFunctions.invert;
+  			expect(invert).to.be.an('function');
+  		});
+  		it('should return key2', () => {
+  			const invert = objectFunctions.invert;
+        const result = invert({key1: 'one', key2: 'two'});
+        // console.log(result);
+  			expect(result['two']).to.equal('key2');
+  		});
+    });
+    describe('`defaults`', () => {
+    	it('should be a function', () => {
+  			const defaults = objectFunctions.defaults;
+  			expect(defaults).to.be.an('function');
+  		});
+  		it('should return one', () => {
+  			const defaults = objectFunctions.defaults;
+        const result = defaults({}, {key1: 'one', key2: 'two'});
+        // console.log(result);
+  			expect(result['key1']).to.equal('one');
+      });
+      it('should return null', () => {
+  			const defaults = objectFunctions.defaults;
+        const result = defaults({key1: 'key'}, {key1: 'one', key2: 'two'});
+        // console.log(result);
+  			expect(result['key1']).to.equal('key');
   		});
   	});
 });
