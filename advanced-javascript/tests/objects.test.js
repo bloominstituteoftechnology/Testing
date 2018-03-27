e3c4d022630cf0b8941550160b2bdb4a884751b3
Mephestys <mephestys@gmail.com>
2018-03-26 20:32:02 -0600

    Created scaffolding for 6 object tests - Currently just expects the `function` type for each

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,9 +1,49 @@
-const assert = require('chai').expect;
+/* eslint-disable prefer-destructuring, no-unused-expressions */
+
+const expect = require('chai').expect;
 const objectFunctions = require('../src/objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {
+describe('Objects', () => {
+  describe('`keys`', () => {
+    const keys = objectFunctions.keys;
+    it('should be a function', () => {
+      expect(keys).to.be.a('function');
+    });
+  });
 
+  describe('`values`', () => {
+    const values = objectFunctions.values;
+    it('should be a function', () => {
+      expect(values).to.be.a('function');
+    });
+  });
+
+  describe('`mapObject`', () => {
+    const mapObject = objectFunctions.mapObject;
+    it('should be a function', () => {
+      expect(mapObject).to.be.a('function');
+    });
+  });
+
+  describe('`pairs`', () => {
+    const pairs = objectFunctions.pairs;
+    it('should be a function', () => {
+      expect(pairs).to.be.a('function');
+    });
+  });
+
+  describe('`invert`', () => {
+    const invert = objectFunctions.invert;
+    it('should be a function', () => {
+      expect(invert).to.be.a('function');
+    });
+  });
+
+  describe('`defaults`', () => {
+    const defaults = objectFunctions.defaults;
+    it('should be a function', () => {
+      expect(defaults).to.be.a('function');
+    });
   });
 });
