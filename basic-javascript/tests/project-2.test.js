a6210f05468cfb9360ecccad5d9e415c56059b5c
german gonzalez <gcomlnk@gmail.com>
2018-03-26 21:52:06 -0400

    creating Project-2 Functions and getBiggest test case

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -4,3 +4,17 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-2 Functions', () => {
+
+    describe('`getBiggest`', () => {
+        const getBiggest = funcs.getBiggest;
+        it('should be a function', () => {
+            assert.typeOf(getBiggest, 'function');
+        });
+        it('should return the biggest number', () => {
+            const resp = getBiggest(5, 4);
+            assert.equal(resp, 5, 'should return 5');
+        });
+    });
+
+});
