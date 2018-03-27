38e81b4d1adbebba4ffd3bcbd6054383fa0a2037
Mike Streltsoff <MStreltsoff@gmail.com>
2018-03-27 00:08:15 -0400

    Start Arrays

--- advanced-javascript/tests/arrays.test.js
+++ advanced-javascript/tests/arrays.test.js
@@ -20,22 +20,70 @@ describe('Arrays', () => {
   describe('`each`', () => {
     it('should be a function', () => {
       const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+      expect(each).to.be.a('function');
     });
-    // begin here
+    it('should run the callback on each element of array', () => {
+      const { each } = arrayFunctions;
+      let str = '';
+      const words = ['Hello', 'World', '!'];
+      each(words, (elem) => {
+        str += elem;
+      });
+      expect(str).to.equal('HelloWorld!');
+    });
+    it('should pass the element and the index to the callback', () => {
+      const { each } = arrayFunctions;
+      let count = 0;
+      each([1, 2, 3, 4], (elem, i) => {
+        count += elem + i;
+      });
+      expect(count).to.equal(16);
+    });
+    it('should invoke the cb on all elements', () => {
+      const { each } = arrayFunctions;
+      const elements = [1, 2, 3];
+      const cb = sinon.spy();
+      each(elements, cb)
+      expect(cb).to.have.callCount(elements.length);
+    });
   });
+
 
   describe('`map`', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.an('function');
+    });
+    it ('should return an array', () => {
+      const map = arrayFunctions;
+      const result = [];
+      expect(result).to.be.an('array');
+    });
+    it('should invoke the cb on all elements', () => {
+      const { map } = arrayFunctions;
+      const elements = [1, 2, 3, 4];
+      const cb = sinon.spy();
+      map(elements, cb)
+      expect(cb).callCount(elements.length);
     });
   });
 
   describe('`reduce`', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
+    });
+    it('should reduce all elements to a single value', () => {
+      const { reduce } = arrayFunctions;
+      const arr = [4, 2, 5, 3, 20];
+      const result = reduce(arr, (sum, next) => (sum + next), 23);
+      expect(result).to.equal(57);
+    });
+    it('should handle different types of data', () => {
+      const { reduce } = arrayFunctions;
+      let arr = ['T', 'e', 'r', 'r', 'i', 'e'];
+      const result = reduce(arr, (sum, current) => (sum + current));
+      expect(result).to.be.equal('Terrie');
     });
   });
 
