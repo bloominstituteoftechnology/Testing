5bd3660237787e74b6c7fbe520cd36e3fb245b1f
Res Yeg <shaqk702@gmail.com>
2018-03-26 19:09:01 -0700

    started objects.test.js

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,9 +1,10 @@
-const assert = require('chai').expect;
+const expect = require('chai').expect;
 const objectFunctions = require('../src/objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
   it('should be an object', () => {
-
+          const keys = objectFunctions.keys;
+          expect(keys).to.be.an('function');
   });
 });
