dacb1e69c64df39c51bf309306628682ce9d9f27
cliffkang <cliffgkang@gmail.com>
2018-03-26 21:38:54 -0700

    started objects

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,4 +1,5 @@
-const assert = require('chai').expect;
+/* eslint-disable */
+const assert = require('chai').assert;
 // const sinon = require('sinon');
 // const sinonChai = require('sinon-chai');
 const objectFunctions = require('../src/objects');
@@ -6,8 +7,16 @@ const objectFunctions = require('../src/objects');
 // assert.use(sinonChai);
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {
+const obj = { 'a': 1, 'b': 2, 'c': 3 };
 
+describe('objects', () => {
+  describe('keys', () => {
+    const { keys } = objectFunctions;
+    it('should be a function', () => {
+      assert.typeOf(keys, 'function');
+    });
+    it('should return an array with the keys', () => {
+      assert.sameOrderedMembers(keys(obj), ['a', 'b', 'c']);
+    });
   });
 });
