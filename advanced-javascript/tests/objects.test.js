3eb3a0e2b28c718c5fba942ad07b577351fe52d0
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-27 11:11:05 -0400

    Latest Commit

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,41 +1,77 @@
-const assert = require('chai').expect;
+const chai = require('chai');
+
+const { assert } = require('chai');
 const objectFunctions = require('../src/objects');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { keys } = objectFunctions;
+const { values } = objectFunctions;
+const { mapObject } = objectFunctions;
+const { pairs } = objectFunctions;
+const { invert } = objectFunctions;
+const { defaults } = objectFunctions;
+
+const obj = { a: 1, b: 2 };
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
   describe('keys', () => {
     it('should be a function', () => {
-      const { keys } = objectFunctions;
       assert.typeOf(keys, 'function');
+    });
+    it('should return an output with data type: array', () => {
+      assert.typeOf(keys(obj), 'array');
+    });
+    it('should return the expected output given a specific input', () => {
+      assert.deepEqual(keys(obj), ['a', 'b']);
     });
   });
   describe('values', () => {
     it('should be a function', () => {
-      const { values } = objectFunctions;
       assert.typeOf(values, 'function');
+    });
+    it('should return an output with data type: array', () => {
+      assert.typeOf(values(obj), 'array');
+    });
+    it('should return the expected output given a specific input', () => {
+      assert.deepEqual(values(obj), [1, 2]);
     });
   });
   describe('mapObject', () => {
     it('should be a function', () => {
-      const { mapObject } = objectFunctions;
       assert.typeOf(mapObject, 'function');
+    });
+    it('should return an output with data type: object', () => {
+      const cb = (value) => {
+        return value * 2;
+      };
+      assert.typeOf(mapObject(obj, cb), 'object');
+    });
+    it('should execute the callback once for every key in the object', () => {
+      const cb = sinon.spy();
+      mapObject(obj, cb);
+      expect(cb).to.have.callCount(2);
+    });
+    it('should return the expected output given a specific input', () => {
+      
     });
   });
   describe('pairs', () => {
     it('should be a function', () => {
-      const { values } = objectFunctions;
-      assert.typeOf(values, 'function');
+      assert.typeOf(pairs, 'function');
     });
   });
   describe('invert', () => {
     it('should be a function', () => {
-      const { invert } = objectFunctions;
       assert.typeOf(invert, 'function');
     });
   });
   describe('defaults', () => {
     it('should be a function', () => {
-      const { defaults } = objectFunctions;
       assert.typeOf(defaults, 'function');
     });
   });
