2cbb667603b75a521a27d12c640c87d4a74376bc
german gonzalez <gcomlnk@gmail.com>
2018-03-26 22:34:18 -0400

    creating greeting testing

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -1,7 +1,11 @@
 /* eslint-disable prefer-destructuring */
 
+// const chai = require('chai');
 const assert = require('chai').assert;
+// const spies = require('chai-spies');
 const funcs = require('../src/project-2');
+
+// chai.use(spies);
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('Project-2 Functions', () => {
@@ -14,6 +18,29 @@ describe('Project-2 Functions', () => {
         it('should return the biggest number', () => {
             const resp = getBiggest(5, 4);
             assert.equal(resp, 5, 'should return 5');
+        });
+        it('should return second arg. if are equal', () => {
+            const resp = getBiggest(2, 2);
+            assert.equal(resp, 2, 'should return 2');
+        });
+    });
+
+    describe('`greeting`', () => {
+        const greeting = funcs.greeting;
+        it('should be a function', () => {
+            assert.typeOf(greeting, 'function');
+        });
+        it('should return German greeting', () => {
+            const resp = greeting('German');
+            assert.equal(resp, 'Guten Tag!', 'should return Guten Tag!');
+        });
+        it('should return Spanish greeting', () => {
+            const resp = greeting('Spanish');
+            assert.equal(resp, 'Hola!', 'should return Hola!');
+        });
+        it('should return a hello greeting', () => {
+            const resp = greeting('XXXXXXX');
+            assert.equal(resp, 'Hello!', 'should return Hello!');
         });
     });
 
