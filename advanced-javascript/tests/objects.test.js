7cd9ef286e64f2ba144103a25ded37e88f1e8a00
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-26 23:18:48 -0400

    Wrote a few tests for objects.js

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,9 +1,25 @@
-const assert = require('chai').expect;
+const assert = require('chai').assert;
 const objectFunctions = require('../src/objects');
+
+const { keys, values, mapObject, pairs, invert, defaults } = objectFunctions;
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {
+  const testObj = { key1: 'value 1', key2: 'value 2' };
+  describe('`keys`', () => {
+    const objKeys = keys(testObj);
+    it('should be an array', () => {
+      assert.typeOf(objKeys, 'array');
+    });
+    it('should contain strings', () => {
+      objKeys.forEach(key => assert.isOk(typeof key === 'string'));
+    });
+  });
 
+  describe('`values`', () => {
+    const objValues = values(testObj);
+    it('should contain the value as array element', () => {
+      assert.equal(objValues[0], 'value 1');
+    });
   });
 });
