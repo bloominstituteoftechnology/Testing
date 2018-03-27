09c5fb60458ef5cb3ac2200347b55bf1a201818e
rawalhome@gmail.com <rawalhome@gmail.com>
2018-03-27 09:36:42 -0500

    Charlie Sparks + Punit Rawal: Working on Objects.js

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,9 +1,64 @@
-const assert = require('chai').expect;
-const objectFunctions = require('../src/objects');
+/* eslint-disable */
+/* prefer-destructuring */
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const objFuncs = require('../src/objects');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {
 
+  describe('keys', () => {
+    const keys = objFuncs.keys;
+    // const spy = sinon.spy();
+    it('should be an object', () => {
+      expect(keys).to.be.a('function');
+    });
+    it('should return a', () => {
+      const result = keys({a: 1, b: 2});
+      expect(result[0]).to.equal('a');
+    });
+  });
+  
+
+  describe('values', () => {
+    const values = objFuncs.values;
+    // const spy = sinon.spy();
+    it('should be an object', () => {
+      expect(values).to.be.a('function');
+    });
+    it('should return 1', () => {
+      const result = values({a: 1, b: 2});
+      expect(result[0]).to.equal(1);
+    });
+  });
+
+  describe('values', () => {
+    const values = objFuncs.values;
+    // const spy = sinon.spy();
+    it('should be an object', () => {
+      expect(values).to.be.a('function');
+    });
+    it('should return 1', () => {
+      const result = values({a: 1, b: 2});
+      expect(result[0]).to.equal(1);
+    });
+  });
+
+  describe('mapObject', () => {
+    const mapObject = objFuncs.mapObject;
+    const spy = sinon.spy();
+    it('should be an object', () => {
+      expect(mapObject).to.be.a('function');
+    });
+    it('should return 2', () => {
+      const result = mapObject({a: 1, b: 2}, spy);
+      expect(spy).to.have.callCount(2);
+    });
   });
 });
