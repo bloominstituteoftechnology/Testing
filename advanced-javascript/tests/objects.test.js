1666c1a6f88bd01f0a49fd350b5fe7779c0a08da
Charlie Sparks <csparks@antioch.edu>
2018-03-27 16:46:20 -0700

    Updated Object.test.js
    We were getting merge issues on commits so I added this manually

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -16,12 +16,12 @@ describe('objects', () => {
   describe('keys', () => {
     const keys = objFuncs.keys;
     // const spy = sinon.spy();
-    it('should be an object', () => {
+    it('should be a function', () => {
       expect(keys).to.be.a('function');
     });
     it('should return a', () => {
       const result = keys({a: 1, b: 2});
-      expect(result[0]).to.equal('a');
+      expect(result[0]).eql('a');
     });
   });
   
@@ -29,36 +29,72 @@ describe('objects', () => {
   describe('values', () => {
     const values = objFuncs.values;
     // const spy = sinon.spy();
-    it('should be an object', () => {
+    it('should be a function', () => {
       expect(values).to.be.a('function');
     });
     it('should return 1', () => {
       const result = values({a: 1, b: 2});
-      expect(result[0]).to.equal(1);
-    });
-  });
-
-  describe('values', () => {
-    const values = objFuncs.values;
-    // const spy = sinon.spy();
-    it('should be an object', () => {
-      expect(values).to.be.a('function');
-    });
-    it('should return 1', () => {
-      const result = values({a: 1, b: 2});
-      expect(result[0]).to.equal(1);
+      expect(result[0]).eql(1);
     });
   });
 
   describe('mapObject', () => {
     const mapObject = objFuncs.mapObject;
     const spy = sinon.spy();
-    it('should be an object', () => {
+    it('should be a function', () => {
       expect(mapObject).to.be.a('function');
     });
     it('should return 2', () => {
       const result = mapObject({a: 1, b: 2}, spy);
       expect(spy).to.have.callCount(2);
+    });
+  });
+
+  describe('pairs', () => {
+    const pairs = objFuncs.pairs;
+    const spy = sinon.spy();
+    it('should be a function', () => {
+      expect(pairs).to.be.a('function');
+    });
+    it('should return a type of array', () => {
+      const result = pairs({a: 1, b: 2});
+      expect(result).to.be.a("array");
+    })
+    it('should return an array with a key/value pair', () => {
+      const result = pairs({a: 1, b: 2});
+      expect(result[0]).eql(['a', 1]);
+    });
+  });
+
+  describe('invert', () => {
+    const invert = objFuncs.invert;
+    const spy = sinon.spy();
+    it('should be a function', () => {
+      expect(invert).to.be.a('function');
+    });
+    it('should return a type of object', () => {
+      const result = invert({a: 1});
+      expect(result).to.be.a("object");
+    })
+    it('should return a value of 1', () => {
+      const result = invert({a: 1});
+      expect(result).eql({"1": "a"});
+    });
+  });
+
+  describe('defaults', () => {
+    const defaults = objFuncs.defaults;
+    const spy = sinon.spy();
+    it('should be a function', () => {
+      expect(defaults).to.be.a('function');
+    });
+    it('should return a type of object', () => {
+      const result = defaults({a: 1}, {a: 1, b: 2});
+      expect(result).to.be.a("object");
+    })
+    it('should return a value of 1', () => {
+      const result = defaults({a: 1});
+      expect(result).to.equal(1);
     });
   });
 });
