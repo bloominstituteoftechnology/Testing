95903d165d7302b916865f614abe4c0d2ceaf7ad
Carlos Andrade <carlos.andrade86@outlook.com>
2018-04-23 18:59:21 -0400

    almost done

--- advanced-javascript/tests/arrays.test.js
+++ advanced-javascript/tests/arrays.test.js
@@ -17,46 +17,61 @@ chai.use(sinonChai);
 // hint 3. - if the function you're testing requires a * callback *, make sure you use a spy like sinon
 
 describe('Arrays', () => {
-  describe('`each`', () => {
-    it('should be a function', () => {
-      const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+  describe('each', () => {
+    it('should invoke cb on each array element', () => {
+      let count = 0;
+      arrayFunctions.each([1, 2, 3], (element) => {
+        count += element;
+      });
+      expect(count).to.equal(6);
     });
-    // begin here
+    it('should pass the element and the index to cb', () => {
+      let count = 0;
+      arrayFunctions.each([1, 2, 3], (element, index) => {
+        count += element + index;
+      });
+      expect(count).to.equal(9);
+    });
+    // it('should call the callback passed to it for each element in array given', () => {
+    //   const callBackMockFn = jest.fn(); // this function is going to act as your callback, if it's not called your test will fail.
+    //   expect(callBackMockFn.mock.calls.length).toBe(0);
+    //   arrayMethods.each([1, 2, 3, 'four'], callBackMockFn);
+    //   expect(callBackMockFn.mock.calls.length).toBe(4);
+    // });
   });
 
   describe('`map`', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.an('function');
     });
   });
 
   describe('`reduce`', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
     });
   });
 
   describe('`find`', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
-      expect(find).to.be.an('array');
+      expect(find).to.be.an('function');
     });
   });
 
   describe('`filter`', () => {
     it('should be a function', () => {
       const filter = arrayFunctions.filter;
-      expect(filter).to.be.a('null');
+      expect(filter).to.be.a('function');
     });
   });
 
   describe('`flatten`', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
-      expect(flatten).to.be.a('promise');
+      expect(flatten).to.be.a('function');
     });
   });
 });
--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -1,6 +1,135 @@
 /* eslint-disable prefer-destructuring */
-
+const chai = require('chai');
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('project-2.js', () => {
+
+  describe('getBiggest', () => {
+    it('should be a function', () => {
+      const getBiggest = funcs.getBiggest;
+      assert.typeOf(getBiggest, 'function');   
+    }); 
+  });
+
+  describe('greeting', () => {
+    it('should be a function', () => {
+      const greeting = funcs.greeting;
+      expect(greeting).to.be.a('function');
+    });
+  });
+
+  describe('isTenOrFive', () => {
+    it('should be a function', () => {
+      const isTenOrFive = funcs.isTenOrFive;
+      assert.typeOf(isTenOrFive, 'function');
+    });
+  });
+
+  describe('isInRange', () => {
+   it('should be a function', () => {
+    const isInRange = funcs.isInRange;
+    expect(isInRange).to.be.a('function'); 
+   });   
+  });
+
+  describe('isInteger', () => {
+    it('should be a function', () => {
+      const isInteger = funcs.isInteger;
+      assert.typeOf(isInteger, 'function');
+    });
+  });
+
+  describe('fizzBuzz', () => {
+    it('should be a function', () => {
+      const fizzBuzz = funcs.fizzBuzz;
+      expect(fizzBuzz).to.be.a('function');
+    });
+  });
+
+  describe('isPrime', () => {
+    it('should be a function', () => {
+      const isPrime = funcs.isPrime;
+      assert.typeOf(isPrime, 'function');
+    });
+  });
+
+  describe('returnFirst', () => {
+    it('should be a function', () => {
+      const returnFirst = funcs.returnFirst;
+      expect(returnFirst).to.be.a('function');
+    });
+  });
+
+  describe('returnLast', () => {
+    it('should be a function', () => {
+      const returnLast = funcs.returnLast;
+      assert.typeOf(returnLast, 'function');
+    });
+  });
+
+  describe('getArrayLength', () => {
+    it('should be a function', () => {
+      const getArrayLength = funcs.getArrayLength;
+      expect(getArrayLength).to.be.a('function');
+    });
+  });
+
+  describe('incrementByOne', () => {
+    it('should be a function', () => {
+      const incrementByOne = funcs.incrementByOne
+      assert.typeOf(incrementByOne, 'function');
+    });
+  });
+
+  describe('addItemToArray', () => {
+    it('should be a function', () => {
+      const addItemToArray = funcs.addItemToArray;
+      expect(addItemToArray).to.be.a('function');
+    });
+  });
+
+  describe('addItemToFront', () => {
+    it('should be a function', () => {
+      const addItemToFront = funcs.addItemToFront;
+      assert.typeOf(addItemToFront, 'function');
+    });
+  });
+
+  describe('wordsToSentence', () => {
+    it('should be a function', () => {
+      const wordsToSentence = funcs.wordsToSentence;
+      expect(wordsToSentence).to.be.a('function');
+    });
+  });
+
+  describe('contains', () => {
+    it('should be a function', () => {
+      const contains = funcs.contains;
+      assert.typeOf(contains, 'function');
+    });
+  });
+
+  describe('addNumbers', () => {
+    it('should be a function', () => {
+      const addNumbers = funcs.addNumbers;
+      expect(addNumbers).to.be.a('function');
+    });
+  });
+
+  describe('averageTestScore', () => {
+    it('should be a function', () => {
+      const averageTestScore = funcs.averageTestScore;
+      assert.typeOf(averageTestScore, 'function');
+    });
+  });
+
+  describe('largestNumber', () => {
+    it('should be a function', () => {
+      const largestNumber = funcs.largestNumber;
+      assert.typeOf(largestNumber, 'function');
+    });
+  });
+});//End of outter describe
