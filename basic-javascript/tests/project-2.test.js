bfd60baf218d1a98d554309dcf94c01a61312000
mjd02 <mostdarryl@gmail.com>
2018-04-23 17:57:11 -0400

    completed basic project 2

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -4,3 +4,19 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-2 Functions', () => {
+  it('should be a function', () => {
+    let functions = Object.values(funcs);
+    functions.forEach(e=>{
+      assert.typeOf(e,'function');
+    });
+  });
+  let getBiggest = funcs.getBiggest;
+  it('should return the correct value', () => {
+    assert.equal(getBiggest(0,100),100);
+  });
+  let greeting = funcs.greeting;
+  it('should return the correct data type', () => {
+    assert.typeOf(greeting('German'),'string');
+  });
+});
