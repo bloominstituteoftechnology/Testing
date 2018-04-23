22b9b323858d8615fee4c0677cb2246b85832349
AJ Genung <DragonRydrz@me.com>
2018-04-23 18:26:37 -0400

    up through returFirst completed

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -44,50 +44,51 @@ describe('basic-javascript-project-2.js', () => {
   });
   describe('isTenOrFive', () => {
     it('should return true if 10 or 5', () => {
-      assert.equal();
-      assert.equal();
-      assert.equal();
+      assert.equal(isTenOrFive(5), true);
+      assert.equal(isTenOrFive(10), true);
+      assert.equal(isTenOrFive(4), false);
+      assert.equal(isTenOrFive(501), false);
     });
   });
-  // describe('isInRange', () => {
-  //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
-  //   });
-  // });
-  // describe('isInteger', () => {
-  //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
-  //   });
-  // });
-  // describe('fizzBuzz', () => {
-  //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
-  //   });
-  // });
-  // describe('isPrime', () => {
-  //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
-  //   });
-  // });
-  // describe('returnFirst', () => {
-  //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
-  //   });
-  // });
+  describe('isInRange', () => {
+    it('should be between 20 and 50', () => {
+      assert.equal(isInRange(20), false);
+      assert.equal(isInRange(50), false);
+      assert.equal(isInRange(26), true);
+      assert.equal(isInRange(-43), false);
+    });
+  });
+  describe('isInteger', () => {
+    it('', () => {
+      assert.equal(isInteger(4.3), false);
+      assert.equal(isInteger(7.4), false);
+      assert.equal(isInteger(-3.2), false);
+      assert.equal(isInteger(5.0), true);
+    });
+  });
+  describe('fizzBuzz', () => {
+    it('shour return fizz, buzz. fizzbuzz, or a number, depending on the input', () => {
+      assert.equal(fizzBuzz(3), 'fizz');
+      assert.equal(fizzBuzz(5), 'buzz');
+      assert.equal(fizzBuzz(15), 'fizzbuzz');
+      assert.equal(fizzBuzz(7), 7);
+    });
+  });
+  describe('isPrime', () => {
+    it('should return if num is prime', () => {
+      assert.equal(isPrime(7), true);
+      assert.equal(isPrime(4), false);
+      assert.equal(isPrime(11), true);
+    });
+  });
+  describe('returnFirst', () => {
+    it('should return the first value from the array', () => {
+      assert.equal(returnFirst([8, 5, 9, 1, 4]), 8);
+      assert.equal(returnFirst(['first', 5, 3, 'last']), 'first');
+    });
+  });
   // describe('returnLast', () => {
   //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
   //     assert.equal();
   //   });
   // });
@@ -101,56 +102,40 @@ describe('basic-javascript-project-2.js', () => {
   // describe('incrementByOne', () => {
   //   it('', () => {
   //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
   //   });
   // });
   // describe('addItemToArray', () => {
   //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
   //     assert.equal();
   //   });
   // });
   // describe('addItemToFront', () => {
   //   it('', () => {
   //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
   //   });
   // });
   // describe('wordsToSencence', () => {
   //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
   //     assert.equal();
   //   });
   // });
   // describe('contains', () => {
   //   it('', () => {
   //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
   //   });
   // });
   // describe('addNumbers', () => {
   //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
   //     assert.equal();
   //   });
   // });
   // describe('averageTestScore', () => {
   //   it('', () => {
   //     assert.equal();
-  //     assert.equal();
-  //     assert.equal();
   //   });
   // });
   // describe('largestNumber', () => {
   //   it('', () => {
-  //     assert.equal();
-  //     assert.equal();
   //     assert.equal();
   //   });
   // });
