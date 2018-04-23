492546402fa5f3344bd74db7aca42f7649718811
AJ Genung <DragonRydrz@me.com>
2018-04-23 17:43:06 -0400

    started basic project 2

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -1,6 +1,47 @@
 /* eslint-disable prefer-destructuring */
 
 const assert = require('chai').assert;
-const funcs = require('../src/project-2');
+const expect = require('chai').expect;
+
+const {
+  greeting,
+  isTenOrFive,
+  isInRange,
+  isInteger,
+  fizzBuzz,
+  isPrime,
+  returnFirst,
+  returnLast,
+  getArrayLength,
+  incrementByOne,
+  addItemToArray,
+  addItemToFront,
+  wordsToSentence,
+  contains,
+  addNumbers,
+  averageTestScore,
+  largestNumber
+} = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('basic-javascript-project-2.js', () => {
+  describe('getBiggest', () => {});
+  describe('greeting', () => {});
+  describe('isTenOrFive', () => {});
+  describe('isInRange', () => {});
+  describe('isInteger', () => {});
+  describe('fizzBuzz', () => {});
+  describe('isPrime', () => {});
+  describe('returnFirst', () => {});
+  describe('returnLast', () => {});
+  describe('getArrayLength', () => {});
+  describe('incrementByOne', () => {});
+  describe('addItemToArray', () => {});
+  describe('addItemToFront', () => {});
+  describe('wordsToSencence', () => {});
+  describe('contains', () => {});
+  describe('addNumbers', () => {});
+  describe('averageTestScore', () => {});
+  describe('largestNumber', () => {});
+});
