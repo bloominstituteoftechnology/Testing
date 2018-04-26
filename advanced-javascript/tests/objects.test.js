ae39ae0a7a73592ab11e69733c9a78cc843afe54
AJ Genung <DragonRydrz@me.com>
2018-04-26 16:13:48 -0400

    started objects

--- advanced-javascript/tests/objects.test.js
+++ advanced-javascript/tests/objects.test.js
@@ -1,4 +1,7 @@
-const assert = require('chai').expect;
+const chai = require('chai');
+
+const { expect } = chai;
+
 const {
   keys,
   values,
@@ -10,5 +13,16 @@ const {
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
+  describe('keys', () => {
+    it('should return an array', () => {
+      expect(keys({ a: 1, b: 2, c: 3 })).to.be.an('array');
+    });
+    it('should return all properties from an object', () => {
+      expect(keys({ a: 1, b: 2 })).to.have.a.lengthOf(2);
+      expect(keys({ d: 1, e: 2, f: 3 })).to.have.a.lengthOf(3);
+      expect(keys({
+        h: 1, g: 2, j: 3, t: 4, y: 5
+      })).to.have.a.lengthOf(5);
+    });
+  });
 });
