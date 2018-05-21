039c4f51a40b2f8fcc90c2619a1c2a6b8dc372a1
harrisonbrock <harrison@harrisonbrock.com>
2018-05-21 17:32:13 -0400

    Added addItemToArray test case

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,12 @@
-const funcs = require('../src/project-2');
+const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+// Test add item to array
+test('Add Item to Array', () => {
+    const arr = [5,9,1];
+    funcs.addItemToArray(arr, 100);
+    const lastItemAdd = arr[arr.length-1];
+    expect(lastItemAdd).toBe(100);
+});
+//////////////////////////////////////////////
