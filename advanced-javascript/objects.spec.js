899fbbd99a83f6f48a4a34814e0fafccea11261b
2940cristian <2940cristian@gmail.com>
2018-05-21 13:36:00 -0700

    Made object methods check to be type of function

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,46 @@
-const objectFunctions = require('../src/objects');
+const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {});
+describe('Objects', () => {
+  describe('keys', () => {
+    it('should be a function', () => {
+      const keys = objectFunctions.keys;
+      expect(typeof keys).toBe('function');
+    });
+  });
+
+  describe('values', () => {
+    it('should be a function', () => {
+      const values = objectFunctions.values;
+      expect(typeof values).toBe('function');
+    });
+  });
+
+  describe('mapObject', () => {
+    it('should be a function', () => {
+      const mapObject = objectFunctions.mapObject;
+      expect(typeof mapObject).toBe('function');
+    });
+  });
+
+  describe('pairs', () => {
+    it('should be a function', () => {
+      const pairs = objectFunctions.pairs;
+      expect(typeof pairs).toBe('function');
+    });
+  });
+
+  describe('invert', () => {
+    it('should be a function', () => {
+      const invert = objectFunctions.invert;
+      expect(typeof invert).toBe('function');
+    });
+  });
+
+  describe('defaults', () => {
+    it('should be a function', () => {
+      const defaults = objectFunctions.defaults;
+      expect(typeof defaults).toBe('function');
+    });
+  });
 });
