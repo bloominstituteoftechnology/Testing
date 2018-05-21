681e9b42143a31b03b81b141ac5954b460198f51
nickbrenn <nbren@tutanota.com>
2018-05-21 17:09:39 -0400

    Finished all the objects tests except one

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -4,3 +4,59 @@ const objectFunctions = require("./objects");
 describe("objects", () => {
   it("should be an object", () => {});
 });
+
+describe("keys", () => {
+  it("should return an array of object keys", () => {
+    const keys = objectFunctions.keys;
+
+    const actual = keys({ one: "two", three: "four" });
+
+    expect(actual).toEqual(["one", "three"]);
+  });
+});
+
+describe("values", () => {
+  it("should return an array of object values", () => {
+    const values = objectFunctions.values;
+
+    const actual = values({ one: "two", three: "four" });
+
+    expect(actual).toEqual(["two", "four"]);
+  });
+});
+
+describe("mapObject", () => {
+  it("should perform a callback function on a given object", () => {
+    const mapObject = objectFunctions.mapObject;
+    function addOne(value) {
+      return (value += 1);
+    }
+
+    const actual = mapObject({ one: "two", three: 2 }, addOne);
+
+    expect(actual).toEqual({ one: "two1", three: 3 });
+  });
+});
+
+describe("pairs", () => {
+  it("should return an array of key value pairs", () => {
+    const pairs = objectFunctions.pairs;
+
+    const actual = pairs({ one: "two", three: "four" });
+
+    expect(actual).toEqual([["one", "two"], ["three", "four"]]);
+  });
+});
+
+describe("invert", () => {
+  it("keys and values should be switched", () => {
+    const invert = objectFunctions.invert;
+
+    const actual = invert({
+      hi: "cheese",
+      pie: { hi: "tacos", cheesey: "pies" }
+    });
+
+    expect(actual).toEqual({ cheese: "hi", "[object Object]": "pie" });
+  });
+});
