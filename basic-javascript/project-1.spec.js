762e93ae4cec4fca8ac99e8d7aa554af2e94f991
Paul Fang <paulfang@pauls-mbp.home>
2018-05-21 15:04:34 -0700

    tested areEqual

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -8,38 +8,62 @@ describe ('default', () => {
 
 describe('multiplyByTen', () => {
     it('should multiply by 10', () => {
-        
-      //arrange
-      const multiply = funcs.multiplyByTen;
 
-      // act
-      const fourty = multiply(4);
+    //   //arrange
+    //   const multiply = funcs.multiplyByTen;
 
+    //   // act
+    //   const fourty = multiply(4);
 
-      //assert
-      expect(fourty).toBe(40);
+    //   //assert
+    //   expect(fourty).toBe(40);
+
+      expect(funcs.multiplyByTen(4)).toBe(40)
+
 
     })
 })
 
 describe('subtractFive', () => {
     it('should subtract five from sum', () => {
-        const subtract = funcs.subtractFive;
 
-        const ten = subtract(15);
+        // const subtract = funcs.subtractFive;
 
-        expect(ten).toBe(10);
+        // const ten = subtract(15);
+
+        // expect(ten).toBe(10);
+
+        expect(funcs.subtractFive(15)).toBe(10);
+
     })
 })
 
 describe('areSameLength', () => {
     it('should check if strings are same length', () => {
-      const sameLength = funcs.areSameLength;
-      const stringLength1 = 'joe';
-      const stringLength2 = 'bob';
 
-      const areSameLength = funcs.areSameLength(stringLength1, stringLength2);
+    //   const stringLength1 = 'joe';
 
-      expect(areSameLength).toEqual(true);
+    //   const stringLength2 = 'bob';
+
+    //   const areSameLength = funcs.areSameLength(stringLength1, stringLength2);
+
+    //   expect(areSameLength).toEqual(true);
+
+      expect(funcs.areSameLength('joe', 'bob')).toEqual(true);
+
     })
 }) 
+
+describe('areEqual', () => {
+    it('should check if x equals y', () => {
+      // const x = '5';
+      // const y = '5';
+
+      // const areEqual = funcs.areEqual(x,y);
+
+      // expect(areEqual).toEqual(true);
+      expect(funcs.areEqual(4,4)).toEqual(true);
+    })
+}) 
+
+
