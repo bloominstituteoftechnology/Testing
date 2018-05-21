d67d185cdaac52435b6e27c2709a92ba7e5389ea
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-21 18:10:42 -0400

    Completed getBiggest test case

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -2,6 +2,22 @@ const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 
+// default
 describe('default', () => {
     it('runs the test', () => {})
 });
+
+// getBiggest
+describe('getBiggest', () => {
+    it('returns the greater number between x and y', () => {
+        // arrange
+        const getBiggest = funcs.getBiggest;
+
+        // act
+        const actual = getBiggest(4, 7);
+
+        // assert
+        expect(actual).toBeGreaterThan(4);
+    })
+})
+
