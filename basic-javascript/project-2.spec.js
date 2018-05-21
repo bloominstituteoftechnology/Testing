6a947ec8af80d052bab424297f9ba57616fdd864
sadrak105 <sadrak10@gmail.com>
2018-05-21 17:15:18 -0400

    getBiggest func

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,13 @@
-const funcs = require('../src/project-2');
+const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe("getBiggest function", () => {
+    // arrange
+    const expected = 11;
+    // act
+    const getBiggest = funcs.getBiggest(8, 11);
+    // assert
+    it("returns which is biggest", () => {
+        expect(getBiggest).toBe(expected);
+    });
+});
