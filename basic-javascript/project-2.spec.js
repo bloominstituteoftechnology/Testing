0eb3b0a62a2a03a68cec62207441a08d1d3aac2f
Imran Ahmed <jappleleaf@gmail.com>
2018-05-21 17:18:01 -0400

    finished upto cube

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,6 @@
-const funcs = require('../src/project-2');
+const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('default', () => {
+  it('runs the test', () => {})
+})
