de88fad85b9d49d9d2d6581879384017c347b2aa
Nedu Robert <nedumrobert@gmail.com>
2018-05-21 20:48:55 +0100

    wrote tests for multiplyByTen function

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,18 @@
 const funcs = require('./project-1');
 
 // write your tests here
+describe('Project 1', () => {
+    describe('multiplyByTen', () => {
+        it('should multiply a number by ten', () => {
+            // arrange
+            const expected = 50;
+            const multiplyByTen = funcs.multiplyByTen;
+
+            // act
+            const actual = multiplyByTen(5);
+
+            // assert
+            expect(actual).toBe(expected);
+        });
+    });
+})
