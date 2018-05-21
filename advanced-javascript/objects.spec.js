caf8a498af1a63394258f759280e4a319d4cf6e7
totsuya <supertatortots@gmail.com>
2018-05-21 18:44:29 -0400

    partway through objects

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,25 @@
-const objectFunctions = require('../src/objects');
+const objectFunctions = require('../advanced-javascript/objects.js');
+
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
   it('should be an object', () => {});
 });
+
+it('Should return a object keys', () => {
+  const obj = {
+    fumo: 'soft',
+    sakuya: 'maid',
+    reimu: 'shrine maiden'
+  }
+  expect(objectFunctions.keys(obj)).toEqual(['fumo', 'sakuya', 'reimu'])
+})
+
+it('Should return object values', () => {
+  const obj = {
+    fumo: 'soft',
+    sakuya: 'maid',
+    reimu: 'shrine maiden'
+  }
+  expect(objectFunctions.values(obj)).toEqual(['soft', 'maid', 'shrine maiden'])
+})
