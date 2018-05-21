6436629c4a7b3afaacfc2106134c041307266ceb
Nedu Robert <nedumrobert@gmail.com>
2018-05-21 22:38:31 +0100

    wrote tests for the getBiggest function

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,21 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project 2', () => {
+    describe('getBiggest', () => {
+        it('should return a boolean value', () => {
+            // arrange
+            const getBiggest = funcs.getBiggest;
+
+            // act
+            const actual = getBiggest(2, 9);
+            const actual2 = getBiggest(23, 9);
+            const actual3 = getBiggest(2, 2);                        
+
+            // assert
+            expect(actual).toBe(9);
+            expect(actual2).toBe(23);
+            expect(actual3).toBe(2);                        
+        });
+    });
+})
