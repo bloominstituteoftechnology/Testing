39baa059ce5bf6fc077bf515d07a4e144f381ad2
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-21 18:35:48 -0400

    Robert Hogan. Half of project completed for day one.

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -1,115 +1,115 @@
 /* eslint-disable arrow-parens */
 
-const multiplyByTen = num => {
+const multiplyByTen = num => { //PASSES
   return num * 10;
 };
 
-const subtractFive = num => {
+const subtractFive = num => {  // passed
   return num - 5;
 };
 
-const areSameLength = (str1, str2) => {
+const areSameLength = (str1, str2) => {    //PASSED
   return str1.length === str2.length;
 };
 
-const areEqual = (x, y) => {
+const areEqual = (x, y) => {    //PASSED
   return x === y;
 };
 
-const lessThanNinety = num => {
+const lessThanNinety = num => {  // passed
   if (num < 90) {
     return true;
   }
   return false;
 };
 
-const greaterThanFifty = num => {
+const greaterThanFifty = num => { // PASSED
   if (num > 50) {
     return true;
   }
   return false;
 };
 
-const add = (x, y) => {
+const add = (x, y) => {   //PASSED
   return x + y;
 };
 
-const subtract = (x, y) => {
+const subtract = (x, y) => {    // PASSED
   return x - y;
 };
 
-const divide = (x, y) => {
+const divide = (x, y) => {  //PASSED
   return x / y;
 };
 
-const multiply = (x, y) => {
+const multiply = (x, y) => {  //PASSED
   return x * y;
 };
 
-const getRemainder = (x, y) => {
+const getRemainder = (x, y) => {    //PASSED
   return x % y;
 };
 
-const isEven = num => {
+const isEven = num => {   //PASSED
   if (num % 2 === 0) {
     return true;
   }
   return false;
 };
 
-const isOdd = num => {
+const isOdd = num => {    //PASSED
   if (num % 2 === 0) {
     return false;
   }
   return true;
 };
 
-const square = num => {
+const square = num => {   //PASSED
   return num * num;
 };
 
-const cube = num => {
+const cube = num => {   //PASED
   return num * num * num;
 };
 
-const raiseToPower = (num, exponent) => {
+const raiseToPower = (num, exponent) => {   //PASSED
   return num ** exponent;
 };
 
-const roundNumber = num => {
+const roundNumber = num => {    //PASSED
   return Math.round(num);
 };
 
-const roundUp = num => {
+const roundUp = num => {    //PASSED
   return Math.ceil(num);
 };
 
-const addExclamationPoint = str => {
+const addExclamationPoint = str => { //PASSED
   return (str += '!');
 };
 
-const combineNames = (firstName, lastName) => {
+const combineNames = (firstName, lastName) => {   //PASSED
   return `${firstName} ${lastName}`;
 };
 
-const getGreeting = name => {
+const getGreeting = name => {     //PASSED
   return `Hello ${name}!`;
 };
 
-const getRectangleArea = (length, width) => {
+const getRectangleArea = (length, width) => { //PASSED
   return length * width;
 };
 
-const getTriangleArea = (base, height) => {
+const getTriangleArea = (base, height) => {   //PASSED
   return 0.5 * base * height;
 };
 
-const getCircleArea = radius => {
+const getCircleArea = radius => {   //PASSED
   return Math.PI * radius * radius;
 };
 
 const getRectangularPrismVolume = (length, width, height) => {
-  return width * height * length;
+  return width * height * length; //PASSED
 };
 
 module.exports = {
