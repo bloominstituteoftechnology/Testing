2bebc4008787badeef0e0699ad59f391d1733370
Imran Ahmed <jappleleaf@gmail.com>
2018-05-21 19:03:28 -0400

    started project 2

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,158 +1,184 @@
-const funcs = require('./project-1');
-
-// write your tests here
-
-
-
-
+// const funcs = require('./project-1');
+//
+// // write your tests here
+//
 describe('default', () => {
   it('runs the test', () => {})
 })
-
-
-describe('multiply by ten', () => {
-  it('should multiply number by ten', () => {
-    const multiply = funcs.multiplyByTen
-
-    const actual = multiply(2);
-    const expected = 20;
-
-    expect(actual).toBe(expected);
-  })
-})
-
-
-describe('areSameLength', () => {
-  it('should check for length of 2 strings if they are of same length', () => {
-
-    const actual = funcs.areSameLength('abcd', '1234');
-    expect(actual).toBeTruthy()
-  })
-})
-
-
-describe('areEqual', () => {
-  it('checkes for eaulity', () => {
-    const actual = funcs.areEqual(5, 5);
-    expect(actual).toBeTruthy()
-  } )
-})
-
-
-
-describe('lessThanNinety', () => {
-  it('checkes if a number is less then ninty', () => {
-    const actual = funcs.lessThanNinety(65);
-    expect(actual).toBeTruthy()
-  } )
-})
-
-
-
-describe('greaterThanFifty', () => {
-  it('checks if a number is greater then fifty', () => {
-    const  actual = funcs.greaterThanFifty(100);
-    expect(actual).toBeTruthy();
-  })
-
-  it('checks if a number is greater then fifty', () => {
-    const  actual = funcs.greaterThanFifty(45);
-    expect(actual).toBeFalsy();
-  })
-})
-
-
-describe('add', () => {
-  it('checks if it adds 2 number', () => {
-    const actual = funcs.add(4, 5);
-    const expected = 9;
-    expect(actual).toEqual(expected)
-
-  })
-})
-
-
-
-describe('subtract', () => {
-  it('checks if it subtracts two number', () => {
-
-    const four = funcs.subtract(8, 4);
-    const negFour = funcs.subtract(4, 8);
-
-    expect(four).toEqual(4)
-    expect(negFour).toEqual(-4)
-
-
-  })
-})
-
-
-describe('divide', () => {
-  it('checks for division of 2 numbers', () => {
-    const four = funcs.divide(8,2);
-    expect(four).toEqual(4)
-  })
-})
-
-describe('multiply', () => {
-  it('checks for multiplication for 2 numbers', () => {
-
-    const six = funcs.multiply(3,2);
-    expect(six).toEqual(6)
-  })
-})
-
-
-
-describe('getRemainder', () => {
-  it('checks if for a Remainder of 2 numbers', () => {
-    const actual = funcs.getRemainder(8, 5);
-    expect(actual).toEqual(3)
-  })
-})
-
-describe('isEven', () => {
-
-  it('checks for a even number', () => {
-    const isEven = funcs.isEven(10);
-    expect(isEven).toBeTruthy();
-
-  })
-
-  it('checks for a even number', () => {
-    const isOdd = funcs.isEven(7)
-    expect(isOdd).toBeFalsy()
-  })
-})
-
-
-describe('isOdd', () => {
-
-  it('checks for a Odd number', () => {
-    const isEven = funcs.isOdd(10);
-    expect(isEven).toBeFalsy()
-  })
-
-  it('checks for a Odd number', () => {
-    const isOdd = funcs.isOdd(7)
-    expect(isOdd).toBeTruthy()
-  })
-})
-
-
-describe('square', () => {
-  it('checks if the function returns the square of a given number', () => {
-    const actual = funcs.square(5);
-    expect(actual).toEqual(25);
-  })
-})
-
-
-
-
-describe('cube', () => {
-  it('checks if the function returns the cube of a given number', () => {
-    const actual = funcs.cube(2);
-    expect(actual).toEqual(8)
-  })
-})
+//
+// describe('multiply by ten', () => {
+//   it('should multiply number by ten', () => {
+//     const multiply = funcs.multiplyByTen
+//
+//     const actual = multiply(2);
+//     const expected = 20;
+//
+//     expect(actual).toBe(expected);
+//   })
+// })
+//
+// describe('areSameLength', () => {
+//   it('should check for length of 2 strings if they are of same length', () => {
+//
+//     const actual = funcs.areSameLength('abcd', '1234');
+//     expect(actual).toBeTruthy()
+//   })
+// })
+//
+// describe('areEqual', () => {
+//   it('checkes for eaulity', () => {
+//     const actual = funcs.areEqual(5, 5);
+//     expect(actual).toBeTruthy()
+//   })
+// })
+//
+// describe('lessThanNinety', () => {
+//   it('checkes if a number is less then ninty', () => {
+//     const actual = funcs.lessThanNinety(65);
+//     expect(actual).toBeTruthy()
+//   })
+// })
+//
+// describe('greaterThanFifty', () => {
+//   it('checks if a number is greater then fifty', () => {
+//     const actual = funcs.greaterThanFifty(100);
+//     expect(actual).toBeTruthy();
+//   })
+//
+//   it('checks if a number is greater then fifty', () => {
+//     const actual = funcs.greaterThanFifty(45);
+//     expect(actual).toBeFalsy();
+//   })
+// })
+//
+// describe('add', () => {
+//   it('checks if it adds 2 number', () => {
+//     const actual = funcs.add(4, 5);
+//     const expected = 9;
+//     expect(actual).toEqual(expected)
+//
+//   })
+// })
+//
+// describe('subtract', () => {
+//   it('checks if it subtracts two number', () => {
+//
+//     const four = funcs.subtract(8, 4);
+//     const negFour = funcs.subtract(4, 8);
+//
+//     expect(four).toEqual(4)
+//     expect(negFour).toEqual(-4)
+//
+//   })
+// })
+//
+// describe('divide', () => {
+//   it('checks for division of 2 numbers', () => {
+//     const four = funcs.divide(8, 2);
+//     expect(four).toEqual(4)
+//   })
+// })
+//
+// describe('multiply', () => {
+//   it('checks for multiplication for 2 numbers', () => {
+//
+//     const six = funcs.multiply(3, 2);
+//     expect(six).toEqual(6)
+//   })
+// })
+//
+// describe('getRemainder', () => {
+//   it('checks if for a Remainder of 2 numbers', () => {
+//     const actual = funcs.getRemainder(8, 5);
+//     expect(actual).toEqual(3)
+//   })
+// })
+//
+// describe('isEven', () => {
+//
+//   it('checks for a even number', () => {
+//     const isEven = funcs.isEven(10);
+//     expect(isEven).toBeTruthy();
+//
+//   })
+//
+//   it('checks for a even number', () => {
+//     const isOdd = funcs.isEven(7)
+//     expect(isOdd).toBeFalsy()
+//   })
+// })
+//
+// describe('isOdd', () => {
+//
+//   it('checks for a Odd number', () => {
+//     const isEven = funcs.isOdd(10);
+//     expect(isEven).toBeFalsy()
+//   })
+//
+//   it('checks for a Odd number', () => {
+//     const isOdd = funcs.isOdd(7)
+//     expect(isOdd).toBeTruthy()
+//   })
+// })
+//
+// describe('square', () => {
+//   it('checks if the function returns the square of a given number', () => {
+//     const actual = funcs.square(5);
+//     expect(actual).toEqual(25);
+//   })
+// })
+//
+// describe('cube', () => {
+//   it('checks if the function returns the cube of a given number', () => {
+//     const actual = funcs.cube(2);
+//     expect(actual).toEqual(8)
+//   })
+// })
+//
+// describe('roundNumber', () => {
+//   it('checks if a decemel number if rounded or not', () => {
+//     const actual = funcs.roundNumber(2.5)
+//     expect(actual).toEqual(3)
+//   })
+// })
+//
+// describe('addExclamationPoint', () => {
+//   it('checks if a ! is added the end the string', () => {
+//     const total = funcs.addExclamationPoint('string');
+//     const actual = total[total.length - 1];
+//     expect(actual).toEqual('!')
+//   })
+// })
+//
+// describe('combineNames', () => {
+//   it('checks if it combines two names', () => {
+//     const fullName = funcs.combineNames('imran', 'ahmed');
+//     const actual = fullName.split(' ');
+//     expect(actual).toContain('imran');
+//     expect(actual).toContain('ahmed');
+//   })
+// })
+//
+// describe('getGreeting', () => {
+//   it('checks for a name at the end of greeting', () => {
+//     const result = funcs.getGreeting('imran');
+//     const actual = result.split(' ');
+//     expect(actual).toContain('imran!');
+//     expect(actual).toContain('Hello')
+//   })
+// })
+//
+// describe('getRectangularPrismVolume', () => {
+//   it('calculates the volume of a rec prism', () => {
+//     const actual = funcs.getRectangularPrismVolume(4, 8, 10);
+//     expect(actual).toEqual(320)
+//   })
+// })
+//
+// describe('getCircleArea', () => {
+//   it('calculates the area of a circle', () => {
+//     const result = funcs.getCircleArea(14);
+//     const acutal = funcs.roundNumber(result)
+//     expect(acutal).toEqual(616)
+//   })
+// })
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -4,3 +4,32 @@ const funcs = require('./project-2');
 describe('default', () => {
   it('runs the test', () => {})
 })
+
+
+describe('greeting', () => {
+  it('should check all the language greeting', () => {
+    const getGreeting = funcs.greeting
+
+    const german = getGreeting('German');
+    const spanish = getGreeting('Spanish')
+    const noLang = getGreeting();
+
+    expect(german).toBe('Guten Tag!');
+    expect(spanish).toBe('Hola!');
+    expect(noLang).toBe('Hello!');
+  })
+})
+
+describe('isTenOrFive', () => {
+  it('should check if passed argument is 5 or 10', () => {
+    const actual = funcs.isTenOrFive(5)
+    expect(actual).toBeTruthy();
+  })
+})
+
+
+describe('isInRange', ()=> {
+  it('should check if passed argument is within range of 20 and 50', () => {
+
+  })
+})
