59fd303a6d4b0c847bb76ff1ca87028c72220249
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-05-21 18:08:37 -0500

    Finished first test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,11 @@
-const funcs = require('../src/project-1');
+const funcs = require('./project-1');
 
 // write your tests here
+it('should multiply a number by 10', () => {
+    //arrange
+    const multiply = funcs.multiplyByTen;
+    //act
+    const actual = multiply(9);
+    //assert
+    expect(actual).toBe(90);
+})
