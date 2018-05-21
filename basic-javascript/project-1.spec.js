ce10cf20d8f7553ea650e369b60e37af8ed052c6
totsuya <supertatortots@gmail.com>
2018-05-21 15:24:39 -0400

    did basic multiplybyTen and subTract five testing

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,31 @@
-const funcs = require('../src/project-1');
+const funcs = require('../basic-javascript/project-1.js');
+
+
 
 // write your tests here
+
+//multiplybyTen
+it('Testing the test', () => {});
+
+it('should multiply by ten', () => {
+  //arrange
+  const expected = 30;
+  const multiplyby10 = funcs.multiplyByTen;
+  
+  //act
+  const product = multiplyby10(3);
+
+  //assert
+  expect(product).toBe(expected);
+});
+
+
+//subtractFive
+it('Should subtract five', () => {
+  const expected = 25;
+  const subtractFive = funcs.subtractFive
+
+  const subtraction = subtractFive(30);
+
+  expect(subtraction).toBe(expected);
+});
