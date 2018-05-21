08be2400402320e9236041e732ca09f9dfd51293
Ashiko124 <greeneselectronics@gmail.com>
2018-05-21 16:29:58 -0600

    Completed up to add

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -7,39 +7,61 @@ describe('default', () => {
 
 
   describe('multiplyByTen', () => {
-      it('should multiply by 10', () => {
-          
+      it('should multiply by 10', () => {        
         //arrange
-        const multiply = funcs.multiplyByTen;
+        //const multiply = funcs.multiplyByTen;
 
         // act
-        const fourty = multiply(4);
-
+        //const fourty = multiply(4);
 
         //assert
-        expect(fourty).toBe(40);
-
+        //expect(multiply).toBe(num);
+        // expect(func).toBeCalledWith(expect.any(Number);
+        //expect(fourty).toBe(40);
+        expect(funcs.multiplyByTen(4)).toBe(40)
       })
   })
 
   describe('subtractFive', () => {
       it('should subtract five from sum', () => {
-          const subtract = funcs.subtractFive;
-
-          const ten = subtract(15);
-
-          expect(ten).toBe(10);
+          expect(funcs.subtractFive(15)).toBe(10);
       })
   })
 
   describe('areSameLength', () => {
       it('should check if strings are same length', () => {
-        const sameLength = funcs.areSameLength;
-        const stringLength1 = 'joe';
-        const stringLength2 = 'bob';
-
-        const areSameLength = funcs.areSameLength(stringLength1, stringLength2);
-
-        expect(areSameLength).toEqual(true);
+        expect(funcs.areSameLength('joe', 'bob')).toEqual(true);
       })
   })
+
+  describe('areEqual', () => {
+      it('should check if x equals y', () => {
+        // const x = '5';
+        // const y = '5';
+
+        // const areEqual = funcs.areEqual(x,y);
+
+        // expect(areEqual).toEqual(true);
+        //expect(funcs).toBeCalledWith(expect.any(Number))
+        expect(funcs.areEqual(4,4)).toEqual(true);
+      })
+  })
+
+
+describe('lessThanNinety', () => {
+   it('should check if less than ninety', () => {
+   expect(funcs.lessThanNinety(80)).toEqual(true);
+})
+})
+
+describe('greaterThanFifty', () => {
+    it('should check if greater than fifty', () => {
+    expect(funcs.greaterThanFifty(60)).toEqual(true);
+    })
+ })
+
+ describe('add', () => {
+    it('should check x + y', () => {
+        expect(funcs.add(5, 6)).toEqual(11);
+    })
+ })
