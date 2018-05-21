7ff7c79e4b9775a30b15d941953c421f57d9ead8
hilal aissani <hilalaissani@gmail.com>
2018-05-21 19:10:46 -0400

    50%

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,232 @@
-const funcs = require('../src/project-1');
+const funcs = require("./project-1");
 
-// write your tests here
+describe("multiply testing", () => {
+  it("multiply by ten ", () => {
+    const multiplyByTen = funcs.multiplyByTen;
+    const actual = multiplyByTen(3);
+    expect(actual).toBe(30);
+  });
+});
+describe("subtract testing", () => {
+  it("Subtract five  ", () => {
+    const subtractFive = funcs.subtractFive;
+    const actual = subtractFive(15);
+    expect(actual).toBe(10);
+  });
+});
+
+describe("length testing", () => {
+  it(" is the same length? ", () => {
+    const areSameLength = funcs.areSameLength;
+    const actual = areSameLength("bill", "bill");
+    expect(actual).toBeTruthy();
+  });
+  it(" is not the same length? ", () => {
+    const areSameLength = funcs.areSameLength;
+    const actual = areSameLength("hilal0", "filal");
+    expect(actual).toBeFalsy;
+  });
+});
+describe("equality", () => {
+  it(" are equal? ", () => {
+    const areEqual = funcs.areEqual;
+    const actual = areEqual(12, 12);
+    expect(actual).toBeTruthy();
+  });
+
+  it(" are not equal? ", () => {
+    const areEqual = funcs.areEqual;
+    const actual = areEqual(12, 2);
+    expect(actual).toBeFalsy();
+  });
+});
+describe("less then 90 ", () => {
+  it(" < 90  ", () => {
+    const lessThanNinety = funcs.lessThanNinety;
+    const actual = lessThanNinety(80);
+    expect(actual).toBeTruthy();
+  });
+
+  it(" > 90  ", () => {
+    const lessThanNinety = funcs.lessThanNinety;
+    const actual = lessThanNinety(100);
+    expect(actual).toBeFalsy();
+  });
+});
+
+describe("greater  then 50 ", () => {
+  it(" < 50  ", () => {
+    const greaterThanFifty = funcs.greaterThanFifty;
+    const actual = greaterThanFifty(80);
+    expect(actual).toBeTruthy();
+  });
+
+  it(" > 50 ", () => {
+    const greaterThanFifty = funcs.greaterThanFifty;
+    const actual = greaterThanFifty(40);
+    expect(actual).toBeFalsy();
+  });
+});
+describe("adding ", () => {
+  it(" < 50  ", () => {
+    const add = funcs.add;
+    const actual = add(80, 20);
+    expect(actual).toBe(100);
+  });
+
+  it(" > 50 ", () => {
+    const add = funcs.add;
+    const actual = add(40, 20);
+    expect(actual).toBe(60);
+  });
+});
+describe("subtracting ", () => {
+  it(" -  60 ", () => {
+    const subtract = funcs.subtract;
+    const actual = subtract(80, 20);
+    expect(actual).toBe(60);
+  });
+
+  it(" - 50", () => {
+    const subtract = funcs.subtract;
+    const actual = subtract(70, 20);
+    expect(actual).toBe(50);
+  });
+});
+
+describe("dividing ", () => {
+  it(" /4", () => {
+    const divide = funcs.divide;
+    const actual = divide(80, 20);
+    expect(actual).toBe(4);
+  });
+});
+describe("multiplying ", () => {
+  it(" multiply", () => {
+    const multiply = funcs.multiply;
+    const actual = multiply(7, 5);
+    expect(actual).toBe(35);
+  });
+});
+describe("getRemaindering  ", () => {
+  it(" getRemainder ", () => {
+    const getRemainder = funcs.getRemainder;
+    const actual = getRemainder(7, 5);
+    expect(actual).toBe(2);
+  });
+});
+
+describe("is even ", () => {
+  it(" is even ", () => {
+    const isEven = funcs.isEven;
+    const actual = isEven(8);
+    expect(actual).toBeTruthy();
+  });
+});
+describe("is odd ", () => {
+  it(" is odd ", () => {
+    const isOdd = funcs.isOdd;
+    const actual = isOdd(9);
+    expect(actual).toBeTruthy();
+  });
+});
+describe("squaring  ", () => {
+  it(" square", () => {
+    const square = funcs.square;
+    const actual = square(4);
+    expect(actual).toBe(16);
+  });
+});
+describe("cubing ", () => {
+  it(" cube ", () => {
+    const cube = funcs.cube;
+    const actual = cube(3);
+    expect(actual).toBe(27);
+  });
+});
+describe("cubing ", () => {
+  it(" cube ", () => {
+    const cube = funcs.cube;
+    const actual = cube(3);
+    expect(actual).toBe(27);
+  });
+});
+describe("raise to power  ", () => {
+  it(" exponential  ", () => {
+    const raiseToPower = funcs.raiseToPower;
+    const actual = raiseToPower(3, 2);
+    expect(actual).toBe(9);
+  });
+});
+describe(" rounding   ", () => {
+  it(" round number   ", () => {
+    const roundNumber = funcs.roundNumber;
+    const actual = roundNumber(9.3);
+    expect(actual).toBe(9);
+  });
+});
+describe(" rounding up    ", () => {
+  it(" round up    ", () => {
+    const roundUp = funcs.roundUp;
+    const actual = roundUp(8.3);
+    expect(actual).toBe(9);
+  });
+});
+describe(" adding exclamation point    ", () => {
+  it(" round up    ", () => {
+    const addExclamationPoint = funcs.addExclamationPoint;
+    const actual = addExclamationPoint("hilal");
+    expect(actual).toBe("hilal!");
+  });
+});
+describe(" combine names     ", () => {
+  it(" combine names    ", () => {
+    const combineNames = funcs.combineNames;
+    const actual = combineNames("hilal", "bilal");
+    expect(actual).toBe("hilal bilal");
+  });
+});
+
+describe(" greeting     ", () => {
+  it(" greeting    ", () => {
+    const getGreeting = funcs.getGreeting;
+    const actual = getGreeting("hilal");
+    expect(actual).toBe("Hello hilal!");
+  });
+});
+describe(" get rectangle area    ", () => {
+  it(" getRectangleArea    ", () => {
+    const getRectangleArea = funcs.getRectangleArea;
+    const actual = getRectangleArea(3, 5);
+    expect(actual).toBe(15);
+  });
+});
+describe(" get rectangle area    ", () => {
+  it(" getRectangleArea    ", () => {
+    const getRectangleArea = funcs.getRectangleArea;
+    const actual = getRectangleArea(3, 5);
+    expect(actual).toBe(15);
+  });
+});
+
+describe(" getTriangleArea   ", () => {
+  it(" getTriangleArea    ", () => {
+    const getRectangleArea = funcs.getTriangleArea;
+    const actual = getRectangleArea(6, 5);
+    expect(actual).toBe(15);
+  });
+});
+describe(" ggetCircleArea   ", () => {
+  it(" getCircleArea   ", () => {
+    const getCircleArea = funcs.getCircleArea;
+    const actual = getCircleArea(3);
+    expect(actual).toBe(28.274333882308138);
+  });
+});
+describe(" getRectangularPrismVolume   ", () => {
+  it(" getCircleArea   ", () => {
+    const getRectangularPrismVolume = funcs.getRectangularPrismVolume;
+    const actual = getRectangularPrismVolume(2, 3, 4);
+    expect(actual).toBe(24);
+  });
+});
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,228 @@
-const funcs = require('../src/project-2');
+const funcs = require("./project-2");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe("first test", () => {
+  it(" return the biggest", () => {
+    const getBiggest = funcs.getBiggest;
+    const actual = getBiggest(-1, 0);
+    expect(actual).toBe(0);
+  });
+});
+describe("greeting test ", () => {
+  it(" greeting in german ", () => {
+    const greeting = funcs.greeting;
+    const german = greeting("German");
+    expect(german).toBe("Guten Tag!"); //// weird
+  });
+  it(" greeting in spanish", () => {
+    const greeting = funcs.greeting;
+    const spanish = greeting("Spanish");
+    expect(spanish).toBe("Hola!"); //// weird
+  });
+  it(" greeting in any language ", () => {
+    const greeting = funcs.greeting;
+    const other = greeting("balalal");
+    expect(other).toBe("Hello!"); //// weird
+  });
+});
+
+describe("ten or five  test", () => {
+  it(" ten or five ", () => {
+    const isTenOrFive = funcs.isTenOrFive;
+    const actual = isTenOrFive(5);
+    expect(actual).toBe(true);
+  });
+
+  it("  not ten or five ", () => {
+    const isTenOrFive = funcs.isTenOrFive;
+    const actual = isTenOrFive(3);
+    expect(actual).toBe(false);
+  });
+});
+describe("the range  test", () => {
+  it(" in  the range  ", () => {
+    const isInRange = funcs.isInRange;
+    const actual = isInRange(40);
+    expect(actual).toBe(true);
+  });
+
+  it("  not in the range  ", () => {
+    const isInRange = funcs.isInRange;
+    const actual = isInRange(5);
+    expect(actual).toBe(false);
+  });
+});
+describe("integer test", () => {
+  it("  is integer", () => {
+    const isInteger = funcs.isInteger;
+    const actual = isInteger(40);
+    expect(actual).toBe(true);
+  });
+
+  it("  not an  integer  ", () => {
+    const isInteger = funcs.isInteger;
+    const actual = isInteger(5.9);
+    expect(actual).toBeFalsy();
+  });
+});
+describe("integer test", () => {
+  it("  is integer", () => {
+    const isInteger = funcs.isInteger;
+    const actual = isInteger(40);
+    expect(actual).toBe(true);
+  });
+
+  it("  not an  integer  ", () => {
+    const isInteger = funcs.isInteger;
+    const actual = isInteger(5.9);
+    expect(actual).toBeFalsy();
+  });
+});
+describe("integer test", () => {
+  it("  is integer", () => {
+    const isInteger = funcs.isInteger;
+    const actual = isInteger(40);
+    expect(actual).toBe(true);
+  });
+
+  it("  not an  integer  ", () => {
+    const isInteger = funcs.isInteger;
+    const actual = isInteger(5.9);
+    expect(actual).toBeFalsy();
+  });
+});
+describe("fuzz buzz test", () => {
+  it(" 5 && 3 ", () => {
+    const fizzBuzz = funcs.fizzBuzz;
+    const actual = fizzBuzz(15);
+    expect(actual).toBe("fizzbuzz");
+  });
+
+  it("  5   ", () => {
+    const fizzBuzz = funcs.fizzBuzz;
+    const actual = fizzBuzz(20);
+    expect(actual).toBe("buzz");
+  });
+  it(" 3 ", () => {
+    const fizzBuzz = funcs.fizzBuzz;
+    const actual = fizzBuzz(3);
+    expect(actual).toBe("fizz");
+  });
+});
+describe("prime number test", () => {
+  it(" < 0  ", () => {
+    const isPrime = funcs.isPrime;
+    const actual = isPrime(-6);
+    expect(actual).toBe(false);
+  });
+
+  it("  1 || 0   ", () => {
+    const isPrime = funcs.isPrime;
+    const actual = isPrime(0);
+    expect(actual).toBe(false);
+  });
+  it(" even  ", () => {
+    const isPrime = funcs.isPrime;
+    const actual = isPrime(8);
+    expect(actual).toBe(false);
+  });
+  it(" prime  ", () => {
+    const isPrime = funcs.isPrime;
+    const actual = isPrime(17);
+    expect(actual).toBe(true);
+  });
+});
+describe("first item in the arr", () => {
+  it("arr[0]", () => {
+    const returnFirst = funcs.returnFirst;
+    const actual = returnFirst([9, 0, 8, 9]);
+    expect(actual).toBe(9);
+  });
+});
+describe("last item in the arr", () => {
+  it("arr[arr.length-1]", () => {
+    const returnLast = funcs.returnLast;
+    const actual = returnLast([9, 0, 8, 9]);
+    expect(actual).toBe(9);
+  });
+});
+describe("length of the arr", () => {
+  it("arr.length", () => {
+    const getArrayLength = funcs.getArrayLength;
+    const actual = getArrayLength([9, 0, 8, 9]);
+    expect(actual).toBe(4);
+  });
+});
+describe("increment by one", () => {
+  it(" ++ ", () => {
+    const incrementByOne = funcs.incrementByOne;
+    const actual = incrementByOne([3, 4, 6, 7]);
+    expect(actual).toEqual([4, 5, 7, 8]);
+  });
+});
+describe("add item to arr ", () => {
+  it(" push", () => {
+    const addItemToArray = funcs.addItemToArray;
+    const actual = addItemToArray([3, 4, 6, 7], 9);
+    expect(actual).toEqual([3, 4, 6, 7, 9]);
+  });
+});
+describe("add item to the front  of  arr ", () => {
+  it(" unshift", () => {
+    const addItemToFront = funcs.addItemToFront;
+    const actual = addItemToFront([3, 4, 6, 7], 9);
+    expect(actual).toEqual([9, 3, 4, 6, 7]);
+  });
+});
+describe("words to sentences ", () => {
+  it(" words to sentence ", () => {
+    const wordsToSentence = funcs.wordsToSentence;
+    const actual = wordsToSentence([
+      "bilal",
+      "aissani",
+      "is",
+      "lambda",
+      "student"
+    ]);
+    expect(actual).toEqual("bilal aissani is lambda student");
+  });
+  it(" words to sentence ", () => {
+    const wordsToSentence = funcs.wordsToSentence;
+    const actual = wordsToSentence(["bilal"]);
+    expect(actual).toEqual("bilal");
+  });
+});
+describe(" arr contains items  ", () => {
+  it(" contains ", () => {
+    const contains = funcs.contains;
+    const actual = contains([3, 4, 6, 7, 9, 9, 9], 9);
+    expect(actual).toBe(true);
+  });
+  it("  does not contain ", () => {
+    const contains = funcs.contains;
+    const actual = contains([3, 4, 6, 7], 9);
+    expect(actual).toBe(false);
+  });
+});
+describe("addNumbers ", () => {
+  it(" the sum of arr  ", () => {
+    const addNumbers = funcs.addNumbers;
+    const actual = addNumbers([3, 4, 6, 7]);
+    expect(actual).toBe(20);
+  });
+});
+describe(" average of scores ", () => {
+  it(" average score ", () => {
+    const averageTestScore = funcs.averageTestScore;
+    const actual = averageTestScore([3, 4, 6, 7]);
+    expect(actual).toBe(5);
+  });
+});
+describe("largest number is arr  ", () => {
+  it(" biggest number ", () => {
+    const largestNumber = funcs.largestNumber;
+    const actual = largestNumber([3, 4, 6, 7]);
+    expect(actual).toBe(7);
+  });
+});
