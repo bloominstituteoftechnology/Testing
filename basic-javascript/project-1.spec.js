3cd620c87db29f56eb759831c5760fec43b0c290
Justin Hong <jhong2009@gmail.com>
2018-05-21 13:01:01 -0700

    in project-1.spec.js: set up test for multiplyByTen

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,12 @@
-const funcs = require('../src/project-1');
+const funcs = require("./project-1");
 
-// write your tests here
+describe("multiplyByTen function", () => {
+	// arrange
+	const expected = 50;
+	// act
+	const multiplyByTen = funcs.multiplyByTen(5);
+	// assert
+	it("multiply a number by 10", () => {
+		expect(multiplyByTen).toBe(expected);
+	});
+});
