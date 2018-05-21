cf51b85364c6e2a4351b596e2e8886b14354424e
ajf412 <aaronjfaulkner@yahoo.com>
2018-05-21 19:37:42 -0400

    project-1 testing complete.

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,6 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Testing test!', () => {
+  it('Checking that test runs.', () => {});
+});
