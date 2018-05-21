2c1d4920051e4e776219cbc02249361437c6fdab
Henry K <mykim1891@hotmail.com>
2018-05-21 14:53:24 -0700

    12 tests written in total for project 1

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -24,16 +24,13 @@ describe("Multiply by ten", () => {
 });
 
 describe("Subtract by five", () => {
-  it("Should multiply by ten", () => {
-    // arrange
+  it("Should subtract by five", () => {
     const subtractFive = funcs.subtractFive;
 
-    // act
     const twnetyEight = subtractFive(33);
     const zero = subtractFive(5);
     const negThree = subtractFive(2);
 
-    // assert
     expect(twnetyEight).toBe(28);
     expect(zero).toBe(0);
     expect(negThree).toBe(-3);
@@ -42,15 +39,12 @@ describe("Subtract by five", () => {
 
 describe("Are same length", () => {
   it("Should have same string length", () => {
-    // arrange
     const areSameLength = funcs.areSameLength;
 
-    // act
     const length1 = areSameLength("james", "jason");
     const length2 = areSameLength("bourne", "bond");
     const length3 = areSameLength("herdy", "gerdies");
 
-    // assert
     expect(length1).toBe(true);
     expect(length2).toBe(false);
     expect(length3).toBe(false);
@@ -59,16 +53,13 @@ describe("Are same length", () => {
 
 describe("Have same value", () => {
   it("Both should have same value", () => {
-    // arrange
     const areEqual = funcs.areEqual;
 
-    // act
     const equal1 = areEqual(1, 1);
     const equal2 = areEqual(1, -1);
     const equal3 = areEqual(24, 42);
     const equal4 = areEqual(0, 0);
 
-    // assert
     expect(equal1).toBe(true);
     expect(equal2).toBe(false);
     expect(equal3).toBe(false);
@@ -76,19 +67,16 @@ describe("Have same value", () => {
   });
 });
 
-describe("Is less than ninety", () => {
+describe("Less than ninety", () => {
   it("Value is less than ninety", () => {
-    // arrange
     const lessThanNinety = funcs.lessThanNinety;
 
-    // act
     const lessNinety1 = lessThanNinety(89);
     const lessNinety2 = lessThanNinety(91);
     const lessNinety3 = lessThanNinety(90);
     const lessNinety4 = lessThanNinety(1000);
     const lessNinety5 = lessThanNinety(-2);
 
-    // assert
     expect(lessNinety1).toBe(true);
     expect(lessNinety2).toBe(false);
     expect(lessNinety3).toBe(false);
@@ -97,23 +85,112 @@ describe("Is less than ninety", () => {
   });
 });
 
-// describe("Is less than ninety", () => {
-//   it("Value is less than ninety", () => {
-//     // arrange
-//     const lessThanNinety = funcs.lessThanNinety;
+describe("Greater than fifty", () => {
+  it("Value is greater than fifty", () => {
+    const greaterThanFifty = funcs.greaterThanFifty;
 
-//     // act
-//     const lessNinety1 = lessThanNinety(89);
-//     const lessNinety2 = lessThanNinety(91);
-//     const lessNinety3 = lessThanNinety(90);
-//     const lessNinety3 = lessThanNinety(1000);
-//     const lessNinety4 = lessThanNinety(-2);
+    const greaterFifty1 = greaterThanFifty(100);
+    const greaterFifty2 = greaterThanFifty(0);
+    const greaterFifty3 = greaterThanFifty(-1);
+    const greaterFifty4 = greaterThanFifty(50);
+    const greaterFifty5 = greaterThanFifty(51);
 
-//     // assert
-//     expect(lessNinety1).toBe(true);
-//     expect(lessNinety2).toBe(false);
-//     expect(lessNinety3).toBe(false);
-//     expect(lessNinety3).toBe(false);
-//     expect(lessNinety4).toBe(true);
-//   });
-// });
+    expect(greaterFifty1).toBe(true);
+    expect(greaterFifty2).toBe(false);
+    expect(greaterFifty3).toBe(false);
+    expect(greaterFifty4).toBe(false);
+    expect(greaterFifty5).toBe(true);
+  });
+});
+
+describe("Add function", () => {
+  it("Add two number together", () => {
+    const add = funcs.add;
+
+    const twenty = add(10, 10);
+    const thrityFive = add(17, 18);
+    const threeFiddy = add(324, 26);
+    const negThree = add(-10, 7);
+    const zero = add(-11, 11);
+
+    expect(twenty).toBe(20);
+    expect(thrityFive).toBe(35);
+    expect(threeFiddy).toBe(350);
+    expect(negThree).toBe(-3);
+    expect(zero).toBe(0);
+  });
+});
+
+describe("Subtract function", () => {
+  it("Subtract y from x", () => {
+    const subtract = funcs.subtract;
+
+    const hundred = subtract(103, 3);
+    const negOne = subtract(17, 18);
+    const threeFiddy = subtract(355, 5);
+    const negThree = subtract(0, 3);
+    const zero = subtract(21, 21);
+
+    expect(hundred).toBe(100);
+    expect(negOne).toBe(-1);
+    expect(threeFiddy).toBe(350);
+    expect(negThree).toBe(-3);
+    expect(zero).toBe(0);
+  });
+});
+
+describe("Divide function", () => {
+  it("Divide x by y", () => {
+    const divide = funcs.divide;
+
+    const one = divide(10, 10);
+    const zero = divide(0, 6);
+    const negThree = divide(-12, 4);
+    const revNegThree = divide(12, -4);
+    const undefined = divide(1, 0);
+
+    expect(one).toBe(1);
+    expect(zero).toBe(0);
+    expect(negThree).toBe(-3);
+    expect(revNegThree).toBe(-3);
+    expect(undefined).toBe(undefined);
+  });
+});
+
+describe("Multiply function", () => {
+  it("Multiply both numbers", () => {
+    const multiply = funcs.multiply;
+
+    const hundred = multiply(10, 10);
+    const zero = multiply(0, 6);
+    const revZero = multiply(1, 0);
+    const negTwelve = multiply(-3, 4);
+    const revNegTwelve = multiply(3, -4);
+
+    expect(hundred).toBe(100);
+    expect(zero).toBe(0);
+    expect(revZero).toBe(0);
+    expect(negTwelve).toBe(-12);
+    expect(revNegTwelve).toBe(-12);
+  });
+});
+
+describe("Modulo function", () => {
+  it("Divide x by y then return remainer", () => {
+    const getRemainder = funcs.getRemainder;
+
+    const three = getRemainder(17, 7);
+    const zero = getRemainder(10, 10);
+    const anthoerZero = getRemainder(0, 6);
+    const negTwo = getRemainder(-14, 3);
+    const threeNegDiv = getRemainder(15, -4);
+    const undefined = getRemainder(1, 0);
+
+    expect(three).toBe(3);
+    expect(zero).toBe(0);
+    expect(anthoerZero).toBe(0);
+    expect(negTwo).toBe(-2);
+    expect(threeNegDiv).toBe(3);
+    expect(undefined).toBe(undefined);
+  });
+});
