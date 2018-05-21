9dc1b0f9c7c4b3acc8d6c6392a6badeefa813f85
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-21 12:45:27 -0700

    first test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,44 @@
-const funcs = require('../src/project-1');
+const funcs = require('./project-1');
 
 // write your tests here
+    
+    describe('multiply by ten',()=>{
+    it("should multiply a number by 10", () => {
+      //arrange
+    const multiplyByTen = funcs.multiplyByTen;
+      //act
+      const thirty = multiplyByTen(3)
+ 
+      //assert
+      expect(thirty).toBe(30)
+      
+    });
+    })
+        
+    describe('subtract by five',()=>{
+        it("should subtract by 5", () => {
+          //arrange
+        const subtractFive = funcs.subtractFive;
+          //act
+          const ten = subtractFive(15)
+     
+          //assert
+          expect(ten).toBe(10)
+          
+        });
+        })
+        describe('check legnth of two arrays',()=>{
+            it("Should return true or false", () => {
+              //arrange
+            const areSameLength = funcs.areSameLength;
+              //act
+              const ten = subtractFive(15)
+         
+              //assert
+              expect(ten).toBe(10)
+              
+            });
+            })
+            
+        
+    
