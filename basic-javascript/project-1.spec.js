a5510088a40448adc78053d4c35c70cf71ac56b3
Yasin Shuman <email@yasin.io>
2018-05-21 12:39:11 -0700

    began writing tests for basic-javascript/project-1.js

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,14 @@
-const funcs = require('../src/project-1');
+const funcs = require("./project-1");
 
-// write your tests here
+describe("project-1 tests", () => {
+  it("should multiply by ten", () => {
+    const twenty = funcs.multiplyByTen(2);
+
+    expect(twenty).toBe(20);
+  });
+  it("should subtract five", () => {
+    const six = funcs.subtractFive(11);
+
+    expect(six).toBe(6);
+  });
+});
