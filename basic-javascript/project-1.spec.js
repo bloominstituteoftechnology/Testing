80344ec9a873d33f8040ddc49c26159013a5f3c6
Justin Rigler <justinmrigler@gmail.com>
2018-05-21 19:00:32 -0400

    install dependencies and began writing unit tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,14 @@
-const funcs = require('../src/project-1');
+const funcs = require('./project-1');
 
 // write your tests here
+describe('multiply by 10 function', () => {
+  it('should multiply by 10', () => {
+    // arrange
+    const multiplyByTen = funcs.multiplyByTen;
+    // act
+    const actual = multiplyByTen(5);
+
+    // assert
+    expect(actual).toBe(50);
+  });
+});
