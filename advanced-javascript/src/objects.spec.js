4b319f567b484f65714e9d68ac27ea8f22bd9154
Luis Hernandez <luishrd@gmail.com>
2018-05-21 09:07:28 -0600

    removes chai from docs

--- advanced-javascript/src/objects.spec.js
+++ advanced-javascript/src/objects.spec.js
@@ -1,9 +1,6 @@
-const assert = require('chai').expect;
 const objectFunctions = require('../src/objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {
-
-  });
+  it('should be an object', () => {});
 });
