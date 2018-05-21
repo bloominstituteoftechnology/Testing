8a7cf6b1ddb71c7c33145ddbf6f5db6590dbf514
unknown <sycode@gmail.com>
2018-05-21 16:44:46 -0600

    working on arrays tests

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('../src/arrays');
+const arrayFunctions = require('./arrays');
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -8,10 +8,20 @@ const arrayFunctions = require('../src/arrays');
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe('Arrays', () => {
+  describe('each', () => {
+    const mockCallBack = jest.fn();
+    it('each', () => {
+      const each = arrayFunctions.each;
+      // expect(typeof map).toBe('function');
+      each([9,1], mockCallBack);
+      expect(mockCallBack.mock.calls[0]).toEqual([9,0]);
+    });
+  });
   describe('map', () => {
-    it('should be a function', () => {
+    it('should an array of all items pushed through a callback', () => {
       const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      expect(typeof map).toBe('function');
+      expect(map([9], (item)=> item)).toEqual([9]);
     });
   });
 });
