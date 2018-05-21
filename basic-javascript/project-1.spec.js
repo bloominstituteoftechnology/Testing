8818c41d1dedd02c94a59e43e47c8d7be61b9bbe
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-05-21 18:11:54 -0500

    working through it

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,10 @@
-const funcs = require('../src/project-1');
+const funcs = require('../basic-javascript/project-1');
 
 // write your tests here
+it('should multiply by ten', () => {
+    const multiplyByTen = funcs.multiplyByTen;
+    const expected = 100;
+    const actual = multiplyByTen(10);
+    expect(actual).toBe(100);
+});
+
