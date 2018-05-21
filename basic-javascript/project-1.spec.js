71e09c0ed1bf355873846ca7ff0b47cac88a1d4e
Jared Cuffe <jcuffe@gmail.com>
2018-05-21 13:28:13 -0700

    Additional test cases

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,10 +1,17 @@
 const funcs = require('./project-1');
 
+const testTo1000 = (fn, expected, ...constants) => {
+  for (let i = 1; i < 1000; i++) {
+    args = constants.map(c => i * c)
+    expect(fn(...args)).toBe(i * expected)
+  }
+}
+
 // write your tests here
 it('runs the tests', () => { })
 
 it('multiplies a number by ten', () => {
-  expect(funcs.multiplyByTen(10)).toBe(100)
+  testTo1000(funcs.multiplyByTen, 10, 1)
 })
 
 it('subtracts five from a number', () => {
@@ -18,7 +25,9 @@ it('checks that two strings are of equal length', () => {
 
 it('checks the exact equality of two inputs', () => {
   expect(funcs.areEqual(1, 1)).toBeTruthy()
+  expect(funcs.areEqual([], [1])).toBeFalsy()
   expect(funcs.areEqual('a', 'b')).toBeFalsy()
+  expect(funcs.areEqual('a', 'a')).toBeTruthy()
 })
 
 it('checks whether a number is less than ninety', () => {
@@ -28,32 +37,43 @@ it('checks whether a number is less than ninety', () => {
 
 it('adds two numbers together', () => {
   expect(funcs.add(1, 2)).toBe(3)
+  expect(funcs.add(-3, -5)).toBe(-8)
 })
 
 it('subtracts one number from another', () => {
   expect(funcs.subtract(1, 2)).toBe(-1)
+  expect(funcs.subtract(23, -2)).toBe(25)
 })
 
 it('divides one number by another', () => {
   expect(funcs.divide(2, 2)).toBe(1)
+  expect(funcs.divide(46, -2)).toBe(-23)
 })
 
 it('multiplies one number by another', () => {
-  expect(funcs.multiply(10,10)).toBe(100)
+  expect(funcs.multiply(10, 10)).toBe(100)
+  expect(funcs.multiply(10, -10)).toBe(-100)
+  expect(funcs.multiply(10, 0)).toBe(0)
 })
 
 it('calculates the remainder of modulus division of two numbers', () => {
   expect(funcs.getRemainder(1, 2)).toBe(1)
+  expect(funcs.getRemainder(20, 40)).toBe(20)
+  expect(funcs.getRemainder(32, 6)).toBe(2)
 })
 
 it('determines whether a number is even', () => {
   expect(funcs.isEven(2)).toBeTruthy()
+  expect(funcs.isEven(-2)).toBeTruthy()
   expect(funcs.isEven(1)).toBeFalsy()
+  expect(funcs.isEven(-1)).toBeFalsy()
 })
 
 it('determines whether a number is odd', () => {
   expect(funcs.isOdd(2)).toBeFalsy()
+  expect(funcs.isOdd(-22)).toBeFalsy()
   expect(funcs.isOdd(1)).toBeTruthy()
+  expect(funcs.isOdd(-1)).toBeTruthy()
 })
 
 it('squares a number', () => {
@@ -66,6 +86,8 @@ it('cubes a number', () => {
 
 it('raises a number to a power', () => {
   expect(funcs.raiseToPower(2, 4)).toBe(16)
+  expect(funcs.raiseToPower(1, -1)).toBe(1)
+  expect(funcs.raiseToPower(2, -1)).toBe(0.5)
 })
 
 it('rounds a number to the nearest integer', () => {
@@ -91,17 +113,25 @@ it('takes a name and returns a greeting', () => {
 })
 
 it('calculates the area of a rectangle given length and width', () => {
-  expect(funcs.getRectangleArea(2,4)).toBe(8)
+  expect(funcs.getRectangleArea(2, 4)).toBe(8)
+  expect(funcs.getRectangleArea(1, 4)).toBe(4)
+  expect(funcs.getRectangleArea(5, .3)).toBe(1.5)
 })
 
 it('calculates the area of a triangle given base and height', () => {
   expect(funcs.getTriangleArea(2, 4)).toBe(4)
+  expect(funcs.getTriangleArea(0.5, 4)).toBe(1)
+  expect(funcs.getTriangleArea(20, 4)).toBe(40)
+  expect(funcs.getTriangleArea(1, 1)).toBe(0.5)
 })
 
 it('calculates the area of a circle given the radius', () => {
   expect(funcs.getCircleArea(2)).toBe(Math.PI * 4)
+  expect(funcs.getCircleArea(Math.PI)).toBe(Math.pow(Math.PI, 3))
+  expect(funcs.getCircleArea(0.5)).toBe(Math.PI * 0.25)
 })
 
 it('calculates the volume of a prism given length, width, and height', () => {
   expect(funcs.getRectangularPrismVolume(1, 2, 3)).toBe(6)
+  expect(funcs.getRectangularPrismVolume(2, 4, 6)).toBe(48)
 })
