5675bfc8640aab6b4e0c70ec0ac594b72ea0f0f3
tanking <tanking@github.com>
2018-05-21 15:52:58 -0400

    Implemented multiplyByTen test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,20 @@
 const funcs = require('./project-1');
 
 // write your tests here
+
+describe('default', () => {
+    it('runs the test', () => {})
+});
+
+describe('multiplyByTen', () => {
+    it('should multiply number by 10', () => {
+        // arrange
+        const multiplyByTen = funcs.multiplyByTen;
+
+        // act
+        const twenty = multiplyByTen(2);
+
+        // assert 
+        expect(twenty).toBe(20);
+    })
+})
