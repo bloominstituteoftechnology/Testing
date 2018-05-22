678ba3cb5ae648af9100e265f38c51267c0db793
sagdish <forsi1@yahoo.com>
2018-05-22 14:09:12 -0700

    working on array.js tests

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -8,10 +8,69 @@ const arrayFunctions = require('./arrays');
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe('Arrays', () => {
+  
   describe('map', () => {
+    const map = arrayFunctions.map;
+    const myFunc = item => {
+      return item * 2;
+    };
+    
     it('should be a function', () => {
-      const map = arrayFunctions.map;
       expect(typeof map).toBe('function');
     });
+    
+    it('output should be an array', () => {
+      const output = map([1, 3, 6], x => x);
+      expect(Array.isArray(output)).toEqual(true);
+    });
+    
+    it ('should invoke the callback', () => {
+      const invoked = map([10, 19, 22], myFunc);
+      expect(invoked).toEqual([20, 38, 44])
+    });
   });
+  
+  describe('each', () => {
+    const each = arrayFunctions.each;
+    
+    it('should be a function', () => {
+      expect(typeof each).toBe('function');
+    });
+    
+    it('should invoke callback on each element', () => {
+      const mockCalls = jest.fn();
+      const arr = [2, 4, 6];
+      each(arr, mockCalls)
+      
+      expect(mockCalls.mock.calls.length).toBe(3);
+    });
+    
+  });
+  
+  
+  describe('reduce', () => {
+    const reduce = arrayFunctions.reduce;
+    
+    it('should be a function', () => {
+      expect(typeof reduce).toBe('function');
+    });
+    
+    it('should reduce an array', () => {
+      const reduced = reduce([342, 34, 1, 3], (memo, item) => {
+        return memo - item;
+      })
+      console.log(reduced);
+      expect(reduced).toEqual(304)
+    });
+    
+    it('should invoke callback on each element', () => {
+      const mockCalls = jest.fn();
+      //console.log(mockCalls);
+      const reduced = reduce([2, 2, 3], (memo) => mockCalls(memo));
+      expect(mockCalls.mock.calls.length).toBe(2);
+    });
+    
+  });
+  
+  
 });
