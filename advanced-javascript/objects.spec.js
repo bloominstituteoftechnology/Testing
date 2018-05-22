9d77033512569a4850168ab7b2c244bbd7ffaca2
Devon Bacon <devon.bacon@gmail.com>
2018-05-22 15:27:38 -0700

    objects.spec.js  - added test case for defaults

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -5,15 +5,51 @@ describe('objects', () => {
   it('should be an object', () => {});
 
   describe('invert', () => {
-    const testObj = { this: 'is', a: 'test', object: 'woot' };
+    const testObj = {
+      this: 'is',
+      a: 'test',
+      object: 'woot'
+    };
+    const expected = {
+      is: 'this',
+      test: 'a',
+      woot: 'object'
+    };
     const result = objectFunctions.invert(testObj);
-    const expected = { is: 'this', test: 'a', woot: 'object' };
 
     it('should return an object', () => {
       expect(typeof result).toBe('object');
       expect(result.length).toBe(undefined);
     });
     it('should invert object key value pairs', () => {
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('defaults', () => {
+    const testObj = {
+      this: 'is',
+      a: 'test',
+      object: 'woot'
+    };
+    const defaultProps = {
+      unset: 'set',
+      values: 'defaults'
+    };
+    const expected = {
+      this: 'is',
+      a: 'test',
+      object: 'woot',
+      unset: 'set',
+      values: 'defaults'
+    };
+    const result = objectFunctions.defaults(testObj, defaultProps);
+
+    it('should return an object', () => {
+      expect(typeof result).toBe('object');
+      expect(result.length).toBe(undefined);
+    });
+    it('adds values from default into obj if not set initially', () => {
       expect(result).toEqual(expected);
     });
   });
