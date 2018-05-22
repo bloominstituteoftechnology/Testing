cdf49b9081eb10718f72e0626892e90c4bb67ce9
Justin M <jekm321@gmail.com>
2018-05-21 20:44:47 -0400

    finished project 2 tests

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -6,9 +6,9 @@ describe('projecct 2 testing', () => {
     it('should return the biggest value', () => {
         const getBiggest = funcs.getBiggest;
 
-        const twelve = getBiggest(12,5);
-        const fifteen = getBiggest(12,15);
-        const fifty = getBiggest(12,50);
+        const twelve = getBiggest(12, 5);
+        const fifteen = getBiggest(12, 15);
+        const fifty = getBiggest(12, 50);
 
         expect(twelve).toBe(12);
         expect(fifteen).toBe(15);
@@ -90,9 +90,9 @@ describe('projecct 2 testing', () => {
     it('should return the first value in the array', () => {
         const returnFirst = funcs.returnFirst;
 
-        const ten = returnFirst([10,5,15]);
-        const fifty = returnFirst([50,20,10]);
-        const thirteen = returnFirst([13,14,15,16]);
+        const ten = returnFirst([10, 5, 15]);
+        const fifty = returnFirst([50, 20, 10]);
+        const thirteen = returnFirst([13, 14, 15, 16]);
 
         expect(ten).toBe(10);
         expect(fifty).toBe(50);
@@ -102,9 +102,9 @@ describe('projecct 2 testing', () => {
     it('should return the last value in the array', () => {
         const returnLast = funcs.returnLast;
 
-        const fifteen = returnLast([10,5,15]);
-        const ten = returnLast([50,20,10]);
-        const sixteen = returnLast([13,14,15,16]);
+        const fifteen = returnLast([10, 5, 15]);
+        const ten = returnLast([50, 20, 10]);
+        const sixteen = returnLast([13, 14, 15, 16]);
 
         expect(fifteen).toBe(15);
         expect(ten).toBe(10);
@@ -114,9 +114,9 @@ describe('projecct 2 testing', () => {
     it('should return the length of the array', () => {
         const getArrayLength = funcs.getArrayLength;
 
-        const three = getArrayLength([10,5,15]);
-        const two = getArrayLength([50,20]);
-        const four = getArrayLength([13,14,15,16]);
+        const three = getArrayLength([10, 5, 15]);
+        const two = getArrayLength([50, 20]);
+        const four = getArrayLength([13, 14, 15, 16]);
 
         expect(three).toBe(3);
         expect(two).toBe(2);
@@ -126,12 +126,96 @@ describe('projecct 2 testing', () => {
     it('should return the values given plus 1', () => {
         const incrementByOne = funcs.incrementByOne;
 
-        const arr1 = incrementByOne([10,5,15]);
-        const arr2 = incrementByOne([50,20]);
-        const arr3 = incrementByOne([13,14,15,16]);
+        const arr1 = incrementByOne([10, 5, 15]);
+        const arr2 = incrementByOne([50, 20]);
+        const arr3 = incrementByOne([13, 14, 15, 16]);
 
-        expect(arr1).toEqual([11,6,16]);
-        expect(arr2).toEqual([51,21]);
-        expect(arr3).toEqual([14,15,16,17]);
+        expect(arr1).toEqual([11, 6, 16]);
+        expect(arr2).toEqual([51, 21]);
+        expect(arr3).toEqual([14, 15, 16, 17]);
+    });
+
+    it('should return the array with the new item added', () => {
+        const addItemToArray = funcs.addItemToArray;
+
+        const fourteen = addItemToArray([10, 5, 15], 14);
+        const twentyOne = addItemToArray([50, 20], 21);
+        const seventeen = addItemToArray([13, 14, 15, 16], 17);
+
+        expect(fourteen).toEqual([10, 5, 15, 14]);
+        expect(twentyOne).toEqual([50, 20, 21]);
+        expect(seventeen).toEqual([13, 14, 15, 16, 17]);
+    });
+
+    it('should return the array with the new item added to the front', () => {
+        const addItemToFront = funcs.addItemToFront;
+
+        const fourteen = addItemToFront([10, 5, 15], 14);
+        const twentyOne = addItemToFront([50, 20], 21);
+        const seventeen = addItemToFront([13, 14, 15, 16], 17);
+
+        expect(fourteen).toEqual([14, 10, 5, 15]);
+        expect(twentyOne).toEqual([21, 50, 20]);
+        expect(seventeen).toEqual([17, 13, 14, 15, 16]);
+    });
+
+    it('shouldthe arr of words into a sentence string', () => {
+        const wordsToSentence = funcs.wordsToSentence;
+
+        const arr1 = wordsToSentence(['why', 'hello', 'there']);
+        const arr2 = wordsToSentence(['hi', 'there']);
+        const arr3 = wordsToSentence(['how', 'is', 'it', 'going']);
+
+        expect(arr1).toEqual('why hello there');
+        expect(arr2).toEqual('hi there');
+        expect(arr3).toEqual('how is it going');
+    });
+
+    it('should check to see if the given array contains the given value', () => {
+        const contains = funcs.contains;
+
+        const fourteen = contains([10, 5, 15], 14);
+        const twenty = contains([50, 20], 20);
+        const sixteen = contains([13, 14, 15, 16], 16);
+
+        expect(fourteen).toEqual(false);
+        expect(twenty).toEqual(true);
+        expect(sixteen).toEqual(true);
+    });
+
+    it('should return the sum of all the numbers in the array', () => {
+        const addNumbers = funcs.addNumbers;
+
+        const thirty = addNumbers([10, 5, 15]);
+        const seventy = addNumbers([50, 20]);
+        const hundred = addNumbers([10, 20, 30, 40]);
+
+        expect(thirty).toEqual(30);
+        expect(seventy).toEqual(70);
+        expect(hundred).toEqual(100);
+    });
+
+    it('should return the avg of all the numbers in the array', () => {
+        const averageTestScore = funcs.averageTestScore;
+
+        const ten = averageTestScore([10, 5, 15]);
+        const thirtyFive = averageTestScore([50, 20]);
+        const twentyFive = averageTestScore([10, 20, 30, 40]);
+
+        expect(ten).toEqual(10);
+        expect(thirtyFive).toEqual(35);
+        expect(twentyFive).toEqual(25);
+    });
+
+    it('should return the largest number in the array', () => {
+        const largestNumber = funcs.largestNumber;
+
+        const fifteen = largestNumber([10, 5, 15]);
+        const fifty = largestNumber([50, 20]);
+        const fourty = largestNumber([10, 20, 30, 40]);
+
+        expect(fifteen).toEqual(15);
+        expect(fifty).toEqual(50);
+        expect(fourty).toEqual(40);
     });
 });
