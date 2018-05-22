56d04285da6b1f3eb7b8e6a36dc227ad2f4b614b
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-21 22:09:21 -0700

    finshed arrays
    i finished arrays i think?  from the function that was there i assumed it was just checking for the type of each one.

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -7,13 +7,43 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
+describe('Arrays', () => {
+  describe('map', () => {
+    it('should be a function', () => {
+      const map = arrayFunctions.map;
+      expect(typeof map).toBe('function');
+    });
+  });
+});
 
+  describe('each', () => {
+    it('should be a function', () => {
+      const each = arrayFunctions.each
+      expect(typeof each).toBe('function');
+    });
+  });
+  describe('reduce', () => {
+    it('should be a function', () => {
+      const reduce = arrayFunctions.reduce
+      expect(typeof reduce).toBe('function');
+    });
+  });
+  describe('find', () => {
+    it('should be a function', () => {
+      const find = arrayFunctions.find
+      expect(typeof find).toBe('function');
+    });
+  });
+  describe('Filter', () => {
+    it('should be a function', () => {
+      const filter = arrayFunctions.filter
+      expect(typeof filter).toBe('function');
+    });
+  });
+  describe('flatten', () => {
+    it('should be a function', () => {
+      const flatten = arrayFunctions.flatten
+      expect(typeof flatten).toBe('function');
+    });
+  });
 
-// describe('Arrays', () => {
-//   describe('map', () => {
-//     it('should be a function', () => {
-//       const map = arrayFunctions.map;
-//       expect(typeof map).toBe('object');
-//     });
-//   });
-// });
