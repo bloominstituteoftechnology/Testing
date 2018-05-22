b4d4fa5fae6dce93a6fdd253565f2b9b762f44bf
Nedu Robert <nedumrobert@gmail.com>
2018-05-22 11:50:38 +0100

    wrote tests for the each function

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -8,6 +8,20 @@ const arrayFunctions = require('./arrays');
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe('Arrays', () => {
+  describe('each', () => {
+    it('should be a function', () => {
+      const each = arrayFunctions.each;
+      expect(typeof each).toBe('function');
+    });
+
+    it('should call a callback function for each element in an array', () => {
+      const mockCallback = jest.fn();
+      const each = arrayFunctions.each;
+      each([2, 5, 6], mockCallback);
+      expect(mockCallback.mock.calls.length).toBe(3);
+    })
+  });
+
   describe('map', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
