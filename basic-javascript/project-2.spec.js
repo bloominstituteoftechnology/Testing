ff15bbc3d2068a763276131dd4b4e9ebb40dea45
Yasin Shuman <email@yasin.io>
2018-05-22 12:20:15 -0700

    2 tests passed on project-2 tests

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -2,4 +2,19 @@ const funcs = require("./project-2");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 
-describe("project-1 tests", () => {});
+describe("project-2 tests", () => {
+  it("should return the bigger number", () => {
+    const big = funcs.getBiggest(7, 2);
+
+    expect(big).toBe(7);
+  });
+  it("should return the proper greeting", () => {
+    const german = funcs.greeting("German");
+    const spanish = funcs.greeting("Spanish");
+    const generic = funcs.greeting();
+
+    expect(german).toBe("Guten Tag!");
+    expect(spanish).toBe("Hola!");
+    expect(generic).toBe("Hello!");
+  });
+});
