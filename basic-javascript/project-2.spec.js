1fcdf0e12c09fa1a1462333b29bdd2522460c658
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-05-22 16:37:47 -0500

    finished project 1

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,37 @@
-const funcs = require('../src/project-2');
+const funcs = require('./project-2');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+it('should get the biggest between x and y', () => {
+    // arrange
+    const check = funcs.getBiggest;
+    // act
+    const actual = check(5, 4);
+    // assert
+    expect(actual).toBe(5);
+})
+
+it('should greet you based on your language', () => {
+    // arrange
+    const check = funcs.greeting;
+    // act
+    const actual = check('German');
+    // assert
+    expect(actual).toBe('Guten Tag!');
+})
+
+it('should decide if the number is ten or five', () => {
+  // arrange
+  const check = funcs.isTenOrFive;
+  // act
+  const actual = check(4);
+  // assert  
+  expect(actual).toBe(false);
+})
+
+it('should check if a number is in range', () => {
+    // arrange
+    const check = funcs.isInRange;
+    //act
+    const actual = check(100);
+    // assert
+    expect(actual).toBe(false);
+})
