a965d8787841a4f05340f1d497301da04b2cb660
Justin M <jekm321@gmail.com>
2018-05-21 20:11:38 -0400

    project 1 tests all written

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -3,7 +3,7 @@ const funcs = require('./project-1');
 
 describe('default', () => {
     it('run the tests', () => { });
-})
+});
 
 describe('projecct 1 testing', () => {
     it('should multiple by 10', () => {
@@ -16,8 +16,7 @@ describe('projecct 1 testing', () => {
         expect(oneHundred).toBe(100);
         expect(oneFifty).toBe(150);
         expect(twenty).toBe(20);
-
-    })
+    });
 
     it('should subtract 5', () => {
         const subtractFive = funcs.subtractFive;
@@ -29,7 +28,7 @@ describe('projecct 1 testing', () => {
         expect(five).toBe(5);
         expect(ten).toBe(10);
         expect(twenty).toBe(20);
-    })
+    });
 
     it('should checck if two strings have the same length', () => {
         const areSameLength = funcs.areSameLength;
@@ -45,7 +44,7 @@ describe('projecct 1 testing', () => {
         expect(istrue).toBe(true);
         expect(isfalse).toBe(false);
         expect(alsoTrue).toBe(true);
-    })
+    });
 
     it('should check if 2 nums are equal', () => {
         const areEqual = funcs.areEqual;
@@ -57,7 +56,7 @@ describe('projecct 1 testing', () => {
         expect(isequal).toBe(true);
         expect(notequal).toBe(false);
         expect(alsofalse).toBe(false);
-    })
+    });
 
     it('should be true for any value less than 90', () => {
         const lessThanNinety = funcs.lessThanNinety;
@@ -69,7 +68,7 @@ describe('projecct 1 testing', () => {
         expect(sixty).toBe(true);
         expect(twenty).toBe(true);
         expect(twoHundred).toBe(false);
-    })
+    });
 
     it('should be true for any value greater than 50', () => {
         const greaterThanFifty = funcs.greaterThanFifty;
@@ -81,67 +80,67 @@ describe('projecct 1 testing', () => {
         expect(sixty).toBe(true);
         expect(twenty).toBe(false);
         expect(twoHundred).toBe(true);
-    })
+    });
 
     it('should return the value sum of 2 digits', () => {
         const add = funcs.add;
 
-        const sixty = add(20,40);
-        const twenty = add(15,5);
-        const twoHundred = add(150,50);
+        const sixty = add(20, 40);
+        const twenty = add(15, 5);
+        const twoHundred = add(150, 50);
 
         expect(sixty).toBe(60);
         expect(twenty).toBe(20);
         expect(twoHundred).toBe(200);
-    })
+    });
 
     it('should return the value difference of 2 digits', () => {
         const subtract = funcs.subtract;
 
-        const sixty = subtract(80,20);
-        const twenty = subtract(25,5);
-        const twoHundred = subtract(250,50);
+        const sixty = subtract(80, 20);
+        const twenty = subtract(25, 5);
+        const twoHundred = subtract(250, 50);
 
         expect(sixty).toBe(60);
         expect(twenty).toBe(20);
         expect(twoHundred).toBe(200);
-    })
+    });
 
     it('should return the value dividend of 2 digits', () => {
         const divide = funcs.divide;
 
-        const sixty = divide(360,6);
-        const twenty = divide(100,5);
-        const twoHundred = divide(500,2.5);
+        const sixty = divide(360, 6);
+        const twenty = divide(100, 5);
+        const twoHundred = divide(500, 2.5);
 
         expect(sixty).toBe(60);
         expect(twenty).toBe(20);
         expect(twoHundred).toBe(200);
-    })
+    });
 
     it('should return the value product of 2 digits', () => {
         const multiply = funcs.multiply;
 
-        const sixty = multiply(10,6);
-        const twenty = multiply(4,5);
-        const twoHundred = multiply(100,2);
+        const sixty = multiply(10, 6);
+        const twenty = multiply(4, 5);
+        const twoHundred = multiply(100, 2);
 
         expect(sixty).toBe(60);
         expect(twenty).toBe(20);
         expect(twoHundred).toBe(200);
-    })
+    });
 
     it('should return the remainder of 2 digits', () => {
         const getRemainder = funcs.getRemainder;
 
-        const four = getRemainder(10,6);
-        const one = getRemainder(5,4);
-        const zero = getRemainder(100,2);
+        const four = getRemainder(10, 6);
+        const one = getRemainder(5, 4);
+        const zero = getRemainder(100, 2);
 
         expect(four).toBe(4);
         expect(one).toBe(1);
         expect(zero).toBe(0);
-    })
+    });
 
     it('should check if a digit is even', () => {
         const isEven = funcs.isEven;
@@ -153,7 +152,7 @@ describe('projecct 1 testing', () => {
         expect(ten).toBe(true);
         expect(five).toBe(false);
         expect(hundred).toBe(true);
-    })
+    });
 
     it('should check if a digit is odd', () => {
         const isOdd = funcs.isOdd;
@@ -165,7 +164,149 @@ describe('projecct 1 testing', () => {
         expect(ten).toBe(false);
         expect(five).toBe(true);
         expect(hundred).toBe(false);
-    })
+    });
 
+    it('should return the squared number', () => {
+        const square = funcs.square;
 
+        const twentyFive = square(5);
+        const fourtyNine = square(7);
+        const hundred = square(10);
+
+        expect(twentyFive).toBe(25);
+        expect(fourtyNine).toBe(49);
+        expect(hundred).toBe(100);
+    });
+
+    it('should return the cubed number', () => {
+        const cube = funcs.cube;
+
+        const eight = cube(2);
+        const twentyseven = cube(3);
+        const sixtyfour = cube(4);
+
+        expect(eight).toBe(8);
+        expect(twentyseven).toBe(27);
+        expect(sixtyfour).toBe(64);
+    });
+
+    it('should return the number raised to the power given', () => {
+        const raiseToPower = funcs.raiseToPower;
+
+        const eight = raiseToPower(2, 3);
+        const sixteen = raiseToPower(4, 2);
+        const sixHundredTwentyFive = raiseToPower(5, 4);
+
+        expect(eight).toBe(8);
+        expect(sixteen).toBe(16);
+        expect(sixHundredTwentyFive).toBe(625);
+    });
+
+    it('should return the number rounded to the nearest whole number', () => {
+        const roundNumber = funcs.roundNumber;
+
+        const eight = roundNumber(7.8);
+        const sixteen = roundNumber(16.3);
+        const four = roundNumber(4.4999999);
+
+        expect(eight).toBe(8);
+        expect(sixteen).toBe(16);
+        expect(four).toBe(4);
+    });
+
+    it('should return the number rounded up', () => {
+        const roundUp = funcs.roundUp;
+
+        const eight = roundUp(7.8);
+        const sixteen = roundUp(15.3);
+        const four = roundUp(3.4999999);
+
+        expect(eight).toBe(8);
+        expect(sixteen).toBe(16);
+        expect(four).toBe(4);
+    });
+
+    it('should return the string with an ! added on', () => {
+        const addExclamationPoint = funcs.addExclamationPoint;
+
+        const eight = addExclamationPoint(8);
+        const sixteen = addExclamationPoint('16');
+        const city = addExclamationPoint('new york');
+
+        expect(eight).toBe('8!');
+        expect(sixteen).toBe('16!');
+        expect(city).toBe('new york!');
+    });
+
+    it('should return a string with the 2 given names combined', () => {
+        const combineNames = funcs.combineNames;
+
+        const justin = combineNames('Justin', 'Mathew');
+        const jack = combineNames('jack', 'dawson');
+        const jekm = combineNames('je', 'km');
+
+        expect(justin).toBe('Justin Mathew');
+        expect(jack).toBe('jack dawson');
+        expect(jekm).toBe('je km');
+    });
+
+    it('should return a greeting with the given name', () => {
+        const getGreeting = funcs.getGreeting;
+
+        const justin = getGreeting('Justin');
+        const jack = getGreeting('jack');
+        const jekm = getGreeting('jekm');
+
+        expect(justin).toBe('Hello Justin!');
+        expect(jack).toBe('Hello jack!');
+        expect(jekm).toBe('Hello jekm!');
+    });
+
+    it('should return area of the rectangle given', () => {
+        const getRectangleArea = funcs.getRectangleArea;
+
+        const twelve = getRectangleArea(4, 3);
+        const nine = getRectangleArea(3, 3);
+        const fifteen = getRectangleArea(3, 5);
+
+        expect(twelve).toBe(12);
+        expect(nine).toBe(9);
+        expect(fifteen).toBe(15);
+    });
+
+    it('should return area of the triangle given', () => {
+        const getTriangleArea = funcs.getTriangleArea;
+
+        const six = getTriangleArea(4, 3);
+        const nine = getTriangleArea(3, 6);
+        const fifteen = getTriangleArea(3, 10);
+
+        expect(six).toBe(6);
+        expect(nine).toBe(9);
+        expect(fifteen).toBe(15);
+    });
+
+    it('should return area of the circle given', () => {
+        const getCircleArea = funcs.getCircleArea;
+
+        const threeSquaredPi = getCircleArea(3);
+        const sixSquaredPi = getCircleArea(6);
+        const tenSquaredPi = getCircleArea(10);
+
+        expect(threeSquaredPi).toBe(9 * Math.PI);
+        expect(sixSquaredPi).toBe(36 * Math.PI);
+        expect(tenSquaredPi).toBe(100 * Math.PI);
+    });
+
+    it('should return volume of the prism given', () => {
+        const getRectangularPrismVolume = funcs.getRectangularPrismVolume;
+
+        const twentySeven = getRectangularPrismVolume(3, 3, 3);
+        const thirtySix = getRectangularPrismVolume(6, 3, 2);
+        const hundred = getRectangularPrismVolume(10, 5, 2);
+
+        expect(twentySeven).toBe(27);
+        expect(thirtySix).toBe(36);
+        expect(hundred).toBe(100);
+    });
 });
