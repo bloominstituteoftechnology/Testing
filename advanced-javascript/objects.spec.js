10effa64b897a4fb7d8e5a7b0c7d97800eb88e5c
totsuya <supertatortots@gmail.com>
2018-05-22 16:07:26 -0400

    finished objects testing

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,7 +1,7 @@
 const objectFunctions = require('../advanced-javascript/objects.js');
 
-
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+// whoops.. there is no test suite for this file. You'll simply just have to
+// create one :/
 describe('objects', () => {
   it('should be an object', () => {});
 });
@@ -23,3 +23,53 @@ it('Should return object values', () => {
   }
   expect(objectFunctions.values(obj)).toEqual(['soft', 'maid', 'shrine maiden'])
 })
+
+it('Should map over object and return changed value based on function', () => {
+  const mapObject = objectFunctions.mapObject;
+  const numbers = {
+    alpha: 1,
+    beta: 2,
+    charlie: 3
+  }
+
+  expect(mapObject(numbers, val => val * 2)).toEqual({alpha: 2, beta: 4, charlie: 6});
+})
+
+it('Should return key value pairs into arrays', () => {
+  const pairs = objectFunctions.pairs;
+  const obj = {
+    fumo: 'soft',
+    sakuya: 'maid',
+    reimu: 'shrine maiden'
+  }
+  expect(pairs(obj)).toEqual([
+    [
+      'fumo', 'soft'
+    ],
+    [
+      'sakuya', 'maid'
+    ],
+    ['reimu', 'shrine maiden']
+  ]);
+})
+
+it('Should invert key value pairings', () => {
+  const invert = objectFunctions.invert;
+  const obj = {
+    fumo: 'soft',
+    sakuya: 'maid',
+    reimu: 'shrine maiden'
+  }
+  expect(invert(obj)).toEqual({"maid": "sakuya", "shrine maiden": "reimu", "soft": "fumo"})
+})
+
+it('Should return default values of an object', () => {
+  const defaults = objectFunctions.defaults;
+  const obj = {
+    fumo: 'soft',
+    sakuya: 'maid',
+    reimu: 'shrine maiden'
+  }
+  expect(defaults(obj, {mokou: 'immortal'})).toEqual({fumo: 'soft', mokou: 'immortal', sakuya: 'maid', reimu: 'shrine maiden'})
+})
+
