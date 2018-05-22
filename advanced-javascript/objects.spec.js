345fbd45f8263f19cee0b2d910be7719ec705313
Nedu Robert <nedumrobert@gmail.com>
2018-05-22 13:40:02 +0100

    wrote tests for the keys function

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -2,5 +2,19 @@ const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
-});
+  describe('keys', () => {
+    it('should be an function', () => {
+      const keys = objectFunctions.keys;
+      expect(typeof keys).toBe('function');
+    });
+
+    it('should return an array of the object keys', () => {
+      const expected = ['a', 'b', 'c']
+      const keys = objectFunctions.keys;
+      const actual = keys({'a': 1, 'b': 4, 'c': 8})
+      expect(Array.isArray(actual)).toBeTruthy();
+      expect(actual.length).toEqual(3);
+      expect(actual).toEqual(expected);
+    });
+  });
+})
