9f468e6f99e2e381834ff685af79380bb176bbbf
sadrak105 <sadrak10@gmail.com>
2018-05-22 18:33:49 -0400

    object keys and values functions

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,30 @@
-const objectFunctions = require('../src/objects');
+const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
   it('should be an object', () => {});
+
+  describe('keys', () => {
+    const keys = objectFunctions.keys;
+    const actual = keys({
+      a: 1,
+      b: 2,
+      c: 3
+    });
+    it('Should return object keys', () => {
+      expect(actual).toEqual(['a', 'b', 'c']);
+    });
+  });
+
+  describe('values', () => {
+    const values = objectFunctions.values;
+    const actual = values({
+      a: 1,
+      b: 2,
+      c: 3
+    });
+    it('Should return values of object', () => {
+      expect(actual).toEqual([1, 2, 3]);
+    })
+  });
 });
