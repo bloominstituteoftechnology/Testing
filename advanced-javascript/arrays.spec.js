a210eba8b614556e11781c0a9a109490a0280bab
sadrak105 <sadrak10@gmail.com>
2018-05-22 17:28:11 -0400

    helper func

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -8,14 +8,36 @@ const arrayFunctions = require('./arrays');
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe('Arrays', () => {
+  //Helper Function
+  function isFunction(func) {
+    it('should be a function', () => {
+      expect(typeof func).toBe('function');
+    });
+  };
+
+  describe('each', () => {
+    const elements = [1, 2, 3];
+    const cb = (el, index) => {
+      // el = `(${el + index})`
+      return el + index;
+    };
+    // const cb = (el, index) => {
+    //   return `${el}` + `${index}`
+    // };
+    const expected = arrayFunctions.each(elements, cb);
+    //Checks if function is type of function
+    isFunction(arrayFunctions.each);
+    it('Should return expected outcome', () => {
+      expect([1, 3, 4]).toBe(expected);
+    });
+  });
+
   describe('map', () => {
     const cb = (item) => item + 2;
     const mappedArray = arrayFunctions.map([1, 2, 3],
       cb);
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('function');
-    });
+    //Checks if function is type of function    
+    isFunction(arrayFunctions.map);
     it('Should return an array', () => {
       expect(mappedArray instanceof Array).toBeTruthy();
     });
