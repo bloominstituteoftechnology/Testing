103bde1371291f4f891402d0a7d8a133b017d62b
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-21 18:39:12 -0700

    Completed Basic JS Project

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -1,113 +1,137 @@
 /* eslint-disable arrow-parens */
-
+//1
 const multiplyByTen = num => {
   return num * 10;
-};
+}; //Done
 
+//2
 const subtractFive = num => {
   return num - 5;
-};
+}; //Done
 
+//3
 const areSameLength = (str1, str2) => {
   return str1.length === str2.length;
-};
+}; //Done
 
+//4
 const areEqual = (x, y) => {
   return x === y;
-};
+}; //Done
 
+//5
 const lessThanNinety = num => {
   if (num < 90) {
     return true;
   }
   return false;
-};
+}; //Done
 
+//6
 const greaterThanFifty = num => {
   if (num > 50) {
     return true;
   }
   return false;
-};
+}; //Done
 
+//7
 const add = (x, y) => {
   return x + y;
-};
+}; //Done
 
+//8
 const subtract = (x, y) => {
   return x - y;
-};
+}; //Done
 
+//9
 const divide = (x, y) => {
   return x / y;
-};
+}; //Done
 
+//10
 const multiply = (x, y) => {
   return x * y;
-};
+}; //Done
 
+//11
 const getRemainder = (x, y) => {
   return x % y;
-};
+}; //Done
 
+//12
 const isEven = num => {
   if (num % 2 === 0) {
     return true;
   }
   return false;
-};
+}; //Done
 
+//13
 const isOdd = num => {
   if (num % 2 === 0) {
     return false;
   }
   return true;
-};
+}; //Done
 
+//14
 const square = num => {
   return num * num;
-};
+}; //Done
 
+//15
 const cube = num => {
   return num * num * num;
-};
+}; //Done
 
+//16
 const raiseToPower = (num, exponent) => {
   return num ** exponent;
-};
+}; //Done
 
+//17
 const roundNumber = num => {
   return Math.round(num);
-};
+}; //Done
 
+//18
 const roundUp = num => {
   return Math.ceil(num);
-};
+}; //Done
 
+//19
 const addExclamationPoint = str => {
   return (str += '!');
-};
+}; //Done
 
+//20
 const combineNames = (firstName, lastName) => {
   return `${firstName} ${lastName}`;
-};
+}; //Done
 
+//21
 const getGreeting = name => {
   return `Hello ${name}!`;
-};
+}; //Done
 
+//22
 const getRectangleArea = (length, width) => {
   return length * width;
-};
+}; //Done
 
+//23
 const getTriangleArea = (base, height) => {
   return 0.5 * base * height;
-};
+}; //Done
 
+//24
 const getCircleArea = radius => {
   return Math.PI * radius * radius;
-};
+}; //Done
 
+//25
 const getRectangularPrismVolume = (length, width, height) => {
   return width * height * length;
 };
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,236 @@
 const funcs = require('./project-1');
 
 // write your tests here
+
+it('multiply number by 10', () => {
+    const numTimesTen = funcs.multiplyByTen;
+    
+    const timesTwo = numTimesTen(2);
+    const timesFour = numTimesTen(4);
+
+    expect(timesTwo).toEqual(20);
+    expect(timesFour).toEqual(40);
+});
+
+it('subtract number by 5', () => {
+    const minusFive = funcs.subtractFive;
+
+    const Ten = minusFive(10);
+    const One = minusFive(1);
+
+    expect(Ten).toEqual(5);
+    expect(One).toEqual(-4);
+});
+
+it('tests is 2 values are same length?', () => {
+    const sameLength = funcs.areSameLength;
+
+    const ExOne = sameLength('pug', '123');
+    const ExTwo = sameLength('kitty', '1234');
+
+    expect(ExOne).toEqual(true);
+    expect(ExTwo).toEqual(false);
+});
+
+it('tests if 2 values are equal?', () => {
+    const equalSame = funcs.areEqual;
+
+    const ExOne = equalSame('pug', 'pug');
+    const ExTwo = equalSame('cat', 'kitty');
+
+    expect(ExOne).toEqual(true);
+    expect(ExTwo).toEqual(false);
+});
+
+it('tests if values are less than 90?', () => {
+    const ninetyLess = funcs.lessThanNinety;
+
+    const ExOne = ninetyLess('25');
+    const ExTwo = ninetyLess('125');
+
+    expect(ExOne).toEqual(true);
+    expect(ExTwo).toEqual(false);
+});
+
+it('tests if values are greater than 50?', () => {
+    const greaterThan50 = funcs.greaterThanFifty;
+
+    const ExOne = greaterThan50('25');
+    const ExTwo = greaterThan50('125');
+
+    expect(ExOne).toEqual(false);
+    expect(ExTwo).toEqual(true);
+});
+
+it('add 2 numbers; return sum', () => {
+    const add = funcs.add;
+
+    const seven = add(3, 4);
+    const ten = add(5, 5);
+
+    expect(seven).toBe(7);
+    expect(ten).toBe(10);
+});
+
+it('minus 2 numbers; return sum', () => {
+    const subtract = funcs.subtract;
+
+    const one = subtract(4, 3);
+    const five = subtract(10, 5);
+
+    expect(one).toBe(1);
+    expect(five).toBe(5);
+});
+
+it('divide 2 numbers; return sum', () => {
+    const divide = funcs.divide;
+
+    const four = divide(12, 3);
+    const two = divide(10, 5);
+
+    expect(four).toBe(4);
+    expect(two).toBe(2);
+});
+
+it('multiply 2 numbers; return sum', () => {
+    const multiply = funcs.multiply;
+
+    const four = multiply(2, 2);
+    const two = multiply(2, 1);
+
+    expect(four).toBe(4);
+    expect(two).toBe(2);
+});
+
+it('get remainder of 2 numbers', () => {
+    const getRemainder = funcs.getRemainder;
+
+    const one = getRemainder(10, 3);
+    const two = getRemainder(18, 4);
+
+    expect(one).toBe(1);
+    expect(two).toBe(2);
+});
+
+it('checks for even number', () => {
+    const isEven = funcs.isEven;
+
+    const ExOne = isEven('22');
+    const ExTwo = isEven('125');
+
+    expect(ExOne).toEqual(true);
+    expect(ExTwo).toEqual(false);
+});
+
+it('checks for odd number', () => {
+    const isOdd = funcs.isOdd;
+
+    const ExOne = isOdd('25');
+    const ExTwo = isOdd('126');
+
+    expect(ExOne).toEqual(true);
+    expect(ExTwo).toEqual(false);
+});
+
+it('checks for square number', () => {
+    const square = funcs.square;
+
+    const nine = square(3, 3);
+    const four = square(2, 2);
+
+    expect(nine).toBe(9);
+    expect(four).toBe(4);
+});
+
+it('checks for cubed number', () => {
+    const cube = funcs.cube;
+
+    const example = cube(2, 2, 2);
+
+    expect(example).toBe(8);
+});
+
+it('checks for raise to power', () => {
+    const raiseToPower = funcs.raiseToPower;
+
+    const example = raiseToPower(2, 7);
+
+    expect(example).toBe(128);
+});
+
+
+it('checks for round number', () => {
+    const roundNumber = funcs.roundNumber;
+
+    const example = roundNumber(1.2);
+
+    expect(example).toBe(1);
+});
+
+it('checks for rounded up number', () => {
+    const roundUp = funcs.roundUp;
+
+    const example = roundUp(.95);
+
+    expect(example).toBe(1);
+});
+
+it('adds exlamation point to string', () => {
+    const addExclamationPoint = funcs.addExclamationPoint;
+
+    const example = addExclamationPoint('pug');
+
+    expect(example).toBe('pug!');
+});
+
+it('combine names', () => {
+    const combineNames = funcs.combineNames;
+
+    const example = combineNames('Clementine', 'Pug');
+
+    expect(example).toBe('Clementine Pug');
+});
+
+it('get greeting', () => {
+    const getGreeting = funcs.getGreeting;
+
+    const example = getGreeting('Clementine');
+
+    expect(example).toBe('Hello Clementine!');
+});
+
+it('get Rectangle Area', () => {
+    const getRectangleArea = funcs.getRectangleArea;
+
+    const example = getRectangleArea(2, 4);
+
+    expect(example).toBe(8);
+});
+
+it('get Triangle Area', () => {
+    const getTriangleArea = funcs.getTriangleArea;
+
+    const example = getTriangleArea(2, 2);
+
+    expect(example).toBe(2);
+});
+
+it('get Circle Area', () => {
+    const getCircleArea = funcs.getCircleArea;
+
+    const example = getCircleArea(2);
+
+    expect(example).toBe(Math.PI * 4);
+});
+
+it('get Rectangular prism Volume', () => {
+    const getRectangularPrismVolume = funcs.getRectangularPrismVolume;
+
+    const example = getRectangularPrismVolume(3, 3, 3);
+
+    expect(example).toBe(27);
+});
+
+
+
+
