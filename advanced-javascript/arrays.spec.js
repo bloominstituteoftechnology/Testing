a51f1ffb3dc33dfe2a09b9d69c434cbb1d273b6c
harrisonbrock <harrison@harrisonbrock.com>
2018-05-22 17:22:59 -0400

    Added array map test case

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('../src/arrays');
+const arrayFunctions = require('./arrays');
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -8,10 +8,20 @@ const arrayFunctions = require('../src/arrays');
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe('Arrays', () => {
-  describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+
+    describe('map', () => {
+
+        it('should be a function', () => {
+            const map = arrayFunctions.map;
+            expect(typeof map).toBe('function');
+        });
+
+        it('should return a array that adds 1 to each item', () => {
+            const map = arrayFunctions.map([1, 2], (item)=> item + 1);
+            expect(map[0]).toBe(2);
+            expect(map[1]).toBe(3);
+        });
     });
-  });
+
+
 });
