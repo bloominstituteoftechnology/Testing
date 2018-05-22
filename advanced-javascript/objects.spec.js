9526459954279b88ddc136afe6df5b332e58ef5c
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-22 15:03:56 -0700

    object
    got the objects working with two functions

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -3,11 +3,18 @@ const objectFunctions = require('./objects');
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
   const obj = {a:22,b:33,c:44}
+  const secondObj={x:55,y:120,z:200}
   describe('keys',()=>{
   it('should be an object', () => {
     const keys = objectFunctions.keys
     const kObj = keys(obj)
     expect(typeof kObj).toBe('object')
+
+  });
+  it('should return the keys of the object', () => {
+    const keys = objectFunctions.keys
+    const kObj = keys(obj)
+    expect(kObj).toEqual( ["a", "b", "c"])
 
   });
 })
@@ -18,12 +25,23 @@ describe('values',()=>{
     expect(typeof vObj).toBe('object')
 
   });
+  it('should return the values', () => {
+    const values = objectFunctions.values
+    const vObj = values(obj)
+    expect(vObj).toEqual([22, 33, 44])
+
+  });
 });
 describe('mapObj',()=>{
   it('should be an object', () => {
     const mapObject = objectFunctions.mapObject
     const mObj = mapObject(obj, ()=>'map')
     expect(typeof mObj).toBe('object')
+
+  });  it('should return an object with the value passed in mapped to each key', () => {
+    const mapObject = objectFunctions.mapObject
+    const mObj = mapObject(obj, ()=>2)
+    expect(mObj).toEqual({"a": 2, "b": 2, "c": 2})
 
   });
 });
@@ -35,12 +53,24 @@ describe('pairs',()=>{
     expect(typeof pObj).toBe('object')
 
   });
+  it('should an array of of the key and value inside an array', () => {
+    const pairs = objectFunctions.pairs
+    const pObj = pairs(secondObj)
+    expect(pObj).toEqual([["x", 55], ["y", 120], ["z", 200]])
+
+  });
 });
 describe('defualts',()=>{
   it('should be an object', () => {
     const defaults = objectFunctions.defaults
-    const dObj = defaults(obj, "default")
+    const dObj = defaults(obj, secondObj)
     expect(typeof dObj).toBe('object')
+
+  });
+  it('should return the default objects keys and values prepended to the the passed in object ', () => {
+    const defaults = objectFunctions.defaults
+    const dObj = defaults(secondObj, obj)
+    expect(dObj).toEqual({"a": 2, "b": 2, "c": 2, "x": 55, "y": 120, "z": 200})
 
   });
 });
