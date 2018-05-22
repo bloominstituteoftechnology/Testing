01b5e3248624e64ffd8a518e349bf479a9a85dee
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-21 23:49:01 -0700

    Obj
    Kinda done, i was not sure what test i should run on these things... but i tried to get at least a basic test for everything.

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -2,5 +2,47 @@ const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
+  const obj = {a:22,b:33,c:44}
+  describe('keys',()=>{
+  it('should be an object', () => {
+    const keys = objectFunctions.keys
+    const kObj = keys(obj)
+    expect(typeof kObj).toBe('object')
+
+  });
+})
+describe('values',()=>{
+  it('should be an object', () => {
+    const values = objectFunctions.values
+    const vObj = values(obj)
+    expect(typeof vObj).toBe('object')
+
+  });
 });
+describe('mapObj',()=>{
+  it('should be an object', () => {
+    const mapObject = objectFunctions.mapObject
+    const mObj = mapObject(obj, ()=>'map')
+    expect(typeof mObj).toBe('object')
+
+  });
+});
+
+describe('pairs',()=>{
+  it('should be an object', () => {
+    const pairs = objectFunctions.pairs
+    const pObj = pairs(obj)
+    expect(typeof pObj).toBe('object')
+
+  });
+});
+describe('defualts',()=>{
+  it('should be an object', () => {
+    const defaults = objectFunctions.defaults
+    const dObj = defaults(obj, "default")
+    expect(typeof dObj).toBe('object')
+
+  });
+});
+
+});//wrapper describe
