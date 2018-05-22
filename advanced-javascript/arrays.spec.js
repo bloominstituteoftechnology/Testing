c0d2cf158d09052e832d0e0427396701d4e0e0d1
totsuya <supertatortots@gmail.com>
2018-05-22 16:34:50 -0400

    more arrays done

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,5 +1,6 @@
-const arrayFunctions = require('../src/arrays');
-
+const arrayFunctions = require('../advanced-javascript/arrays.js');
+const jsFuncs = require("../basic-javascript/project-1");
+const {add, multiply, subtract} = jsFuncs;
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
 // first step is to make sure all these assertions work.
@@ -7,11 +8,21 @@ const arrayFunctions = require('../src/arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
+
+
 describe('Arrays', () => {
   describe('map', () => {
-    it('should be a function', () => {
+    it('Should return a mapped array with values being changed', () => {
       const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      const numsArr = [1, 2, 3, 4];
+      expect(map(numsArr, val => val * 2)).toEqual([2, 4, 6, 8])
     });
   });
 });
+
+it('Should reduce value based on callback', () => {
+  const reduce = arrayFunctions.reduce;
+  const numsArr = [1, 2, 3, 4];
+  expect(reduce(numsArr, add)).toBe(10)
+})
+
