4df1621926a5379c6f642062123a40c8d5294180
Devon Bacon <devon.bacon@gmail.com>
2018-05-22 15:11:32 -0700

    objects.spec.js - added test case for invert function

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,20 @@
-const objectFunctions = require('../src/objects');
+const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
   it('should be an object', () => {});
+
+  describe('invert', () => {
+    const testObj = { this: 'is', a: 'test', object: 'woot' };
+    const result = objectFunctions.invert(testObj);
+    const expected = { is: 'this', test: 'a', woot: 'object' };
+
+    it('should return an object', () => {
+      expect(typeof result).toBe('object');
+      expect(result.length).toBe(undefined);
+    });
+    it('should invert object key value pairs', () => {
+      expect(result).toEqual(expected);
+    });
+  });
 });
