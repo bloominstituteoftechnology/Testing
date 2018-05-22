53c3475819766111e763a4e7beb24d0a1bb7a9f0
Justin Hong <jhong2009@gmail.com>
2018-05-22 15:47:07 -0700

    in objects.spec.js: set up test for mapObject function

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -2,6 +2,23 @@ const objectFunctions = require("./objects");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe("objects", () => {
+	const obj = {
+		a: 0,
+		b: 1,
+		c: 2
+	};
+	const cb = value => {
+		return value + 10;
+	};
+	describe("mapObject function", () => {
+		const mapped = objectFunctions.mapObject(obj, cb);
+		it("Should return an obj", () => {
+			expect(typeof mapped).toBe("object");
+		});
+		it("Should return expected outcome", () => {
+			expect(mapped).toEqual({ a: 10, b: 11, c: 12 });
+		});
+	});
 	describe("pairs function", () => {
 		const obj = {
 			a: 1,
