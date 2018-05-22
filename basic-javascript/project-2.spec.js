2742148482cdb96c70b48e130a0c2c794be47b9d
David Finley <dfinley2917@gmail.com>
2018-05-22 15:29:03 -0400

    project1 updated/started project2

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,18 @@
-const funcs = require("./project-2");
+const funcs = require('./project-2');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('project-2 tests', () => {
+	test('getBiggest', () => {
+		const biggest = funcs.getBiggest(2, 1);
+		const biggest1 = funcs.getBiggest(-2, 1);
+		const biggest2 = funcs.getBiggest(0, 1);
+
+		expect(biggest).toEqual(2);
+		expect(biggest1).toEqual(1);
+		expect(biggest2).toEqual(1);
+	});
+	test('greeting', () => {
+		const German = funcs.greeting();
+
+		expect(German).toBe('Guten Tag!');
+	});
+});
