a21f38b0dbba7d3c404bbe0da96785c74cd27f69
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-05-22 16:04:12 -0700

    Not sharing a globla object

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,48 +1,83 @@
 const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-const obj1 = {
-  key1: 1,
-  key2: 2,
-  key3: 3
-};
 
-  // keys
+
+// keys
 describe('keys', () => {
-    const objKeys = objectFunctions.keys(obj1);
-    expect(objKeys).toEqual(["key1", "key2", "key3"]);
-  });
+  const obj1 = {
+    key1: 1,
+    key2: 2,
+    key3: 3
+  };
+  
+  const objKeys = objectFunctions.keys(obj1);
+  expect(objKeys).toEqual(["key1", "key2", "key3"]);
+});
 
-  // values
+// values
 describe('values', () => {
-    const objValues = objectFunctions.values(obj1);
-    expect(objValues).toEqual([1, 2, 3]);
-  });
+  const obj1 = {
+    key1: 1,
+    key2: 2,
+    key3: 3
+  };
+  
+  const objValues = objectFunctions.values(obj1);
+  expect(objValues).toEqual([1, 2, 3]);
+});
 
-  // mapObject
+// mapObject
 describe('mapObject', () => {
-    const cb1 = (n) => {
-      let m = n + 5;
-      return m;
-    };
-    const objMapObject = objectFunctions.mapObject(obj1, cb1)
-    expect(objMapObject).toEqual({ "key1": 6, "key2": 7, "key3": 8 });
-  });
+  const obj1 = {
+    key1: 1,
+    key2: 2,
+    key3: 3
+  };
+  
+  const cb1 = (n) => {
+    let m = n + 5;
+    return m;
+  };
+  
+  const objMapObject = objectFunctions.mapObject(obj1, cb1)
+  expect(objMapObject).toEqual({ "key1": 6, "key2": 7, "key3": 8 });
+});
 
 // pairs
 describe('pairs', () => {
+  const obj1 = {
+    key1: 1,
+    key2: 2,
+    key3: 3
+  };
+  
   const objPairs = objectFunctions.pairs(obj1);
   expect(objPairs).toEqual([["key1", 1], ["key2", 2], ["key3", 3]]);
 });
 
+
 // invert
 describe('invert', () => {
+  const obj1 = {
+    key1: 1,
+    key2: 2,
+    key3: 3
+  };
+  
   const objInvert = objectFunctions.invert(obj1);
   expect(objInvert).toEqual({ 1: "key1", 2: "key2", 3: "key3" });
 });
 
 // defaults - WIP
-describe('invert', () => {
+describe('default', () => {
+
+  const obj1 = {
+    key1: 1,
+    key2: 2,
+    key3: 3
+  };
+
   const defaultProps = {
     key1: "value1",
     key2: "value2",
@@ -52,3 +87,4 @@ describe('invert', () => {
   const objDefaults = objectFunctions.defaults(obj1, defaultProps);
   expect(objDefaults).toEqual();
 });
+
