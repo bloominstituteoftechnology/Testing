034e570b1c26aa16d33a79726be4aa6f1e32b7bf
David Finley <dfinley2917@gmail.com>
2018-05-22 18:54:31 -0400

    started Arrays testing

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require("./arrays");
+const arrayFunctions = require('./arrays');
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -7,11 +7,37 @@ const arrayFunctions = require("./arrays");
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
-describe("Arrays", () => {
-  describe("map", () => {
-    it("should be a function", () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe("object");
-    });
-  });
+describe('Arrays', () => {
+	function isFunction(func) {
+		it('should be a function', () => {
+			expect(typeof func).toBe('function');
+		});
+	}
+	// describe('each', () => {
+	// 	it('should be a function', () => {
+	// 		const map = arrayFunctions.map;
+	// 		expect(typeof map).toBe('function');
+	// 	});
+	// });
+	describe('map', () => {
+		it('should be a function', () => {
+			const map = arrayFunctions.map;
+			expect(typeof map).toBe('function');
+			expect(typeof map).not.toBe('object');
+			expect(typeof map).not.toBe('array');
+		});
+	});
+	describe('reduce', () => {
+		isFunction(arrayFunctions.reduce);
+
+		const elements = [1, 2, 3];
+		const memo = elements.shift();
+		const cb = (memo, item) => {
+			return memo + item;
+		};
+		const reduce = arrayFunctions.reduce(elements, cb, memo);
+		it('Should rerturn expected outcome', () => {
+			expect(reduce).toBe(6);
+		});
+	});
 });
