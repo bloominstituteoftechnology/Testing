0644fbde746169773f789036b3e623ed28d5a032
Ashiko124 <greeneselectronics@gmail.com>
2018-05-22 16:51:33 -0600

    Finished project 1 onto project 2

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -65,3 +65,112 @@ describe('greaterThanFifty', () => {
         expect(funcs.add(5, 6)).toEqual(11);
     })
  })
+
+ describe('subtract', () => {
+    it('should check x - y', () => {
+        expect(funcs.subtract(6, 5)).toEqual(1);
+    })
+ })
+
+ describe('divide', () => {
+    it('should check x / y', () => {
+        expect(funcs.divide(10, 5)).toEqual(2);
+    })
+ })
+
+ describe('multiply', () => {
+    it('should check x * y', () => {
+        expect(funcs.multiply(10, 5)).toEqual(50);
+    })
+ })
+
+ describe('getRemainder', () => {
+    it('should check x % y', () => {
+        expect(funcs.getRemainder(10, 5)).toEqual(0);
+    })
+ })
+
+ describe('isEven', () => {
+    it('should check num % 2 = 0', () => {
+        expect(funcs.isEven(10)).toEqual(true);
+    })
+ })
+ 
+ describe('isOdd', () => {
+    it('should check num % 2 = 0', () => {
+        expect(funcs.isOdd(9)).toEqual(true);
+    })
+ })
+
+ describe('square', () => {
+    it('should check num * num', () => {
+        expect(funcs.square(10)).toEqual(100);
+    })
+ })
+
+ describe('cube', () => {
+    it('should check num * num * num', () => {
+        expect(funcs.cube(10)).toEqual(1000);
+    })
+ })
+
+ describe('raiseToPower', () => {
+    it('should check num ** exponent', () => {
+        expect(funcs.raiseToPower(10, 2)).toEqual(100);
+    })
+ })
+
+ describe('roundNumber', () => {
+    it('should check rounded up or down number', () => {
+        expect(funcs.roundNumber(9.8)).toEqual(10);
+    })
+ })
+
+ describe('roundUp', () => {
+    it('should check number rounded up', () => {
+        expect(funcs.roundUp(9.3)).toEqual(10);
+    })
+ })
+
+
+ describe('addExclamationPoint', () => {
+    it('should add exclamation point', () => {
+        expect(funcs.addExclamationPoint(10)).toEqual(10+'!');
+    })
+ })
+
+ describe('combineNames', () => {
+     it('should combine two names into one string', () => {
+         expect(funcs.combineNames('Jonathan', 'greene')).toEqual('Jonathan greene');
+     })
+ })
+
+ describe('getGreeting', () => {
+    it('should add hello to the string variable', () => {
+        expect(funcs.getGreeting('Jonathan')).toEqual('Hello Jonathan!');
+    })
+ })
+
+ describe('getRectangleArea', () => {
+     it('Should take in two parameters and find the area of a rectangle', () => {
+        expect(funcs.getRectangleArea(3,5)).toEqual(15);
+    })
+ })
+
+ describe('getTriangleArea', () => {
+     it('should find the area of a triangle', () => {
+         expect(funcs.getTriangleArea(5,3)).toEqual(7.5);
+     })
+ })
+
+ describe('getCircleArea', () => {
+     it('should find the area of circle', () => {
+         expect(funcs.getCircleArea(3)).toBe(Math.PI * 9);
+     })
+ })
+
+ describe ('getRectangularPrismVolume', () => {
+    it('should return width * height * length of prism', () => {
+        expect(funcs.getRectangularPrismVolume(3,4,5)).toEqual(60);
+    })
+ })
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,117 @@
-const funcs = require('../src/project-2');
+const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('default', () => {
+    it('run the tests', () => {});
+  });
+
+
+  describe("getBiggest", () => {
+    it('Decide which is larger x or y', () => {
+      expect(funcs.getBiggest(4,5)).toBe(5);
+    });
+  });
+
+      describe ('greeting', () => {
+    it('Return you r greeting in english, spanish or German', () => {
+      expect(funcs.greeting('Spanish')).toBe('Hola!');
+      expect(funcs.greeting('German')).toBe('Guten Tag!');
+      expect(funcs.greeting()).toMatch('Hello!')
+    });
+  });
+ 
+      describe ('isTenorFive', () => {
+    it('return true if number is ten or five, else return false', () => {
+      expect(funcs.isTenOrFive(5)).toBe(true);
+      expect(funcs.isTenOrFive(10)).toBe(true);
+      expect(funcs.isTenOrFive('sdfsd')).toBe(false)
+    })
+  })
+
+
+      describe ('isInRange', () => {
+    it('return true if number is lesser than 50 and greater than 20, else return false', () => {
+      expect(funcs.isInRange(30)).toBe(true);
+      expect(funcs.isInRange(30)).toBe(true);
+      expect(funcs.isInRange(50)).toBe(false);
+    })
+  })
+      describe ('isInteger', () => {
+    it('return true if number is an integer, else return false', () => {
+      expect(funcs.isInteger(4)).toBe(true);
+      expect(funcs.isInteger(4.5)).toBe(false);
+    })
+  })
+      describe ('fizzBuzz', () => {
+    it('return fizzbuzz if number is divisible by 5 and 3, return buzz if number is divisible by 5, return fizz if number is divisible by 3', () => {
+    expect(funcs.fizzBuzz(15)).toBe('fizzbuzz');
+    expect(funcs.fizzBuzz(5)).toBe('buzz');
+    expect(funcs.fizzBuzz(3)).toBe('fizz');
+    expect(funcs.fizzBuzz(16)).toBe(16);
+
+
+    })
+  })
+      describe ('isPrime', () => {
+    it('return false if number is negative, zero, one, or even, else return true', () => {
+      expect(funcs.isPrime(1)).toBe(false);
+    })
+  })
+      describe ('returnFirst', () => {
+    it('return first number in an array', () => {
+    })
+  })
+      describe ('returnLast', () => {
+    it('return last number in an array', () => {
+    })
+  })
+      describe ('getArrayLength', () => {
+    it('return the length of an array', () => {
+    })
+  })
+      describe ('incrementByOne', () => {
+    it('return array with values incremented by one', () => {
+    })
+  })
+      describe ('addItemtoArray', () => {
+    it('return an array with item added', () => {
+    })
+  })
+      describe ('addItemtoFront', () => {
+    it('return array with item added to front of array', () => {
+    })
+  })
+      describe ('wordsToSentence', () => {
+    it('return sentence from words', () => {
+    })
+  })
+      describe ('contains', () => {
+    it('return true if item is contained in the array, return false if not', () => {
+    })
+  })
+      describe ('addNumbers', () => {
+    it('return sum of numbers added together', () => {
+    })
+  })
+      describe ('averageTestScore', () => {
+    it('return average of all test score sums', () => {
+    })
+  })
+      describe ('largestNumber', () => {
+    it('return the largest number in an array', () => {
+    })
+  })
+// describe('should concatenate words into a sentence', () => {
+//     it('It should be called with an array', () => {
+//         expect(funcs.wordsToSentences)
+//         .toHaveBeenCalledWith(expect.any(Array));
+
+//     });
+//     it('should be an array of strings', () => {
+//         it('it should return', () => {
+//             const sentence = funcs.wordsToSentences(['hello', 'i', 'am', 'Jonathan']);
+//             expect(sentence).toBe('hello i am Jonathan');
+//         })
+//         //expect(funcs.wordsToSentences(['Tommy' + 'jones'])).toEqual('Tommy Jones')
+//     })
+// })
