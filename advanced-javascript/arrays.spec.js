4cba38c1201f5936b4181073697c65ab7d065ca3
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-22 13:43:45 -0700

    got a few array test done
    hard stuff, but i feel like i am learning a lot

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -6,14 +6,21 @@ const arrayFunctions = require('./arrays');
 // then make sure you have at least 2-3 more assertions for every function.
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
-
+const numArr = [5,6,7,5,6,7,8]
+const strArr =["Kaladin","Dalanar","Adalin","Nevanie"]
 describe('Arrays', () => {
   describe('map', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
       expect(typeof map).toBe('function');
     });
+  
+    it('should return if a boolean of true if an array is passed',()=>{
+    const map =arrayFunctions.map;
+   const arr = map(numArr,()=>'map')
+   expect(Array.isArray(arr)).toBe(true)
   });
+})
 });
 
   describe('each', () => {
@@ -21,11 +28,24 @@ describe('Arrays', () => {
       const each = arrayFunctions.each
       expect(typeof each).toBe('function');
     });
+    it('should return a call count for a given call back',()=>{
+      const each = arrayFunctions.each;
+     const arr = each(numArr,()=>'each')
+   expect(arr).toBe(3)
+    });
   });
   describe('reduce', () => {
     it('should be a function', () => {
       const reduce = arrayFunctions.reduce
       expect(typeof reduce).toBe('function');
+    });
+    it('should return a reduced array',()=>{
+      let memo =null
+      const reduce =arrayFunctions.reduce;
+     const arr = reduce(numArr,(x,y)=> x+y,memo)
+     
+     console.log('arr', typeof arr )
+     expect(arr).toBe("42")
     });
   });
   describe('find', () => {
@@ -33,6 +53,7 @@ describe('Arrays', () => {
       const find = arrayFunctions.find
       expect(typeof find).toBe('function');
     });
+
   });
   describe('Filter', () => {
     it('should be a function', () => {
