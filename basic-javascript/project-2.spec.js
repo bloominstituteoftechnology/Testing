962cf7da8560f007831acf3e8167fbde53b32e3f
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-22 15:30:48 -0400

    Completed greeting and isTenOrFive tests

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -21,3 +21,64 @@ describe('getBiggest', () => {
     })
 })
 
+// // greeting
+// describe('greeting', () => {
+//     it('should return a German greeting', () => {
+//         // arrange
+//         const greeting = funcs.greeting;
+
+//         // act
+//         const actual = greeting('German');
+
+//         // assert
+//         expect(actual).toBe('Guten Tag!')
+//     })
+//     it('should return a Spanish greeting', () => {
+//         // arrange
+//         const greeting = funcs.greeting;
+
+//         // act
+//         const actual = greeting('Spanish');
+
+//         // assert
+//         expect(actual).toBe('Hola!')
+//     })
+//     it('should return a English greeting', () => {
+//         // arrange
+//         const greeting = funcs.greeting;
+
+//         // act
+//         const actual = greeting('English');
+
+//         // assert
+//         expect(actual).toBe('Hello!')
+//     })
+// })
+
+describe('greeting', () => {
+    it('should check all the language greeting', () => {
+      const getGreeting = funcs.greeting
+  
+      const german = getGreeting('German');
+      const spanish = getGreeting('Spanish')
+      const noLang = getGreeting();
+  
+      expect(german).toBe('Guten Tag!');
+      expect(spanish).toBe('Hola!');
+      expect(noLang).toBe('Hello!');
+    })
+})
+
+describe('isTenOrFive', () => {
+    it('checks if number is 10 or a 5', () => {
+        // arrange
+        const isTenOrFive = funcs.isTenOrFive;
+        const expected = true;
+
+        // act
+        const actual = isTenOrFive(5)
+
+        // assert 
+        expect(actual).toBe(expected)
+    })
+})
