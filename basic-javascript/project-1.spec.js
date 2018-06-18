cd1b4224d8faf6e8e0206ec73ca2d66ed49f3f49
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-18 10:34:41 -0700

    initial commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,32 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1', () => {
+    describe('multiplyByTen', () => {
+        it('')
+    });
+    describe('subtractFive', () => {});
+    describe('areSameLength', () => {});
+    describe('areEqual', () => {});
+    describe('lessThanNinety', () => {});
+    describe('greaterThanFifty', () => {});
+    describe('add', () => {});
+    describe('subtract', () => {});
+    describe('divide', () => {});
+    describe('multiply', () => {});
+    describe('getRemainder', () => {});
+    describe('isEven', () => {});
+    describe('isOdd', () => {});
+    describe('square', () => {});
+    describe('cube', () => {});
+    describe('raiseToPower', () => {});
+    describe('roundNumber', () => {});
+    describe('roundUp', () => {});
+    describe('addExclamationPoint', () => {});
+    describe('combineNames', () => {});
+    describe('getGreeting', () => {});
+    describe('getRectangleArea', () => {});
+    describe('getTriangleArea', () => {});
+    describe('getCircleArea', () => {});
+    describe('getRectangularPrismVolume', () => {});
+})
