0cefd1fa074905460773a2c612f2532df7529ee6
samar vir <samarvir1996@gmail.com>
2018-06-19 01:46:54 +0530

    pushing changes

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,96 @@
 const funcs = require('./project-2');
 
+describe('project-2', () => {
+    describe('getBiggest', () => {
+        it('returns getBiggest', () => {
+            expect(funcs.getBiggest(3,4)).toEqual(4)
+        })
+    })
+
+    describe('greeting', () => {
+        it('returns greeting', () => {
+            expect(funcs.greeting("German")).toEqual("Guten Tag!")
+        })
+        it('returns greeting', () => {
+            expect(funcs.greeting("boom")).toEqual("Hello!")
+        })
+    })
+
+    describe('isTenOrFive', () => {
+        it('returns isTenOrFive', () => {
+            expect(funcs.isTenOrFive(4)).toEqual(false)
+            expect(funcs.isTenOrFive(5)).toEqual(true)
+        })
+    })
+
+    describe('isInRange', () => {
+        it('returns isInRange', () => {
+            expect(funcs.isInRange(21)).toEqual(true)
+            expect(funcs.isInRange(5)).toEqual(false)
+        })
+    })
+
+    describe('isInteger', () => {
+        it('returns isInteger', () => {
+            expect(funcs.isInteger(4.3)).toEqual(false)
+            expect(funcs.isInteger(5)).toEqual(true)
+        })
+    })
+
+    describe('fizzBuzz', () => {
+        it('returns fizzBuzz', () => {
+            expect(funcs.fizzBuzz(60)).toEqual("fizzbuzz")
+            expect(funcs.fizzBuzz(5)).toEqual("buzz")
+            expect(funcs.fizzBuzz(3)).toEqual("fizz")
+        })
+    })
+
+    describe('isPrime', () => {
+        it('returns isPrime', () => {
+            expect(funcs.isPrime(3)).toEqual(true)
+            expect(funcs.isPrime(6)).toEqual(false)
+            expect(funcs.isPrime(7)).toEqual(true)
+        })
+    })
+
+    describe('returnFirst', () => {
+        it('returns returnFirst', () => {
+            expect(funcs.returnFirst([4,5,6])).toEqual(4)
+        })
+    })
+
+    describe('returnLast', () => {
+        it('returns returnLast', () => {
+            expect(funcs.returnLast([4,5,6])).toEqual(6)
+        })
+    })
+
+    describe('getArrayLength', () => {
+        it('returns getArrayLength', () => {
+            expect(funcs.getArrayLength([4,5,6])).toEqual(3)
+        })
+    })
+
+
+    describe('incrementByOne', () => {
+        it('returns incrementByOne', () => {
+            expect(funcs.incrementByOne([5])).toEqual([6])
+            expect(funcs.incrementByOne([5,6])).toEqual([6,7])
+        })
+    })
+
+    describe('addItemToArray', () => {
+        it('returns addItemToArray', () => {
+            expect(funcs.addItemToArray([5,6], 8)).toEqual([5,6,8])
+        })
+    })
+
+    describe('addItemToFront', () => {
+        it('returns addItemToFront', () => {
+            expect(funcs.addItemToFront([5,6], 8)).toEqual([8,5,6])
+        })
+    })
+
+
+})
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
