9b39bab4491fe3b3a5ef034efa600edc2c3ec5b9
jaklundun <govi.escudero@gmail.com>
2018-06-18 14:12:09 -0700

    Initial Commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,36 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+describe('project-1', () => {
+    describe('multiplyByTen', () => {
+        it('return NaN when given a non numeric value', () => {
+            expect(helpers.multiplyByTen(undefined)).toBeNaN();
+            expect(helpers.multiplyByTen('ten')).toBeNan();
+            expect(helpers.multiplyByTen(false)).toEqual(0);
+            expect(helpers.multiplyByTen(true)).toEqual(10);
+        });
+
+        it('should return the given number multiplied by 10', () => {
+            // arrange
+            const expected = 30;
+            // act
+            const actual = helpers.multiplyByTen(3)
+            // assert 
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('subtractFive', () => {
+        it('returns NaN when not given a integer', () => {
+            expect(helpers.subtractFive(undefined)).toBeNan();
+        });
+    });
+
+});
+
+
+
+
+
+
