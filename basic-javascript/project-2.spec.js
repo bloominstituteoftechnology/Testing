ce688025be969117e4a7464001eefd7806cde262
maxwashington <mnwashington18@gmail.com>
2018-06-18 16:25:19 -0500

    --added describe to project1 --started on project2

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,113 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('project-2', () => {
+    describe('getBiggest', () => {
+        it('returns NaN when given a non numeric value', () => {
+            expect(funcs.getBiggest(2, 4)).toBe(4);
+            expect(funcs.getBiggest(4, 4)).toBe(4);
+        });
+
+        it('should return the bigger number', () => {});
+    });
+
+    describe("greeting", () =>{
+        it("return greeting in German, Spanish, or default to English", () => {
+            expect(funcs.greeting('German')).toBe("Guten Tag!")
+            expect(funcs.greeting('Spanish')).toBe("Hola!")
+            expect(funcs.greeting('')).toBe("Hello!")
+        })
+    })
+
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+    describe("", () =>{
+        it("", () => {
+            expect()    
+        })
+    })
+});
