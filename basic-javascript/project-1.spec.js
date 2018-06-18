c92e5d354f098bef4fe0f297e5aadfcdf235aa22
jesuarva <jesuarva.developer@gmail.com>
2018-06-18 23:03:34 +0200

    ADD describes to 'basic Javascript'

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,4 +1,27 @@
 const helpers = require('./project-1');
 
-hello Cole!
-
+describe('multiplyByTen', () => {});
+describe('subtractFive', () => {});
+describe('areSameLength', () => {});
+describe('areEqual', () => {});
+describe('lessThanNinety', () => {});
+describe('greaterThanFifty', () => {});
+describe('add', () => {});
+describe('subtract', () => {});
+describe('divide', () => {});
+describe('multiply', () => {});
+describe('getRemainder', () => {});
+describe('isEven', () => {});
+describe('isOdd', () => {});
+describe('square', () => {});
+describe('cube', () => {});
+describe('raiseToPower', () => {});
+describe('roundNumber', () => {});
+describe('roundUp', () => {});
+describe('addExclamationPoint', () => {});
+describe('combineNames', () => {});
+describe('getGreeting', () => {});
+describe('getRectangleArea', () => {});
+describe('getTriangleArea', () => {});
+describe('getCircleArea', () => {});
+describe('getRectangularPrismVolum', () => {});
