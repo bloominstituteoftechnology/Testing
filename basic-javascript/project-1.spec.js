2164db7a7986b49eee104109b20c59d574082f15
Marco A Guzman <marcoguzman16@gmail.com>
2018-06-18 12:49:27 -0700

    Marco Guzman: pul req and test(fail)

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,13 @@
 const helpers = require('./project-1');
 
+
+describe('multiplyByTen', () => {
+    it('should multiply a number by ten', () => {
+        const expected = 350;
+        const multiply = utilities.multiply;
+        const actual = multiply(35, 10);
+        expected(multiply(35, 10)).toBe(350)
+    })
+});
+
 // start testing!
