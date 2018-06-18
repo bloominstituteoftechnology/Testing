22efceb97d9b8f52ef56f12dfd421ac755e9370a
Xang Thao <xangthao24@yahoo.com>
2018-06-18 16:03:54 -0700

    Added more test case

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -8,19 +8,29 @@ describe("Multiply by 10", () => {
     expect(helpers.multiplyByTen(null)).toEqual(0);
     expect(helpers.multiplyByTen(undefined)).toBeNaN()
   })
+  it("return a number multiply by 10", () => {
+    expect(helpers.multiplyByTen(3)).toEqual(30)
+    expect(helpers.multiplyByTen(-2)).toEqual(-20)
+  })
 })
 
 //subtractFive
 describe("Subtract by 5", () => {
   it("return a number that is subtracted by 5", () => {
     expect(helpers.subtractFive(10)).toBe(5);
+    expect(helpers.subtractFive(-6)).toEqual(-11);
   })
 })
 
 //areSameLength
 describe("return true if the length are the same false otherwise", () => {
-  it("place holder text", () => {
+  it("return true if same length", () => {
     expect(helpers.areSameLength("abc", "def")).toBeTruthy();
+    expect(helpers.areSameLength("this is a test", "tset a si siht")).toBeTruthy()
+  })
+  it("return false if length are different", () => {
+    expect(helpers.areSameLength("abc", "efgd")).toBeFalsy()
+    expect(helpers.areSameLength("this is a test", "tobe")).toBeFalsy()
   })
 })
 
@@ -28,6 +38,11 @@ describe("return true if the length are the same false otherwise", () => {
 describe("areEqual", () => {
   it("return true when x and y are the same number and type",() => {
     expect(helpers.areEqual(10, 10)).toBeTruthy();
+    expect(helpers.areEqual("str", "str")).toBeTruthy();
+  })
+  it("return false when x and y are not the same number and type", () => {
+    expect(helpers.areEqual(10, 3)).toBeFalsy()
+    expect(helpers.areEqual("str", "strdfs")).toBeFalsy()
   })
 })
 
@@ -35,6 +50,7 @@ describe("areEqual", () => {
 describe("less than 90", () => {
   it("return true if the number pass in is less than 90 otherwise false", () => {
     expect(helpers.lessThanNinety(70)).toBeTruthy();
+    expect(helpers.lessThanNinety(100)).toBeFalsy();
   })
 })
 
@@ -42,6 +58,7 @@ describe("less than 90", () => {
 describe("greater than 50", () => {
   it("return true if the number pass in is greater than 50 otherwise false", () => {
     expect(helpers.greaterThanFifty(60)).toBeTruthy();
+    expect(helpers.greaterThanFifty(25)).toBeFalsy();
   })
 })
 
@@ -49,20 +66,23 @@ describe("greater than 50", () => {
 describe("add 2 numbers", () => {
   it("return the sum of the 2 numbers pass in", () => {
     expect(helpers.add(2,3)).toBe(5);
+    expect(helpers.add(-5, -6)).toEqual(-11);
   })
 })
 
 //subtract
 describe("subtract 2 numbers", () => {
   it("return the difference of the 2 numbers pass in", () => {
-    expect(helpers.subtract(10, 5)).toBe(5)
+    expect(helpers.subtract(10, 5)).toBe(5);
+    expect(helpers.subtract(-7, -2)).toEqual(-5);
   })
 })
 
 //divide
 describe("divide 2 numbers", () => {
   it("return the quotient of the 2 numbers pass in", () => {
-    expect(helpers.divide(10, 5)).toEqual(2)
+    expect(helpers.divide(10, 5)).toEqual(2);
+    expect(helpers.divide(-18, 3)).toEqual(-6);
   })
 })
 
@@ -70,6 +90,7 @@ describe("divide 2 numbers", () => {
 describe("multiply 2 numbers", () => {
   it("returns the product of the 2 numbers pass in", () => {
     expect(helpers.multiply(10, 2)).toEqual(20)
+    expect(helpers.multiply(-5, 6)).toEqual(-30)
   })
 })
 
@@ -85,6 +106,9 @@ describe("return true if the number is even otherwise false", () => {
   it("returns true when the number pass in is even", () => {
     expect(helpers.isEven(6)).toBeTruthy()
   })
+  it("return false when the number pass in is odd", () => {
+    expect(helpers.isEven(3)).toBeFalsy()
+  })
 })
 
 //isOdd
@@ -92,12 +116,16 @@ describe("return true if the number is odd otherwise false", () => {
   it("returns true when the number pass in is odd", () => {
     expect(helpers.isOdd(3)).toBeTruthy();
   })
+  it("return false when number is even", () => {
+    expect(helpers.isOdd(4)).toBeFalsy()
+  })
 })
 
 //square 
 describe("square the number", () => {
   it("return the square of the number pass in", () => {
     expect(helpers.square(2)).toEqual(4)
+    expect(helpers.square(-5)).toEqual(25)
   })
 })
 
@@ -105,6 +133,7 @@ describe("square the number", () => {
 describe("cube the number", () => {
   it("return the cube of the number pass in", () => {
     expect(helpers.cube(3)).toEqual(27)
+    expect(helpers.cube(-3)).toEqual(-27)
   })
 })
 
@@ -112,6 +141,7 @@ describe("cube the number", () => {
 describe("raise to the power", () => {
   it("return the first number to the power of the second number", () => {
     expect(helpers.raiseToPower(3, 3)).toEqual(27)
+    expect(helpers.raiseToPower(-3, 3)).toEqual(-27)
   })
 })
 
@@ -119,6 +149,7 @@ describe("raise to the power", () => {
 describe("rounded number", () => {
   it("return the number rounded off to the nearest whole number", () => {
     expect(helpers.roundNumber(7.5)).toEqual(8)
+    expect(helpers.roundNumber(7.1)).toEqual(7)
   })
 })
 
@@ -126,6 +157,7 @@ describe("rounded number", () => {
 describe("round up number", () => {
   it("return the number rounded up to the nearest whole number", () => {
     expect(helpers.roundUp(8.2)).toEqual(9)
+    expect(helpers.roundUp(8.6)).toEqual(9)
   })
 })
 
@@ -134,6 +166,7 @@ describe("round up number", () => {
 describe("add exclamation point to string", () => {
   it("return the string with an exclamation point append to it", () => {
     expect(helpers.addExclamationPoint("Hello")).toEqual("Hello!")
+    expect(helpers.addExclamationPoint("test")).not.toEqual("test")
   })
 })
 
@@ -141,6 +174,7 @@ describe("add exclamation point to string", () => {
 describe("combine names", () => {
   it("return the combination of the 2 names pass in", () => {
     expect(helpers.combineNames("Xang", "Thao")).toEqual("Xang Thao")
+    expect(helpers.combineNames("Xang", "Thao")).not.toEqual("XangThao")
   })
 })
 
@@ -148,6 +182,7 @@ describe("combine names", () => {
 describe("Greet person", () => {
   it("return greeting with the name pass in", () => {
     expect(helpers.getGreeting("Rome")).toEqual("Hello Rome!")
+    expect(helpers.getGreeting("Rome")).not.toEqual("Rome!");
   })
 })
 
@@ -180,3 +215,11 @@ describe("get the rectangular prism volume", () => {
     expect(helpers.getRectangularPrismVolume(2,3,4)).toEqual(24)
   })
 })
+
+//getRectangularPrismVolume
+describe('get rectangular prism volume', () => {
+  it('return the prism volume of the rectangle', () => {
+      expect(helpers.getRectangularPrismVolume(2,3,4)).toEqual(24)
+  })
+})
+
