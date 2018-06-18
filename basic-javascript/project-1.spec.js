7bd762d3bd2ec797919f17e7816d7576d31aa76b
udykas <udykas@gmail.com>
2018-06-18 15:59:11 -0400

    First commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,12 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+describe('project-1', () => {
+    describe('multiplyByTen', () => {
+        it('return NaN when given a non-numeric value', () => {
+            expect(helpers.multiplyByTen(undefined)).toBeNaN();
+            expect(helpers.multiplyByTen('words')).toBeNaN();
+        })
+    })
+})
