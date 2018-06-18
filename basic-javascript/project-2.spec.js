48fdd3302e47d9b9ee23e7f1c95d23ee4dcdfb44
udykas <udykas@gmail.com>
2018-06-18 18:28:18 -0400

    First test in Project 2 done

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,15 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('Project-2', () => {
+    describe('getBiggest', () => {
+        it('returns the bigger of two numbers', () => {
+            expect(funcs.getBiggest(14, 12)).toBe(14);
+            expect(funcs.getBiggest(-40, 18)).toBe(18);
+        })
+        it('returns the second parameter if they are equal', () => {
+            expect(funcs.getBiggest(7, 7)).toBe(7);
+        })
+    })
+})
