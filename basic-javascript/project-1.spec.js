0580254ea8f0c6ece631fef332dec6fe7eef6971
udykas <udykas@gmail.com>
2018-06-18 16:14:34 -0400

    MultiplyByTen and subtractFive are done

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -8,5 +8,24 @@ describe('project-1', () => {
             expect(helpers.multiplyByTen(undefined)).toBeNaN();
             expect(helpers.multiplyByTen('words')).toBeNaN();
         })
+        it('returns the number multiplied by 10', () => {
+            expect(helpers.multiplyByTen(2)).toBe(20);
+            expect(helpers.multiplyByTen(1)).toBe(10);
+            expect(helpers.multiplyByTen(0)).toBe(0);
+            expect(helpers.multiplyByTen(10)).toBe(100);
+            expect(helpers.multiplyByTen(2.5)).toBe(25);
+            expect(helpers.multiplyByTen(0.001)).toBe(0.01);
+        })
+    })
+    describe('subtractFive', () => {
+        it('return NaN when given a non-numeric value', () => {
+            expect(helpers.subtractFive(undefined)).toBeNaN();
+            expect(helpers.subtractFive('words')).toBeNaN();
+        })
+        it('subtracts 5 from a number', () => {
+            expect(helpers.subtractFive(5)).toBe(0);
+            expect(helpers.subtractFive(1)).toBe(-4);
+            expect(helpers.subtractFive(26)).toBe(21);
+        })
     })
 })
