16d50b4fa3696f360a5cd0b927565c1eab696eac
Zach Campbell <beazlebubbles@gmail.com>
2018-06-18 15:43:54 -0600

    More functions

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -36,26 +36,32 @@ const greaterThanFifty = num => {
 };
 
 const add = (x, y) => {
+  if (typeof x !== "number" || typeof y !== "number") return undefined;
   return x + y;
 };
 
 const subtract = (x, y) => {
+  if (typeof x !== "number" || typeof y !== "number") return undefined;
   return x - y;
 };
 
 const divide = (x, y) => {
+  if (typeof x !== "number" || typeof y !== "number") return undefined;
   return x / y;
 };
 
 const multiply = (x, y) => {
+  if (typeof x !== "number" || typeof y !== "number") return undefined;
   return x * y;
 };
 
 const getRemainder = (x, y) => {
+  if (typeof x !== "number" || typeof y !== "number") return undefined;
   return x % y;
 };
 
 const isEven = num => {
+  if (typeof num !== "number") return undefined;
   if (num % 2 === 0) {
     return true;
   }
@@ -63,57 +69,76 @@ const isEven = num => {
 };
 
 const isOdd = num => {
-  if (num % 2 === 0) {
-    return false;
+  if (typeof num !== "number") return undefined;
+  if (num % 2 === 1) {
+    return true;
   }
-  return true;
+  return false;
 };
 
 const square = num => {
+  if (typeof num !== "number") return undefined;
   return num * num;
 };
 
 const cube = num => {
+  if (typeof num !== "number") return undefined;
   return num * num * num;
 };
 
 const raiseToPower = (num, exponent) => {
+  if (typeof num !== "number" || typeof exponent !== "number") return undefined;
   return num ** exponent;
 };
 
 const roundNumber = num => {
+  if (typeof num !== "number") return undefined;
   return Math.round(num);
 };
 
 const roundUp = num => {
+  if (typeof num !== "number") return undefined;
   return Math.ceil(num);
 };
 
 const addExclamationPoint = str => {
+  if (typeof str !== "string") return undefined;
   return (str += "!");
 };
 
 const combineNames = (firstName, lastName) => {
+  if (typeof firstName !== "string" || typeof lastName !== "string")
+    return undefined;
   return `${firstName} ${lastName}`;
 };
 
 const getGreeting = name => {
+  if (typeof star !== "string") return undefined;
   return `Hello ${name}!`;
 };
 
 const getRectangleArea = (length, width) => {
+  if (typeof length !== "number" || typeof width !== "number") return undefined;
   return length * width;
 };
 
 const getTriangleArea = (base, height) => {
+  if (typeof base !== "number" || typeof height !== "number") return undefined;
   return 0.5 * base * height;
 };
 
 const getCircleArea = radius => {
+  if (typeof radius !== "number") return undefined;
   return Math.PI * radius * radius;
 };
 
 const getRectangularPrismVolume = (length, width, height) => {
+  if (
+    typeof length !== "number" ||
+    typeof width !== "number" ||
+    typeof height !== "number"
+  )
+    return undefined;
   return width * height * length;
 };
 
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -96,3 +96,115 @@ describe("greaterThanFifty", () => {
     expect(helpers.greaterThanFifty(Number.POSITIVE_INFINITY)).toBe(true);
   });
 });
+
+describe("add", () => {
+  testWrongTypes(helpers.add, "number", undefined);
+
+  it("Adds two numbers together", () => {
+    expect(helpers.add(0, 5)).toBe(5);
+    expect(helpers.add(-10, 7)).toBe(-3);
+    expect(helpers.add(30, 12)).toBe(42);
+    expect(helpers.add(-81, -9)).toBe(-90);
+    expect(helpers.add(45.62, 1.1)).toBe(46.72);
+    expect(helpers.add(Number.NEGATIVE_INFINITY, 1)).toBe(
+      Number.NEGATIVE_INFINITY
+    );
+  });
+});
+
+describe("subtract", () => {
+  testWrongTypes(helpers.subtract, "number", undefined);
+
+  it("Subtracts two numbers", () => {
+    expect(helpers.subtract(0, 5)).toBe(-5);
+    expect(helpers.subtract(-10, 7)).toBe(-17);
+    expect(helpers.subtract(30, 12)).toBe(18);
+    expect(helpers.subtract(-81, -9)).toBe(-72);
+    expect(Number(helpers.subtract(45.62, 1.1).toFixed(2))).toBe(44.52);
+    expect(helpers.subtract(Number.NEGATIVE_INFINITY, 1)).toBe(
+      Number.NEGATIVE_INFINITY
+    );
+  });
+});
+
+describe("multiply", () => {
+  testWrongTypes(helpers.multiply, "number", undefined);
+
+  it("Multiplies two numbers", () => {
+    expect(helpers.multiply(0, 5)).toBe(0);
+    expect(helpers.multiply(-10, 7)).toBe(-70);
+    expect(helpers.multiply(30, 12)).toBe(360);
+    expect(helpers.multiply(-81, -9)).toBe(729);
+    expect(Number(helpers.multiply(45.62, 1.1).toFixed(2))).toBe(50.18);
+    expect(helpers.multiply(Number.NEGATIVE_INFINITY, -1)).toBe(
+      Number.POSITIVE_INFINITY
+    );
+  });
+});
+
+describe("divide", () => {
+  testWrongTypes(helpers.divide, "number", undefined);
+
+  it("Divides two numbers", () => {
+    expect(helpers.divide(0, 5)).toBe(0);
+    expect(helpers.divide(-42, 7)).toBe(-6);
+    expect(helpers.divide(30, 12)).toBe(2.5);
+    expect(helpers.divide(-81, -9)).toBe(9);
+    expect(Number(helpers.divide(45.62, 1.1).toFixed(2))).toBe(41.47);
+    expect(helpers.divide(Number.NEGATIVE_INFINITY, 1)).toBe(
+      Number.NEGATIVE_INFINITY
+    );
+  });
+});
+
+describe("isEven", () => {
+  testWrongTypes(helpers.isEven, "number", undefined);
+
+  it("Checks to see if a number is even", () => {
+    expect(helpers.isEven(7)).toBe(false);
+    expect(helpers.isEven(12)).toBe(true);
+    expect(helpers.isEven(3.5)).toBe(false);
+    expect(helpers.isEven(0)).toBe(true);
+    expect(helpers.isEven(Number.NEGATIVE_INFINITY)).toBe(false);
+  });
+});
+
+describe("isOdd", () => {
+  testWrongTypes(helpers.isOdd, "number", undefined);
+
+  it("Checks to see if a number is odd", () => {
+    expect(helpers.isOdd(7)).toBe(true);
+    expect(helpers.isOdd(12)).toBe(false);
+    expect(helpers.isOdd(3.5)).toBe(false);
+    expect(helpers.isOdd(0)).toBe(false);
+    expect(helpers.isOdd(Number.NEGATIVE_INFINITY)).toBe(false);
+  });
+});
+
+describe("square", () => {
+  testWrongTypes(helpers.square, "number", undefined);
+
+  it("Squares a number", () => {
+    expect(helpers.square(7)).toBe(49);
+    expect(helpers.square(-2)).toBe(4);
+    expect(helpers.square(3.5)).toBe(12.25);
+    expect(helpers.square(0)).toBe(0);
+    expect(helpers.square(Number.NEGATIVE_INFINITY)).toBe(
+      Number.POSITIVE_INFINITY
+    );
+  });
+});
+
+describe("cube", () => {
+  testWrongTypes(helpers.cube, "number", undefined);
+
+  it("Cubes a number", () => {
+    expect(helpers.cube(7)).toBe(343);
+    expect(helpers.cube(-2)).toBe(-8);
+    expect(helpers.cube(3.5)).toBe(42.875);
+    expect(helpers.cube(0)).toBe(0);
+    expect(helpers.cube(Number.NEGATIVE_INFINITY)).toBe(
+      Number.NEGATIVE_INFINITY
+    );
+  });
+});
