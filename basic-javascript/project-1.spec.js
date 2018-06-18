aaf7af8e714bd1a164963a879a21a54715876933
Marco A Guzman <marcoguzman16@gmail.com>
2018-06-18 14:27:02 -0700

    2 test passed

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,13 +1,61 @@
 const helpers = require('./project-1');
 
+describe('multiplication helpers', () => {
+    describe('multiplyByTen', () => {
+        it('should multiply a number by ten', () => {
+            const expected = 350;
+            
+            const actual = multiply(35, 10);
+        
 
-describe('multiplyByTen', () => {
-    it('should multiply a number by ten', () => {
-        const expected = 350;
-        const multiply = utilities.multiply;
-        const actual = multiply(35, 10);
-        expected(multiply(35, 10)).toBe(350)
+            expect(actual).toEqual(expected);
+        
+        });
+    });
+});    
+
+describe('subtractFive', () => {
+    it('should subtract a number by five', () => { 
+        const expected = 95;
+        const actual = subtract(100, 5);
+
+        expect(actual).toEqual(expected);
+
     })
+
 });
 
+function multiply(a, b) {
+    return a * b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
 // start testing!
+
+
+
+
+// const {
+//     getBiggest,
+//     greeting,
+//     isTenOrFive,
+//     isInRange,
+//     isInteger,
+//     fizzBuzz,
+//     isPrime,
+//     returnFirst,
+//     returnLast,
+//     getArrayLength,
+//     incrementByOne,
+//     addItemToArray,
+//     addItemToFront,
+//     wordsToSentence,
+//     contains,
+//     addNumbers,
+//     averageTestScore,
+//     largestNumber
+// } = func;
+
+// module.exports = func;
