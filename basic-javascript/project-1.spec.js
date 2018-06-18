45397608fc8d85bf2ead130ae0dabe8800d3d0b2
Jacob Henry <jacobiswinning@gmail.com>
2018-06-18 16:56:10 -0600

    Jake Henry

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,17 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1', () => {
+  describe('multiplyByTen', () => {
+    it('returns Nan when given s non numeric value', () => {
+      // arrange
+      expect(helpers.multiplyByTen(undefined)).toBeNaN();
+      expect(helpers.multiplyByTen(true)).toBeNaN();
+      expect(helpers.multiplyByTen(false)).toBeNaN();
+      expect(helpers.multiplyByTen('two')).toBeNaN();
+    });
+    it('mutiplies by ten', () => {
+      expect(helpers.multiplyByTen(2)).toBe(20)
+    })
+  });
+});
