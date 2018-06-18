34a5ba66c452a2ad373f5892d13f147400d1fe60
Lavell Burton <taylormade800@gmail.com>
2018-06-18 11:30:11 -0700

    initial commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,8 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe.nly('project-1', () => {
+    it('returns NaN when there is a non numerical value', () => {
+        expect(helpers.multiplyByTen(undefined)).toBeNaN();
+    })
+})
