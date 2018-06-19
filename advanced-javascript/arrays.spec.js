cf3803f4230077a258521dc7319291363b81fe56
Anne Courtney <annecourtney_1@yahoo.com>
2018-06-19 17:50:53 -0500

    tried some of the advanced portion.

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -7,11 +7,48 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
+// {
+//   each,
+//   map,
+//   reduce,
+//   find,
+//   filter,
+//   flatten,
+// }
+
+
 describe('Arrays', () => {
+  describe('each', () => {
+    it('should be a function type', () => {
+      const each = arrayFunctions.each;
+      expect(typeof each).toBe('function');
+    })
+    it('should iterate over elements, but nothing is returned', () => {
+      expect(arrayFunctions.each([3, 2, 3, 4], item => item)).toBe(undefined)
+    })
+  })
+})
+
   describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
-    });
-  });
-});
+    it('should be a function type', () => {
+      const map = arrayFunctions.each;
+      expect(typeof map).toBe('function');
+    })
+    it('should return new array', () => {
+      const mappedArr = arrayFunctions.map([3,4,5,6], item => item + 1);
+      expect(mappedArr).toEqual([4,5,6,7]);
+      expect(typeof mappedArr).toBe('object');
+    })
+  })
+
+  describe('reduce', () => {
+    it('should be a function type', () => {
+      const reduce = arrayFunctions.reduce;
+      expect(typeof reduce).toBe('function');
+    })
+    it('should return some value', () => {
+      const reducedArr = arrayFunctions.reduce([1,2,3,4], (item1, item2) => item1+item2);
+      expect(reducedArr).toEqual(10);
+      expect(typeof reducedArr).toBe('number');
+    })
+  })
