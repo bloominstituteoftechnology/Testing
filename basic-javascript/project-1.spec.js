536af017fe4c2f28e80fd1d9c496e28283b69015
A99 <A99@99s-macbook-pro.home>
2018-06-19 02:45:19 +0200

    Complete Project 1 Tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -148,11 +148,12 @@ describe('project-1', () => {
         });
     });
 
-    describe.only('isEven', () => {
+    describe('isEven', () => {
         it('returns false if certain conditions are met', () => {
             expect(helpers.isEven(undefined)).toBeFalsy();
             expect(helpers.isEven('abc')).toBeFalsy();
             expect(helpers.isEven(1)).toBeFalsy();
+            expect(helpers.isEven(-1)).toBeFalsy();
             expect(helpers.isEven(true)).toBeFalsy();
         });
         it('returns true if certain conditions are met', () => {
@@ -163,267 +164,178 @@ describe('project-1', () => {
         });
     });
 
+    describe('isOdd', () => {
+        it('returns false if certain conditions are met', () => {
+            expect(helpers.isOdd(2)).toBeFalsy();
+            expect(helpers.isOdd(-2)).toBeFalsy();
+            expect(helpers.isOdd(false)).toBeFalsy();
+            expect(helpers.isOdd(null)).toBeFalsy();
+        });
+        it('returns true if certain conditions are met', () => {
+            expect(helpers.isOdd('abcd')).toBeTruthy();
+            expect(helpers.isOdd(undefined)).toBeTruthy();
+            expect(helpers.isOdd(1)).toBeTruthy();
+            expect(helpers.isOdd(-1)).toBeTruthy();
+            expect(helpers.isOdd(true)).toBeTruthy();
+        });
+    });
 
+    describe('square', () => {
+        it('returns NaN if certain types are passed', () => {
+            expect(helpers.square(undefined)).toBeNaN();
+        });
+        it('returns the square of the number passed', () => {
+            expect(helpers.square('1')).toBe(1);
+            expect(helpers.square(1)).toBe(1);
+            expect(helpers.square(true)).toBe(1);
+            expect(helpers.square(false)).toBe(0);
+            expect(helpers.square(null)).toBe(0);
+            expect(helpers.square(3)).toBe(9);
+            expect(helpers.square(-3)).toBe(9);
+        });
+    });
 
+    describe('cube', () => {
+        it('returns NaN if certain types are passed', () => {
+            expect(helpers.cube(undefined)).toBeNaN();
+        });
+        it('returns the cube of the number passed', () => {
+            expect(helpers.cube('1')).toBe(1);
+            expect(helpers.cube(1)).toBe(1);
+            expect(helpers.cube(false)).toBe(0);
+            expect(helpers.cube(null)).toBe(0);
+            expect(helpers.cube(true)).toBe(1);
+            expect(helpers.cube(3)).toBe(27);
+            expect(helpers.cube(-3)).toBe(-27);
+        });
+    });
 
+    describe('raiseToPower', () => {
+        it('returns NaN if certain types are passed', () => {
+            expect(helpers.raiseToPower(undefined)).toBeNaN();
+        });
+        it('returns the power of the number passed', () => {
+            expect(helpers.raiseToPower('1', '5')).toBe(1);
+            expect(helpers.raiseToPower(-1, 3)).toBe(-1);
+            expect(helpers.raiseToPower(null, 3)).toBe(0);
+            expect(helpers.raiseToPower(true, false)).toBe(1);
+            expect(helpers.raiseToPower(3, false)).toBe(1);
+            expect(helpers.raiseToPower(3, 3)).toBe(27);
+            expect(helpers.raiseToPower(-3, -3)).toBe(-0.037037037037037035);
+        });
+    });
+
+    describe('roundNumber', () => {
+        it('returns NaN if certain types are passed', () => {
+            expect(helpers.roundNumber(undefined)).toBeNaN();
+        });
+        it('returns the number rounded of the number passed', () => {
+            expect(helpers.roundNumber('1.3')).toBe(1);
+            expect(helpers.roundNumber(1.3)).toBe(1);
+            expect(helpers.roundNumber(-1.3)).toBe(-1);
+            expect(helpers.roundNumber(0)).toBe(0);
+            expect(helpers.roundNumber(false)).toBe(0);
+            expect(helpers.roundNumber(null)).toBe(0);
+            expect(helpers.roundNumber(true)).toBe(1);
+        });
+    });
+
+    describe('roundUp', () => {
+        it('returns NaN if certain types are passed', () => {
+            expect(helpers.roundUp(undefined)).toBeNaN();
+        });
+        it('returns the number rounded of the number passed', () => {
+            expect(helpers.roundUp('1.3')).toBe(2);
+            expect(helpers.roundUp(1.3)).toBe(2);
+            expect(helpers.roundUp(-1.3)).toBe(-1);
+            expect(helpers.roundUp(0)).toBe(0);
+            expect(helpers.roundUp(false)).toBe(0);
+            expect(helpers.roundUp(null)).toBe(0);
+            expect(helpers.roundUp(true)).toBe(1);
+        });
+    });
+
+    describe('addExclamationPoint', () => {
+        it('returns a string with an ! added at the end', () => {
+            expect(helpers.addExclamationPoint(undefined)).toBe('undefined!');
+            expect(helpers.addExclamationPoint('1.3')).toBe('1.3!');
+            expect(helpers.addExclamationPoint(1.3)).toBe('1.3!');;
+            expect(helpers.addExclamationPoint(false)).toBe('false!');
+            expect(helpers.addExclamationPoint(null)).toBe('null!');
+            expect(helpers.addExclamationPoint(true)).toBe('true!');
+        });
+    });
+
+    describe('combineNames', () => {
+        it('returns a string with firstName & lastName', () => {
+            expect(helpers.combineNames('1', '5')).toBe('1 5');
+            expect(helpers.combineNames(-1, 3)).toBe('-1 3');
+            expect(helpers.combineNames(null, 3)).toBe('null 3');
+            expect(helpers.combineNames(true, false)).toBe('true false');
+            expect(helpers.combineNames(undefined, false)).toBe('undefined false');
+        });
+    });
+
+    describe('getGreeting', () => {
+        it('returns a string with Hello at the beginning and an ! at the end', () => {
+            expect(helpers.getGreeting(undefined)).toBe('Hello undefined!');
+            expect(helpers.getGreeting('1.3')).toBe('Hello 1.3!');
+            expect(helpers.getGreeting(1.3)).toBe('Hello 1.3!');;
+            expect(helpers.getGreeting(false)).toBe('Hello false!');
+            expect(helpers.getGreeting(null)).toBe('Hello null!');
+            expect(helpers.getGreeting(true)).toBe('Hello true!');
+        });
+    });
+
+    describe('getRectangleArea', () => {
+        it('returns NaN according to specific conditions', () => {
+            expect(helpers.getRectangleArea(undefined, 1)).toBeNaN();
+        });
+        it('returns the area from two parameters', () => {
+            expect(helpers.getRectangleArea('1.3', 1)).toBe(1.3);
+            expect(helpers.getRectangleArea(2, -3)).toBe(-6);
+            expect(helpers.getRectangleArea('2', false)).toBe(0);
+            expect(helpers.getRectangleArea(null, null)).toBe(0);
+            expect(helpers.getRectangleArea(true, true)).toBe(1);
+        });
+    });
+
+    describe('getTriangleArea', () => {
+        it('returns NaN according to specific conditions', () => {
+            expect(helpers.getTriangleArea(undefined, 1)).toBeNaN();
+        });
+        it('returns the triangle area from two parameters', () => {
+            expect(helpers.getTriangleArea('1.3', 1)).toBe(0.65);
+            expect(helpers.getTriangleArea(2, -3)).toBe(-3);
+            expect(helpers.getTriangleArea('2', false)).toBe(0);
+            expect(helpers.getTriangleArea(null, null)).toBe(0);
+            expect(helpers.getTriangleArea(true, true)).toBe(0.5);
+        });
+    });
+
+    describe('getCircleArea', () => {
+        it('returns NaN according to specific conditions', () => {
+            expect(helpers.getCircleArea(undefined, 1)).toBeNaN();
+        });
+        it('returns the circle area from one radius', () => {
+            expect(helpers.getCircleArea('1.3', 1)).toBe(5.3092915845667505);
+            expect(helpers.getCircleArea(2, -3)).toBe(12.566370614359172);
+            expect(helpers.getCircleArea('2', false)).toBe(12.566370614359172);
+            expect(helpers.getCircleArea(null, null)).toBe(0);
+            expect(helpers.getCircleArea(true, true)).toBe(3.141592653589793);
+        });
+    });
+
+    describe('getRectangularPrismVolume', () => {
+        it('returns NaN according to specific conditions', () => {
+            expect(helpers.getRectangularPrismVolume(undefined, 1, 3)).toBeNaN();
+        });
+        it('returns the prism volume of a rectangle', () => {
+            expect(helpers.getRectangularPrismVolume('1.3', 1, '2')).toBe(2.6);
+            expect(helpers.getRectangularPrismVolume(2, -3, 10)).toBe(-60);
+            expect(helpers.getRectangularPrismVolume('2', false, true)).toBe(0);
+            expect(helpers.getRectangularPrismVolume(null, null, null)).toBe(0);
+            expect(helpers.getRectangularPrismVolume(true, true, true)).toBe(1);
+        });
+    });
 });
-
-
-// describe.only('project-1', () => {
-//     describe('subtractFive', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.subtractFive(undefined)).toBeNaN();
-//             expect(helpers.subtractFive('five')).toBeNaN();
-//             expect(helpers.subtractFive(false)).toEqual(-5);
-//             expect(helpers.subtractFive(true)).toEqual(-4);
-//         })
-//         it('should return the given number subtracted by 5', () => {})
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('areSameLength', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.areSameLength(1, 13)).toBeTruthy();
-//             expect(helpers.areSameLength('1', 'Aa')).toBeFalsy();
-//             expect(helpers.areSameLength('1', '1')).toBeTruthy();
-//         })
-//         it('should return the same length', () => {})
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('areEqual', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.areEqual(undefined, undefined)).toBeTruthy();
-//             expect(helpers.areEqual(1, 3)).toBeFalsy();
-//             expect(helpers.areEqual('x', 'y')).toBeFalsy();
-//             expect(helpers.areEqual('x', 'x')).toBeTruthy();
-//         })
-//         it('should return the given number that are equal', () => {})
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('lessThanNinety', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.lessThanNinety(undefined)).toBeFalsy();
-//             expect(helpers.lessThanNinety('paris')).toBeFalsy();
-//             expect(helpers.lessThanNinety(80)).toBeTruthy();
-//             expect(helpers.lessThanNinety(91)).toBeFalsy();
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('greaterThanFifty', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.greaterThanFifty(undefined)).toBeFalsy();
-//             expect(helpers.greaterThanFifty('paris')).toBeFalsy();
-//             expect(helpers.greaterThanFifty(40)).toBeFalsy();
-//             expect(helpers.greaterThanFifty(91)).toBeTruthy();
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('add', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.add(undefined, undefined)).toBeNaN();
-//             expect(helpers.add('paris', 'italy')).toEqual('parisitaly');
-//             expect(helpers.add(1, 3)).toEqual(4);
-//             expect(helpers.add(3, -5)).toEqual(-2);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('subtract', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.subtract(undefined, undefined)).toBeNaN();
-//             expect(helpers.subtract('paris', 'italy')).toBeNaN();
-//             expect(helpers.subtract(1, 3)).toEqual(-2);
-//             expect(helpers.subtract(5, 3)).toEqual(2);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('divide', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.divide(undefined, undefined)).toBeNaN();
-//             expect(helpers.divide('germany', 'london')).toBeNaN();
-//             expect(helpers.divide(4, -2)).toEqual(-2);
-//             expect(helpers.divide(8, 4)).toEqual(2);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('multiply', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.multiply(undefined, undefined)).toBeNaN();
-//             expect(helpers.multiply('swiss', 'sweden')).toBeNaN();
-//             expect(helpers.multiply(4, -2)).toEqual(-8);
-//             expect(helpers.multiply(8, 4)).toEqual(32);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('getRemainder', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.getRemainder(undefined, undefined)).toBeNaN();
-//             expect(helpers.getRemainder('spain', 'amsterdam')).toBeNaN();
-//             expect(helpers.getRemainder(6, -2)).toEqual(0);
-//             expect(helpers.getRemainder(8, 3)).toEqual(2);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('isEven', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.isEven(undefined)).toBeFalsy();
-//             expect(helpers.isEven('zurich', 'czech')).toBeFalsy();
-//             expect(helpers.isEven(7)).toBeFalsy();
-//             expect(helpers.isEven(8)).toBeTruthy();
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('isOdd', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.isOdd(undefined)).toBeTruthy();
-//             expect(helpers.isOdd('zurich', 'czech')).toBeTruthy();
-//             expect(helpers.isOdd(10)).toBeFalsy();
-//             expect(helpers.isOdd(7)).toBeTruthy();
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('square', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.square(undefined)).toBeFalsy();
-//             expect(helpers.square('finland', 'russia')).toBeNaN();
-//             expect(helpers.square(-10)).toEqual(100);
-//             expect(helpers.square(7)).toEqual(49);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('cube', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.cube(undefined)).toBeNaN();
-//             expect(helpers.cube('australia', 'new zealand')).toBeNaN();
-//             expect(helpers.cube(-3)).toEqual(-27);
-//             expect(helpers.cube(2)).toEqual(8);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('raiseToPower', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.raiseToPower(undefined, 1)).toBeFalsy();
-//             expect(helpers.raiseToPower('bahamas', 'jamaica')).toBeNaN();
-//             expect(helpers.raiseToPower(5, -3)).toEqual(.008);
-//             expect(helpers.raiseToPower(2, 2)).toEqual(4);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('roundNumber', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.roundNumber(undefined)).toBeNaN();
-//             expect(helpers.roundNumber('cayman', 'barbados')).toBeNaN();
-//             expect(helpers.roundNumber(-3.3)).toEqual(-3);
-//             expect(helpers.roundNumber(2.4)).toEqual(2);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('roundUp', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.roundUp(undefined)).toBeNaN();
-//             expect(helpers.roundUp('cayman', 'barbados')).toBeNaN();
-//             expect(helpers.roundUp(-3.7)).toEqual(-3);
-//             expect(helpers.roundUp(2.6)).toEqual(3);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('addExclamationPoint', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.addExclamationPoint(undefined)).toBe('undefined!');
-//             expect(helpers.addExclamationPoint(4)).toBe('4!');
-//             expect(helpers.addExclamationPoint('str')).toBe('str!');
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('combineNames', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.combineNames(undefined, undefined)).toBe('undefined undefined');
-//             expect(helpers.combineNames(4, 6)).toBe('4 6');
-//             expect(helpers.combineNames('str', 'rts')).toBe('str rts');
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('getGreeting', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.getGreeting(undefined)).toBe('Hello undefined!');
-//             expect(helpers.getGreeting(4)).toBe('Hello 4!');
-//             expect(helpers.getGreeting('flo')).toBe('Hello flo!');
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('getRectangleArea', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.getRectangleArea(undefined)).toBeNaN();
-//             expect(helpers.getRectangleArea(4, 6)).toEqual(24);
-//             expect(helpers.getRectangleArea(2, 2)).toEqual(4);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('getTriangleArea', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.getTriangleArea(undefined)).toBeNaN();
-//             expect(helpers.getTriangleArea(4, 6)).toEqual(12);
-//             expect(helpers.getTriangleArea(2, 2)).toEqual(2);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('getCircleArea', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.getCircleArea(undefined)).toBeNaN();
-//             expect(helpers.getCircleArea(5)).toEqual(78.53981633974483);
-//             expect(helpers.getCircleArea(2)).toEqual(12.566370614359172);
-//         })
-//     })
-// })
-
-// describe.only('project-1', () => {
-//     describe('getRectangularPrismVolume', () => {
-//         it('returns NaN when there is a non numerical value', () => {
-//             expect(helpers.getRectangularPrismVolume(undefined)).toBeNaN();
-//             expect(helpers.getRectangularPrismVolume(-5, -2, -3)).toEqual(-30);
-//             expect(helpers.getRectangularPrismVolume(2, 2, 2)).toEqual(8);
-//         })
-//     })
-// })
