4157ac8b96101829325333d5e13ff859af0da415
Chris Honda <christopher.honda92@gmail.com>
2018-06-19 16:37:15 -0700

    Added a few more tests in objects file

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -6,7 +6,9 @@ describe('objects', () => {
         it('should return object keys in a new array', () => {
             const keys = objectFunctions.keys;
             const newObj = keys({ 'mickey': 'mouse', 'donald': 'duck', 'fuzzy': 'lumpkins' });
+            const freshObj = keys({ 'joe': 'shmoe', 'dapper': 'dan', 'lucky': 'lisa', 'joyful': 'jane' })
             expect(newObj).toEqual(['mickey', 'donald', 'fuzzy']);
+            expect(freshObj).toEqual(['joe', 'dapper', 'lucky', 'joyful']);
         });
     });
 
@@ -14,7 +16,9 @@ describe('objects', () => {
         it('should return object values in a new array', () => {
             const values = objectFunctions.values;
             const newObj = values({ 'mickey': 'mouse', 'donald': 'duck', 'fuzzy': 'lumpkins' });
+            const freshObj = values({ 'joe': 'shmoe', 'dapper': 'dan', 'lucky': 'lisa', 'joyful': 'jane' })
             expect(newObj).toEqual(['mouse', 'duck', 'lumpkins']);
+            expect(freshObj).toEqual(['shmoe', 'dan', 'lisa', 'jane']);
         });
     });
 
@@ -22,7 +26,9 @@ describe('objects', () => {
         it('should map over the object and manipulate its value according to the given cb', () => {
             const mapObject = objectFunctions.mapObject;
             const newObj = mapObject({ 'mickey': 'mouse', 'donald': 'duck', 'fuzzy': 'lumpkins' }, val => val.length);
+            const freshObj = mapObject({ 'joe': 'shmoe', 'dapper': 'dan', 'lucky': 'lisa', 'joyful': 'jane' }, val => val.length);
             expect(newObj).toEqual({ 'mickey': 5, 'donald': 4, 'fuzzy': 8});
+            expect(freshObj).toEqual({ 'joe': 5, 'dapper': 3, 'lucky': 4, 'joyful': 4 });
         })
     ]);
 
