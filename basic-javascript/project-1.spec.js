c1d9729d9cb4f1f91d9d7501d0a469f20e2550d7
Marco A Guzman <marcoguzman16@gmail.com>
2018-06-18 19:29:07 -0700

    subtract

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -51,7 +51,37 @@ describe('lessThanNinety', () => {
     })
 });
 
+describe('greaterThanFifty', () => {
+    it('Should return a number more than 50 as true, or else is false', () => {
+        expect(helpers.greaterThanFifty(89)).toBe(true);
+        expect(helpers.greaterThanFifty(61)).toBe(true);
+        expect(helpers.greaterThanFifty(100)).toBe(true);
+        expect(helpers.greaterThanFifty(91)).toBe(true);
+        expect(helpers.greaterThanFifty(12)).toBe(false);
+        expect(helpers.greaterThanFifty(14)).toBe(false);
+        expect(helpers.greaterThanFifty(49)).toBe(false);
+    });
+});
 
+
+describe('add', () => {
+    it('Should return two numbers and add them', () => { 
+        expect(helpers.add(5, 11)).toBe(16);
+        expect(helpers.add(7, 17)).toBe(24);
+        expect(helpers.add(22, 11)).toBe(33);
+        expect(helpers.add(2, 33)).toBe(35);
+    })
+});
+
+describe('subtract', () => {
+    it('Should return two numbers and subtract them', () => {
+        expect(helpers.subtract(4, 3)).toBe(1);
+        expect(helpers.subtract(7, 3)).toBe(4);
+        expect(helpers.subtract(14, 7)).toBe(7);
+        expect(helpers.subtract(45, 4)).toBe(41);
+
+    })
+});
 
 // start testing!
 
