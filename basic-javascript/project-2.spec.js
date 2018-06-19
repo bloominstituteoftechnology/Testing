fa6680d95fa5a20d45c29c2392671f5812957986
Marco A Guzman <marcoguzman16@gmail.com>
2018-06-19 12:54:50 -0700

    fix getBiggest

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -2,13 +2,23 @@ const funcs = require('./project-2');
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 
 describe('getBiggest', () => {
-    it('should return the biggest number of the two or the same number if they are equal', () => {
+    it('should return X if its larger than Y', () => {
         // expect(funcs.getBiggest(undefined)).toBeNaN(true);
-        // expect(funcs.getBiggest('two')).toBeNaN(false);
-        expect(funcs.getBiggest(5, 5)).toBe(5);
-        expect(funcs.getBiggest(7, 5)).toBe(7);
-        expect(funcs.getBiggest(5, 7)).toEqual(7);
-
-
+        // expect(funcs.getBiggest('two')).toBeNaN();
+        
+        expect(funcs.getBiggest(7, 5)).toEqual(7);
+       
+    })
+    it('Should return y if its larger than X', () => { 
+        expect(funcs.getBiggest(85, 1000)).toBe(1000);
+    })
+    it('Should return either X or Y if they are the same', () => { 
+        expect(funcs.getBiggest(800, 800)).toBe(800);
     })
 });
+
+describe('greeting', () => { 
+
+
+
+})
