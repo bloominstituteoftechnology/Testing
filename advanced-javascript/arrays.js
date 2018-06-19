6422393e56e1d09c76b14a7d9f6cd72386a8b9f4
sagardesai90 <saagardesai@gmail.com>
2018-06-19 16:39:38 -0500

    wrapped up array spec tests

--- advanced-javascript/arrays.js
+++ advanced-javascript/arrays.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars, max-len */
-
+//#1
 const each = (elements, cb) => {
   for (let i = 0; i < elements.length; i++) {
     cb(elements[i], i);
   }
 };
-
+//#2
 const map = (elements, cb) => {
   const mappedArr = [];
   each(elements, item => {
@@ -13,29 +13,29 @@ const map = (elements, cb) => {
   });
   return mappedArr;
 };
-
+//#3
 const reduce = (elements, cb, memo = elements.shift()) => {
   each(elements, item => {
     memo = cb(memo, item);
   });
   return memo;
 };
-
+//#4
 const find = (elements, cb) => {
   for (let i = 0; i < elements.length; i++) {
     if (cb(elements[i])) return elements[i];
   }
   return undefined;
 };
-
+//#5
 const filter = (elements, cb) => {
   const filteredValues = [];
-  each(element, item => {
+  each(elements, item => {
     if (cb(item)) filteredValues.push(item);
   });
   return filteredValues;
 };
-
+//#6
 const flatten = elements => {
   const flattenedArr = reduce(
     elements,
