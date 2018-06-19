2e7c568b20976e2acd38fadb92afabb0f66be0ed
Lavell Burton <taylormade800@gmail.com>
2018-06-19 14:22:41 -0700

    first test passed

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -8,10 +8,70 @@ const arrayFunctions = require('./arrays');
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe('Arrays', () => {
-  describe('map', () => {
+  
+  describe('each', () => {
+    const each = arrayFunctions.each;
     it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      expect(typeof each).toBe('function');
     });
+      it('should iterate over elements', () => {
+        expect(arrayFunctions.each([6, 7, 8, 9], item => item)).toEqual(undefined)
+        const arr = [6, 7, 8, 9];
+        const actual = each(arr, addElements);
+        expect(actual).toBeUndefined();
+      });
   });
+
+
+  // describe('map', () => {
+  //   it('should be a function', () => {
+  //     const map = arrayFunctions.map;
+  //     expect(typeof map).toBe('function');
+  //   });
+  // });
+
+
+  // describe('reduce', () => {
+  //   it('should be a function', () => {
+  //     const reduce = arrayFunctions.reduce;
+  //     expect(typeof reduce).toBe('function');
+  //   });
+  // });
+
+  // describe('find', () => {
+  //   it('should be a function', () => {
+  //     const find = arrayFunctions.find;
+  //     expect(typeof find).toBe('function');
+  //   });
+  // });
+
+
+  // describe('filter', () => {
+  //   it('should be a function', () => {
+  //     const filter = arrayFunctions.filter;
+  //     expect(typeof filter).toBe('function');
+  //   });
+  // });
+
+  // describe('flatten', () => {
+  //   it('should be a function', () => {
+  //     const flatten = arrayFunctions.flatten;
+  //     expect(typeof flatten).toBe('function');
+  //   });
+  // });
 });
+
+
+// function createArray() {
+//   return [1, 2, 3, 4];
+// }
+
+function addElements(x, y) {
+  return x + y;
+}
+
+
+
+// const actual = each(elements, cb);
+// const expected = ;
+// expect(actual).toEqual(expected);
