39e201742e03d16510ee1b279a17aa856140236c
udykas <udykas@gmail.com>
2018-06-19 17:30:12 -0400

    Arrays.spec.js is done

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -23,13 +23,38 @@ describe('Arrays', () => {
     });
   });
   describe('reduce', () => {
-    it('removes first item in array', () => {
+    it('reduce an array to a single value using the callback', () => {
       const reduce = arrayFunctions.reduce;
       expect(typeof reduce).toBe('function');
       expect(reduce([1,2,3,4], (num, acc) => acc + num)).toEqual(10);
       expect(reduce([1,2,3,4], (num, acc) => acc * num)).toEqual(24);
     })
-    
+  })
+  describe('find', () => {
+    it('should find a given value in an array', () => {
+      const find = arrayFunctions.find;
+      expect(typeof find).toBe('function');
+      expect(find([0,1,2,3,4,5], num => num === 4)).toBe(4);
+      expect(find([0,1,2,3,4,5], num => num === 17)).toBeUndefined();
+      expect(find(['a','s','d','f'], num => num === 'f')).toBe('f');
+    })
+  })
+  describe('filter', () => {
+    it('filters out items in array that dont meet conditions of cb', () => {
+      const filter = arrayFunctions.filter;
+      expect(typeof filter).toBe('function');
+      expect(filter([0,1,2,3,4,5,6,7,8,9], num => num > 3)).toEqual([4,5,6,7,8,9]);
+      expect(filter([0,1,2,3,4,5,6,7,8,9], num => num === 3)).toEqual([3]);
+      expect(filter(['can', 'jam', 'poetry', 'slam'], num => num === 'can')).toEqual(['can']);
+    })
+  })
+  describe('flatten', () => {
+    it('alters multi-dimensional arrays to single-dimension array', () => {
+      const flatten = arrayFunctions.flatten;
+      expect(typeof flatten).toBe('function');
+      expect(flatten([1,2,3, [4,5, [6,7,8]]])).toEqual([1,2,3,4,5,6,7,8]);
+      expect(flatten(['a', 'b', ['c', ['d']]])).toEqual(['a', 'b', 'c', 'd']);
+    })
   })
 });
 
