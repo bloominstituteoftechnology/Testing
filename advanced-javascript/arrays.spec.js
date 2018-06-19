942db24668caa798b0aa2fb86d1be7b774062662
udykas <udykas@gmail.com>
2018-06-19 17:09:20 -0400

    First three tests in arrays done

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -7,11 +7,32 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
+
 describe('Arrays', () => {
-  describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+  describe('each', () => {
+    it('it should be a function', () => {
+      const each = arrayFunctions.each
+      expect(typeof each).toBe('function')
     });
   });
+  describe('map', () => {
+    it('should map over an array and apply the callback to each item', () => {
+      const map = arrayFunctions.map;
+      expect(typeof map).toBe('function');
+      expect(map([1,2,3], num => num * 2)).toEqual([2,4,6])
+    });
+  });
+  describe('reduce', () => {
+    it('removes first item in array', () => {
+      const reduce = arrayFunctions.reduce;
+      expect(typeof reduce).toBe('function');
+      expect(reduce([1,2,3,4], (num, acc) => acc + num)).toEqual(10);
+      expect(reduce([1,2,3,4], (num, acc) => acc * num)).toEqual(24);
+    })
+    
+  })
 });
+
+
+
+
