2432b638801b9f315be366bd3cbda6f147bdb596
Obo Agboghidi <oagboghi2@gmail.com>
2018-06-19 13:42:13 -0700

    Obo Agboghidi: Working o Basic_2

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -112,7 +112,7 @@ describe('project-1', () => {
     })
 })
 
-describe('multiply', () => {
+describe('project-1', () => {
     describe('multiply', () => {
         it('returns NaN if argument is non-numeric', () => {
             expect(helpers.multiply(undefined)).toBeNaN();
@@ -124,3 +124,204 @@ describe('multiply', () => {
         it('returns x*y if both arguments are true', () => {})
     })
 })
+
+describe('getRemainder', () => {
+    describe('getRemainder', () => {
+        it('returns NaN if argument is non-numeric', () => {
+            expect(helpers.getRemainder(undefined)).toBeNaN();
+            expect(helpers.getRemainder('String')).toBeNaN();
+        })
+        it('returns x if argument is false', () => {
+            expect(helpers.getRemainder(2, false)).toBeNaN()
+        })
+        it('returns a remainder if the arguments are both true', () => {})
+    })
+})
+
+describe('isEven', () => {
+    describe('isEven', () => {
+        it('returns false if argument is non-numeric', () => {
+            expect(helpers.isEven(undefined)).toBeFalsy();
+            expect(helpers.isEven('String')).toBeFalsy();
+        })
+        it('returns false if argument is true', () => {
+            expect(helpers.isEven(true)).toBeFalsy();
+        })
+        it('return true if argument is divisible by 2', () => {})
+        })
+})
+
+describe('project-1', () => {
+    describe('isOdd', () => {
+        it('returns true if argument is non-numeric', () => {
+            expect(helpers.isOdd(undefined)).toBeTruthy();
+            expect(helpers.isOdd('String')).toBeTruthy();
+        })
+        it('returns true if argument is true', () => {
+            expect(helpers.isOdd(true)).toBeTruthy();
+        })
+        it('return false if argument is divisible by 2', () => { })
+    })
+})
+
+describe('project-1', () => {
+    describe('square', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.square("string")).toBeNaN();
+            expect(helpers.square(undefined)).toBeNaN();
+        })
+        it('should return the product of num * num', () => {})
+    })
+})
+
+describe('project-1', () => {
+    describe('cube', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.cube("string")).toBeNaN();
+            expect(helpers.cube(undefined)).toBeNaN();
+        })
+        it('should return the product of num * num * num', () => { })
+    })
+})
+
+describe('project-1', () => {
+    describe('raiseToPower', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.raiseToPower("string")).toBeNaN();
+            expect(helpers.raiseToPower(undefined)).toBeNaN();
+        })
+        it('returns number if exponent is true', () => {
+            expect(helpers.raiseToPower(12,true)).toEqual(12);
+        })
+        it('should return a product of number to a certain power', () => {})
+    })
+})
+
+describe('project-1', () => {
+    describe('roundNumber', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.roundNumber("string")).toBeNaN();
+            expect(helpers.roundNumber(undefined)).toBeNaN();
+        })
+        it('returns a 1 if the argument is true', () => {
+            expect(helpers.roundNumber(true)).toEqual(1);
+        })
+        it('returns a 0 if the argument is false', () => {
+            expect(helpers.roundNumber(false)).toEqual(0);
+        })
+        it('returns a given number rounded up', () => {})
+    })
+})
+
+describe('project-1', () => {
+    describe('roundUp', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.roundNumber("string")).toBeNaN();
+            expect(helpers.roundNumber(undefined)).toBeNaN();
+        })
+        it('returns a 1 if the argument is true', () => {
+            expect(helpers.roundNumber(true)).toEqual(1);
+        })
+        it('returns a 0 if the argument is false', () => {
+            expect(helpers.roundNumber(false)).toEqual(0);
+        })
+        it('returns a given number rounded up', () => { })
+    })
+})
+
+describe('project-1', () => {
+    describe('addExclamationPoint', () => {
+        it('returns a string length', () => {
+            expect(helpers.addExclamationPoint('cat')).toHaveLength(4);
+            expect(helpers.addExclamationPoint('cat')).toEqual('cat!');
+        })
+    })
+})
+
+describe('project-1', () => {
+    describe('combineNames', () => {
+        it('returns a string length', () => {
+            expect(helpers.combineNames('Adam', 'Smith')).toHaveLength(10);
+        })
+        it('returns the string as intended', () => {
+            expect(helpers.combineNames('Adam', 'Smith')).toEqual('Adam Smith');
+        })
+    })
+})
+
+describe('project-1', () => {
+    describe('getGreeting', () => {
+        it('returns length of name', () => {
+            expect(helpers.getGreeting('Adam')).toHaveLength(11);
+        })
+        it('returns the string as intended', () => {
+            expect(helpers.getGreeting('Adam')).toEqual('Hello Adam!');
+        })
+    })
+})
+
+describe('project-1', () => {
+    describe('getRectangleArea', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.getRectangleArea("string")).toBeNaN();
+            expect(helpers.getRectangleArea(undefined)).toBeNaN();
+        })
+        it('returns num if the argument is true', () => {
+            expect(helpers.getRectangleArea(3,true)).toEqual(3);
+        })
+        it('returns a 0 if the argument is false', () => {
+            expect(helpers.getRectangleArea(3, false)).toEqual(0);
+        })
+        it('returns a number based off of 2 arguments multiplying, length * width', () => {})
+    })
+})
+
+describe('project-1', () => {
+    describe('getTriangleArea', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.getTriangleArea("string")).toBeNaN();
+            expect(helpers.getTriangleArea(undefined)).toBeNaN();
+        })
+        it('returns num if the argument is true', () => {
+            expect(helpers.getTriangleArea(3, true)).toEqual(1.5);
+        })
+        it('returns a 0 if the argument is false', () => {
+            expect(helpers.getTriangleArea(3,false)).toEqual(0);
+        })
+        it('returns the prouct of base * height* 1.5', () => {})
+    })
+})
+
+describe('project-1', () => {
+    describe('getCircleArea', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.getCircleArea("string")).toBeNaN();
+            expect(helpers.getCircleArea(undefined)).toBeNaN();
+        })
+        it('returns pi if the argument is true', () => {
+            expect(helpers.getCircleArea(true)).toEqual(3.141592653589793);
+        })
+        it('returns a 0 if the argument is false', () => {
+            expect(helpers.getCircleArea(false)).toEqual(0);
+        })
+        it('return a number multiplied by Pi', () => {})
+
+    })
+})
+
+describe('project-1', () => {
+    describe('getRectangularPrismVolume', () => {
+        it('returns NaN if argument is non-numberic', () => {
+            expect(helpers.getRectangularPrismVolume("string")).toBeNaN();
+            expect(helpers.getRectangularPrismVolume(undefined)).toBeNaN();
+        })
+        it('returns 6 if the argument is true', () => {
+            expect(helpers.getRectangularPrismVolume(2,3,true)).toEqual(6);
+        })
+        it('returns a 0 if the argument is false', () => {
+            expect(helpers.getRectangularPrismVolume(2,3,false)).toEqual(0);
+        })
+        it('return the volume of a rectangular prims by multiplying width, height and length', () => {})
+    })
+})
+
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,43 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('getBiggest', () => {
+    const testFunc = funcs.getBiggest;
+
+    it('returns type number given type numbers', () => {
+
+        const expected = "number";
+        
+        const actual = typeof testFunc(5, 7);
+        expect(actual).toBe(expected);
+    });
+
+    describe('identifies bigger number', () => {
+        it('returns 5 given -1, 5', () => {
+    
+            const expected = 5;
+            
+            const actual = testFunc(-1, 5);
+
+            expect(actual).toBe(expected);
+        });
+        it('returns -3 given -11, ', () => {
+    
+            const expected = -3;
+            
+            const actual = testFunc(-3, -11);
+
+            expect(actual).toBe(expected);
+        });
+    });
+});
+
+describe('greeting', () => {
+    const testFunc = funcs.greeting;
+
+    it('returns type string', () => {
+
+        const expected = "string";
+        const actual = typeof testFunc();
+        expect(actual).toBe(expected);
+    });
