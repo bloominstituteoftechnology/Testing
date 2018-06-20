b66f59cd913619d68ced1e0077b99a2bb1169d54
AmyShackles <amyshackles@gmail.com>
2018-06-19 18:04:15 -0700

    Completed Arrays.spec.js

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -55,27 +55,57 @@ describe('Arrays', () => {
     it('should reduce an array to a single value', () => {
     const reduce1 = reduce([1,2,3], (num, total) => num + total)
     const reduce2 = reduce([5, 10, 15], (num, total) => num + total + 5)
+    const reduce3 = reduce([.2, .5, .1], (num, total) => num + total * 2)
 
     expect(reduce1).toEqual(6)
-    expect(reduce2).toEqual(40)      
+    expect(reduce2).toEqual(40)
+    expect(reduce3).toEqual(1.4)   
     })
   })
-  describe('find', () => {
+  describe('find', () => {      
+    const find = arrayFunctions.find
     it('should be a function', () => {
-      const find = arrayFunctions.find
+
       expect(typeof find).toBe('function')
     })
-  })
-  describe('filter', () => {
-    it('should be a function', () => {
-      const filter = arrayFunctions.filter
-      expect(typeof filter).toBe('function')
+    it('should return the value of the first element in the array that satisfies the testing function', () => {
+      const find1 = find([1, 3, 7, 5, 2, 8], num => num % 2 === 0)
+      const find2 = find([2.1, 3, 5, -2, 0], num => num < 0)
+      const find3 = find([null, 0, undefined, false, true], num => typeof num === 'boolean')
+
+      expect(find1).toEqual(2)
+      expect(find2).toEqual(-2)
+      expect(find3).toEqual(false)
     })
   })
-  describe('flatten', () => {
+  describe('filter', () => {      
+    const filter = arrayFunctions.filter
     it('should be a function', () => {
-      const flatten = arrayFunctions.flatten
+      expect(typeof filter).toBe('function')
+    })
+    it('should return an array of elements that satisfy the testing condition', () => {
+      const filter1 = filter([2, 7, 4, 2, 0, 3, 8, 99], num => num % 2 === 0)
+      const filter2 = filter([-2, 0, false, -0, -2, -.01, -2], num => num < 0)
+      const filter3 = filter([false, null, true, undefined, false, true, 0, 1], num => typeof num === 'boolean')
+
+      expect(filter1).toEqual([2, 4, 2, 0, 8])
+      expect(filter2).toEqual([-2, -2, -.01, -2])
+      expect(filter3).toEqual([false, true, false, true])
+    })
+  })
+  describe('flatten', () => {      
+    const flatten = arrayFunctions.flatten
+    it('should be a function', () => {
       expect(typeof flatten).toBe('function')
+    })
+    it('should return an array with elements concatted into it', () => {
+      const flatten1 = flatten([2, 5, 7, [1, 2], [2, 3, [7]]])
+      const flatten2 = flatten([1, 2, 3, [4, 5, 6, [7, 8, 9]]])
+      const flatten3 = flatten([1, 2, 3])
+
+      expect(flatten1).toEqual([2, 5, 7, 1, 2, 2, 3, 7])
+      expect(flatten2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      expect(flatten3).toEqual([1, 2, 3])
     })
   })
 });
