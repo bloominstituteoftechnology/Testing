411ffca313f0e91f0868f8fff1aeeb61d2fce8c2
Zach Campbell <beazlebubbles@gmail.com>
2018-06-19 18:59:38 -0600

    More tests

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -2,19 +2,7 @@ const objectFunctions = require("./objects");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe("objects", () => {
-  describe("each", () => {
-    it("should return the keys of the object", () => {
-      const keys = objectFunctions.keys;
-      expect(typeof keys).toBe("function");
-      let testVar = "";
-
-      expect(
-        keys({ a: "what", b: "is", c: "the", d: "frequency", e: "Kenneth?" })
-      ).toEqual(["a", "b", "c", "d", "e"]);
-    });
-  });
-
-  describe("each", () => {
+  describe("keys", () => {
     it("should return the keys of the object", () => {
       const keys = objectFunctions.keys;
       expect(typeof keys).toBe("function");
