38315a1fca868c06162435b1fc314ab96404a96e
Zach Campbell <beazlebubbles@gmail.com>
2018-06-19 18:58:31 -0600

    More tests

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -8,10 +8,21 @@ const arrayFunctions = require("./arrays");
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
 describe("Arrays", () => {
+  describe("each", () => {
+    it("should execute the callback on each item in the array", () => {
+      const each = arrayFunctions.each;
+      expect(typeof each).toBe("function");
+      let testVar = "";
+      each([1, 2, 3], (item, index) => (testVar = testVar + item + index));
+      expect(testVar).toEqual("102132");
+    });
+  });
+
   describe("map", () => {
-    it("should be a function", () => {
+    it("should execute the callback on each item in the array, return an array", () => {
       const map = arrayFunctions.map;
       expect(typeof map).toBe("function");
+      expect(map([1, 2, 3], a => a + 1)).toEqual([2, 3, 4]);
     });
   });
 });
