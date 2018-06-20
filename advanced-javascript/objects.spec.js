c8ea822e7f77a9670565150e22c8008239c2b5e4
AmyShackles <amyshackles@gmail.com>
2018-06-20 11:30:08 -0700

    Changed Objects.spec.js

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,21 @@
 const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {});
+describe('Objects', () => {
+  const arg = Array.from(arguments)
+  it('should be an object', () => {
+    expect(typeof arg).toEqual('object')
+  });
+  describe('keys', () => {
+    const keys = objectFunctions.keys
+    it('should return the keys of an object', () => {
+      const key1 = keys({one: 'two', two: 'three', four: 'four'})
+      expect(key1).toEqual(['one', 'two', 'four'])
+  })
+
+  })
+ 
+
+
 });
+
