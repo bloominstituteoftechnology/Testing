77f97444eef679cf26538f60a3253388a04bc576
Herbert Hodgson <herberthodgson@hotmail.com>
2018-06-19 20:21:57 -0400

    Added tests to keys and values functions in objects.spec.js file.

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,36 @@
-const objectFunctions = require('./objects');
+const objectFunctions = require("./objects");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {});
+describe("objects", () => {
+  describe("keys", () => {
+    it("should be an function", () => {
+      expect(typeof objectFunctions.keys).toEqual("function");
+    });
+    it("should return the object keys", () => {
+      expect(
+        objectFunctions.keys({
+          windows: "10",
+          make: "toyota",
+          model: "prius",
+          firstName: "Bill"
+        })
+      ).toEqual(["windows", "make", "model", "firstName"]);
+    });
+  });
+
+  describe("values", () => {
+    it("should be an function", () => {
+      expect(typeof objectFunctions.values).toEqual("function");
+    });
+    it("should return the object values", () => {
+      expect(
+        objectFunctions.values({
+          windows: "10",
+          make: "toyota",
+          model: "prius",
+          firstName: "Bill"
+        })
+      ).toEqual(["10", "toyota", "prius", "Bill"]);
+    });
+  });
 });
