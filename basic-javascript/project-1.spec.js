fe56031aee6d9499427a8602313c881df3f81292
Usman Javed <ujaved931@gmail.com>
2018-06-26 03:55:25 +0500

    Initial commit working on concepts was reading and watching videos for testing to get a better understanding, also bad internet hence limited time to work

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,19 @@
 const helpers = require('./project-1');
 
+
 // start testing!
+describe.only('project-1', () => {
+    describe('multiplyByTen', () => {
+        it('return NaN when given a non numeric value', () => {
+            expect(helpers.multiplyByTen(undefined)).toBeNaN();
+            expect(helpers.multiplyByTen('two')).toBeNaN();
+            expect(helpers.multiplyByTen(false)).toBeNaN();
+            expect(helpers.multiplyByTen(true)).toBeNaN();
+        })
+        it('should return the given number multiplied by 10', () => {
+
+        })
+    })
+})
+
+
