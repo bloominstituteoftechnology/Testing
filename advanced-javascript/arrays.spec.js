6d8d5ab4093ce5131256b3084fe6b3004d68c343
CMVNK <Kopecky12112@gmail.com>
2018-06-25 22:30:23 -0700

    wrote some tests for arrays.spec.js

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -13,13 +13,45 @@ describe('arrays', () => {
       const map = arrayFunctions.map;
       expect(typeof map).toBe('function');
     });
+
+    it("should invoke callback passed in", () => {
+      const cb= jest.fn();
+      const map = arrayFunctions.map;
+      const elements = [1,2,3,4];
+
+      map(elements, cb);
+      expect(cb).toHaveBeenCalled();
+  
+    })
+
+    it("should have length", ()=> {
+      const elements = [1,2,3,4];
+      expect(elements).toHaveLength(4);
+    })
+
+    
+   
   });
 
   describe('each', () => {
+    it("should invoke callback passed in", () => {
+      const elements = [1,2,3,4];
+      const cb= jest.fn();
+      arrayFunctions.each(elements, cb);
+      expect(cb).toHaveBeenCalled();
+  
+    })
+
     it('should be a function', () => {
       const each = arrayFunctions.each;
       expect(typeof each).toBe('function');
     });
+
+    it("should have length", ()=> {
+      const elements = [1,2,3,4];
+      expect(elements).toHaveLength(4);
+    })
+
     
   });
 
@@ -28,12 +60,29 @@ describe('arrays', () => {
       const reduce = arrayFunctions.reduce;
       expect(typeof reduce).toBe('function');
     });
+
+    it("should invoke callback passed in", () => {
+      const elements = [1,2,3,4];
+      const cb= jest.fn();
+      arrayFunctions.reduce(elements, cb);
+      expect(cb).toHaveBeenCalled();
+    });
+
   });
+
+  
 
   describe('find', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
       expect(typeof find).toBe('function');
+    });
+
+    it("should invoke callback passed in", () => {
+      const elements = [1,2,3,4];
+      const cb= jest.fn();
+      arrayFunctions.find(elements, cb);
+      expect(cb).toHaveBeenCalled();
     });
   });
 
@@ -42,12 +91,26 @@ describe('arrays', () => {
       const filter = arrayFunctions.filter;
       expect(typeof filter).toBe('function');
     });
+
+    it("should invoke callback passed in", () => {
+      const elements = [1,2,3,4];
+      const cb= jest.fn();
+      arrayFunctions.find(elements, cb);
+      expect(cb).toHaveBeenCalled();
+    });
   });
 
   describe('flatten', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
       expect(typeof flatten).toBe('function');
+    });
+
+    it("should invoke callback passed in", () => {
+      const elements = [1,2,3,4];
+      const cb= jest.fn();
+      arrayFunctions.find(elements, cb);
+      expect(cb).toHaveBeenCalled();
     });
   });
 });
