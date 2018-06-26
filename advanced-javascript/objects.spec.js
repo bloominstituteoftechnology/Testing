9a78a3b83fc76e00bfe51d1bca373ab001f1fcdc
CMVNK <Kopecky12112@gmail.com>
2018-06-25 23:50:47 -0700

    did what I could - feel like I need a walk through on the HOF and obj

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -2,5 +2,49 @@ const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
+  it('should be an object', () => {
+    expect(typeof objectFunctions).toBe('object');
+  });
 });
+
+describe('keys', () => {
+  it('should be a key', ()=> {
+    expect(objectFunctions.keys({a: 0, b:1, c:3})).toEqual(["a", "b", "c"]);
+  });
+});
+
+describe('values', () => {
+  it('should be a value', () => {
+    expect(objectFunctions.values({a: 0, b:1, c:3})).toEqual([0,1,3]);
+  });
+});
+
+describe('mapObject', () => {
+  it('should invoke a callback', () => {
+    const cb= jest.fn();
+    const mapObject = objectFunctions.mapObject;
+    const obj = {a:0, b:1, c:3};
+
+    mapObject(obj, cb);
+    expect(cb).toHaveBeenCalled();
+  });
+});
+
+describe('pairs', () => {
+  it('should return key/value pairs', () => {
+    expect(objectFunctions.pairs({a:0, b:1, c:3})).toEqual([["a",0], ["b",1], ["c", 3]]);
+  });
+});
+
+describe('invert', () => {
+  it('should invert so keys become values and values become keys', () => {
+    expect(objectFunctions.invert({a:0, b:1, c:3})).toEqual({"0":"a", "1": "b", "3": "c"});
+  });
+});
+
+describe('defaultProps', () => {
+  it('should pass default props', () => {
+    const defaultProps = jest.fn;
+    expect(objectFunctions.defaults({a:0, b:1, c:3}, defaultProps)).toEqual({a:0, b:1, c:3});
+  })
+})
