d80a2b8d37db2ca07923741c43102c5c1e7e353d
darthMinimus <irishseanvaldivia@gmail.com>
2018-06-28 16:50:41 -0500

    Milestone, figured out how to use mock callbacks!

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -2,12 +2,14 @@ const arrayFunctions = require('./arrays');
 const mockCB = jest.fn();
 
 describe('Arrays', () => {
-    it('each', () => {
-        const callBack = (elem, indx) => {
-            elem += indx;  
-        }
-        const arr = [1, 2, 3, 4];
-        arrayFunctions.each(arr, callBack);
-        expect(arr).toEqual([1, 3, 5, 7]);
+    describe('each', () => {
+      it('must be a function', () => {
+          const instanceOfEach = arrayFunctions.each;
+          expect(typeof instanceOfEach).toBe('function');
+      });
+      it('should invoke callback on each item in array', () => {
+          arrayFunctions.each([1, 2, 3, 4], mockCB);
+          expect(mockCB.mock.calls.length).toBe(4);
+      })
     })
 });
