b051781fc997398533d8775bf3f3e2706f644997
cmok4290 <cmok4290@gmail.com>
2018-08-13 15:03:35 -0400

    started basic-javascript tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,8 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('multiplyByTen', () => {
+    it('should return num multiplied by 10', () => {
+        expect(helpers.multiplyByTen(5)).toEqual(50);
+    });
+});
