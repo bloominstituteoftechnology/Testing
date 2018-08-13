16af4481aa3a8dfd3115275324151f72f4225970
DirupT <iamcooled@gmail.com>
2018-08-13 14:13:51 -0500

    added tests for lessthanninety

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -12,7 +12,7 @@ it('should subtract the number passed by five', () => {
     expect(helpers.subtractFive(30)).toEqual(25);
 })
 
-it('should return true if the two values provided are the same length', () => {
+it('should return true if the two values provided are the same length, otherwise false', () => {
     expect(helpers.areSameLength('hello', 'test5')).toEqual(true);
     expect(helpers.areSameLength('hell', 'test5')).toEqual(false);
     expect(helpers.areSameLength(5, 'test5ssssss')).toEqual(false);
@@ -22,4 +22,10 @@ it('should return true if the two values provided are equal', () => {
     expect(helpers.areEqual(5, 5)).toEqual(true);
     expect(helpers.areEqual('hello', 'hello')).toEqual(true);
     expect(helpers.areEqual(5, 6)).toEqual(false);
+})
+
+it('should return true if value provided is less than 90, otherwise false', () => {
+    expect(helpers.lessThanNinety(50)).toEqual(true);
+    expect(helpers.lessThanNinety(90)).toEqual(false);
+    expect(helpers.lessThanNinety(-20)).toEqual(true);
 })
