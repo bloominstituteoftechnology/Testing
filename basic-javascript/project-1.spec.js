d05183e9f5b2d7708acee772fbd1b5c3aacfa1b1
Vu Cao <cdhvu@yahoo.com>
2018-08-13 14:13:53 -0700

    Created test for some more functions.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -86,19 +86,100 @@ describe('Project 1 Assessment', () => {
         })
     })
 
-    // describe('Add', () => {
-    //     it('should return the sum of 2 number.', () => {
-    //         expect(helpers.add(1,2)).toBe(3);
-    //         expect(helpers.add(-1,2)).toBe(1);
-    //         expect(helpers.add(-1,-1)).toBe(-2);
-    //     })
-    // })
+    describe('Multiply', () => {
+        it('should return the product of 2 numbers.', () => {
+            expect(helpers.multiply(1,2)).toBe(2);
+            expect(helpers.multiply(-2,3)).toBe(-6);
+            expect(helpers.multiply(-3,-10)).toBe(30);
+        })
+    })
 
-    // describe('Add', () => {
-    //     it('should return the sum of 2 number.', () => {
-    //         expect(helpers.add(1,2)).toBe(3);
-    //         expect(helpers.add(-1,2)).toBe(1);
-    //         expect(helpers.add(-1,-1)).toBe(-2);
-    //     })
-    // })
+    describe('Divide', () => {
+        it('should return the quotient of 2 numbers.', () => {
+            expect(helpers.divide(1,2)).toBe(0.5);
+            expect(helpers.divide(-10,2)).toBe(-5);
+            expect(helpers.divide(6,2)).toBe(3);
+        })
+    }) 
+
+    describe('Get Remainder', () => {
+        it('should return the the remainder of 2 numbers.', () => {
+            expect(helpers.getRemainder(17, 5)).toBe(2);
+            expect(helpers.getRemainder(-5, 20)).toBe(-5);
+            expect(helpers.getRemainder(10, 8)).toBe(2);
+        })
+    })
+
+    describe('Is Even', () => {
+        it('should return true if number is even.', () => {
+            expect(helpers.isEven(10)).toBe(true);
+            expect(helpers.isEven(20)).toBe(true);
+        })
+
+        it('should return false if number is odd.', () => {
+            expect(helpers.isEven(91)).toBe(false);
+            expect(helpers.isEven(101)).toBe(false);
+        })
+    })
+
+    describe('Is Odd', () => {
+        it('should return true if number is odd.', () => {
+            expect(helpers.isOdd(11)).toBe(true);
+            expect(helpers.isOdd(21)).toBe(true);
+        })
+
+        it('should return false if number is even.', () => {
+            expect(helpers.isOdd(90)).toBe(false);
+            expect(helpers.isOdd(100)).toBe(false);
+        })
+    })
+
+    describe('Square', () => {
+        it('should return the square of number passed.', () => {
+            expect(helpers.square(11)).toBe(121);
+            expect(helpers.square(5)).toBe(25);
+        })
+    })
+
+    describe('Cube', () => {
+        it('should return the cube of number passed.', () => {
+            expect(helpers.cube(3)).toBe(27);
+            expect(helpers.cube(5)).toBe(125);
+        })
+    })
+
+    describe('Raise To Power', () => {
+        it('should return the based number to the exponent power.', () => {
+            expect(helpers.raiseToPower(3,2)).toBe(9);
+            expect(helpers.raiseToPower(5,2)).toBe(25);
+        })
+    })
+
+    describe('Round Number', () => {
+        it('should return the number passed in, rounded to the nearest integer.', () => {
+            expect(helpers.roundNumber(3.2)).toBe(3);
+            expect(helpers.roundNumber(5.6)).toBe(6);
+        })
+    })
+
+    describe('Round Up', () => {
+        it('should return the number passed in, rounded up to the nearest integer.', () => {
+            expect(helpers.roundUp(3.2)).toBe(4);
+            expect(helpers.roundUp(5.6)).toBe(6);
+        })
+    })
+
+    describe('Add Exclaimation Point', () => {
+        it('should return the string passed in with an exclaimation at the end.', () => {
+            expect(helpers.addExclamationPoint('test')).toBe('test!');
+            expect(helpers.addExclamationPoint('')).toBe('!');
+        })
+    })
+
+    describe('Combine Names', () => {
+        it('should return the concatenation of the names passed.', () => {
+            expect(helpers.combineNames('John', 'Doe')).toBe('John Doe');
+            expect(helpers.combineNames('Jean', 'Doe')).toBe('Jean Doe');
+        })
+    })
 })
