35a9d0be9d2d38eab81a1d8840ccb0f0290c7dcf
Allen Hai <allenhai03@gmail.com>
2018-08-13 18:01:10 -0500

    write tests for multiplyByTen to areSameLength functions

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,26 @@
 const helpers = require('./project-1');
 
-// start testing!
+describe('multiplyByTen', () => {
+  it('should multiply a number by 10',() => {
+    const { multiplyByTen } = helpers
+    expect(multiplyByTen(4)).toEqual(40)
+  })
+})
+
+describe('subtractFive', () => {
+  it('should subtract 5 from number', () => {
+    const { subtractFive } = helpers
+    expect(subtractFive(10)).toEqual(5)
+  })
+})
+
+describe('areSameLength', () => {
+  it('strings of different lengths should return false', () => {
+    const { areSameLength } = helpers
+    expect(areSameLength('yo', 'yoo')).toBe(false)
+  })
+  it('strings of same length should return true', () => {
+    const { areSameLength } = helpers
+    expect(areSameLength('yo', 'yo')).toBe(true)
+  })
+})
