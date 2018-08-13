093542bb478656e0c1355f4a3789935e1a0f04b9
Alec <afroenygam@gmail.com>
2018-08-13 16:48:06 -0400

    project-1.spec.js updated

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -24,3 +24,79 @@ test('subtracts input by 5', () => {
 test('subtracts input by 5', () => {
   expect(helpers.subtractFive(0)).toBe(-5);
 });
+
+it('checks if two strings are the same length', () => {
+  expect(helpers.areSameLength('coke','soda')).toBe(true);
+})
+
+test('checks if two strings are the same length', () => {
+  expect(helpers.areSameLength('coke', 'pepsi')).toBe(false);
+})
+
+test('checks if two numbers are equal', () => {
+  expect(helpers.areEqual(2,2)).toBe(true);
+})
+
+test('checks if two numbers are equal', () => {
+  expect(helpers.areEqual(2,3)).toBe(false);
+})
+
+test('checks if two numbers are equal', () => {
+  expect(helpers.areEqual(2,'2')).toBe(false)
+})
+
+test('checks if two numbers are equal', () => {
+  expect(helpers.areEqual('2','2')).toBe(true);
+})
+
+test('checks if a number is less than 90', () => {
+  expect(helpers.lessThanNinety(89.99999999999)).toBe(true);
+})
+
+test('checks if a number is less than 90', () => {
+  expect(helpers.lessThanNinety(90.000000000001)).toBe(false);
+})
+
+test('checks if a number is greater than 50', () => {
+  expect(helpers.greaterThanFifty(50.0000000001)).toBe(true);
+})
+
+test('checks if a number is greater than 50', () => {
+  expect(helpers.greaterThanFifty(49.999999999999)).toBe(false);
+})
+
+test('sum of two numbers', () => {
+  expect(helpers.add(2,2)).toBe(4);
+})
+
+test('sum of two numbers', () => {
+  expect(helpers.add(2,5)).not.toBe(6);
+})
+
+test('difference of two numbers', () => {
+  expect(helpers.subtract(2,5)).toBe(-3);
+})
+
+test('product of two numbers', () => {
+  expect(helpers.multiply(2,5)).toBe(10);
+})
+
+test('product of two numbers', () => {
+  expect(helpers.multiply(2,-5)).toBe(-10);
+})
+
+test('product of two numbers', () => {
+  expect(helpers.multiply(-2,-5)).toBe(10);
+})
+
+test('remainder of two numbers', () => {
+  expect(helpers.getRemainder(2,5)).toBe(2);
+})
+
+test('remainder of two numbers', () => {
+  expect(helpers.getRemainder(5,5)).toBe(0);
+})
+
+test('remainder of two numbers', () => {
+  expect(helpers.getRemainder(6,5)).toBe(1);
+})
