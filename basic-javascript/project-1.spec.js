732802771302e380af4deedc9b56a6d1d26b77cd
DirupT <iamcooled@gmail.com>
2018-08-13 14:00:44 -0500

    added tests for multiplybyten

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,7 @@
 const helpers = require('./project-1');
 
-// start testing!
+it('should multiply the number passed by ten', () => {
+    expect(helpers.multiplyByTen(4)).toEqual(40);
+    expect(helpers.multiplyByTen(6)).toEqual(60);
+    expect(helpers.multiplyByTen(10)).toEqual(100);
+})
