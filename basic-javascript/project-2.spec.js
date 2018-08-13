b7e9d29018c9729fb36298c5408acc42c1ff39b7
DirupT <iamcooled@gmail.com>
2018-08-13 15:18:52 -0500

    added tests for greeting

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -5,3 +5,17 @@ it('should return the biggest of the 2 values passed, if equal return one of the
     expect(funcs.getBiggest(10, 10)).toEqual(10);
     expect(funcs.getBiggest(500, 5)).toEqual(500);
 })
+
+describe('Return Greeting depending on value passed in', () => {
+    it(`should return 'Guten Tag!`, () => {
+        expect(funcs.greeting('German')).toEqual('Guten Tag!');
+    })
+
+    it('should return Hola!', () => {
+        expect(funcs.greeting('Spanish')).toEqual('Hola!');
+    })
+
+    it('should return Hello!', () => {
+        expect(funcs.greeting('Random Language!')).toEqual('Hello!');
+    })
+});
