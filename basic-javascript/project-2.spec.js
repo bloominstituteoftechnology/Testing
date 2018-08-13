dcf56c64b657ba3b54e1c29ae49f971c9be175e5
DirupT <iamcooled@gmail.com>
2018-08-13 15:14:54 -0500

    added tests for getbiggest

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,7 @@
 const funcs = require('./project-2');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+it('should return the biggest of the 2 values passed, if equal return one of the values', () => {
+    expect(funcs.getBiggest(2, 5)).toEqual(5);
+    expect(funcs.getBiggest(10, 10)).toEqual(10);
+    expect(funcs.getBiggest(500, 5)).toEqual(500);
+})
