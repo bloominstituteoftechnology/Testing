3bdc7f94116cd20434f080bf6c5696af891e27c2
DirupT <iamcooled@gmail.com>
2018-08-13 14:01:55 -0500

    added tests for subtractbyfive

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -4,4 +4,10 @@ it('should multiply the number passed by ten', () => {
     expect(helpers.multiplyByTen(4)).toEqual(40);
     expect(helpers.multiplyByTen(6)).toEqual(60);
     expect(helpers.multiplyByTen(10)).toEqual(100);
+});
+
+it('should subtract the number passed by five', () => {
+    expect(helpers.subtractFive(5)).toEqual(0);
+    expect(helpers.subtractFive(100)).toEqual(95);
+    expect(helpers.subtractFive(30)).toEqual(25);
 })
