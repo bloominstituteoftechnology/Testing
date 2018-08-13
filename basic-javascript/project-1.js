a0d15b1c927c5ad42b1f51eac51a81123a3c8bb1
Simon <Simon_Freelance@hotmail.com>
2018-08-13 15:37:48 -0400

    multiplyByTen testing completed

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -1,6 +1,9 @@
 /* eslint-disable arrow-parens */
 
 const multiplyByTen = num => {
+  if(!Number.isInteger(num) || typeof num !== 'number') {
+      return false;
+  }
   return num * 10;
 };
 
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,18 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
 
+describe("Multiply the number passed with 10", () => {
+  const multiplyByTen = helpers.multiplyByTen;
+  it("Should multiply two numbers together", () => {
+    expect(multiplyByTen(20)).toBe(200);
+  });
+  it("Should multiply a negative number", () => {
+      expect(multiplyByTen(-2)).toBe(-20);
+  });
+  it("the argument passed should equal a whole number", () => {
+    expect(multiplyByTen(5.5)).toBe(false);
+  });
+  it("the argument passed should be a number type", () => {
+    expect(multiplyByTen("10")).toBe(false);
+  });
+});
 // start testing!
