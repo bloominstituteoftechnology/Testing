7fcf60b5bfc9e95c6a6dc8ef1cfa8c9718dabe32
Vu Cao <cdhvu@yahoo.com>
2018-08-14 11:42:30 -0700

    updated some test functions

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -2,5 +2,7 @@ const objectFunctions = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
+  it('should be an object', () => {
+    expect(true).toBe(true);
+  });
 });
