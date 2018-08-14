221d868327aa4a2393cbc3a68ee7b9759a814d68
JJ Ashcraft <jj@jjashcraft.com>
2018-08-14 10:57:21 -0400

    completed project-1

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -128,4 +128,77 @@ describe('math helpers', ()=>{
       expect(result).toBeLessThanOrEqual(max);
     })
 
+    it('should round the value passed', () => {
+      const value1 = 19.742;
+      const expected = 20;
+      const result = helpers.roundNumber(value1);
+      expect(result).toEqual(expected);
+    })
+
+    it('should round the value passed up to the next integer', () => {
+      const value1 = 19.742;
+      const expected = 20;
+      const result = helpers.roundUp(value1);
+      expect(result).toEqual(expected);
+    })
+    
+    it('should return a string with an exclamation point added to the end', () => {
+      const string = 'you are crazy';
+      const expected = 'you are crazy!';
+      const result = helpers.addExclamationPoint(string);
+      expect(result).toBe(expected);
+    })
+
+    it('should return a single string with the provided first and last names combined', () => {
+      const first = 'Charlie';
+      const last = 'Day';
+      const expected = 'Charlie Day';
+      const result = helpers.combineNames(first, last);
+      expect(result).toBe(expected);
+    })
+
+    it('should return a single string that greets the user with provided value as name', () => {
+      const name = 'Charlie';
+      const expected = 'Hello Charlie!';
+      const result = helpers.getGreeting(name);
+      expect(result).toEqual(expect.stringContaining(expected));
+      expect(result).toBe(expected);
+    })
+
+     it('should take the first value passed as length and second value passed as width, returning rectangle area', () => {
+       const max = Number.MAX_VALUE;
+       const value1 = 5;
+       const value2 = 2;
+       const result = helpers.getRectangleArea(value1, value2);
+       expect(result).toEqual(value1 * value2);
+       expect(result).toBeLessThanOrEqual(max);
+     })
+
+     it('should take the first value passed as the triangle base and second value passed as triangle height, returning triangle area', () => {
+       const max = Number.MAX_VALUE;
+       const value1 = 5;
+       const value2 = 2;
+       const result = helpers.getTriangleArea(value1, value2);
+       expect(result).toEqual(0.5 * value1 * value2);
+       expect(result).toBeLessThanOrEqual(max);
+     })
+
+     it('should take the value passed as circle radius, returning circle area', () => {
+       const max = Number.MAX_VALUE;
+       const value1 = 5;
+       const result = helpers.getCircleArea(value1);
+       expect(result).toEqual(Math.PI * value1 * value1);
+       expect(result).toBeLessThanOrEqual(max);
+     })
+
+     it('should take the first value passed as prism width, second value passed as prism height and third value passed as prism length, returning rectangular prism volume', () => {
+       const max = Number.MAX_VALUE;
+       const value1 = 5;
+       const value2 = 2;
+       const value3 = 7;
+       const result = helpers.getRectangularPrismVolume(value1, value2, value3);
+       expect(result).toEqual(value1 * value2 * value3);
+       expect(result).toBeLessThanOrEqual(max);
+     })
+
 })
