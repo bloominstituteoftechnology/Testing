014760fe33f599bfbad9927f0274c8439a048971
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-09-17 17:45:57 -0500

    Working on Project 2
    Just started finished the first two getBiggest and greeting

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,21 @@
-const funcs = require('./project-2');
+const helpers = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('find the higher number', () => {
+    test('Should return the higher number', () => {
+      expect(helpers.getBiggest(2, 9)).toEqual(9);
+      expect(helpers.getBiggest(10, 1)).toEqual(10);
+      expect(helpers.getBiggest(-10, 100)).toEqual(100);
+    })
+    test('Should return false if either value is undefined', () => {
+      expect(helpers.getBiggest(undefined)).toEqual(false)
+    })
+  })
+
+  describe('greeting', () => {
+      test('should return a greeting', () => {
+          expect(helpers.greeting("German")).toEqual("Guten Tag!"); 
+          expect(helpers.greeting("Spanish")).toEqual("Hola!"); 
+          
+      })
+  })
