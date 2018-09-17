0ee15bd9b854a6eb94dc6ca0cb3f47cd9fd42126
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-17 18:08:37 -0500

    working on MVP
    just finished isInteger alot of this is very repetitive.

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -19,3 +19,29 @@ describe('find the higher number', () => {
           
       })
   })
+
+  describe("is ten of five", () => {
+      test("should true of number is ten or five", () => {
+          expect(helpers.isTenOrFive(5)).toEqual(true); 
+          expect(helpers.isTenOrFive(10)).toEqual(true); 
+          expect(helpers.isTenOrFive(3)).toEqual(false); 
+          expect(helpers.isTenOrFive(undefined)).toEqual(false); 
+      })
+  })
+
+  describe("is i range", () => {
+      test("should return true if in range", () => {
+          expect(helpers.isInRange(30)).toEqual(true); 
+          expect(helpers.isInRange(65)).toEqual(false); 
+          expect(helpers.isInRange(10)).toEqual(false); 
+          expect(helpers.isInRange(undefined)).toEqual(false);
+      })
+  })
+
+  describe("is an integer", () => {
+      test("should return true if input is integer", () => {
+          expect(helpers.isInteger(1)).toEqual(true); 
+          expect(helpers.isInteger(3.4)).toEqual(false); 
+          expect(helpers.isInteger(undefined)).toEqual(false); 
+      })
+  })
