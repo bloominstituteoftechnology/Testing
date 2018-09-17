b956e7475676f44a0f72ccae55fb4027e87b1425
kamalnrf <kamalnrf@hotmail.com>
2018-09-18 03:49:01 +0530

    Added tests for array

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -11,7 +11,62 @@ describe('Arrays', () => {
   describe('map', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      expect(typeof map).toBe('function');
     });
+
+    it ('should add number by itself', () => {
+      expect(arrayFunctions.map([1,2,3], num => num + num)).toEqual([2,4,6])
+    })
+  });
+
+  describe('each', () => {
+    it('should be a function', () => {
+      const map = arrayFunctions.each;
+      expect(typeof map).toBe('function');
+    });
+  });
+
+  describe('reduce', () => {
+    it('should be a function', () => {
+      const map = arrayFunctions.reduce;
+      expect(typeof map).toBe('function');
+    });
+
+    it ('should add number by itself', () => {
+      expect(arrayFunctions.map([1,2,3], num => num + num)).toEqual([2,4,6])
+    })
+  });
+
+  describe('find', () => {
+    it('should be a function', () => {
+      const map = arrayFunctions.find;
+      expect(typeof map).toBe('function');
+    });
+
+    it ('should find number', () => {
+      expect(arrayFunctions.map([1,2,3], num => num === '1')).toEqual([false, false, false])
+    })
+  });
+
+  describe('filter', () => {
+    it('should be a function', () => {
+      const map = arrayFunctions.filter;
+      expect(typeof map).toBe('function');
+    });
+``
+    it ('should filter number', () => {
+      expect(arrayFunctions.map([1,2,3], num => num === 1)).toEqual([true, false, false])
+    })
+  });
+
+  describe('flatten', () => {
+    it('should be a function', () => {
+      const map = arrayFunctions.flatten;
+      expect(typeof map).toBe('function');
+    });
+``
+    it ('should flatten number', () => {
+      expect(arrayFunctions.map([1,2,3], num => num + num)).toEqual([2, 4, 6])
+    })
   });
 });
