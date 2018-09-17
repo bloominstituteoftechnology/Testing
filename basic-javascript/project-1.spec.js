1f4ebcae39aca47a00e1211dac616c1104b4aa60
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-17 13:02:41 -0600

    Initial commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,17 @@
 const helpers = require('./project-1');
 
-// start testing!
+// Tests for multiply by ten
+
+//check if test passes for positive number, 
+//check if test passes for negative number, 
+
+it('should multiply the number by 10', () => {
+    expect(helpers.multiplyByTen(2)).toBe(20);
+    expect(helpers.multiplyByTen(10)).toBe(100); 
+})
+
+it('should allow for negative parameters outputting negative results', () => {
+    expect(helpers.multiplyByTen(-3)).toBe(-30); 
+    expect(helpers.multiplyByTen(-100)).toBe(-1000); 
+}); 
+
