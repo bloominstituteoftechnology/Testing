5c591c520dfd27cc4ccfc14eba38a155858203ee
skennedy93 <sean47900@gmail.com>
2018-09-17 14:34:55 -0500

    initial commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,8 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+it('should multiply the number by 10', () => {
+    expect(helpers.multiplyByTen(2)).toBe(20);
+    expect(helpers.multiplyByTen(10)).toBe(100); 
+})
