ad5628188b2d8f91e2be9645d6b807690cb73420
Lauren <llwor94@gmail.com>
2018-09-17 13:34:51 -0700

    all complete

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,7 @@
 const funcs = require('./project-2');
 
+test('multiplyByTen function', () => {
+  expect(helpers.getBiggest(1)).toBe(10);
+  expect(helpers.getBiggest(-1)).toBe(-10);
+});
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
