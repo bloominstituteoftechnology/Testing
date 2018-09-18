ac020ed9afc0cf457b805aab634acc2b9c379b6f
Erik Kimsey <erik@erikkimsey.com>
2018-09-18 16:07:02 -0400

    Created GameItem class. Reading and re-reading the instructions; each read is a different understanding of the instructions.

---
+++ enhance-item-game/game.js
@@ -0,0 +1,32 @@
+// https://github.com/LambdaSchool/Testing/pull/241
+
+let item = 0,
+    fails = 0,
+    durability = 100;
+
+class GameItem {
+  constructor(){
+    super();
+    this.fails = 0;
+    this.durability = 100;
+
+  }
+
+  enhance(){
+    // if succeed, 
+  }
+
+  succeedOrFail(){
+    // return bool
+  }
+
+  fail(){
+
+  }
+
+  resetFailCount(){
+    
+  }
+
+
+}
