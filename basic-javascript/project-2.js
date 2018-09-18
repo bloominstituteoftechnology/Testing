43e40ac39ddc96f9d966f1a079bba7ce65d90e30
codejoncode <jonathanjholloway@gmail.com>
2018-09-17 21:32:38 -0500

    Created Navigation Project-2. Also created helper function isArray to check if the argument is an array it returns false if it is not

--- basic-javascript/project-2.js
+++ basic-javascript/project-2.js
@@ -10,10 +10,10 @@ const getBiggest = (x, y) => {
 };
 
 const greeting = language => {
-  switch (language) {
-    case 'German':
+  switch (language.toLowerCase()) {
+    case 'german':
       return 'Guten Tag!';
-    case 'Spanish':
+    case 'spanish':
       return 'Hola!';
     default:
       return 'Hello!';
@@ -140,14 +140,100 @@ const averageTestScore = testScores => {
 };
 
 const largestNumber = numbers => {
-  let biggestInteger = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > biggestInteger) {
-      biggestInteger = numbers[i];
+  let biggestInteger = 0; 
+   const arr = isArray(numbers)
+   if(arr){
+     for (let i = 0; i< arr.length; i++){
+       if(arr[i] > biggestInteger){
+         biggestInteger = arr[i]; 
+       }
+     }
+     return biggestInteger; 
+   } else {
+     return false; // not an array 
+   }
+};
+
+function isArray (arr){
+  if(Array.isArray(arr) === true ){
+    return arr;
+  } else {
+    return false; 
+  }
+}
+
+function isNumber(...args){
+  for(let i = 0; i<args.length; i++){
+    if(typeof args[i] === 'string'){
+      //Perform a check if it can be converted to Number.
+      test = Number(args[i])
+      if(test){
+        args[i] = test
+      }else {
+        args[i] = 0; 
+      }
+    } else if (args[i] === undefined){
+      args[i] = 0; 
     }
   }
-  return biggestInteger;
-};
+  return args; 
+}
+
+function anyZeros (...args) {
+  for(let i = 0; i < args.length; i++){
+    if(args[i] === 0){
+      return 0; 
+    }
+  }
+  return args; 
+ }
+
+ function checkArray (ar1, ar2){
+  for (let i = 0; i < ar1.length; i++) {
+    if (Array.isArray(ar1[i]) && Array.isArray(ar2[i])) {
+      if (ar1[i].length === ar2[i].length) {
+        const anotherCheck = checkArray(ar1[i], ar2[i])
+        if (!anotherCheck) {
+          return false;
+        }
+      // if length is not the same no need to check. 
+      } else {
+        return false;
+       }
+    //first if 
+    } else if (typeof ar1[i] === 'object' && typeof ar2[i] === 'object' && !(ar1[i] === null && ar2[i] === null)) {
+        const checkAgain = isObject(ar1[i], ar2[i]);
+        if (!checkAgain) {
+            return false;
+        }
+    } else if (ar1[i] !== ar2[i]) {
+        return false;
+    }
+} // end of for loop. 
+return true;
+}
+
+function isObject (obj1, obj2) {
+  if (Array.isArray(obj1 && Array.isArray(obj2))) {
+    if (obj1.length === obj2.length) {
+      const anotherCheck = checkArray(obj1, obj2)
+      if (!anotherCheck) {
+        return false;
+      }
+    } else {
+       //length doesn't match don't do nothing
+       return false;
+      }
+  }
+  if (Object.keys(obj1).length === Object.keys(obj2).length) {
+    for (let key in obj1) {
+      if (typeof obj1[key] === "object") {
+            if (!isObject(obj1[key], obj2[key])) return false;
+        } else if (obj1[key] !== obj2[key]) return false;
+    }
+  } else return false;
+  return true;   
+}
 
 module.exports = {
   getBiggest,
