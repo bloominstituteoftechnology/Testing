de86afb0cd0d3f6254326a5154d7fb8003a872aa
William <kwontaewoongah@gmail.com>
2018-09-18 02:07:39 -0700

    finished project-2

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -26,3 +26,59 @@ it('should return true for an integer else return false',()=>{
     expect(funcs.isInteger(9)).toBe(true);
     expect(funcs.isInteger(10)).toBe(true);
 })
+it('should return a string or the given num that is passed in depending on parameter met or unmet',()=>{
+    expect(funcs.fizzBuzz(15)).toBe('fizzbuzz');
+    expect(funcs.fizzBuzz(5)).toBe('buzz');
+    expect(funcs.fizzBuzz(3)).toBe('fizz');
+    expect(funcs.fizzBuzz(1)).toBe(1);
+})
+it('should return a bool depending on whether or not a number is prime',()=>{
+    expect(funcs.isPrime(-1)).toBe(false);
+    expect(funcs.isPrime(1)).toBe(false);
+    expect(funcs.isPrime(2)).toBe(true)
+})
+it('should return first item of an array',()=>{
+    expect(funcs.returnFirst([1,2,3])).toBe(1);
+    expect(funcs.returnFirst(['string',2])).toBe('string');
+    expect(funcs.returnFirst(['word','dog',true])).toBe('word');
+})
+it('should return last item of an array',()=>{
+    expect(funcs.returnLast([1,2,3])).toBe(3);
+    expect(funcs.returnLast([])).toBe(undefined);
+    expect(funcs.returnLast(['word',2,true])).toBe(true);
+})
+it('should return length of an array',()=>{
+    expect(funcs.getArrayLength([1,2,3])).toBe(3);
+    expect(funcs.getArrayLength([])).toBe(0);
+    expect(funcs.getArrayLength([1,2])).toBe(2);
+})
+it('should increment each item of an array by 1',()=>{
+    expect(funcs.incrementByOne([1,2,3])).toEqual([2,3,4]);
+    expect(funcs.incrementByOne(['a',2,4])).toEqual([NaN,3,5]);
+    expect(funcs.incrementByOne([3,5,7])).toEqual([4,6,8]);
+})
+it('should add an item to array at end',()=>{
+    expect(funcs.addItemToArray([1,2,3],4)).toEqual([1,2,3,4]);
+})
+it('should add an item to front of array',()=>{
+    expect(funcs.addItemToFront([1,2,3],4)).toEqual([4,1,2,3]);
+})
+it('should concatenate an array of words to a sentence',()=>{
+    expect(funcs.wordsToSentence(['Hello','world!'])).toBe('Hello world!');
+})
+it('should return a bool on whether an array contains an item or not.',()=>{
+    expect(funcs.contains([1,2,3],3)).toBe(true);
+    expect(funcs.contains([1,2,3],4)).toBe(false);
+})
+it('should return the total of all of the numbers of an array added together',()=>{
+    expect(funcs.addNumbers([1,2,3])).toBe(6);
+    expect(funcs.addNumbers([2,5,8])).toBe(15);
+})
+it('should return the average of test scores given an array of test scores',()=>{
+    expect(funcs.averageTestScore([22,88,99])).toBeCloseTo(69.67);
+    expect(funcs.averageTestScore([80,90,100])).toBeCloseTo(90);
+})
+it('should return the largest number given an array of integers',()=>{
+    expect(funcs.largestNumber([1,3,4])).toBe(4);
+    expect(funcs.largestNumber([99,100,1000])).toBe(1000);
+})
