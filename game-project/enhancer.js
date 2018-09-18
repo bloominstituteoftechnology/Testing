a09ff1157ee789cd1fb8bc24b95bd28cbacb5446
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-18 16:26:57 -0500

    added a folder for the item enhancer project
    started building out the enhancer functions and tests

---
+++ game-project/enhancer.js
@@ -0,0 +1,31 @@
+module.exports = {
+    enhance,
+    fail,
+
+}
+
+function enhance(item){
+    return {...item, level: ++item.level}
+}
+
+function fail(item){
+    if (item.level < 16) {
+        return {...item, failCount: ++item.failCount};
+    };
+    // {...item, failCount: (
+    //     if (item.level < 16) {
+    //         return ++item.failCount;
+    // })}
+}
+
+
+// -ITEM
+//   -fail count(no max, starts at 0)
+//   -duribility(max 100, starts at 100)
+//   -enhanceLevel(max 20, 1-15[arabic], 16-20[roman])
+//   -attempt (either adds to fail count or enhance)
+//     -enhance
+            // -if fail(fail count increases(according to enhanceLevel), durability decreases by 5)
+//          -if suceeds(failcount === 0)
+//     -repair
+            // -(durability+10)
---
+++ game-project/enhancer.spec.js
@@ -0,0 +1,47 @@
+const enhancer = require('./enhancer.js');
+
+describe('actions', () => {
+    describe('enhance', () => {
+        it('should increase level by one', () => {
+            const item = {
+                level: 0, 
+            }
+    
+            const actual = enhancer.enhance(item); //should be 1
+    
+            expect(actual.level).toBe(1);
+            expect(enhancer.enhance(actual).level).toBe(2);
+            expect(enhancer.enhance(actual).level).toBe(3);
+        })
+    })
+
+    describe('fail', () => {
+        it('should return fail +1 if level is < 16', () => {
+            const item = {
+                level: 0,
+                failCount: 0
+            }
+
+            const actual = enhancer.fail(item);
+
+            expect(actual.failCount).toBe(1);
+        })
+    })
+})
+
+
+
+
+
+
+// -ITEM
+//   -fail count(no max, starts at 0)
+//   -duribility(max 100, starts at 100)
+//   -Level(max 20, 1-15[arabic], 16-20[roman])
+//   
+
+//     -enhance(attempt) (either adds to fail count or enhance)
+//          -if fail(fail count increases(according to enhanceLevel), durability decreases by 5)
+//          -if suceeds(failcount === 0)
+//     -repair
+            // -(durability+10)
