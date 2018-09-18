e80b3dd876eabefe8d80cf201afeae70810971c8
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-18 16:28:50 -0500

    Made new js file
    Started a Js file that will hold alot of the necessary game data need for the testing

---
+++ testing 2/enhancer-game.js
@@ -0,0 +1,17 @@
+
+
+let item = 0, 
+    fails = 0, 
+    durability = 100,
+    repair = 0; 
+    
+
+class gameItem{
+    constructor(){
+        super(); 
+        this.fails = 0; 
+        this.durability = 100;
+    }
+}
+
+
