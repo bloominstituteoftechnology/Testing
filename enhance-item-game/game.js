3c330a8293c8457d7df6d3c6e8f2fc08ec04d274
Erik Kimsey <erik@erikkimsey.com>
2018-09-18 16:40:00 -0400

    Building out getters, setters, and other methods of GameItem class

--- enhance-item-game/game.js
+++ enhance-item-game/game.js
@@ -9,24 +9,68 @@ class GameItem {
     super();
     this.fails = 0;
     this.durability = 100;
+    this.repairs = 0;
+  }
+
+
+  repair() {
 
   }
 
-  enhance(){
-    // if succeed, 
+  getRepairs(){
+    return this.repairs;
   }
 
-  succeedOrFail(){
-    // return bool
+  incrRepairs(){
+    this.repairs += 1;
+  }
+
+  enhanceOrFail(){
+    // if enhance, reset fails to zero
+    // if fail, decrement durability
   }
 
   fail(){
+    let fails = this.getFails();
+    if(fails < 16){
+      this.incrFail(1);
+    } else if (fails === 16) {
+      this.incrFail(2);
+    } else if (fails === 17) {
+      this.incrFail(3);
+    } else if (fails === 18) {
+      this.incrFail(4);
+    } else if (fails === 19) {
+      this.incrFail(5);
+    } else if (fails === 20) {
+      console.log('GAME OVER!');
+      return;
+    }
+  }
 
+  incrFail(fc){
+    this.fails += fc;
+  }
+
+  decrDurability(){
+    this.durability -= 5;
+  }
+
+  incrDurability() {
+    let dur = this.getDurability();
+    if(dur <= 90 ) this.durability += 10;
+  }
+
+  getDurability(){
+    return this.durability;
   }
 
   resetFailCount(){
-    
+    this.fails = 0;
   }
 
+  getFails(){
+    return this.fails;
+  }
 
 }
