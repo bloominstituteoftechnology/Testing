87e7e3e3f7cb9cae9fb88f20844a5e4477fc22ed
Ashwin <aswinjeganath@gmail.com>
2018-09-19 04:50:26 +0530

    Write specs for objects.js

--- advanced-javascript/objects.js
+++ advanced-javascript/objects.js
@@ -3,25 +3,26 @@
 
 /* eslint-disable no-unused-vars, arrow-body-style, arrow-parens */
 
-const keys = (obj) => {
+const keys = obj => {
   return Object.keys(obj);
 };
 
-const values = (obj) => {
-  return Object.keys(obj).map((key) => {
+const values = obj => {
+  return Object.keys(obj).map(key => {
     return obj[key];
   });
 };
 
 const mapObject = (obj, cb) => {
-  Object.keys(obj).forEach((key) => (obj[key] = cb(obj[key])));
-  return obj;
+  const newObj = {};
+  Object.keys(obj).forEach(key => (newObj[key] = cb(obj[key])));
+  return newObj;
 };
 
-const pairs = (obj) => Object.keys(obj).map((key) => [key, obj[key]]);
+const pairs = obj => Object.keys(obj).map(key => [key, obj[key]]);
 
-const invert = (obj) => {
-  Object.keys(obj).forEach((key) => {
+const invert = obj => {
+  Object.keys(obj).forEach(key => {
     const newKey = obj[key];
     obj[newKey] = key;
     delete obj[key];
@@ -30,7 +31,7 @@ const invert = (obj) => {
 };
 
 const defaults = (obj, defaultProps) => {
-  Object.keys(defaultProps).forEach((key) => {
+  Object.keys(defaultProps).forEach(key => {
     if (Object.prototype.hasOwnProperty.call(obj, key)) return;
     obj[key] = defaultProps[key];
   });
@@ -45,5 +46,5 @@ module.exports = {
   mapObject,
   pairs,
   invert,
-  defaults
+  defaults,
 };
--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,63 @@
-const objectFunctions = require('./objects');
+const of = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
+  describe('keys', () => {
+    it('returns all the enumerable keys in the object', () => {
+      const obj = { a: 1, b: 2 };
+      const keys = of.keys(obj);
+
+      expect(keys.constructor).toBe(Array);
+      expect(keys).toEqual(['a', 'b']);
+    });
+  });
+
+  describe('values', () => {
+    it('returns all values in an array', () => {
+      const obj = { a: 1, b: 2 };
+      const values = of.values(obj);
+      expect(values.constructor).toBe(Array);
+      expect(values).toEqual([1, 2]);
+    });
+  });
+
+  describe('mapObject', () => {
+    it('maps values using the callback', () => {
+      const obj = { a: 1, b: 2 };
+      const cb = v => v * 2;
+      const mapped = of.mapObject(obj, cb);
+      expect(mapped).toEqual({ a: 2, b: 4 });
+    });
+
+    it('does not mutate original object', () => {
+      const obj = { a: 1, b: 2 };
+      const cb = v => v * 2;
+      const mapped = of.mapObject(obj, cb);
+      expect(obj).not.toBe(mapped);
+    });
+  });
+
+  describe('pairs', () => {
+    it('returns an array of entries', () => {
+      const obj = { a: 1, b: 2 };
+      const entries = of.pairs(obj);
+      expect(entries).toEqual([['a', 1], ['b', 2]]);
+    });
+  });
+
+  describe('invert', () => {
+    it('inverts keys and values', () => {
+      const obj = { a: 1, b: 2 };
+      of.invert(obj);
+      expect(obj).toEqual({ 1: 'a', 2: 'b' });
+    });
+  });
+
+  describe('defaults', () => {
+    it("sets values if it isn't set already", () => {
+      const obj = { a: 1 };
+      of.defaults(obj, { b: 2, a: 3 });
+      expect(obj).toEqual({ a: 1, b: 2 });
+    });
+  });
 });
