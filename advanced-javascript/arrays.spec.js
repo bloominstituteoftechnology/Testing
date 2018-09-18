4c8ac1e88bc590046d9f7d2d986aa606838f7f03
johnoro <officialjohnorourke@gmail.com>
2018-09-18 18:06:10 -0400

    Completed layout for tests for advanced JS folder (arrays and objects).

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -5,13 +5,112 @@ const arrayFunctions = require('./arrays');
 // first step is to make sure all these assertions work.
 // then make sure you have at least 2-3 more assertions for every function.
 // hint 1. - you need to pass data to the functions and get expected output.
-// hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
+// hint 2. - you should test the data type being called back, 
+//   and perform some sort of operation on the data.
 
-describe('Arrays', () => {
+describe.skip('arrays', () => {
+  describe('all array functions', () => {
+    it('should be functions', () => {
+      for (let func in arrayFunctions) {
+        expect(typeof arrayFunctions[func]).toBe('function');
+      }
+    });
+  });
+
   describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('function');
+    it('should return mapped arr', () => {
+      expect(arrayFunctions
+        .map([1, 2, 3], el => {
+          return el + 1;
+        })
+      ).toEqual([ 2, 3, 4 ]);
+    });
+
+    it('should return a new array', () => {
+      let arr = [1, 2, 3];
+      expect(arrayFunctions
+        .map(arr, el => {
+          return el + 1;
+        })
+      ).not.toBe(arr);
+    });
+  });
+
+  describe('reduce', () => {
+    it('should return reduced val', () => {
+      expect(arrayFunctions
+        .reduce([1, 2, 3], (acc, el) => {
+          return acc + el;
+        }, 0)
+      ).toBe(6);
+    });
+
+    it('should return a new array', () => {
+      let arr = [1, 2, 3];
+      expect(arrayFunctions
+        .reduce(arr, (acc, el) => {
+          return acc + el;
+        }, 0)
+      ).not.toBe(arr);
+    });
+  });
+
+  describe('find', () => {
+    it('should return found val', () => {
+      expect(arrayFunctions
+        .find([1, 2, 3], el => {
+          return el % 2 === 0;
+        })
+      ).toBe(2);
+    });
+
+    it('should undefined if not found', () => {
+      expect(arrayFunctions
+        .find([1, 2, 3], el => {
+          return el === 0;
+        }, 0)
+      ).toBe(undefined);
+    });
+  });
+
+  describe('filter', () => {
+    it('should return filtered vals', () => {
+      expect(arrayFunctions
+        .filter([1, 2, 3], el => {
+          return el % 2 === 0;
+        })
+      ).toEqual([ 2 ]);
+
+      expect(arrayFunctions
+        .filter([1, 2, 3], el => {
+          return el % 2 !== 0;
+        })
+      ).toEqual([ 1, 3 ]);
+    });
+
+    it('should an empty arr if no elements met condition', () => {
+      expect(arrayFunctions
+        .filter([1, 2, 3], el => {
+          return el === 0;
+        }, 0)
+      ).toEqual([]);
+    });
+  });
+
+  describe('flatten', () => {
+    it('should return a flattened array', () => {
+      expect(arrayFunctions.flatten([1, 2, 3, [ 4, 5 ]]))
+        .toEqual([ 1, 2, 3, 4, 5 ]);
+
+      expect(arrayFunctions.flatten([1, [2], 3, [ 4, 5, [ 6, 7 ] ], 8]))
+        .toEqual([ 1, 2, 3, 4, 5, 6, 7, 8 ]);
+
+      expect(arrayFunctions.flatten([1, 2, 3]))
+        .toEqual([ 1, 2, 3 ]);
+    });
+
+    it('should an empty arr if no elements met condition', () => {
+      expect(arrayFunctions.flatten([], 0)).toEqual([]);
     });
   });
 });
--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,56 @@
-const objectFunctions = require('./objects');
+const objFuncs = require('./objects');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
 describe('objects', () => {
-  it('should be an object', () => {});
+  describe('object functions', () => {
+    it('should be a function', () => {
+      for (let func in objFuncs) {
+        expect(typeof objFuncs[func]).toBe('function');
+      }
+    });
+  });
+
+  describe('keys', () => {
+    it('should return the keys of an object', () => {
+      expect(objFuncs.keys({ a: 1, b: 2, c: 3 }))
+        .toEqual([ 'a', 'b', 'c' ]);
+    });
+  });
+
+  describe('values', () => {
+    it('should return the values of an object\'s keys', () => {
+      expect(objFuncs.values({ a: 1, b: 2, c: 3 }))
+        .toEqual([ 1, 2, 3 ]);
+    });
+  });
+
+  describe('mapObject', () => {
+    it('should return the mapped object', () => {
+      expect(objFuncs
+        .mapObject({ a: 1, b: 2, c: 3 }, (i) => {
+          return i + 1;
+        }))
+        .toEqual({ a: 2, b: 3, c: 4 });
+    });
+  });
+
+  describe('pairs', () => {
+    it('should return the key-value pairs of an object', () => {
+      expect(objFuncs.pairs({ a: 1, b: 2, c: 3 }))
+        .toEqual([ ['a', 1], ['b', 2], ['c', 3] ]);
+    });
+  });
+
+  describe('invert', () => {
+    it('should invert the keys and values of an object', () => {
+      expect(objFuncs.invert({ a: 1, b: 2, c: 3 }))
+        .toEqual({ '1': 'a', '2': 'b', '3': 'c' });
+    });
+  });
+
+  describe('defaults', () => {
+    it('should return object with the given defaults', () => {
+      expect(objFuncs.defaults({ a: 1, b: 2, c: 3 }, { c: 22, d: 5 }))
+        .toEqual({ a: 1, b: 2, c: 3, d: 5 });
+    });
+  });
 });
