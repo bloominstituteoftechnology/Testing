d127eaf1f96b68ef0e29a9e28bc85de1c00807b1
Max <maxkajiwara@gmail.com>
2018-10-22 16:04:39 -0700

    Get started

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -1,141 +1,182 @@
 /* eslint-disable arrow-parens */
 
 const multiplyByTen = num => {
-  return num * 10;
+	if (!num) {
+		return 0;
+	}
+
+	if (typeof num !== 'number') {
+		throw new Error('value must be a number');
+	}
+
+	return num * 10;
 };
 
 const subtractFive = num => {
-  return num - 5;
+	if (!num) {
+		return -5;
+	}
+
+	if (typeof num !== 'number') {
+		throw new Error('value must be a number');
+	}
+	return num - 5;
 };
 
 const areSameLength = (str1, str2) => {
-  return str1.length === str2.length;
+	if (typeof str1 !== 'string' || typeof str2 !== 'string') {
+		throw new Error('values must be strings');
+	}
+
+	return str1.length === str2.length;
 };
 
 const areEqual = (x, y) => {
-  return x === y;
+	return x === y;
 };
 
 const lessThanNinety = num => {
-  if (num < 90) {
-    return true;
-  }
-  return false;
+	if (!num) {
+		return true;
+	}
+
+	if (typeof num !== 'number') {
+		throw new Error('value must be a number');
+	}
+
+	if (num < 90) {
+		return true;
+	}
+	return false;
 };
 
 const greaterThanFifty = num => {
-  if (num > 50) {
-    return true;
-  }
-  return false;
+	if (!num) {
+		return false;
+	}
+
+	if (typeof num !== 'number') {
+		throw new Error('value must be a number');
+	}
+
+	if (num > 50) {
+		return true;
+	}
+	return false;
 };
 
 const add = (x, y) => {
-  return x + y;
+	if (typeof x !== 'number' || typeof y !== 'number') {
+		throw new Error('values must be numbers');
+	}
+	return x + y;
 };
 
 const subtract = (x, y) => {
-  return x - y;
+	if (typeof x !== 'number' || typeof y !== 'number') {
+		throw new Error('values must be numbers');
+	}
+	return x - y;
 };
 
 const divide = (x, y) => {
-  return x / y;
+	return x / y;
 };
 
 const multiply = (x, y) => {
-  return x * y;
+	return x * y;
 };
 
 const getRemainder = (x, y) => {
-  return x % y;
+	return x % y;
 };
 
 const isEven = num => {
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
+	if (num % 2 === 0) {
+		return true;
+	}
+	return false;
 };
 
 const isOdd = num => {
-  if (num % 2 === 0) {
-    return false;
-  }
-  return true;
+	if (num % 2 === 0) {
+		return false;
+	}
+	return true;
 };
 
 const square = num => {
-  return num * num;
+	return num * num;
 };
 
 const cube = num => {
-  return num * num * num;
+	return num * num * num;
 };
 
 const raiseToPower = (num, exponent) => {
-  return num ** exponent;
+	return num ** exponent;
 };
 
 const roundNumber = num => {
-  return Math.round(num);
+	return Math.round(num);
 };
 
 const roundUp = num => {
-  return Math.ceil(num);
+	return Math.ceil(num);
 };
 
 const addExclamationPoint = str => {
-  return (str += '!');
+	return (str += '!');
 };
 
 const combineNames = (firstName, lastName) => {
-  return `${firstName} ${lastName}`;
+	return `${firstName} ${lastName}`;
 };
 
 const getGreeting = name => {
-  return `Hello ${name}!`;
+	return `Hello ${name}!`;
 };
 
 const getRectangleArea = (length, width) => {
-  return length * width;
+	return length * width;
 };
 
 const getTriangleArea = (base, height) => {
-  return 0.5 * base * height;
+	return 0.5 * base * height;
 };
 
 const getCircleArea = radius => {
-  return Math.PI * radius * radius;
+	return Math.PI * radius * radius;
 };
 
 const getRectangularPrismVolume = (length, width, height) => {
-  return width * height * length;
+	return width * height * length;
 };
 
 module.exports = {
-  multiplyByTen,
-  subtractFive,
-  areSameLength,
-  areEqual,
-  lessThanNinety,
-  greaterThanFifty,
-  add,
-  subtract,
-  divide,
-  multiply,
-  getRemainder,
-  isEven,
-  isOdd,
-  square,
-  cube,
-  raiseToPower,
-  roundNumber,
-  roundUp,
-  addExclamationPoint,
-  combineNames,
-  getGreeting,
-  getRectangleArea,
-  getTriangleArea,
-  getCircleArea,
-  getRectangularPrismVolume
+	multiplyByTen,
+	subtractFive,
+	areSameLength,
+	areEqual,
+	lessThanNinety,
+	greaterThanFifty,
+	add,
+	subtract,
+	divide,
+	multiply,
+	getRemainder,
+	isEven,
+	isOdd,
+	square,
+	cube,
+	raiseToPower,
+	roundNumber,
+	roundUp,
+	addExclamationPoint,
+	combineNames,
+	getGreeting,
+	getRectangleArea,
+	getTriangleArea,
+	getCircleArea,
+	getRectangularPrismVolume
 };
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,207 @@
 const helpers = require('./project-1');
 
-// start testing!
+describe('helpers.js', () => {
+	describe('multiplyByTen()', () => {
+		it('should multiply provided number by ten', () => {
+			const expected = 100;
+
+			const actual = helpers.multiplyByTen(10);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return 0 when called with no value', () => {
+			const product = helpers.multiplyByTen();
+
+			expect(product).toBe(0);
+		});
+
+		it('should throw an error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.multiplyByTen('2');
+			}).toThrow();
+		});
+	});
+
+	describe('subtractFive()', () => {
+		it('should subtract five from the provided number', () => {
+			const expected = 4;
+
+			const actual = helpers.subtractFive(9);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return -5 when called with no value', () => {
+			const product = helpers.subtractFive();
+
+			expect(product).toBe(-5);
+		});
+
+		it('should throw error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.subtractFive('2');
+			}).toThrow();
+		});
+	});
+
+	describe('areSameLength()', () => {
+		it('should return true when called with two strings that are the same length', () => {
+			const expected = true;
+
+			const actual = helpers.areSameLength('string', 'cheese');
+
+			expect(actual).toEqual(expected);
+		});
+
+		it("should throw error when called with any values that aren't strings", () => {
+			expect(() => {
+				helpers.areSameLength('string', ['string']);
+			}).toThrow();
+		});
+	});
+
+	describe('areEqual()', () => {
+		it('should return true when called with two values that are equal', () => {
+			const expected = true;
+
+			const actual = helpers.areEqual('string', 'string');
+
+			expect(actual).toEqual(expected);
+		});
+
+		it("should return false when called with two values that aren't equal", () => {
+			const expected = false;
+
+			const actual = helpers.areEqual(23, 56);
+
+			expect(actual).toEqual(expected);
+		});
+	});
+
+	describe('lessThanNinety()', () => {
+		it('should return true when called with a value that is less than 90', () => {
+			const expected = true;
+
+			const actual = helpers.lessThanNinety(89);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return false when called with a value that is equal to 90', () => {
+			const expected = false;
+
+			const actual = helpers.lessThanNinety(90);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return false when called with a value that is greater than 90', () => {
+			const expected = false;
+
+			const actual = helpers.lessThanNinety(91);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return true when called with no value', () => {
+			const product = helpers.lessThanNinety();
+
+			expect(product).toBe(true);
+		});
+
+		it('should throw error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.lessThanNinety('90');
+			}).toThrow();
+		});
+	});
+
+	describe('greaterThanFifty()', () => {
+		it('should return true when called with a value that is greater than 50', () => {
+			const expected = true;
+
+			const actual = helpers.greaterThanFifty(51);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return false when called with a value that is equal to 50', () => {
+			const expected = false;
+
+			const actual = helpers.greaterThanFifty(50);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return false when called with a value that is less than 50', () => {
+			const expected = false;
+
+			const actual = helpers.greaterThanFifty(49);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should return false when called with no value', () => {
+			const product = helpers.greaterThanFifty();
+
+			expect(product).toBe(false);
+		});
+
+		it('should throw error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.greaterThanFifty('50');
+			}).toThrow();
+		});
+	});
+
+	describe('add()', () => {
+		it('should return the sum of the values', () => {
+			const expected = 7;
+
+			const actual = helpers.add(3, 4);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should throw error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.add(1, '2');
+			}).toThrow();
+		});
+	});
+
+	describe('subtract()', () => {
+		it('should return the difference of the values', () => {
+			const expected = -1;
+
+			const actual = helpers.subtract(3, 4);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should throw error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.subtract(1, '2');
+			}).toThrow();
+		});
+	});
+
+	// NO MORE TYPEOF CHECKS FROM HERE ONWARD
+
+	describe('add()', () => {
+		it('should return the sum of the values', () => {
+			const expected = 7;
+
+			const actual = helpers.add(3, 4);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should throw error when called with anything other than a number', () => {
+			expect(() => {
+				helpers.add(1, '2');
+			}).toThrow();
+		});
+	});
+});
