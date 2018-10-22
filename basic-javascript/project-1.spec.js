049c6059f1d130b60567d7063b2411633b376f0f
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-22 14:59:20 -0400

    tests for functions multiply through getRectangularPrismVolume

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -84,3 +84,115 @@ it("should divide the first value by the second", () => {
   const actual = helpers.divide(100, 2);
   expect(actual).toEqual(expected);
 });
+
+it("should give the product of two numbers", () => {
+  const expected = 200;
+
+  const actual = helpers.multiply(100, 2);
+  expect(actual).toEqual(expected);
+});
+
+it("should get the remainder of the first value divided by the second", () => {
+  const expected = 1;
+
+  const actual = helpers.getRemainder(13, 2);
+  expect(actual).toEqual(expected);
+});
+
+it("should return true if value is even", () => {
+  const expected = true;
+
+  const actual = helpers.isEven(2);
+  expect(actual).toEqual(expected);
+});
+
+it("should return true if value is odd", () => {
+  const expected = true;
+
+  const actual = helpers.isOdd(3);
+  expect(actual).toEqual(expected);
+});
+
+it("should return a value squared", () => {
+  const expected = 36;
+
+  const actual = helpers.square(6);
+  expect(actual).toEqual(expected);
+});
+
+it("should return a value cubed", () => {
+  const expected = 27;
+
+  const actual = helpers.cube(3);
+  expect(actual).toEqual(expected);
+});
+
+it("should return a value raised to an exponent", () => {
+  const expected = 9;
+
+  const actual = helpers.raiseToPower(3, 2);
+  expect(actual).toEqual(expected);
+});
+
+it("should return a value rounded to the nearest whole number", () => {
+  const expected = 10;
+
+  const actual = helpers.roundNumber(10.4);
+  expect(actual).toEqual(expected);
+});
+
+it("should return a value rounded up to the nearest whole number", () => {
+  const expected = 11;
+
+  const actual = helpers.roundUp(10.4);
+  expect(actual).toEqual(expected);
+});
+
+it("should return a string with a ! at the end", () => {
+  const expected = "hey!";
+
+  const actual = helpers.addExclamationPoint("hey");
+  expect(actual).toEqual(expected);
+});
+
+it("should return two strings concatenated", () => {
+  const expected = "banana fruit";
+
+  const actual = helpers.combineNames("banana", "fruit");
+  expect(actual).toEqual(expected);
+});
+
+it("should return a greeting to the variable passed in", () => {
+  const expected = "Hello you!";
+
+  const actual = helpers.getGreeting("you");
+  expect(actual).toEqual(expected);
+});
+
+it("should return the area of a rectangle that has the specified length and width", () => {
+  const expected = 30;
+
+  const actual = helpers.getRectangleArea(5, 6);
+  expect(actual).toEqual(expected);
+});
+
+it("should return the area of a triangle that has the specified base and height", () => {
+  const expected = 15;
+
+  const actual = helpers.getTriangleArea(5, 6);
+  expect(actual).toEqual(expected);
+});
+
+it("should return the area of a circle that has the specified radius", () => {
+  const expected = 50.26548245743669;
+
+  const actual = helpers.getCircleArea(4);
+  expect(actual).toEqual(expected);
+});
+
+it("should return the volume of a retangular prism that has the specified length, width and height", () => {
+  const expected = 27;
+
+  const actual = helpers.getRectangularPrismVolume(3, 3, 3);
+  expect(actual).toEqual(expected);
+});
