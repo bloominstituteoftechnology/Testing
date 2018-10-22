702d7f1d71a0f555625fdd4c7cbcb5fbbff21092
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-22 13:51:10 -0700

    finished mvp, didnt commit a lot got to sidetracked with testing

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -1,141 +1,150 @@
 /* eslint-disable arrow-parens */
 
 const multiplyByTen = num => {
-  return num * 10;
+	return num * 10;
 };
 
 const subtractFive = num => {
-  return num - 5;
+	return num - 5;
 };
 
 const areSameLength = (str1, str2) => {
-  return str1.length === str2.length;
+	return str1.length === str2.length;
 };
 
 const areEqual = (x, y) => {
-  return x === y;
+	return x === y;
 };
 
 const lessThanNinety = num => {
-  if (num < 90) {
-    return true;
-  }
-  return false;
+	if (num < 90) {
+		return true;
+	}
+	return false;
 };
 
 const greaterThanFifty = num => {
-  if (num > 50) {
-    return true;
-  }
-  return false;
+	if (num > 50) {
+		return true;
+	}
+	return false;
 };
 
 const add = (x, y) => {
-  return x + y;
+	return x + y;
 };
 
 const subtract = (x, y) => {
-  return x - y;
+	return x - y;
 };
 
 const divide = (x, y) => {
-  return x / y;
+	return x / y;
 };
 
 const multiply = (x, y) => {
-  return x * y;
+	return x * y;
 };
 
 const getRemainder = (x, y) => {
-  return x % y;
+	return x % y;
 };
 
 const isEven = num => {
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
+	if (num % 2 === 0) {
+		return true;
+	}
+	return false;
 };
 
 const isOdd = num => {
-  if (num % 2 === 0) {
-    return false;
-  }
-  return true;
+	if (num % 2 === 0) {
+		return false;
+	}
+	return true;
 };
 
 const square = num => {
-  return num * num;
+	return num * num;
 };
 
 const cube = num => {
-  return num * num * num;
+	return num * num * num;
 };
 
 const raiseToPower = (num, exponent) => {
-  return num ** exponent;
+	return num ** exponent;
 };
 
 const roundNumber = num => {
-  return Math.round(num);
+	return Math.round(num);
 };
 
 const roundUp = num => {
-  return Math.ceil(num);
+	return Math.ceil(num);
 };
 
 const addExclamationPoint = str => {
-  return (str += '!');
+	let trimmedString = str.trim();
+	return (trimmedString += "!");
 };
 
 const combineNames = (firstName, lastName) => {
-  return `${firstName} ${lastName}`;
+	let trimFirstName = firstName.trim();
+	let trimLastName = lastName.trim();
+	return `${trimFirstName} ${trimLastName}`;
 };
 
 const getGreeting = name => {
-  return `Hello ${name}!`;
+	let trimName = name.trim();
+	return `Hello ${trimName}!`;
 };
 
 const getRectangleArea = (length, width) => {
-  return length * width;
+	if (isNaN(length)) {
+		return false;
+	} else if (isNaN(width)) {
+		return false;
+	}
+	return length * width;
 };
 
 const getTriangleArea = (base, height) => {
-  return 0.5 * base * height;
+	return 0.5 * base * height;
 };
 
 const getCircleArea = radius => {
-  return Math.PI * radius * radius;
+	return Math.PI * radius * radius;
 };
 
 const getRectangularPrismVolume = (length, width, height) => {
-  return width * height * length;
+	return width * height * length;
 };
 
 module.exports = {
-  multiplyByTen,
-  subtractFive,
-  areSameLength,
-  areEqual,
-  lessThanNinety,
-  greaterThanFifty,
-  add,
-  subtract,
-  divide,
-  multiply,
-  getRemainder,
-  isEven,
-  isOdd,
-  square,
-  cube,
-  raiseToPower,
-  roundNumber,
-  roundUp,
-  addExclamationPoint,
-  combineNames,
-  getGreeting,
-  getRectangleArea,
-  getTriangleArea,
-  getCircleArea,
-  getRectangularPrismVolume
+	multiplyByTen,
+	subtractFive,
+	areSameLength,
+	areEqual,
+	lessThanNinety,
+	greaterThanFifty,
+	add,
+	subtract,
+	divide,
+	multiply,
+	getRemainder,
+	isEven,
+	isOdd,
+	square,
+	cube,
+	raiseToPower,
+	roundNumber,
+	roundUp,
+	addExclamationPoint,
+	combineNames,
+	getGreeting,
+	getRectangleArea,
+	getTriangleArea,
+	getCircleArea,
+	getRectangularPrismVolume
 };
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,124 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
 
 // start testing!
+
+test("should not fail", () => {});
+test("should multiply provided number by ten", () => {
+	const expected = 100;
+	const actual = helpers.multiplyByTen(10);
+	expect(actual).toEqual(expected);
+});
+
+test("should subract 5", () => {
+	const expected = 5;
+	const actual = helpers.subtractFive(10);
+
+	expect(actual).toEqual(expected);
+});
+test("should be same length", () => {
+	const expected = true;
+	const actual = helpers.areSameLength("dog", "boy");
+	expect(actual).toEqual(expected);
+});
+test("should have deep equality", () => {
+	const expected = true;
+	const actual = helpers.areEqual(7, 7);
+	expect(actual).toEqual(expected);
+});
+test("should be less than 90", () => {
+	const expected = true;
+	const actual = helpers.lessThanNinety(56);
+	expect(actual).toEqual(expected);
+});
+test("should be more than 90", () => {
+	const expected = false;
+	const actual = helpers.lessThanNinety(96);
+	expect(actual).toEqual(expected);
+});
+test("should be greater than 50", () => {
+	const expected = true;
+	const actual = helpers.greaterThanFifty(56);
+	expect(actual).toEqual(expected);
+});
+test("should be less than 50", () => {
+	const expected = false;
+	const actual = helpers.greaterThanFifty(11);
+	expect(actual).toEqual(expected);
+});
+test("should add two numbers", () => {
+	const expected = 33;
+	const actual = helpers.add(11, 22);
+	expect(actual).toEqual(expected);
+});
+test("should subtract two numbers", () => {
+	const expected = -11;
+	const actual = helpers.subtract(11, 22);
+	expect(actual).toEqual(expected);
+});
+test("should divide two numbers", () => {
+	const expected = 2;
+	const actual = helpers.divide(50, 25);
+	expect(actual).toEqual(expected);
+});
+test("should multiply two numbers", () => {
+	const expected = 100;
+	const actual = helpers.multiply(50, 2);
+	expect(actual).toEqual(expected);
+});
+test("should give the remainder of two numbers", () => {
+	const expected = 1;
+	const actual = helpers.getRemainder(3, 2);
+	expect(actual).toEqual(expected);
+});
+test("should check if a number is even", () => {
+	const actual = helpers.isEven(6);
+	expect(actual).toBe(true);
+});
+test("should check if a number is odd", () => {
+	const actual = helpers.isOdd(7);
+	expect(actual).toBe(true);
+});
+test("should square a number", () => {
+	const actual = helpers.square(3);
+	expect(actual).toBe(9);
+});
+test("should cube a number", () => {
+	const actual = helpers.cube(2);
+	expect(actual).toBe(8);
+});
+test("should round a number", () => {
+	const actual = helpers.roundNumber(3.3);
+	expect(actual).toBe(3);
+});
+test("should round a number up", () => {
+	const actual = helpers.roundUp(3.3);
+	expect(actual).toBe(4);
+});
+test("should add an exclamation point", () => {
+	expect(helpers.addExclamationPoint("  I like pie  ")).toBe("I like pie!");
+	expect(helpers.addExclamationPoint("I like pie")).toBe("I like pie!");
+});
+test("should combine a first and last name", () => {
+	expect(helpers.combineNames("Patrick", "Thompson")).toBe("Patrick Thompson");
+	expect(helpers.combineNames(" Patrick ", " Thompson ")).toBe(
+		"Patrick Thompson"
+	);
+});
+test("should give a greeting from name", () => {
+	expect(helpers.getGreeting("Patrick")).toBe("Hello Patrick!");
+	expect(helpers.getGreeting(" Patrick ")).toBe("Hello Patrick!");
+});
+test("should give area of a rectangle", () => {
+	expect(helpers.getRectangleArea(3, 5)).toBe(15);
+	expect(helpers.getRectangleArea("3", 5)).toBe(15);
+});
+test("should give area of a triangle", () => {
+	expect(helpers.getTriangleArea(4, 4)).toBe(8);
+	expect(helpers.getTriangleArea("4", 4)).toBe(8);
+});
+test("should give area of a circle", () => {
+	expect(helpers.getCircleArea(1)).toBe(3.141592653589793);
+});
+test("should give the volume of a rectangular prism", () => {
+	expect(helpers.getRectangularPrismVolume(2, 2, 2)).toBe(8);
+});
