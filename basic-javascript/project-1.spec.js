2379582096764a6c4c75e37bcf8d34b50264305b
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-22 14:59:21 -0700

    Working on project-1.spec tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,100 @@
 const helpers = require('./project-1');
 
-// start testing!
+describe('helpers.js', () => {
+    describe('jest', () => {
+        test('should run tests', () => {
+            //test everything
+        });
+        
+        it('should run tests using it() in addition to test()', () =>{
+            // Arrange : set up the world
+            // Act : execute the code you're testing
+            // Assert : check that it works
+        });
+    });
+    
+    describe('multiplyByTen()', () => {
+        it('should multiply provided number by 10', () => {
+            const expected = 100;
+        
+            const actual = helpers.multiplyByTen(10);
+        
+            expect(actual).toEqual(expected);
+        });
+        
+        it('should return 0 when called with no value', () => {
+            const product = helpers.multiplyByTen();
+        
+            expect(product).toBe(0);
+        });
+
+        it('should throw error when called with a string', () => {
+            expect(() => { helpers.multiplyByTen('five') }).toThrow();
+        });
+    });
+
+    describe('subtractFive', () => {
+        it('should subtract 5 from provided number', () => {
+            const expected = 5;
+
+            const actual = helpers.subtractFive(10);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return 0 when called with no value', () => {
+            const product = helpers.multiplyByTen();
+        
+            expect(product).toBe(0);
+        });
+        
+        it('should throw error when called with a string', () => {
+            expect(() => { helpers.multiplyByTen('five') }).toThrow();
+        });
+    });
+    
+    describe('areSameLength', () => {
+        it('should return false if strings have different lengths', () => {
+            const expected = false;
+
+            const actual = helpers.areSameLength('hello', 'hey');
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return true if strings have equal lengths', () => {
+            const expected = true;
+
+            const actual = helpers.areSameLength('hello', 'phone');
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('areEqual', () => {
+        it('should return false if numbers are not equal', () => {
+            const expected = false;
+
+            const actual = helpers.areEqual(5, 6);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return true if numbers are equal', () => {
+            const expected = true;
+
+            const actual = helpers.areEqual(5, 5);
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    
+    
+    // it ('should check deep equality', () => {
+    //     const a = { foo: 'bar' };
+    //     const b = {foo: 'bar' };
+    
+    //     expect(a).toEqual(b);
+    // });
+});
