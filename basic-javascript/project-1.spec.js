cbf602156bdefd0c37863aef9f5a483e6601b235
jmbradley <jmradley808@gmail.com>
2018-10-22 17:15:34 -0400

    testing project1 through equality

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,34 @@
 const helpers = require('./project-1');
 
 // start testing!
+it('should mult by ten', () => {
+    const expected = 100;
+
+    const actual = helpers.multiplyByTen(10);
+
+    expect(actual).toEqual(expected);
+});
+
+it('should sub by five', () => {
+    const expected = 10;
+
+    const actual = helpers.subtractFive(15);
+
+    expect(actual).toEqual(expected);
+});
+
+it('should compare string lgth', () => {
+    const expected = true;
+
+    const actual = helpers.areSameLength('node', 'smal');
+
+    expect(actual).toEqual(expected);
+});
+
+it('compare values for equality', () => {
+    const expected = true;
+
+    const actual = helpers.areEqual(8, 8);
+
+    expect(actual).toEqual(expected);
+});
