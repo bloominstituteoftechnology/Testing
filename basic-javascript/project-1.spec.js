0d239d0f9d85f32458f4b250414a14b0a3cc8b2e
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-22 14:12:53 -0400

    Completed test for functions lessThanNinety through divide

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -45,3 +45,42 @@ it("should check if values are the equal", () => {
   const actual = helpers.areEqual("bear", "bear");
   expect(actual).toEqual(expected);
 });
+
+//Are the values less than 90
+it("should check if values are less than 90", () => {
+  const expected = true;
+
+  const actual = helpers.lessThanNinety(80);
+  expect(actual).toEqual(expected);
+});
+
+//Are the values greater than 50
+it("should check if values are greater than 50", () => {
+  const expected = true;
+
+  const actual = helpers.greaterThanFifty(80);
+  expect(actual).toEqual(expected);
+});
+
+//Add two values
+it("should give the sum of two numbers", () => {
+  const expected = 180;
+
+  const actual = helpers.add(80, 100);
+  expect(actual).toEqual(expected);
+});
+
+//Subtract the second value from first
+it("should subtract the second value from the first", () => {
+  const expected = 100;
+
+  const actual = helpers.subtract(180, 80);
+  expect(actual).toEqual(expected);
+});
+
+it("should divide the first value by the second", () => {
+  const expected = 50;
+
+  const actual = helpers.divide(100, 2);
+  expect(actual).toEqual(expected);
+});
