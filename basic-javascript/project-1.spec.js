671036131b2734f28597fed1133f02a1e1092762
Kevin Nguyen <kevinn858@gmail.com>
2018-10-22 15:49:23 -0700

    wrote tests for project-1 up to a true test from areSameLength()

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,45 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1.js', () => {
+    describe('multiplyByTen()', () => {
+        it('Should multiply the number by 10.', () => {
+            const actual = helpers.multiplyByTen(4);
+            const expected = 40;
+            expect(actual).toEqual(expected);
+        });
+        it('Should throw error when called with no value.', () => {
+            const actual = () => helpers.multiplyByTen();
+            expect(actual).toThrow();
+        });
+        it('Should throw error when called with a string', () => {
+            const actual = () => helpers.multiplyByTen('25');
+            expect(actual).toThrow();
+        });
+    });
+    describe('subtractFive()', () => {
+        it('Should subtract the number by 5.', () => {
+            const actual = helpers.subtractFive(4);
+            const expected = -1;
+            expect(actual).toEqual(expected);
+        });
+        it('Should throw error when called with no value.', () => {
+            const actual = () => helpers.multiplyByTen();
+            expect(actual).toThrow();
+        });
+        it('Should throw error when called with a string', () => {
+            const actual = () => helpers.multiplyByTen('25');
+            expect(actual).toThrow();
+        });
+    });
+    describe('areSameLength()', () =>{
+        const apple = 'apple';
+        const guava = 'guava';
+        const pear = 'pear';
+        it('Should return true if called with 2 strings of same length', () => {
+            const actual = helpers.areSameLength(apple, guava);
+            const expected = true;
+            expect(actual).toBe(expected);
+        });
+    });
+});
