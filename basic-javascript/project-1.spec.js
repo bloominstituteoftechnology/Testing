9e737e7c2265e1fe6203fae503a9a90d7348fee0
Andrew <ghobrial.andrew@gmail.com>
2018-10-22 17:06:50 -0400

    tests written up to areSameLength

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,28 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+describe('project-1.js', () => {
+ 
+  // multiplyByTen
+  test("should provide correct value when multiplying by 10", () => {
+    const expected = 50;
+    const actual = helpers.multiplyByTen(5);
+    expect(actual).toEqual(expected);
+  });
+
+  // subtractFive
+  test("should provide correct value when subtracting by 5", () => {
+    const expected = 20;
+    const actual = helpers.subtractFive(25);
+    expect(actual).toEqual(expected);
+  });
+
+  // areSameLength
+  test("should return true when both inputs are in fact the same length", () => {
+    const expected = true;
+    const actual = helpers.areSameLength("car", "and");
+    expect(actual).toEqual(expected);
+  });
+
+}); 
