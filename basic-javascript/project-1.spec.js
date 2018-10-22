c82c1772521a12ba6c2f59f4280ef7cdf03abf5e
Nicocchi <nicocchilover@gmail.com>
2018-10-22 15:45:34 -0400

    multiply and subtract added

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,24 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1.js', () => {
+   describe('multiplyByTen', () => {
+       it('should multiply provided number by ten', () => {
+           const expected = 100;
+
+           const actual = helpers.multiplyByTen(10);
+
+           expect(actual).toEqual(expected)
+       });
+   });
+
+    describe('subtractFive', () => {
+        it('should subtract a number by five', () => {
+            const expected = 5;
+
+            const actual = helpers.subtractFive(10);
+
+            expect(actual).toEqual(expected)
+        });
+    });
+});
