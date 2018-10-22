803f11df70da24da69b7d7f29d8d55a4a98047bd
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-22 16:19:44 -0400

    initial commit, basic multiplyByTen() test run with incorrect and correct expected values

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,10 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+test('should multiply provided number by 10', () => {
+  const expected = 50;
+  const actual = helpers.multiplyByTen(5);
+
+  expect(actual).toEqual(expected);
+})
