48cb495adaa3c81ce442d15a3530c041355f7e27
Vera Simon <ghjkl>
2018-10-22 11:59:34 -0700

    Testing - project-1.spec.js
    Added some commenting in project-1.spec.js and project-2.spec.js to make things easier to follow as I write tests

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -1,141 +1,141 @@
 /* eslint-disable arrow-parens */
 
-const multiplyByTen = num => {
-  return num * 10;
+const multiplyByTen = (num) => {
+	return num * 10;
 };
 
-const subtractFive = num => {
-  return num - 5;
+const subtractFive = (num) => {
+	return num - 5;
 };
 
 const areSameLength = (str1, str2) => {
-  return str1.length === str2.length;
+	return str1.length === str2.length;
 };
 
 const areEqual = (x, y) => {
-  return x === y;
+	return x === y;
 };
 
-const lessThanNinety = num => {
-  if (num < 90) {
-    return true;
-  }
-  return false;
+const lessThanNinety = (num) => {
+	if (num < 90) {
+		return true;
+	}
+	return false;
 };
 
-const greaterThanFifty = num => {
-  if (num > 50) {
-    return true;
-  }
-  return false;
+const greaterThanFifty = (num) => {
+	if (num > 50) {
+		return true;
+	}
+	return false;
 };
 
 const add = (x, y) => {
-  return x + y;
+	return x + y;
 };
 
 const subtract = (x, y) => {
-  return x - y;
+	return x - y;
 };
 
 const divide = (x, y) => {
-  return x / y;
+	return x / y;
 };
 
 const multiply = (x, y) => {
-  return x * y;
+	return x * y;
 };
 
 const getRemainder = (x, y) => {
-  return x % y;
+	return x % y;
 };
 
-const isEven = num => {
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
+const isEven = (num) => {
+	if (num % 2 === 0) {
+		return true;
+	}
+	return false;
 };
 
-const isOdd = num => {
-  if (num % 2 === 0) {
-    return false;
-  }
-  return true;
+const isOdd = (num) => {
+	if (num % 2 === 0) {
+		return false;
+	}
+	return true;
 };
 
-const square = num => {
-  return num * num;
+const square = (num) => {
+	return num * num;
 };
 
-const cube = num => {
-  return num * num * num;
+const cube = (num) => {
+	return num * num * num;
 };
 
 const raiseToPower = (num, exponent) => {
-  return num ** exponent;
+	return num ** exponent;
 };
 
-const roundNumber = num => {
-  return Math.round(num);
+const roundNumber = (num) => {
+	return Math.round(num);
 };
 
-const roundUp = num => {
-  return Math.ceil(num);
+const roundUp = (num) => {
+	return Math.ceil(num);
 };
 
-const addExclamationPoint = str => {
-  return (str += '!');
+const addExclamationPoint = (str) => {
+	return (str += '!');
 };
 
 const combineNames = (firstName, lastName) => {
-  return `${firstName} ${lastName}`;
+	return `${firstName} ${lastName}`;
 };
 
-const getGreeting = name => {
-  return `Hello ${name}!`;
+const getGreeting = (name) => {
+	return `Hello ${name}!`;
 };
 
 const getRectangleArea = (length, width) => {
-  return length * width;
+	return length * width;
 };
 
 const getTriangleArea = (base, height) => {
-  return 0.5 * base * height;
+	return 0.5 * base * height;
 };
 
-const getCircleArea = radius => {
-  return Math.PI * radius * radius;
+const getCircleArea = (radius) => {
+	return Math.PI * radius * radius;
 };
 
 const getRectangularPrismVolume = (length, width, height) => {
-  return width * height * length;
+	return width * height * length;
 };
 
 module.exports = {
-  multiplyByTen,
-  subtractFive,
-  areSameLength,
-  areEqual,
-  lessThanNinety,
-  greaterThanFifty,
-  add,
-  subtract,
-  divide,
-  multiply,
-  getRemainder,
-  isEven,
-  isOdd,
-  square,
-  cube,
-  raiseToPower,
-  roundNumber,
-  roundUp,
-  addExclamationPoint,
-  combineNames,
-  getGreeting,
-  getRectangleArea,
-  getTriangleArea,
-  getCircleArea,
-  getRectangularPrismVolume
+	multiplyByTen,
+	subtractFive,
+	areSameLength,
+	areEqual,
+	lessThanNinety,
+	greaterThanFifty,
+	add,
+	subtract,
+	divide,
+	multiply,
+	getRemainder,
+	isEven,
+	isOdd,
+	square,
+	cube,
+	raiseToPower,
+	roundNumber,
+	roundUp,
+	addExclamationPoint,
+	combineNames,
+	getGreeting,
+	getRectangleArea,
+	getTriangleArea,
+	getCircleArea,
+	getRectangularPrismVolume
 };
