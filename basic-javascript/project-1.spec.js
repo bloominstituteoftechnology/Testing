11eee540f03c6c590cbde88c5a65270069d7ba8c
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-22 13:43:16 -0600

    Clone/Setup Repo + First Test
    Clone repository and install dependencies. Implement first test which
    checks the return value of the multiplyByTen function in project-1.js.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,20 @@
 const helpers = require('./project-1');
 
-// start testing!
+// Some things to test:
+// 
+// Type Handling (string, number, boolean, etc)
+
+
+/// --- Project One Tests ---
+describe('project-1.js', () => {
+    // -- Multiply by 10 --
+    describe('Multiply by 10', () => {
+
+        it('Should return the provided input multiplied by 10', () => {
+            let expected = 900;
+            expect(helpers.multiplyByTen(90)).toBe(expected);
+        })
+    })
+
+
+})
