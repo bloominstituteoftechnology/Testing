10ca64237f5e45e53165143982cbfcc382c831e8
Anthony Greb <agreb255@gmail.com>
2018-10-23 18:45:38 -0400

    still working on proj1

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -16,3 +16,75 @@ describe("subtractFive()", () => {
     expect(actual).toEqual(expected);
   });
 });
+
+describe("areSameLength()", () => {
+  it("should make sure the length of two objects are the same", () => {
+    const expected = true;
+    const actual = helpers.areSameLength("someObj", "someObj");
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("lessThanNinety()", () => {
+  it("should check that the value is less than ninety", () => {
+    const expected = true;
+    const actual = helpers.lessThanNinety(50);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("greaterThanFifty()", () => {
+  it("should check that the value is greater than fifty", () => {
+    const expected = true;
+    const actual = helpers.greaterThanFifty(55);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("add()", () => {
+  it("should add two given numbers and return a sum", () => {
+    const expected = 12;
+    const actual = helpers.add(5, 7);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("subtract()", () => {
+  it("should subtract two given numbers and return a sum", () => {
+    const expected = 10;
+    const actual = helpers.subtract(15, 5);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("divide()", () => {
+  it("should divide two numbers and return a sum", () => {
+    const expected = 10;
+    const actual = helpers.divide(20, 2);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("multiply()", () => {
+  it("should multiply two numbers and return a sum", () => {
+    const expected = 15;
+    const actual = helpers.multiply(3, 5);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("getRemainder()", () => {
+  it("should divide two numbers and return the remainder", () => {
+    const expected = 1;
+    const actual = helpers.getRemainder(6, 5);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("isEven()", () => {
+  it("should determine whether a number is even or not", () => {
+    const expected = true;
+    const actual = helpers.isEven(6);
+    expect(actual).toEqual(expected);
+  });
+});
