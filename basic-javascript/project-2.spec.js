494ddbc9c79e36bd88c121b73d5aa07d08fa1921
Vera Simon <ghjkl>
2018-10-23 12:58:25 -0700

    Testing - project-2.spec.js
    Set up the "describe" for all the functions

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,151 +1,189 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-// const getBiggest = (x, y) => {
-// 	if (x > y) {
-// 		return x;
-// 	} else if (x === y) {
-// 		return y;
-// 	}
-// 	return y;
-// };
+describe.only('project-2.js', () => {
+	describe('', () => {
+		// const getBiggest = (x, y) => {
+		// 	if (x > y) {
+		// 		return x;
+		// 	} else if (x === y) {
+		// 		return y;
+		// 	}
+		// 	return y;
+		// };
+	});
 
-// const greeting = (language) => {
-// 	switch (language) {
-// 		case 'German':
-// 			return 'Guten Tag!';
-// 		case 'Spanish':
-// 			return 'Hola!';
-// 		default:
-// 			return 'Hello!';
-// 	}
-// };
+	describe('greeting()', () => {
+		// const greeting = (language) => {
+		// 	switch (language) {
+		// 		case 'German':
+		// 			return 'Guten Tag!';
+		// 		case 'Spanish':
+		// 			return 'Hola!';
+		// 		default:
+		// 			return 'Hello!';
+		// 	}
+		// };
+	});
 
-// const isTenOrFive = (num) => {
-// 	if (num === 10 || num === 5) {
-// 		return true;
-// 	}
-// 	return false;
-// };
+	describe('isTenOrFive()', () => {
+		// const isTenOrFive = (num) => {
+		// 	if (num === 10 || num === 5) {
+		// 		return true;
+		// 	}
+		// 	return false;
+		// };
+	});
 
-// const isInRange = (num) => {
-// 	if (num < 50 && num > 20) {
-// 		return true;
-// 	}
-// 	return false;
-// };
+	describe('isInRange()', () => {
+		// const isInRange = (num) => {
+		// 	if (num < 50 && num > 20) {
+		// 		return true;
+		// 	}
+		// 	return false;
+		// };
+	});
 
-// const isInteger = (num) => {
-// 	if (Math.floor(num) === num) {
-// 		return true;
-// 	}
-// 	return false;
-// };
+	describe('isInteger()', () => {
+		// const isInteger = (num) => {
+		// 	if (Math.floor(num) === num) {
+		// 		return true;
+		// 	}
+		// 	return false;
+		// };
+	});
 
-// const fizzBuzz = (num) => {
-// 	if (num % 5 === 0 && num % 3 === 0) {
-// 		return 'fizzbuzz';
-// 	} else if (num % 5 === 0) {
-// 		return 'buzz';
-// 	} else if (num % 3 === 0) {
-// 		return 'fizz';
-// 	}
-// 	return num;
-// };
+	describe('fizzBuzz()', () => {
+		// const fizzBuzz = (num) => {
+		// 	if (num % 5 === 0 && num % 3 === 0) {
+		// 		return 'fizzbuzz';
+		// 	} else if (num % 5 === 0) {
+		// 		return 'buzz';
+		// 	} else if (num % 3 === 0) {
+		// 		return 'fizz';
+		// 	}
+		// 	return num;
+		// };
+	});
 
-// const isPrime = (num) => {
-// 	if (num < 0) {
-// 		return false;
-// 	}
-// 	if (num === 1 || num === 0) {
-// 		return false;
-// 	}
-// 	for (let i = 2; i < num; i++) {
-// 		if (num % i === 0) {
-// 			return false;
-// 		}
-// 	}
-// 	return true;
-// };
+	describe('isPrime()', () => {
+		// const isPrime = (num) => {
+		// 	if (num < 0) {
+		// 		return false;
+		// 	}
+		// 	if (num === 1 || num === 0) {
+		// 		return false;
+		// 	}
+		// 	for (let i = 2; i < num; i++) {
+		// 		if (num % i === 0) {
+		// 			return false;
+		// 		}
+		// 	}
+		// 	return true;
+		// };
+	});
 
-// const returnFirst = (arr) => {
-// 	return arr[0];
-// };
+	describe('returnFirst()', () => {
+		// const returnFirst = (arr) => {
+		// 	return arr[0];
+		// };
+	});
 
-// const returnLast = (arr) => {
-// 	return arr[arr.length - 1];
-// };
+	describe('returnLast()', () => {
+		// const returnLast = (arr) => {
+		// 	return arr[arr.length - 1];
+		// };
+	});
 
-// const getArrayLength = (arr) => {
-// 	return arr.length;
-// };
+	describe('getArrayLength()', () => {
+		// const getArrayLength = (arr) => {
+		// 	return arr.length;
+		// };
+	});
 
-// const incrementByOne = (arr) => {
-// 	for (let i = 0; i < arr.length; i++) {
-// 		arr[i]++;
-// 	}
-// 	return arr;
-// };
+	describe('incrementByOne()', () => {
+		// const incrementByOne = (arr) => {
+		// 	for (let i = 0; i < arr.length; i++) {
+		// 		arr[i]++;
+		// 	}
+		// 	return arr;
+		// };
+	});
 
-// const addItemToArray = (arr, item) => {
-// 	arr.push(item);
-// 	return arr;
-// };
+	describe('addItemToArray()', () => {
+		// const addItemToArray = (arr, item) => {
+		// 	arr.push(item);
+		// 	return arr;
+		// };
+	});
 
-// const addItemToFront = (arr, item) => {
-// 	arr.unshift(item);
-// 	return arr;
-// };
+	describe('addItemToFront()', () => {
+		// const addItemToFront = (arr, item) => {
+		// 	arr.unshift(item);
+		// 	return arr;
+		// };
+	});
 
-// const wordsToSentence = (words) => {
-// 	let newSentence = '';
-// 	for (let i = 0; i < words.length; i++) {
-// 		if (i === 0) {
-// 			newSentence += `${words[i]}`;
-// 		} else {
-// 			newSentence += ` ${words[i]}`;
-// 		}
-// 	}
-// 	return newSentence;
-// };
+	describe('wordsToSentence()', () => {
+		// const wordsToSentence = (words) => {
+		// 	let newSentence = '';
+		// 	for (let i = 0; i < words.length; i++) {
+		// 		if (i === 0) {
+		// 			newSentence += `${words[i]}`;
+		// 		} else {
+		// 			newSentence += ` ${words[i]}`;
+		// 		}
+		// 	}
+		// 	return newSentence;
+		// };
+	});
 
-// const contains = (arr, item) => {
-// 	let itemCounter = 0;
-// 	for (let i = 0; i < arr.length; i++) {
-// 		if (arr[i] === item) {
-// 			itemCounter++;
-// 		}
-// 	}
-// 	if (itemCounter > 0) {
-// 		return true;
-// 	}
-// 	return false;
-// };
+	describe('contains()', () => {
+		// const contains = (arr, item) => {
+		// 	let itemCounter = 0;
+		// 	for (let i = 0; i < arr.length; i++) {
+		// 		if (arr[i] === item) {
+		// 			itemCounter++;
+		// 		}
+		// 	}
+		// 	if (itemCounter > 0) {
+		// 		return true;
+		// 	}
+		// 	return false;
+		// };
+	});
 
-// const addNumbers = (numbers) => {
-// 	let sumOfNumbers = 0;
-// 	for (let i = 0; i < numbers.length; i++) {
-// 		sumOfNumbers += numbers[i];
-// 	}
-// 	return sumOfNumbers;
-// };
+	describe('addNumbers()', () => {
+		// const addNumbers = (numbers) => {
+		// 	let sumOfNumbers = 0;
+		// 	for (let i = 0; i < numbers.length; i++) {
+		// 		sumOfNumbers += numbers[i];
+		// 	}
+		// 	return sumOfNumbers;
+		// };
+	});
 
-// const averageTestScore = (testScores) => {
-// 	let totalSumScores = 0;
-// 	let numberOfScore = 0;
-// 	for (let i = 0; i < testScores.length; i++) {
-// 		totalSumScores += testScores[i];
-// 		numberOfScore++;
-// 	}
-// 	return totalSumScores / numberOfScore;
-// };
+	describe('averageTestScore()', () => {
+		// const averageTestScore = (testScores) => {
+		// 	let totalSumScores = 0;
+		// 	let numberOfScore = 0;
+		// 	for (let i = 0; i < testScores.length; i++) {
+		// 		totalSumScores += testScores[i];
+		// 		numberOfScore++;
+		// 	}
+		// 	return totalSumScores / numberOfScore;
+		// };
+	});
 
-// const largestNumber = (numbers) => {
-// 	let biggestInteger = 0;
-// 	for (let i = 0; i < numbers.length; i++) {
-// 		if (numbers[i] > biggestInteger) {
-// 			biggestInteger = numbers[i];
-// 		}
-// 	}
-// 	return biggestInteger;
-// };
+	describe('largestNumber()', () => {
+		// const largestNumber = (numbers) => {
+		// 	let biggestInteger = 0;
+		// 	for (let i = 0; i < numbers.length; i++) {
+		// 		if (numbers[i] > biggestInteger) {
+		// 			biggestInteger = numbers[i];
+		// 		}
+		// 	}
+		// 	return biggestInteger;
+		// };
+	});
+});
