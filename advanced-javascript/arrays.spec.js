1d44a304ced423c41d652a54b8a4edb8a6a6163a
Luisan <luisangelsc@gmail.com>
2018-10-23 18:34:17 -0500

    missing -array flatten and find, and -object defaultprops

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -7,11 +7,66 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
-describe('Arrays', () => {
+describe.only('Arrays', () => {
+  const array = [1,2,3,4,5]
+  const callback = (element) => {return element += 1}
+  const reducer = (accum, value) => {return accum + value}
+  const filtered = item => { return item > 3 ? item : null }
+
   describe('map', () => {
+    const map = arrayFunctions.map;
     it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      expect(typeof map).toBe('function');
+    });
+
+    it('should return a modified array', () => {
+      expect(map(array, callback)).toEqual([2,3,4,5,6])
     });
   });
+
+  describe('reduce', () => {
+    const reduce = arrayFunctions.reduce;
+    it('should be a function', () => {
+      expect(typeof reduce).toBe('function');
+    });
+
+    it('should return a reduced array', () => {
+      expect(reduce(array, reducer)).toBe(15)
+    });
+  });
+
+  describe('find', () => {
+    const find = arrayFunctions.find;
+
+    it('should be a function', () => {
+      expect(typeof find).toBe('function');
+    });
+
+    // it('should return an element if it is in the array', () => {
+    //   expect(find(array, reducer)).toBe(15)
+    // });
+  });
+
+  describe('filter', () => {
+    const filter = arrayFunctions.filter;
+    it('should be a function', () => {
+      expect(typeof filter).toBe('function');
+    });
+
+    it('should return an element if it is in the array', () => {
+      expect(filter(array, filtered)).toEqual([4,5])
+    });
+  });
+
+  describe('flatten', () => {
+    const flatten = arrayFunctions.flatten;
+    it('should be a function', () => {
+      expect(typeof flatten).toBe('function');
+    });
+
+    it('should return a flattened array', () => {
+      expect(flatten(array)).toEqual(['12345'])
+    });
+  });
+
 });
