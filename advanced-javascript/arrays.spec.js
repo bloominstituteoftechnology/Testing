61df68ced5b66632b0ae79e4f1941cf62c8d6192
Benny Oseguera <oseguerabenny27@gmail.com>
2018-10-23 15:29:23 -0700

    FInished Day 2 - Benny Oseguera

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('./arrays');
+const arrayFunctions = require("./arrays");
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -7,11 +7,95 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
-describe('Arrays', () => {
-  describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+describe("Arrays", () => {
+  // describe('map', () => {
+  //   it('should be a function', () => {
+  //     const map = arrayFunctions.map;
+  //     expect(typeof map).toBe('object');
+  //   });
+  // });
+
+  describe("each", () => {
+    test("maps array of [3,4,5,6] * 2 and returns [6,8,10,12]", () => {
+      expect(
+        arrayFunctions.map([3, 4, 5, 6], num => {
+          return num * 2;
+        })
+      ).toEqual([6, 8, 10, 12]);
+    });
+    test("tests type of each returns function", () => {
+      expect(typeof arrayFunctions.each).toBe("function");
+    });
+  });
+
+  describe("map", () => {
+    test("maps array of [3,4,5,6] * 3 and returns [9,12,15,18]", () => {
+      expect(
+        arrayFunctions.map([3, 4, 5, 6], num => {
+          return num * 3;
+        })
+      ).toEqual([9, 12, 15, 18]);
+    });
+    test("tests type of map returns function", () => {
+      expect(typeof arrayFunctions.map).toBe("function");
+    });
+  });
+
+  describe("reduce", () => {
+    test("tests if reduce [3, 3] x * y returns [9]", () => {
+      expect(
+        arrayFunctions.reduce([3, 3], (x, y) => {
+          return x * y;
+        })
+      ).toEqual(9);
+    });
+    test("tests type of reduce returns function", () => {
+      expect(typeof arrayFunctions.reduce).toBe("function");
+    });
+  });
+
+  describe("find", () => {
+    test("tests if find [1, 2, 3, 4, 5, 6] where x = 2 returns 2", () => {
+      expect(
+        arrayFunctions.find([1, 2, 3, 4, 5, 6], x => {
+          if (x === 2) {
+            return x;
+          }
+        })
+      ).toBe(2);
+    });
+    test("tests type of find returns function", () => {
+      expect(typeof arrayFunctions.find).toBe("function");
+    });
+  });
+
+  describe("filter", () => {
+    test("test filter on [1, 2, 2, 3, 3, 4, 5, 6] where x = 2 returns [2, 2]", () => {
+      expect(
+        arrayFunctions.filter([1, 2, 2, 3, 3, 4, 5, 6], x => {
+          if (x === 2) {
+            return x;
+          }
+        })
+      ).toEqual([2, 2]);
+    });
+    test("tests type of filter returns function", () => {
+      expect(typeof arrayFunctions.filter).toBe("function");
+    });
+  });
+
+  describe("flatten", () => {
+    test("tests if flatten of [1, 4, 3, [4 , 4]] returns [1, 4, 3, 4, 4]", () => {
+      expect(arrayFunctions.flatten([1, 4, 3, [4, 4]])).toEqual([
+        1,
+        4,
+        3,
+        4,
+        4
+      ]);
+    });
+    it("tests type of flatten returns function", () => {
+      expect(typeof arrayFunctions.flatten).toBe("function");
     });
   });
 });
--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,39 @@
-const objectFunctions = require('./objects');
+const objectFunctions = require("./objects");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {});
+describe("objects", () => {
+  it("should be an object", () => {});
+});
+
+describe("keys", () => {
+  test("returns keys of object", () => {
+    expect(objectFunctions.keys({ lambda: 1, school: 2 })).toEqual([
+      "lambda",
+      "school"
+    ]);
+  });
+});
+
+describe("value", () => {
+  test("returns values of object", () => {
+    expect(objectFunctions.values({ lambda: 1, school: 2 })).toEqual([1, 2]);
+  });
+});
+
+describe("pairs", () => {
+  test("returns pairs of object", () => {
+    expect(objectFunctions.pairs({ lambda: 1, school: 2 })).toEqual([
+      ["lambda", 1],
+      ["school", 2]
+    ]);
+  });
+});
+
+describe("invert", () => {
+  test("returns invert of object", () => {
+    expect(objectFunctions.invert({ lambda: 1, school: 2 })).toEqual({
+      1: "lambda",
+      2: "school"
+    });
+  });
 });
