cc6f509ea8faecfde1051569ee2df71c34fd02fa
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-23 14:16:39 -0700

    get biggest finish

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,11 @@
-const funcs = require('./project-2');
+const funcs = require("./project-2");
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe("getBiggest()", () => {
+	test("x bigger than y", () => {
+		expect(funcs.getBiggest(7, 5)).toBe(7);
+	});
+	test("y bigger than x", () => {
+		expect(funcs.getBiggest(3, 7)).toBe(7);
+	});
+});
