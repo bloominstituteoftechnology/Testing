67dad799c44fa7b62c39028e03d003fa1d21a36a
Lucas Beemer <lucasbeemer@gmail.com>
2018-10-23 14:36:49 -0700

    finished project 2 tests

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -59,33 +59,33 @@ describe('isInteger()', () => {
 
 describe('fizzBuzz()', () => {
     it('should return correct string', () => {
-	const expected1 = 'fizzbuzz'
-	const expected2 = 'buzz'
-	const expected3 = 'fizz'
-	const expected4 = 1
-	const actual1 = funcs.fizzBuzz(15)
-	const actual2 = funcs.fizzBuzz(5)
-	const actual3 = funcs.fizzBuzz(3)
-	const actual4 = funcs.fizzBuzz(1)
-	expect(actual1).toEqual(expected1)
-	expect(actual2).toEqual(expected2)
-	expect(actual3).toEqual(expected3)
-	expect(actual4).toEqual(expected4)
+	const expected1 = 'fizzbuzz';
+	const expected2 = 'buzz';
+	const expected3 = 'fizz';
+	const expected4 = 1;
+	const actual1 = funcs.fizzBuzz(15);
+	const actual2 = funcs.fizzBuzz(5);
+	const actual3 = funcs.fizzBuzz(3);
+	const actual4 = funcs.fizzBuzz(1);
+	expect(actual1).toEqual(expected1);
+	expect(actual2).toEqual(expected2);
+	expect(actual3).toEqual(expected3);
+	expect(actual4).toEqual(expected4);
     });
 });
 
 describe('isPrime()', () => {
 it('should return correct boolean', () => {
 	const expected1 = true;
-	const expected2 = false
-	const actual1 = funcs.isPrime(-1)
-	const actual2 = funcs.isPrime(1)
-	const actual3 = funcs.isPrime(7)
-	const actual4 = funcs.isPrime(4)
-	expect(actual1).toEqual(expected2)
-	expect(actual2).toEqual(expected2)
-	expect(actual3).toEqual(expected1)
-	expect(actual4).toEqual(expected2)
+	const expected2 = false;
+	const actual1 = funcs.isPrime(-1);
+	const actual2 = funcs.isPrime(1);
+	const actual3 = funcs.isPrime(7);
+	const actual4 = funcs.isPrime(4);
+	expect(actual1).toEqual(expected2);
+	expect(actual2).toEqual(expected2);
+	expect(actual3).toEqual(expected1);
+	expect(actual4).toEqual(expected2);
     });
 });
 
@@ -133,6 +133,49 @@ describe('addItemToFront()', () => {
     it('should add item to the beginnning of array', () => {
         const expected = ['bread', 'peanut butter', 'jelly'];
         const actual = funcs.addItemToFront(['peanut butter', 'jelly'], 'bread');
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe('wordsToSentence()', () => {
+    it('should return new sentence', () => {
+        const expected = 'H e l l o   W o r l d';
+        const actual = funcs.wordsToSentence('Hello World');
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe('contains()', () => {
+    it('should return correct boolean', () => {
+	const expected1 = true;
+	const expected2 = false;
+	const actual1 = funcs.contains([1,2,3], 2);
+	const actual2 = funcs.contains([1,2,3], 5);
+	expect(actual1).toEqual(expected1);
+	expect(actual2).toEqual(expected2);
+    });
+});
+
+describe('addNumbers()', () => {
+    it('should return correct boolean', () => {
+	const expected = 15;
+	const actual = funcs.addNumbers([1,2,3,4,5]);
+	expect(actual).toEqual(expected);
+    });
+});
+
+describe('averageTestScore()', () => {
+    it('should return average test score', () => {
+        const expected = 39.6;
+        const actual = funcs.averageTestScore([49,40,42,37,30]);
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe('largestNumber()', () => {
+    it('should return the largest number from array', () => {
+        const expected = 49;
+        const actual = funcs.largestNumber([49,40,42,37,30]);
         expect(actual).toEqual(expected);
     });
 });
