961b1061962203a37d9bacbbb6d2d4eb4f9348cb
Eva <evalee904@gmail.com>
2018-10-24 03:40:43 -0500

    Some basic testing.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,42 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1', () => {
+
+  describe('multiplyByTen()', () => {
+    it('should multiply the param by ten', () => {
+      const expected = 100;
+      const actual = helpers.multiplyByTen(10);
+      expect(actual).toEqual(expected);
+    });
+    it('should return 0 if there is no param', () => {
+      const param = helpers.multiplyByTen();
+      expect(param).toBe(0);
+    });
+    it('should throw an error if the param is not a number', () => {
+      expect(() => {
+        helpers.multiplyByTen('ten');
+      }).toThrow();
+    });
+  }); // multiplyByTen
+  
+  describe('subtractFive()', () => {
+    it('should subtract five from the param', () => {
+      const expected = 5;
+      const actual = helpers.subtractFive(10);
+      expect(actual).toEqual(expected);
+    });
+    it('should return 0 if there is no param', () => {
+      const param = helpers.subtractFive();
+      expect(param).toBe(0);
+    });
+    it('should throw an error if the param is not a number', () => {
+      expect(() => {
+        helpers.subtractFive('ten');
+      }).toThrow();
+    });
+  }); // subtractFive
+
+}); //project-1
+
+
