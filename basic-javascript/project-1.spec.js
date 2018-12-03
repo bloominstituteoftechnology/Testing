32887f3f5c9331f00c61f896dfd17f9567d40db1
Gannon <gkleblanc@gmail.com>
2018-12-03 18:00:36 -0500

    used testing to confirm that expected outcomes were the actual outcomes

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -71,3 +71,36 @@ const returnsBooleanNum = func => {
 
 const boolNum = [lessThanNinety, greaterThanFifty, isEven, isOdd]
 boolNum.forEach(func => test('returns a boolean', () => returnsBooleanNum(func)))
+
+//check actual vs expected outcomes.
+const validateResults = expected => {
+    let actual = []
+    const nums = [multiplyByTen, subtractFive, square, cube, roundNumber, roundUp, getCircleArea, add, subtract, divide, multiply, getRemainder, raiseToPower, getRectangleArea, getTriangleArea, getRectangularPrismVolume]
+    const strs = [addExclamationPoint, combineNames, getGreeting]
+    const boolStr = [areSameLength, areEqual]
+    const boolNum = [lessThanNinety, greaterThanFifty, isEven, isOdd]
+    nums.forEach(func => {
+        let result = func(5)
+        result ? result : result = func(5, 2)
+        result ? result : result = func(1, 2, 3)
+        actual.push(result)
+    })
+    strs.forEach(func => {
+        let result = func('test')
+        result.includes('undefined') ? result = func('infinite', 'jest') : result
+        actual.push(result)
+    })
+    boolStr.forEach(func => {
+        let result = func('jest', 'jest')
+        actual.push(result)
+    })
+    boolNum.forEach(func => {
+        let result = func(2)
+        actual.push(result)
+    })
+
+    expect(actual).toEqual(expected)
+}
+
+let expected = [50, -4, 25, 125, 5, 5, 78.53981633974483, 7, 3, 2.5, 10, 1, 25, 10, 5, 6, 'test!', 'infinite jest', 'Hello test!', true, true, true, false, true, false]
+test('validate results', () => validateResults(expected))
