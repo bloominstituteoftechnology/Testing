45daae6146c138152eee66beb982403ec86eace6
Axolotl <samuelmachat@gmail.com>
2018-12-03 17:08:26 -0500

    did more tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -32,6 +32,10 @@ const {
 // 4. Get Green - fix thing that's getting tested, have it pass
 // 5. Refactor - optional, make it better, keep it green
 
+// Arrange: setup the world
+// Act: execute the code you're testing
+// Assert: check that it works
+
 describe('Jest', () => {
   test('Jest runs', () => {})
 })
@@ -43,16 +47,50 @@ describe('multiplyByTen', () => {
     const actual = multiplyByTen(input)
     expect(actual).toBe(expected)
   })
+})
 
-  // test('returns a string', () => {
+describe('subtractFive', () => {
+  test('subtracts by five', () => {
+    const input = 10
+    const expected = 5
+    const actual = subtractFive(input)
+    expect(actual).toBe(expected)
+  })
+})
 
-  // // Arrange: setup the world
-  // const input = 'kam';
+describe('areSameLength', () => {
+  test('tests whether two strings are same length', () => {
+    const string1 = 'blah'
+    const string2 = 'rarr'
+    expect(areSameLength(string1, string2)).toBeTruthy()
+  })
+  test('tests whether two strings are different length', () => {
+    const string1 = 'blah'
+    const string2 = 'rar'
+    expect(areSameLength(string1, string2)).toBeFalsy()
+  })
+})
 
-  // // Act: execute the code you're testing
-  // const result = reverseString(input);
+describe('areEqual', () => {
+  test('tests whether two numbers are equal', () => {
+    const num1 = 10
+    const num2 = 10
+    expect(areEqual(num1, num2)).toBeTruthy()
+  })
+  test('tests whether two numbers are not equal', () => {
+    const num1 = 10
+    const num2 = 14
+    expect(areEqual(num1, num2)).toBeFalsy()
+  })
+})
 
-  // // Assert: check that it works
-  // expect(typeof result === 'string').toBeTruthy();
-  // });
+describe('lessThanNinety', () => {
+  test('test whether a number is less than 90', () => {
+    const num = 10
+    expect(lessThanNinety(num)).toBeTruthy()
+  })
+  test('test whether a number is greater than 90', () => {
+    const num = 99
+    expect(lessThanNinety(num)).toBeFalsy()
+  })
 })
