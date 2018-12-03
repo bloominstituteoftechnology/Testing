ae0eafbc2e4b73955b010d5289cf88eaf986a1f2
Michael Checo <checomichael2@gmail.com>
2018-12-03 15:21:09 -0500

    tests for 1st 10 functions

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -18,3 +18,39 @@ test('should be same length', () => {
     const actual = helpers.areSameLength('boot', 'toob')
     expect(actual).toEqual(expected)
 })
+
+test('are equal', () => {
+    const expected = true;
+    const actual = helpers.areEqual(4, 4)
+    expect(actual).toEqual(expected)
+})
+
+test('should be less than 90', () => { 
+    const actual = helpers.lessThanNinety(85);
+    expect(actual).toBeTruthy();
+})
+test('should be more than 90', () => { 
+    const actual = helpers.greaterThanFifty(63);
+    expect(actual).toBeTruthy();
+})
+test('properly adds two numbers', () => {
+    const expected = 4;
+    const actual = helpers.add(2,2)
+    expect(actual).toEqual(expected)
+})
+test('properly subtracts two numbers', () => {
+    const expected = 6;
+    const actual = helpers.subtract(8,2)
+    expect(actual).toEqual(expected)
+})
+test('properly divides two numbers', () => {
+    const expected = 4;
+    const actual = helpers.divide(16,4)
+    expect(actual).toEqual(expected)
+})
+test('properly multiplies two numbers', () => {
+    const expected = 4;
+    const actual = helpers.multiply(2,2)
+    expect(actual).toEqual(expected)
+})
+
