48c01690bed235e898467ab599b52656fa380ad3
Unknown <zachary.arney@gmail.com>
2018-12-03 17:14:23 -0600

    describe global for math tests and tests for specific equations

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,30 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('math things', () => {
+  test('should mulitply by ten', () => {
+    // arrange
+    const expected = 50;
+    const multiplyByTen = helpers.multiplyByTen;
+
+    // act
+    const product = multiplyByTen(5);
+
+    // assert
+    expect(product).toBe(expected);
+  });
+
+  test('should subtract five', () => {
+    // arrange
+    const expected = 5;
+    const subtractFive = helpers.subtractFive;
+
+    // act
+    const result = subtractFive(10);
+
+    // assert
+    expect(result).toBe(expected);
+  });
+
+
+});
