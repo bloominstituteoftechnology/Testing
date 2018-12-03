87ba566c8917b4c14d39738e1ca8b75d20271897
Yusuf Nafey <yusufnafey@yahoo.com>
2018-12-03 15:42:31 -0800

    finished day 1 mvp, set up day 2 project

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,119 @@
-const funcs = require('./project-2');
+const {
+    getBiggest,
+    greeting,
+    isTenOrFive,
+    isInRange,
+    isInteger,
+    fizzBuzz,
+    isPrime,
+    returnFirst,
+    returnLast,
+    getArrayLength,
+    incrementByOne,
+    addItemToArray,
+    addItemToFront,
+    wordsToSentence,
+    contains,
+    addNumbers,
+    averageTestScore,
+    largestNumber
+} = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project 2', () => {
+    describe('1-6', () => {
+        // 1.
+        test('getBiggest', () => {
+
+        });
+
+        // 2.
+        test('greeting', () => {
+
+        });
+
+        // 3.
+        test('isTenOrFive', () => {
+
+        });
+
+        // 4.
+        test('isInRange', () => {
+
+        });
+
+        // 5.
+        test('isInteger', () => {
+
+        });
+
+        // 6.
+        test('fizzBuzz', () => {
+
+        });
+    });
+
+    describe('7-12', () => {
+        // 7.
+        test('isPrime', () => {
+
+        });
+
+        // 8.
+        test('', () => {
+
+        });
+
+        // 9.
+        test('', () => {
+
+        });
+
+        // 10.
+        test('', () => {
+
+        });
+
+        // 11.
+        test('', () => {
+
+        });
+
+        // 12.
+        test('', () => {
+
+        });
+    });
+
+    describe('13-18', () => {
+        // 13.
+        test('', () => {
+
+        });
+
+        // 14.
+        test('', () => {
+
+        });
+
+        // 15.
+        test('', () => {
+
+        });
+
+        // 16.
+        test('', () => {
+
+        });
+
+        // 17.
+        test('', () => {
+
+        });
+
+        // 18.
+        test('', () => {
+
+        });
+    });
+});
