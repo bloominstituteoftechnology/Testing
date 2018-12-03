0fe425414a04978cdb1cec10e1c8f8f75dd29d88
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-12-03 15:49:45 -0800

    multiplyByTen

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,19 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
 
 // start testing!
+
+describe("project-1.js helpers", () => {
+  test("jest runs", () => {});
+
+  beforeEach(() => {
+    console.log("running");
+  });
+
+  describe("multiplyByTen()", () => {
+    it("should multiply provided number by 10", () => {
+      const expected = 100;
+      const actual = helpers.multiplyByTen(10);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
