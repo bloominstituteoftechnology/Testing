7579e221d253e5e364feee8290be2d5cfabf1f8c
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-12-03 16:24:10 -0500

    adds project-1.js tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,25 @@
 const helpers = require('./project-1');
 
-// start testing!
+// Arrange: setup the world
+// Act: execute the code you're testing
+// Assert: check that it works
+
+describe('project-1.js', () => {
+    // multiplyByTen  
+    describe('multipyByTen', () => {
+        it('should multipy provided number by ten', () => {
+            const expected = 100; // expected = 101 --> FAIL
+            const actual = helpers.multiplyByTen(10);
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    // subtractFive
+    describe('subtractFive', () => {
+        it('should subtract five from provided number', () => {
+            const expected = 25;
+            const actual = helpers.subtractFive(30);
+            expect(actual).toEqual(expected);
+        });
+    });
+});
