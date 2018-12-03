c81822c44f57c82c4c4fc69b315901712ea6fe37
Peter Pham <phamhere@gmail.com>
2018-12-03 14:26:11 -0700

    added five function tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,39 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
 
 // start testing!
+test("multiply ten times ten", () => {
+  const input = 10;
+  const output = 100;
+
+  expect(helpers.multiplyByTen(input)).toBe(output);
+});
+
+test("subtract five from ten", () => {
+  const input = 10;
+  const output = 5;
+
+  expect(helpers.subtractFive(input)).toBe(output);
+});
+
+test("strings same length", () => {
+  const str1 = "stringof9";
+  const str2 = "peterpham";
+
+  expect(helpers.areSameLength(str1, str2)).toBe(true);
+});
+
+test("strict equals", () => {
+  const x = 10;
+  const y = 10;
+
+  expect(helpers.areEqual(x, y)).toBe(true);
+});
+
+describe("lessThanNinety", () => {
+  test("70 < 90", () => {
+    expect(helpers.lessThanNinety(70)).toBe(true);
+  });
+  test("100 < 90", () => {
+    expect(helpers.lessThanNinety(100)).toBe(false);
+  });
+});
