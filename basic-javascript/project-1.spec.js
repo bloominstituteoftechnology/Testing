2ecded9a36fc036dc0521e4a755e9b5711e4312b
Drewsup123c <drewsup123c@gmail.com>
2018-12-03 13:59:55 -0700

    finished first 4

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -28,3 +28,35 @@ const {
 
 // start testing!
 
+describe('Jest',() => {
+    test('Jest is Running',()=>{
+
+    })
+});
+
+describe('multiply by ten',() => {
+    test('multiplys correctly',()=>{
+        const input = 5;
+        const expected = 50;
+        const result = multiplyByTen(input);
+        expect(result === expected)
+    })
+})
+
+describe('Subtract Five',() => {
+    test('minus five', () => {
+        const input = 5;
+        const expected = 0;
+        const result = subtractFive(input);
+        expect(expected === result)
+    })
+})
+
+describe('Strings are the Same Length', () => {
+    test('same length',() => {
+        const i1 = i2 = "same length?";
+        const result = areSameLength(i1,i2);
+        expect(result === true)
+    })
+})
+
