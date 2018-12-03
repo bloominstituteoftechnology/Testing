ffa441ab6ee8c6e9f6de2603c16be39ed7219ce7
Chiboba <briangfelix@gmail.com>
2018-12-03 15:12:32 -0800

    Started MVP for tomorrow

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,49 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('getBiggest', () => {
+    const y = 3;
+    const x = 3;
+
+    test('returns x when biggest', () => {
+        expect(funcs.getBiggest(x, 2)).toBe(x)
+    })
+
+    test('returns y when biggest', () => {
+        expect(funcs.getBiggest(2, y)).toBe(y)
+    })
+
+    test('returns y when same values', () => {
+        expect(funcs.getBiggest(3, y)).toBe(y)
+    })
+})
+
+describe('greeting', () => {
+
+    test('expected return for german', () => {
+        expect(funcs.greeting('German')).toBe('Guten Tag!')
+    })
+
+    test('expected return for spanish', () => {
+        expect(funcs.greeting('Spanish')).toBe('Hola!')
+    })
+
+    test('expected default return', () => {
+        expect(funcs.greeting()).toBe('Hello!')
+    })
+})
+
+describe('isTenOrFive', () => {
+
+    test('returns expected for num value 5', () => {
+        expect(funcs.isTenOrFive(5)).toBe(true)
+    })
+
+    test('returns expected for num value 10', () => {
+        expect(funcs.isTenOrFive(10)).toBe(true)
+    })
+
+    test('returns false when not 5 or 10', () => {
+        expect(funcs.isTenOrFive(1)).toBe(false)
+    })
+})
