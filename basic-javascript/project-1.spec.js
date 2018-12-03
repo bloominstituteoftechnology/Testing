7656233939f773ab6462ba2a97f4b202c3b411f6
sean-one <basement365@gmail.com>
2018-12-03 15:24:05 -0800

    just getting started

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,14 @@
 const helpers = require('./project-1');
 
 // start testing!
+test('not sure what i am doing yet', () => {
+
+});
+
+it('should multiply by ten', () => {
+    const expected = 100;
+
+    const actual = helpers.multiplyByTen(10);
+
+    expect(actual).toEqual(expected);
+});
