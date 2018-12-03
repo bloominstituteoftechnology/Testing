69dbeef90a3055dc11387618326ebf1bded7c9e2
Edward J <edjeong99@gmail.com>
2018-12-03 18:07:14 -0500

    finish proj2

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -30,3 +30,63 @@ describe("getBiggest", () => {
   test("isInRange 50/20, 10", () => {
     expect(funcs.isInRange(10)).toBe(false);
   });
+
+
+
+  test('is Integer', () => {
+  
+    expect(funcs.isInteger(2)).toBeTruthy();
+  });
+  
+  test('fizzbuzz', () => {
+    expect(funcs.fizzBuzz(15)).toBe('fizzbuzz');
+  });
+  
+  test('is Prime', () => {
+    expect(funcs.isPrime(11)).toBeTruthy();
+  });
+  
+  test('returnfirst', () => {
+    expect(funcs.returnFirst(['carlo', 'test'])).toEqual('carlo');
+  });
+  
+  test('return last', () => {
+    expect(funcs.returnLast(['carlo', 'test'])).toEqual('test');
+  });
+  
+  test('get arr length', () => {
+    expect(funcs.getArrayLength(['carlo'])).toEqual(1);
+  });
+  
+  test('incrementbyone', () => {
+    expect(funcs.incrementByOne([1, 2])).toEqual([2, 3]);
+  });
+  
+  test('add item to array', () => {
+    expect(funcs.addItemToArray(['carlo'], 'test')).toEqual(['carlo', 'test']);
+  });
+  
+  test('add item to front', () => {
+    expect(funcs.addItemToFront(['carlo'], 'test')).toEqual(['test', 'carlo']);
+  });
+  
+  test('words to sentence', () => {
+    expect(funcs.wordsToSentence(['sup', 'dude'])).toEqual('sup dude');
+  });
+  
+  test('contains', () => {
+    expect(funcs.contains(['carlo', 'test'], 'carlo')).toBeTruthy();
+  });
+  
+  test('add numbers', () => {
+    expect(funcs.addNumbers([2, 3, 4])).toEqual(9);
+  });
+  
+  test('average test score', () => {
+    expect(funcs.averageTestScore([90, 95, 92.5])).toEqual(92.5);
+  });
+  
+  test('largest numbers', () => {
+    expect(funcs.largestNumber([30, 20, 100])).toEqual(100);
+  });
+
