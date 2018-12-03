1777ed2e45c8ad28dd0312b70a15419ce99fbeef
bkwillia052 <bkwillia052@gmail.com>
2018-12-03 14:58:13 -0600

    Initial pull request

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,181 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+
+
+describe.only('project-1', ()=>{
+
+describe('multipyByTen', ()=>{
+    it('should multiply num by 10', ()=>{
+    const variable = Math.floor(Math.random())
+    expect(helpers.multiplyByTen(variable)).toBe(variable * 10)
+    
+    
+    })
+
+})
+describe('subtractFive', ()=>{
+it('should subtract by five', ()=>{
+
+    const variable = Math.floor(Math.random())
+    expect(helpers.subtractFive(variable)).toBe(variable - 5)
+}) 
+})
+
+describe('areSameLength', ()=>{
+it('should be same length', ()=>{
+    const variable = "Math.floor(Math.random())"
+    expect(helpers.areSameLength(variable.length, variable.length)).toBe(true)
+}) 
+})
+
+describe('areEqual', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('lessThanNinety', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('greaterThanFifty', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('add', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('subtract', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('divide', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('multiply', ()=>{
+it('', ()=>{
+
+}) 
+})  
+
+describe('getRemainder', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('isEven', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('isOdd', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('square', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('cube', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('raiseToPower', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('roundNumber', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('roundUp', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('addExclamationPoint', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('combineNames', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('getGreeting', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('getRectangleArea', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('getCircleArea', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('getTriangleArea', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('getCircleArea', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+describe('getRectangularPrismVolume', ()=>{
+it('', ()=>{
+
+}) 
+})
+
+
+
+
+
+
+
+
+
+
+
+})
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,9 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe('project-2', ()=>{
+    it('should be a test', ()=>{
+        
+    })
+})
