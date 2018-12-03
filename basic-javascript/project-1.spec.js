22518a6ae147428ac6f3e23e19f97e2e1baa1a1b
Brandon Vilaychith <blvilaychith@outlook.com>
2018-12-03 13:57:01 -0600

    multiply by 10 tested

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,13 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('multiplyByTen', () => {
+  test('multiply by 10', () => {
+    const input = 10;
+    const expected = 100;
+
+    const actual = helpers.multiplyByTen(input);
+
+    expect(actual).toBe(expected);
+  });
+});
