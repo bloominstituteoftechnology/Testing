4235814c5c035c27c398503b6b47777ed8868983
bummings <eabummings@gmail.com>
2018-12-03 17:38:52 -0500

    more tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -17,4 +17,47 @@ describe('project-1', () => {
       expect(minusFiveResult).toEqual(minusFiveExpected);
     });
   });
+
+  describe('areSameLength', () => {
+    it('should determine if lengths match', () => {
+      const areSameLengthA = 'four';
+      const areSameLengthB = 'four';
+      expect(areSameLengthA.length).toEqual(areSameLengthB.length);
+    });
+  });
+
+  describe('areEqual', () => {
+    it('should determine if values match', () => {
+      const areEqualA = 1;
+      const areEqualB = 1;
+      expect(areEqualA).toEqual(areEqualB);
+    });
+  });
+
+  describe('lessThanNinety', () => {
+    it('should determine if value is less than 90', () => {
+      const lessThanNinety = 89;
+      expect(lessThanNinety).toBeLessThan(90);
+    });
+  });
+
+  describe('greaterThanFifty', () => {
+    it('should determine if value is greater than 50', () => {
+      const greaterThanFifty = 51;
+      expect(greaterThanFifty).toBeGreaterThan(50);
+    });
+  });
+
+  // describe('add', () => {
+  //   it('should add two numbers together', () => {
+  //     const addExpected = helpers.add(2, 3);
+  //     expect(addExpected).toBe(5)
+  //   });
+
+  describe('add', () => {
+    it('should add two values together', () => {
+      const addExpected = helpers.add(2, 3);
+      expect(addExpected).toBe(5);
+    });
+  });
 });
