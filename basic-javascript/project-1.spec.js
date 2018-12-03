646ea3af087ac4f55fdd30023e07927eee260d99
Gannon <gkleblanc@gmail.com>
2018-12-03 16:48:46 -0500

    created and successfully executed tests for all functions that are number based problems.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,43 @@
-const helpers = require('./project-1');
+const {
+    multiplyByTen,
+    subtractFive,
+    areSameLength,
+    areEqual,
+    lessThanNinety,
+    greaterThanFifty,
+    add,
+    subtract,
+    divide,
+    multiply,
+    getRemainder,
+    isEven,
+    isOdd,
+    square,
+    cube,
+    raiseToPower,
+    roundNumber,
+    roundUp,
+    addExclamationPoint,
+    combineNames,
+    getGreeting,
+    getRectangleArea,
+    getTriangleArea,
+    getCircleArea,
+    getRectangularPrismVolume } = require('./project-1');
 
 // start testing!
+
+
+test('jest working', () => {
+
+})
+
+const returnsNumber = func => {
+    let result = func(3)
+    result ? result : result = func(2, 6)
+    result ? result : result = func(1, 2, 5)
+    expect(typeof result === 'number').toBeTruthy()
+}
+
+const nums = [multiplyByTen, subtractFive, square, cube, roundNumber, roundUp, getCircleArea, add, subtract, divide, multiply, getRemainder, raiseToPower, getRectangleArea, getTriangleArea, getRectangularPrismVolume]
+nums.forEach(func => test('returns a number', () => returnsNumber(func)))
