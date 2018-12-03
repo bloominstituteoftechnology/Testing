7f2ff42eddb6598a50ba7763d73af18673214af0
Daniel Ferrer <danfrrr13@gmail.com>
2018-12-03 15:32:42 -0500

    Have completed 10 of the tests.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -38,3 +38,42 @@ test('It accurately checks if input is less than 90', () => {
     const actual = helpers.lessThanNinety(input);
     expect(actual).toBe(expected);
 });
+
+test('It accurately checks if input is greater than 50', () => {
+    const input = 51;
+    const expected = true;
+    const actual = helpers.lessThanNinety(input);
+    expect(actual).toBe(expected);
+});
+
+test('It correctly adds two inputs', () => {
+    const input1 = 2;
+    const input2 = 2;
+    const expected = 4;
+    const actual = helpers.add(input1, input2);
+    expect(actual).toBe(expected);
+});
+
+test('It correctly subtracts input2 from input1', () => {
+    const input1 = 5;
+    const input2 = 3;
+    const expected = 2;
+    const actual = helpers.subtract(input1, input2);
+    expect(actual).toBe(expected);
+});
+
+test('It correctly divides input1 by input2', () => {
+    const input1 = 9;
+    const input2 = 3;
+    const expected = 3;
+    const actual = helpers.divide(input1, input2);
+    expect(actual).toBe(expected);
+});
+
+test('It correctly multiplies both inputs', () => {
+    const input1 = 2;
+    const input2 = 3;
+    const expected = 6;
+    const actual = helpers.multiply(input1, input2);
+    expect(actual).toBe(expected);
+});
