cb04a9b2bbc4f5b9efce90038c8b87ad863af24b
shawn <shawnantonucci2018@gmail.com>
2018-12-03 14:13:21 -0800

    added getBiggest, greeting test

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,32 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('jest', () => {
+    test('Jest runs', () => {
+
+    });
+});
+
+describe('funcs', () => {
+    beforeEach(() => {
+        console.log('running');
+    })
+
+    describe('getBiggest', () => {
+        test('get the biggest number', () => {
+            expect(funcs.getBiggest(5, 3)).toBe(5);
+            expect(funcs.getBiggest(8, 8)).toBe(8);
+            expect(funcs.getBiggest(2, 4)).toBe(4);
+        });
+    })
+
+    describe('greeting', () => {
+        test('get a greeting in different language', () => {
+            expect(funcs.greeting('German')).toBe('Guten Tag!');
+            expect(funcs.greeting('Spanish')).toBe('Hola!');
+            expect(funcs.greeting('English')).toBe('Hello!');
+        });
+    })
+    
+
+})
