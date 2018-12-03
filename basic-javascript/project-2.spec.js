d8e77dd4f95e8ca66b4ec6ed600d4b316bd5b8ba
Drewsup123c <drewsup123c@gmail.com>
2018-12-03 15:09:04 -0700

    imported everything from project-2 to testing file

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,22 @@
-const funcs = require('./project-2');
+const {
+    getBiggest,
+    greeting,
+    isTenOrFive,
+    isInRange,
+    isInteger,
+    fizzBuzz,
+    isPrime,
+    returnFirst,
+    returnLast,
+    getArrayLength,
+    incrementByOne,
+    addItemToArray,
+    addItemToFront,
+    wordsToSentence,
+    contains,
+    addNumbers,
+    averageTestScore,
+    largestNumber
+    } = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
