4bc91f2f5b626c14c264d60d92d7d379075082a4
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-03 12:59:17 -0600

    wrote first 2 tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,14 @@
-const helpers = require('./project-1');
+const { multiplyByTen, subtractFive } = require('./project-1');
 
 // start testing!
+test('multiply by then', () => {
+    const input = 10
+    const expected = 100
+    expect(multiplyByTen(input)).toEqual(expected)
+})
+
+test('substract five', () => {
+    const input = 25
+    const expected = 20
+    expect(subtractFive(input)).toEqual(expected)
+})
