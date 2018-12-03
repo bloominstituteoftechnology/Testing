f1af533e9053f019098218f8b86710b5bcb95a44
ryanboris <ryan.boris@gmail.com>
2018-12-03 16:04:21 -0500

    Creates unit tests for multiplyByTen(), subtractFive(), and areSameLength()

--- basic-javascript/project-1.js
+++ basic-javascript/project-1.js
@@ -12,6 +12,7 @@ const areSameLength = (str1, str2) => {
   return str1.length === str2.length;
 };
 
+
 const areEqual = (x, y) => {
   return x === y;
 };
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,38 @@
-const helpers = require('./project-1');
+const helpers = require('./project-1')
 
 // start testing!
+describe('multiplyByTen', () => {
+  it('is defined', () => {
+    expect(helpers.multiplyByTen).toBeDefined()
+  })
+  it('multiplies by ten', () => {
+    expect(helpers.multiplyByTen(2)).toStrictEqual(20)
+    expect(helpers.multiplyByTen(-3)).toStrictEqual(-30)
+    expect(helpers.multiplyByTen('hi')).toBeNaN()
+    expect(helpers.multiplyByTen([ 'a' ])).toBeNaN()
+  })
+})
+
+describe('subtractFive', () => {
+  it('is defined', () => {
+    expect(helpers.subtractFive).toBeDefined()
+  })
+  it('multiplies by ten', () => {
+    expect(helpers.subtractFive(2)).toStrictEqual(-3)
+    expect(helpers.subtractFive(-3)).toStrictEqual(-8)
+    expect(helpers.subtractFive('hi')).toBeNaN()
+  })
+})
+
+describe('areSameLength', () => {
+  it('is defined', () => {
+    expect(helpers.areSameLength).toBeDefined()
+  })
+
+  it('returns true if the strings are equal', () => {
+    expect(helpers.areSameLength('foo', 'bar')).toBeTruthy()
+  })
+  it('returns false if the strings are equal', () => {
+    expect(helpers.areSameLength('foo', 'bars')).toBeFalsy()
+  })
+})
