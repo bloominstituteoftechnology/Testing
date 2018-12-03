226811e54981302702b0555bf491268273b3832f
Lee Len Formento <leebatef@gmail.com>
2018-12-03 08:47:23 -0500

    lessthanninety

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -6,33 +6,40 @@ describe('project-1.js', () => {
     describe('jest', () => {});
     test('should run tests', () => {});
 
-    describe('multiplyByTen', () => {
+    describe('multiplyByTen()', () => {
         it('should multiply by ten', () => {
             const expected = 120;
             const actual = helpers.multiplyByTen(12);
             expect(actual).toEqual(expected);
         })
     }) // multiplybyten
-    describe('subtractFive', () => {
+    describe('subtractFive()', () => {
         it('should subtract by five', () => {
         const expected = 5;
         const actual = helpers.subtractFive(10);
         expect(actual).toEqual(expected);
     })
     }) // subtractfive
-    describe('areSameLength', () => {
+    describe('areSameLength()', () => {
         it('should return if the same length', () => {
             const expected = true;
             const answer = helpers.areSameLength('hola', 'ball');
             expect(answer).toBe(expected)
         })
     }) // areSameLength
-    describe('areEqual', () => {
-        it('should say if eqyal', () => {
+    describe('areEqual()', () => {
+        it('should say if equal', () => {
             const expected = true;
             const answer = helpers.areSameLength('100', '100');
             expect(answer).toBe(expected)
         })
     }) // areEqual
+    describe('lessThanNinety()', () => {
+        it('should say if less than 90', () => {
+            const expected = true;
+            const answer = helpers.lessThanNinety(50);
+            expect(answer).toEqual(expected);
+        });
+    });
 
 }) // project js
