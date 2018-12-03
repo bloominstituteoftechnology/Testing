bb27623578c061ddf53b8ccdaeacfe36c479365b
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-12-03 11:25:03 -0600

    finished the array section

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('./arrays');
+const aF = require('./arrays.js');
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -7,11 +7,46 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
-describe('Arrays', () => {
-  describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
-    });
-  });
+test('each', () => {
+  expect(
+    aF.each(['carlo', 'test'], (item, index) => {
+      return item;
+    })
+  ).toBe();
+});
+
+test('map', () => {
+  expect(
+    aF.map(['carlo', 'test'], item => {
+      return item;
+    })
+  ).toEqual(['carlo', 'test']);
+});
+
+test('reduce', () => {
+  expect(
+    aF.reduce(['carlo', 'test'], item => {
+      return item;
+    })
+  ).toEqual('carlo');
+});
+
+test('find', () => {
+  expect(
+    aF.find(['carlo', 'test'], item => {
+      return item === 'carlo' ? item : null;
+    })
+  ).toBe('carlo');
+});
+
+test('filter', () => {
+  expect(
+    aF.filter(['carlo', 'test'], item => {
+      return item === 'carlo' ? item : null;
+    })
+  ).toEqual(['carlo']);
+});
+
+test('flatten', () => {
+  expect(aF.flatten(['carlo', 'test'])).toEqual(['carlo', 'test']);
 });
