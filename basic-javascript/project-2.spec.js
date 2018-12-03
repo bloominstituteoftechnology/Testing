0aeb96a7a0e327ae8ff9bf0c043c5772fe9b0175
Edward J <edjeong99@gmail.com>
2018-12-03 17:58:07 -0500

    started on proj2

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,32 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe("getBiggest", () => {
+  
+
+    test("2 , 5", () => {
+      expect(funcs.getBiggest(2,5)).toBe(5);
+    });
+    test("10, 4", () => {
+      expect(funcs.getBiggest(10, 4)).toBe(10);
+    });
+  });
+
+  describe("greeting", () => {
+  
+
+    test("German", () => {
+      expect(funcs.greeting("German")).toBe('Guten Tag!');
+    });
+    test("Default", () => {
+      expect(funcs.greeting("KR")).toBe('Hello!');
+    });
+  });
+
+  test("ten or Five 10", () => {
+    expect(funcs.isTenOrFive(10)).toBe(true);
+  });
+
+  test("isInRange 50/20, 10", () => {
+    expect(funcs.isInRange(10)).toBe(false);
+  });
