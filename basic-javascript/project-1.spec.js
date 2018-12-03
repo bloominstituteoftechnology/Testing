ff75e49b0c1df4319e24f0a88ac4b9846661d75e
Michael Checo <checomichael2@gmail.com>
2018-12-03 15:13:35 -0500

    initial commit, three tests passing

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,20 @@
 const helpers = require('./project-1');
 
 // start testing!
+test('multiplies number by ten', () => {
+    const expected = 100;
+    const actual = helpers.multiplyByTen(10);
+    expect(actual).toEqual(expected)
+})
+
+test('subtracts 5', () => {
+    const expected = 5;
+    const actual = helpers.subtractFive(10);
+    expect(actual).toEqual(expected);
+})
+
+test('should be same length', () => {
+    const expected = true;
+    const actual = helpers.areSameLength('boot', 'toob')
+    expect(actual).toEqual(expected)
+})
