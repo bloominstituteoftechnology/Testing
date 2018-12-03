24af3f2d44a036593cf1bd439e5a63ca16d764eb
Yanrong Xu <xuyanrong@icloud.com>
2018-12-03 17:45:33 -0500

    first test muliply by 10

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,12 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1', () => {
+    describe('multiplyByTen', () => {
+        it('should provide right answer when mutiply by 10', () => {
+            expect(helpers.multiplyByTen(10)).toEqual(100);
+        })
+    })
+
+    
+})
