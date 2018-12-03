f15aaab24aa2dd5aeb14ec2e68854746ebe8f411
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-12-03 11:48:39 -0600

    added tests on objects.spec

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,6 +1,65 @@
-const objectFunctions = require('./objects');
+const oF = require('./objects');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-describe('objects', () => {
-  it('should be an object', () => {});
+test('keys', () => {
+  expect(
+    oF.keys({
+      name: 'Carlo',
+      lastName: 'Clamucha'
+    })
+  ).toEqual(['name', 'lastName']);
+});
+
+test('values', () => {
+  expect(
+    oF.values({
+      name: 'Carlo',
+      lastName: 'Clamucha'
+    })
+  ).toEqual(['Carlo', 'Clamucha']);
+});
+
+test('map', () => {
+  expect(
+    oF.mapObject(
+      {
+        name: 'Carlo',
+        lastName: 'Clamucha'
+      },
+      item => {
+        return item;
+      }
+    )
+  ).toEqual({
+    name: 'Carlo',
+    lastName: 'Clamucha'
+  });
+});
+
+test('pairs', () => {
+  expect(oF.pairs({ name: 'Carlo', lastName: 'Clamucha' })).toEqual([
+    ['name', 'Carlo'],
+    ['lastName', 'Clamucha']
+  ]);
+});
+
+test('invert', () => {
+  expect(
+    oF.invert({
+      name: 'Carlo',
+      lastName: 'Clamucha'
+    })
+  ).toEqual({ Carlo: 'name', Clamucha: 'lastName' });
+});
+
+test('defaults', () => {
+  expect(
+    oF.defaults(
+      { name: 'Carlo', lastName: 'Clamucha' },
+      {
+        name: 'Test',
+        lastName: 'Test1'
+      }
+    )
+  ).toEqual({ name: 'Carlo', lastName: 'Clamucha' });
 });
