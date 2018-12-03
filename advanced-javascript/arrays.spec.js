8a6c5e6ff1093d235941d60ce59907b2d20e46ac
unknown <brandongrych@gmail.com>
2018-12-03 17:29:04 -0600

    finished mvp + stretch

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('./arrays');
+const aF = require('./arrays.js');
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -7,11 +7,46 @@ const arrayFunctions = require('./arrays');
 // hint 1. - you need to pass data to the functions and get expected output.
 // hint 2. - you should test the data type being called back, and perform some sort of operation on the data.
 
-describe('Arrays', () => {
-  describe('map', () => {
-    it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
-    });
-  });
+test('each', () => {
+  expect(
+    aF.each(['brandon', 'test'], (item, index) => {
+      return item;
+    })
+  ).toBe();
+});
+
+test('map', () => {
+  expect(
+    aF.map(['brandon', 'test'], item => {
+      return item;
+    })
+  ).toEqual(['brandon', 'test']);
+});
+
+test('reduce', () => {
+  expect(
+    aF.reduce(['brandon', 'test'], item => {
+      return item;
+    })
+  ).toEqual('brandon');
+});
+
+test('find', () => {
+  expect(
+    aF.find(['brandon', 'test'], item => {
+      return item === 'brandon' ? item : null;
+    })
+  ).toBe('brandon');
+});
+
+test('filter', () => {
+  expect(
+    aF.filter(['brandon', 'test'], item => {
+      return item === 'brandon' ? item : null;
+    })
+  ).toEqual(['brandon']);
+});
+
+test('flatten', () => {
+  expect(aF.flatten(['brandon', 'test'])).toEqual(['brandon', 'test']);
 });
--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,6 +1,190 @@
-const helpers = require("./project-1");
+const {
+    multiplyByTen,
+    subtractFive,
+    areSameLength,
+    areEqual,
+    lessThanNinety,
+    greaterThanFifty,
+    add,
+    subtract,
+    divide,
+    multiply,
+    getRemainder,
+    isEven,
+    isOdd,
+    square,
+    cube,
+    raiseToPower,
+    roundNumber,
+    roundUp,
+    addExclamationPoint,
+    combineNames,
+    getGreeting,
+    getRectangleArea,
+    getTriangleArea,
+    getCircleArea,
+    getRectangularPrismVolume
+} = require('./project-1');
 
 // start testing!
-describe("project-1.js", () => {
-    test("Try", () => {});
+
+test('multiply by Ten', () => {
+    const input = 10;
+    const expected = 100;
+    expect(multiplyByTen(input)).toEqual(expected);
+});
+
+test('subtract by five', () => {
+    const input = 10;
+    const expected = 5;
+    expect(subtractFive(input)).toEqual(expected);
+});
+
+test('are by same length', () => {
+    const input = 'toy';
+    const expected = 'dog';
+    expect(areSameLength(input, expected)).toBeTruthy();
+});
+
+test('are equal', () => {
+    const input = 20;
+    const expected = 20;
+    expect(areEqual(input, expected)).toBeTruthy();
+});
+
+test('less than ninety', () => {
+    const input = 100;
+    const expected = false;
+    expect(lessThanNinety(input, expected)).toBe(expected);
+});
+
+test('greater than fifty', () => {
+    const input = 100;
+    const expected = true;
+    expect(greaterThanFifty(input, expected)).toBe(expected);
+});
+
+test('add', () => {
+    const input = 2;
+    const inputTwo = 3;
+    const expected = 5;
+    expect(add(input, inputTwo)).toBe(expected);
+});
+
+test('subtract', () => {
+    const input = 3;
+    const inputTwo = 2;
+    const expected = 1;
+    expect(subtract(input, inputTwo)).toBe(expected);
+});
+
+test('divide', () => {
+    const input = 6;
+    const inputTwo = 2;
+    const expected = 3;
+    expect(divide(input, inputTwo)).toBe(expected);
+});
+
+test('multiply', () => {
+    const input = 6;
+    const inputTwo = 2;
+    const expected = 12;
+    expect(multiply(input, inputTwo)).toBe(expected);
+});
+
+test('get Remainder', () => {
+    const input = 6;
+    const inputTwo = 3;
+    const expected = 0;
+    expect(getRemainder(input, inputTwo)).toBe(expected);
+});
+
+test('is Even', () => {
+    const input = 6;
+    const expected = true;
+    expect(isEven(input)).toBe(expected);
+});
+
+test('is Odd', () => {
+    const input = 7;
+    const expected = true;
+    expect(isOdd(input)).toBe(expected);
+});
+
+test('square', () => {
+    const input = 3;
+    const expected = 9;
+    expect(square(input)).toBe(expected);
+});
+
+test('cube', () => {
+    const input = 3;
+    const expected = 27;
+    expect(cube(input)).toBe(expected);
+});
+
+test('raise To Power', () => {
+    const input = 3;
+    const inputTwo = 3;
+    const expected = 27;
+    expect(raiseToPower(input, inputTwo)).toBe(expected);
+});
+
+test('round Number', () => {
+    const input = 3.7;
+    const expected = 4;
+    expect(roundNumber(input)).toBe(expected);
+});
+
+test('roundUp', () => {
+    const input = 2.3;
+    const expected = 3;
+    expect(roundUp(input)).toBe(expected);
+});
+
+test('add Exclamation Point', () => {
+    const input = 'heyy';
+    const expected = 'heey!';
+    expect(addExclamationPoint(input)).toBe(expected);
+});
+
+test('combineNames', () => {
+    const input = 'Lennix';
+    const inputTwo = 'Sployte';
+    const expected = 'Lennix Sployte';
+    expect(combineNames(input, inputTwo)).toBe(expected);
+});
+
+test('getGreeting', () => {
+    const input = 'Lennix';
+    const expected = 'Hello Lennix!';
+    expect(getGreeting(input)).toBe(expected);
+});
+
+test('get Rectangle Area', () => {
+    const input = 3;
+    const inputTwo = 2;
+    const expected = 6;
+    expect(getRectangleArea(input, inputTwo)).toBe(expected);
+});
+
+test('get Triangle Area', () => {
+    const input = 3;
+    const inputTwo = 2;
+    const expected = 3;
+    expect(getTriangleArea(input, inputTwo)).toBe(expected);
+});
+
+test('get Circle Area', () => {
+    const input = 4;
+    const expected = 50.26548245743669;
+    expect(getCircleArea(input)).toBe(expected);
+});
+
+test('get Circle Area', () => {
+    const input = 4;
+    const inputTwo = 2;
+    const inputThree = 3;
+    const expected = 24;
+    expect(getRectangularPrismVolume(input, inputTwo, inputThree)).toBe(expected);
 });
