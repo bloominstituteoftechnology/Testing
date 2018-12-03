c233aea1082121b2c51ad9869276fd2dcc5b1d67
ssmodish <smodish@hotmail.com>
2018-12-03 18:30:40 -0500

    tuesday - at least one test for each case

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,40 +1,89 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
-test('Jest runs', () => {});
+test('getBiggest', () => {
+  expect(funcs.getBiggest(5, 9)).toBe(9);
+  expect(funcs.getBiggest(5, 5)).toBe(5);
+  expect(funcs.getBiggest(9, 5)).toBe(9);
+});
 
-test('getBiggest', () => {});
+test('greeting', () => {
+  expect(funcs.greeting('German')).toBe('Guten Tag!');
+  expect(funcs.greeting('Spanish')).toBe('Hola!');
+  expect(funcs.greeting('English')).toBe('Hello!');
+  expect(funcs.greeting()).toBe('Hello!');  
+});
 
-test('greeting', () => {});
+test('isTenOrFive', () => {
+  expect(funcs.isTenOrFive(10)).toBe(true);
+  expect(funcs.isTenOrFive(5)).toBe(true);
+  expect(funcs.isTenOrFive(9)).toBe(false);
+});
 
-test('isTenOrFive', () => {});
+test('isInRange', () => {
+  expect(funcs.isInRange(9)).toBe(false);
+  expect(funcs.isInRange(29)).toBe(true);
+  expect(funcs.isInRange(129)).toBe(false);
+});
 
-test('isInRange', () => {});
+test('isInteger', () => {
+  expect(funcs.isInteger(9)).toBe(true);
+  expect(funcs.isInteger(9.321)).toBe(false);
+});
 
-test('isInteger', () => {});
+test('fizzBuzz', () => {
+  expect(funcs.fizzBuzz(90)).toBe('fizzbuzz');
+  expect(funcs.fizzBuzz(80)).toBe('buzz');
+  expect(funcs.fizzBuzz(9)).toBe('fizz');
+  expect(funcs.fizzBuzz(7)).toBe(7);
+});
 
-test('fizzBuzz', () => {});
+test('isPrime', () => {
+  expect(funcs.isPrime(90)).toBe(false);
+  expect(funcs.isPrime(7)).toBe(true);
+});
 
-test('isPrime', () => {});
+test('returnFirst', () => {
+  expect(funcs.returnFirst([90, 42, 1863])).toBe(90);
+});
 
-test('returnFirst', () => {});
+test('returnLast', () => {
+  expect(funcs.returnLast([90, 42, 1863])).toBe(1863);
+});
 
-test('returnLast', () => {});
+test('getArrayLength', () => {
+  expect(funcs.getArrayLength([90, 42, 1863])).toBe(3);
+});
 
-test('getArrayLength', () => {});
+test('incrementByOne', () => {
+  expect(funcs.incrementByOne([90, 42, 1863])).toEqual([91, 43, 1864]);
+});
 
-test('incrementByOne', () => {});
+test('addItemToArray', () => {
+  expect(funcs.addItemToArray([90, 42, 1863], 19)).toEqual([90, 42, 1863, 19]);
+});
 
-test('addItemToArray', () => {});
+test('addItemToFront', () => {
+  expect(funcs.addItemToFront([90, 42, 1863], 19)).toEqual([19, 90, 42, 1863]);
+});
 
-test('addItemToFront', () => {});
+test('wordsToSentence', () => {
+  expect(funcs.wordsToSentence(["Don't", "be", "too", "proud", "of", "this", "technological", "terror", "you've", "constructed"])).toBe("Don't be too proud of this technological terror you've constructed");  
+});
 
-test('wordsToSentence', () => {});
+test('contains', () => {
+  expect(funcs.contains([90, 42, 1863], 19)).toBe(false);
+  expect(funcs.contains([90, 42, 1863], 42)).toBe(true);
+});
 
-test('contains', () => {});
+test('addNumbers', () => {
+  expect(funcs.addNumbers([90, 42, 1863])).toBe(1995);
+});
 
-test('addNumbers', () => {});
+test('averageTestScore', () => {
+  expect(funcs.averageTestScore([90, 42, 98])).toBe(76.666666666666666666666666666667);
+});
 
-test('averageTestScore', () => {});
-
-test('largestNumber', () => {});
+test('largestNumber', () => {
+  expect(funcs.largestNumber([90, 42, 1863])).toBe(1863);
+});
