104ae2ea4906920a3638b99c517fa27261b32144
samsisle <samko9522@gmail.com>
2018-12-03 13:24:47 -0800

    Configure first couple tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,21 @@
 const helpers = require('./project-1');
 
-// start testing!
+describe('multiply by 10', () => {
+  test('multiply by positive & negative 10', () => {
+    expect(helpers.multiplyByTen(3)).toBe(30);
+    expect(helpers.multiplyByTen(-3)).toBe(-30);
+  });
+  test('multiply with strings', () => {
+    expect(helpers.multiplyByTen('3')).toBe(30);
+    expect(helpers.multiplyByTen('-3')).toBe(-30);
+  });
+});
+
+test('subtract by 5', () => {
+  expect(helpers.subtractFive(3)).toBe(-2);
+});
+
+test('return true if strings are same length', () => {
+  expect(helpers.areSameLength('Sam', 'Eli')).toBeTruthy();
+  expect(helpers.areSameLength('Sam', 'Leroy')).toBeFalsy();
+});
