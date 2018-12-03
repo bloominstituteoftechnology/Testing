600e8187540fa2f5c3b74c4dcdefdcfa113484ad
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-03 15:35:00 -0600

    started testing

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,17 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+test('should run tests', () => {});
+
+
+describe('project-1.js', () => {
+
+    describe('multiplyByTen', () => {
+	it('should multiply number by 10', () => {
+	    const expected = 100;
+	    const actual = helpers.multiplyByTen(10);
+	    expect(actual).toEqual(expected);
+	});
+    });
+});
