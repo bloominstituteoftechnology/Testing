56e11c2398a1aa1c1ade641ee2026c5174b966ca
Drewsup123c <drewsup123c@gmail.com>
2018-12-03 13:41:43 -0700

    imported all the functions into project-1spec

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,30 @@
-const helpers = require('./project-1');
+const {
+    multiplyByTen,
+    subtractFive,
+    areSameLength,
+    areEqual,
+    lessThanNinety,
+    greaterThanFifty,
+    add,
+    subtract,
+    divide,
+    multiply,
+    getRemainder,
+    isEven,
+    isOdd,
+    square,
+    cube,
+    raiseToPower,
+    roundNumber,
+    roundUp,
+    addExclamationPoint,
+    combineNames,
+    getGreeting,
+    getRectangleArea,
+    getTriangleArea,
+    getCircleArea,
+    getRectangularPrismVolume
+    } = require('./project-1');
 
 // start testing!
+
