4402c5ba9ff39917d8005efc8656af6192fca705
Matthew Wright <matthewbwright@gmail.com>
2018-12-03 13:36:09 -0700

    completes multiplyByTen

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,6 +1,38 @@
-const helpers = require('./project-1');
+const {
+  multiplyByTen,
+  subtractFive,
+  areSameLength,
+  areEqual,
+  lessThanNinety,
+  greaterThanFifty,
+  add,
+  subtract,
+  divide,
+  multiply,
+  getRemainder,
+  isEven,
+  isOdd,
+  square,
+  cube,
+  raiseToPower,
+  roundNumber,
+  roundUp,
+  addExclamationPoint,
+  combineNames,
+  getGreeting,
+  getRectangleArea,
+  getTriangleArea,
+  getCircleArea,
+  getRectangularPrismVolume
+} = require('./project-1');
 
 // start testing!
 describe('Jest', () => {
-  test('jest runs', () => {})
-})
+  test('jest runs', () => {});
+});
+
+test('multiplyByTen', () => {
+  const input = 3;
+  const expected = 30;
+  expect(multiplyByTen(input)).toEqual(expected);
+});
