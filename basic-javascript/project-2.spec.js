77ebc1c59c9c99ed8ee33556b83f66654bd2433e
ssmodish <smodish@hotmail.com>
2018-12-03 17:11:45 -0500

    woops, first commit

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,40 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+test('Jest runs', () => {});
+
+test('getBiggest', () => {});
+
+test('greeting', () => {});
+
+test('isTenOrFive', () => {});
+
+test('isInRange', () => {});
+
+test('isInteger', () => {});
+
+test('fizzBuzz', () => {});
+
+test('isPrime', () => {});
+
+test('returnFirst', () => {});
+
+test('returnLast', () => {});
+
+test('getArrayLength', () => {});
+
+test('incrementByOne', () => {});
+
+test('addItemToArray', () => {});
+
+test('addItemToFront', () => {});
+
+test('wordsToSentence', () => {});
+
+test('contains', () => {});
+
+test('addNumbers', () => {});
+
+test('averageTestScore', () => {});
+
+test('largestNumber', () => {});
