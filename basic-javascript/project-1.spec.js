70b4e4542143e05f4de96ec39fdab68fecc0aa09
Brian G <brian.gregorius@gmail.com>
2018-12-03 14:54:39 -0500

    initial commit

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,35 @@
-const helpers = require('./project-1');
+const { 
+    multiplyByTen,
+    subtractFive,
+    areSameLength,
+    areEqual,
+    lessThanNinety,
+    greaterThanFifty,
+    add,
+    subtract,
+    divide,
+    multiply,
+    getRemainder,
+    isEven,
+    isOdd,
+    square,
+    cube,
+    raiseToPower,
+    roundNumber,
+    roundUp,
+    addExclamationPoint,
+    combineNames,
+    getGreeting,
+    getRectangleArea,
+    getTriangleArea,
+    getCircleArea,
+    getRectangularPrismVolume
+} = require('./project-1');
 
 // start testing!
+
+test('multiply by Ten', () => {
+    const input = 5;
+    const expected = 50;
+    expect(multiplyByTen(input)).toEqual(expected);
+})
