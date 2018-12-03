f3ecdb9781e94150b7ac4d43f97249755afca0b2
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-03 14:20:08 -0700

    last 5 tests, restructured from added every export, to only using helpers as import

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,30 +1,4 @@
-const {
-    multiplyByTen,
-    subtractFive,
-    areSameLength,
-    areEqual,
-    lessThanNinety,
-    greaterThanFifty,
-    add,
-    subtract,
-    divide,
-    multiply,
-    getRemainder,
-    isEven,
-    isOdd,
-    square,
-    cube,
-    raiseToPower,
-    roundNumber,
-    roundUp,
-    addExclamationPoint,
-    combineNames,
-    getGreeting,
-    getRectangleArea,
-    getTriangleArea,
-    getCircleArea,
-    getRectangularPrismVolume
-  } = require('./project-1');
+const helpers = require('./project-1');
 
 
 // start testing!
@@ -35,37 +9,37 @@ test('Jest runs', () => {
 test('multiply by ten', () => {
     const input = 10;
     const expected = 100;
-    expect(multiplyByTen(input)).toEqual(expected);
+    expect(helpers.multiplyByTen(input)).toEqual(expected);
 })
 
 test('subtract 5', () => {
     const input = 25;
     const expected = 20;
-    expect(subtractFive(input)).toEqual(expected);
+    expect(helpers.subtractFive(input)).toEqual(expected);
 })
 
 test('same length string', () => {
     const input1 = 'mary';
     const input2 = 'lary';
-    expect(areSameLength(input1,input2)).toBeTruthy();
+    expect(helpers.areSameLength(input1,input2)).toBeTruthy();
 })
 
 test('x & y are equal', () => {
     const input1 = 4;
     const input2 = 4;
-    expect(areEqual(input1,input2)).toBeTruthy();
+    expect(helpers.areEqual(input1,input2)).toBeTruthy();
 })
 
 test('number is less than 90', () => {
     const input = 40;
     
-    expect(lessThanNinety(input)).toBeTruthy();
+    expect(helpers.lessThanNinety(input)).toBeTruthy();
 })
 
 test('number is greater than 50', () => {
     const input = 60;
     
-    expect(greaterThanFifty(input)).toBeTruthy();
+    expect(helpers.greaterThanFifty(input)).toBeTruthy();
 })
 
 test('adding successful', () => {
@@ -73,7 +47,7 @@ test('adding successful', () => {
     const input2 = 4;
     const expected = 10; 
     
-    expect(add(input1,input2)).toEqual(expected);
+    expect(helpers.add(input1,input2)).toEqual(expected);
 })
 
 test('subtracting successful', () => {
@@ -81,7 +55,7 @@ test('subtracting successful', () => {
     const input2 = 4;
     const expected = 2; 
     
-    expect(subtract(input1,input2)).toEqual(expected);
+    expect(helpers.subtract(input1,input2)).toEqual(expected);
 })
 
 test('dividing successful', () => {
@@ -89,7 +63,7 @@ test('dividing successful', () => {
     const input2 = 3;
     const expected = 2; 
     
-    expect(divide(input1,input2)).toEqual(expected);
+    expect(helpers.divide(input1,input2)).toEqual(expected);
 })
 
 test('multiplying successful', () => {
@@ -97,7 +71,7 @@ test('multiplying successful', () => {
     const input2 = 2;
     const expected = 12; 
     
-    expect(multiply(input1,input2)).toEqual(expected);
+    expect(helpers.multiply(input1,input2)).toEqual(expected);
 })
 
 test('get the remainder', () => {
@@ -105,33 +79,33 @@ test('get the remainder', () => {
     const input2 = 4;
     const expected = 2; 
     
-    expect(getRemainder(input1,input2)).toEqual(expected);
+    expect(helpers.getRemainder(input1,input2)).toEqual(expected);
 })
 
 test('is even', () => {
     const input1 = 10;
     
-    expect(isEven(input1)).toBeTruthy();
+    expect(helpers.isEven(input1)).toBeTruthy();
 })
 
 test('is odd', () => {
     const input1 = 11;
     
-    expect(isOdd(input1)).toBeTruthy();
+    expect(helpers.isOdd(input1)).toBeTruthy();
 })
 
 test('number squared', () => {
     const input1 = 10;
     const expected = 100;
     
-    expect(square(input1)).toEqual(expected);
+    expect(helpers.square(input1)).toEqual(expected);
 })
 
 test('number cubed', () => {
     const input1 = 2;
     const expected = 8;
     
-    expect(cube(input1)).toEqual(expected);
+    expect(helpers.cube(input1)).toEqual(expected);
 })
 
 test('number raised to power of second number', () => {
@@ -139,28 +113,28 @@ test('number raised to power of second number', () => {
     const input2 = 3;
     const expected = 1000;
     
-    expect(raiseToPower(input1,input2)).toEqual(expected);
+    expect(helpers.raiseToPower(input1,input2)).toEqual(expected);
 })
 
 test('rounded number', () => {
     const input1 = 10.1;
     const expected = 10;
     
-    expect(roundNumber(input1)).toEqual(expected);
+    expect(helpers.roundNumber(input1)).toEqual(expected);
 })
 
 test('round number up', () => {
     const input1 = 10.1;
     const expected = 11;
     
-    expect(roundUp(input1)).toEqual(expected);
+    expect(helpers.roundUp(input1)).toEqual(expected);
 })
 
 test('add exclamation point to string', () => {
     const string = 'wow';
     const expected = 'wow!'; 
     
-    expect(addExclamationPoint(string)).toBe(expected);
+    expect(helpers.addExclamationPoint(string)).toBe(expected);
 })
 
 test('add names together', () => {
@@ -168,7 +142,46 @@ test('add names together', () => {
     const string2 = 'mom';
     const expected = 'wow mom'; 
     
-    expect(combineNames(string1,string2)).toBe(expected);
+    expect(helpers.combineNames(string1,string2)).toBe(expected);
+})
+
+test('get a greeting with name', () => {
+    const name = 'Savannah';
+    const expected = 'Hello Savannah!'; 
+    
+    expect(helpers.getGreeting(name)).toBe(expected);
+})
+
+test('get area of rectangle', () => {
+    const length = 8;
+    const width = 2;
+    const expected = 16; 
+    
+    expect(helpers.getRectangleArea(length,width)).toEqual(expected);
+})
+
+test('get area of triangle', () => {
+    const base = 10;
+    const height = 10;
+    const expected = 50; 
+    
+    expect(helpers.getTriangleArea(base,height)).toEqual(expected);
+})
+
+test('get area of circle', () => {
+    const radius = 10;
+    const expected = 314.1592653589793; 
+    
+    expect(helpers.getCircleArea(radius)).toEqual(expected);
+})
+
+test('get volume of rectangular prism', () => {
+    const width = 10; 
+    const height = 10; 
+    const length = 10;
+    const expected = 1000; 
+    
+    expect(helpers.getRectangularPrismVolume(width, height, length)).toEqual(expected);
 })
 
 
