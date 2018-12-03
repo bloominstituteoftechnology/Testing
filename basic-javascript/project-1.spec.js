0c2a37f6b16ba0717e5ac8e9b4d38b9f45d181d4
bummings <eabummings@gmail.com>
2018-12-03 17:07:48 -0500

    initial tests working

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,20 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1', () => {
+  describe('multiplyByTen', () => {
+    it('should multiply by ten', () => {
+      const timesTenExpected = 100;
+      const timesTenResult = helpers.multiplyByTen(10);
+      expect(timesTenResult).toEqual(timesTenExpected);
+    });
+  });
+
+  describe('subtractFive', () => {
+    it('should subtract five', () => {
+      const minusFiveExpected = 20;
+      const minusFiveResult = helpers.subtractFive(25);
+      expect(minusFiveResult).toEqual(minusFiveExpected);
+    });
+  });
+});
