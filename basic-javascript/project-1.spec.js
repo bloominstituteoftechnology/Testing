33cac1f2825708062d9bcf112f1a79b332ae0729
Edward J <edjeong99@gmail.com>
2018-12-03 16:33:00 -0500

    init

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,271 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+
+test('multiplyByTen', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.multiplyByTen(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('subtractFive', () => {
+    // Arrange
+    const input = 50;
+    const expected = 45;
+
+    // Act
+    let actual = helpers.subtractFive(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('areSameLength', () => {
+    // Arrange
+    const input1 = "wow";
+    const input2 = "who";
+    const expected = true;
+
+    // Act
+    let actual = helpers.(input1, input2);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+/*
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+
+test('', () => {
+    // Arrange
+    const input = 5;
+    const expected = 50;
+
+    // Act
+    let actual = helpers.(input);
+
+    // Assert
+    expect(actual).toBe(expected);
+});
+*/
