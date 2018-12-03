5b0c1f279df9b0de0c824b6dafb7bb824bcaddad
Lee Len Formento <leebatef@gmail.com>
2018-12-03 08:18:49 -0500

    Multiply by ten passed

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -5,4 +5,12 @@ const helpers = require('./project-1');
 describe('project-1.js', () => {
     describe('jest', () => {});
     test('should run tests', () => {});
+
+    describe('multiplyByTen', () => {
+        it('should multiply by ten', () => {
+            const expected = 120;
+            const actual = helpers.multiplyByTen(12);
+            expect(actual).toEqual(expected);
+        })
+    })
 })
