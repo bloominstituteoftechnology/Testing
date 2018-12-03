35fd2335f05b6c1c8da18c478c155df4a626540c
Yusuf Nafey <yusufnafey@yahoo.com>
2018-12-03 14:40:30 -0800

    first 10 complete

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -32,14 +32,69 @@ describe('Project 1', () => {
         test('multiplyByTen', () => {
             const input = 5;
             const expected = 50;
-            const actual = multiplyByTen(input)
-            expect(actual).toBe(expected)
-        })
+            const actual = multiplyByTen(input);
+            expect(actual).toEqual(expected);
+        });
+
+        test('subtractFive', () => {
+            const input = 5;
+            const expected = 0;
+            const actual = subtractFive(input);
+            expect(actual).toEqual(expected);
+        });
+
+        test('areSameLength', () => {
+            const a = 'yusuf';
+            const b = 'nafey';
+            const actual = areSameLength(a, b);
+            expect(actual).toBeTruthy;
+        });
+
+        test('areEqual', () => {
+            const a = 5;
+            const b = 9;
+            const actual = areEqual(a, b)
+            expect(actual).toBeTruthy;
+        });
+
+        test('lessThanNinety', () => {
+            const input = 1;
+            const actual = lessThanNinety(input)
+            expect(actual).toBeTruthy;
+        });
     });
 
-    // describe('6-10', () => {
-    
-    // });
+    describe('6-10', () => {
+        test('greaterThanFifty', () => {
+            const input = 51;
+            const actual = greaterThanFifty(input);
+            expect(actual).toBeTruthy;
+        });
+
+        test('add', () => {
+            const expected = 4;
+            const actual = add(2, 2);
+            expect(actual).toEqual(expected);
+        });
+
+        test('subtract', () => {
+            const expected = 4;
+            const actual = subtract(8, 4);
+            expect(actual).toEqual(expected);
+        });
+
+        test('divide', () => {
+            const expected = 4;
+            const actual = divide(16, 4);
+            expect(actual).toEqual(expected)
+        });
+
+        test('multiply', () => {
+            const expected = 16;
+            const actual = multiply(4, 4);
+            expect(actual).toEqual(expected);
+        });
+    });
 
     // describe('11-16', () => {
     
