589fbe9badcc30327c525daf8ac03153ff69d9b9
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-12-04 01:30:30 -0800

    multiplyByTen, subtractFive, areSameLength, areEqual, lessThanNinety

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -5,15 +5,48 @@ const helpers = require("./project-1");
 describe("project-1.js helpers", () => {
   test("jest runs", () => {});
 
-  beforeEach(() => {
-    console.log("running");
+  describe("multiplyByTen()", () => {
+    it("should multiply number by 10", () => {
+      expect(helpers.multiplyByTen(10)).toEqual(100);
+    });
+
+    it("should return a number", () => {
+      expect(typeof 10 === "number").toBeTruthy();
+    });
   });
 
-  describe("multiplyByTen()", () => {
-    it("should multiply provided number by 10", () => {
-      const expected = 100;
-      const actual = helpers.multiplyByTen(10);
-      expect(actual).toEqual(expected);
+  describe("subtractFive()", () => {
+    it("should subtract number by 5", () => {
+      expect(helpers.subtractFive(10)).toEqual(5);
+    });
+    it("should return a number", () => {
+      expect(typeof 5).toBe("number");
+    });
+  });
+
+  describe("areSameLength()", () => {
+    it("should be the same length", () => {
+      expect(helpers.areSameLength("string", "string")).toBeTruthy();
+    });
+  });
+
+  describe("areEqual()", () => {
+    it("should be equal", () => {
+      expect(helpers.areEqual(5, 5)).toBeTruthy();
+    });
+
+    it("should return a boolean", () => {
+      expect(typeof helpers.areEqual(5, 5)).toBe("boolean");
+    });
+  });
+
+  describe("lessThanNinety()", () => {
+    it("should be less than 90", () => {
+      expect(helpers.lessThanNinety(91)).not.toBeTruthy();
+    });
+
+    it("should return a boolean", () => {
+      expect(typeof helpers.lessThanNinety(89)).toBe("boolean");
     });
   });
 });
