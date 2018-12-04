b0cd05557b2e52727e99ef56c2866c89a51521d7
Axolotl <samuelmachat@gmail.com>
2018-12-04 17:06:58 -0500

    MOAR tests

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -1,4 +1,4 @@
-const arrayFunctions = require('./arrays');
+const { each, map, reduce, find, filter, flatten } = require('./arrays')
 
 // we've gone ahead and gotten a start here for you,
 // except, for some reason, none of our current assertions are working.
@@ -10,8 +10,57 @@ const arrayFunctions = require('./arrays');
 describe('Arrays', () => {
   describe('map', () => {
     it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
-    });
-  });
-});
+      expect(typeof map).toBe('function')
+    })
+  })
+})
+
+describe('each', () => {
+  test('each can access each item', () => {
+    const array = [2, 4, 6]
+    const newArray = []
+    const expected = [3, 5, 7]
+    const cb = item => newArray.push(item + 1)
+    each(array, cb)
+    expect(newArray).toEqual(expected)
+  })
+
+  test('each can access the indices', () => {
+    const array = [2, 4, 6]
+    const newArray = []
+    const expected = [0, 1, 2]
+    const cb = (_, index) => newArray.push(index)
+    each(array, cb)
+    expect(newArray).toEqual(expected)
+  })
+})
+
+describe('map', () => {
+  test('tests that each can modify each item', () => {
+    const array = [2, 4, 6]
+    const cb = item => item + 1
+    const expected = [3, 5, 7]
+    const actual = map(array, cb)
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('reduce', () => {
+  test('reduce works', () => {
+    const array = [1, 2, 3, 4, 5]
+    const cb = (acc, item) => acc + item
+    const expected = 15
+    const actual = reduce(array, cb)
+    expect(actual).toEqual(expected)
+  })
+
+  // test('reduce works with third param', () => {
+  //   const array = [1, 2, 3, 4, 5]
+  //   const cb = (acc, item) => {
+  //     items: acc.items ? [...acc.items, item] : [item]
+  //   }
+  //   const expected = { items: [1, 2, 3, 4, 5] }
+  //   const actual = reduce(array, cb, { items: [] })
+  //   expect(actual).toEqual(expected)
+  // })
+})
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,60 @@
-const funcs = require('./project-2');
+const {
+  getBiggest,
+  greeting,
+  isTenOrFive,
+  isInRange,
+  isInteger,
+  fizzBuzz,
+  isPrime,
+  returnFirst,
+  returnLast,
+  getArrayLength,
+  incrementByOne,
+  addItemToArray,
+  addItemToFront,
+  wordsToSentence,
+  contains,
+  addNumbers,
+  averageTestScore,
+  largestNumber
+} = require('./project-2')
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Jest', () => {
+  test('Jest runs', () => {})
+})
+
+describe('getBiggest', () => {
+  test('returns biggest of different sizes', () => {
+    const input1 = 10
+    const input2 = 12
+    const expected = input2
+    const actual = getBiggest(input1, input2)
+    expect(actual).toBe(expected)
+  })
+
+  test('returns second if same sizes', () => {
+    const input1 = 10
+    const input2 = 10
+    const expected = input2
+    const actual = getBiggest(input1, input2)
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('greeting', () => {
+  test('returns German', () => {
+    expect(greeting('German')).toBe('Guten Tag!')
+  })
+
+  test('returns Spanish', () => {
+    expect(greeting('Spanish')).toBe('Hola!')
+  })
+
+  test('returns English from no input', () => {
+    expect(greeting()).toBe('Hello!')
+  })
+
+  test('returns English from unknown input', () => {
+    expect(greeting(';lkjafoiehafpoieaiea')).toBe('Hello!')
+  })
+})
