2a37c715de8d03d2f6bacea14298ea8ec562a60f
unknown <quan.nguyen199712@gmail.com>
2018-12-03 20:04:47 -0500

    some more :(

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -35,9 +35,47 @@ describe("areEqual", () => {
       const result= helpers.areEqual(5,5);
       expect(result).toBe(true)
   })
-  test("areEqualFalse", ()=>{
+  test("areEqualIntFalse", ()=>{
       expect(helpers.areEqual(4,5)).toBe(false)
   })
 });
-
+describe('lessThan90',()=>{
+    test('true', ()=>{
+        expect(helpers.lessThanNinety(5)).toBe(true)
+    })
+    test('false', ()=>{
+        expect(helpers.lessThanNinety(91)).toBe(false)
+    })
+})
+describe('greaterThan50',()=>{
+    test('true', ()=>{
+        expect(helpers.greaterThanFifty(51)).toBe(true)
+    })
+    test('false', ()=>{
+        expect(helpers.greaterThanFifty(49)).toBe(false)
+    })
+})
+describe('add',()=>{
+    test('number',()=>{
+        expect(helpers.add(5,4)).toBe(9)
+    })
+})
+describe('subtract',()=>{
+    test('number',()=>{
+        expect(helpers.subtract(5,4)).toBe(1)
+    })
+})
+describe('divide',()=>{
+    test('number',()=>{
+        expect(helpers.divide(5,1)).toBe(5)
+    })
+})
+describe('multiply', ()=>{
+    test('number', ()=>{
+        expect(helpers.multiply(5,6)).toBe(30)
+    })
+})
+describe('getRemainder',()=>{
+    
+})
 // start testing!
