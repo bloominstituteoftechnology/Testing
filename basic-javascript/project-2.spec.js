d89ea06318dd214620f781a21691d2189f5d4cc5
sean-one <basement365@gmail.com>
2018-12-04 14:04:59 -0800

    working through day 2

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -5,5 +5,29 @@ describe('project-2 testing', () => {
     it('find the largest number', () => {
         expect(funcs.getBiggest(10, 8)).toBe(10);
         expect(funcs.getBiggest(8, 12)).toBe(12);
+        // make sure the input is a number
+    });
+
+    it('greetings in different languages', () => {
+        expect(funcs.greeting('English')).toBe('Hello!');
+        expect(funcs.greeting('Jibberish')).toBe('Hello!');
+        // make sure the input is a string
+    });
+
+    it('check if it is 10 or 5', () => {
+        expect(funcs.isTenOrFive(2)).toBe(false);
+        expect(funcs.isTenOrFive(15)).toBe(false);
+        // make sure input is a number
+    });
+
+    it('check if the number is <50 and >20', () => {
+        expect(funcs.isInRange(30)).toBe(true);
+        expect(funcs.isInRange(72)).toBe(false);
+        // make sure input is a number
+    });
+
+    it('change a float into an integer', () => {
+        expect(funcs.isInteger(3.2)).toBe(false);
+        expect(funcs.isInteger(4)).toBe(true);
     });
 });
