f8fe5d88541b4e27ec59f4020182c5deb7cd91df
Rahul Desai <rd825@georgetown.edu>
2018-12-04 18:58:33 -0500

    Edited array tests

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -20,38 +20,34 @@ describe('each', () => {
   test('each can access each item', () => {
     const array = [2, 4, 6]
     const newArray = []
-    const expected = [3, 5, 7]
     const cb = item => newArray.push(item + 1)
     arrayFunctions.each(array, cb)
-    expect(newArray).toEqual(expected)
+    expect(newArray).toEqual([3,5,7])
   })
 
   test('each can access the indices', () => {
     const array = [2, 4, 6]
     const newArray = []
-    const expected = [0, 1, 2]
     const cb = (_, index) => newArray.push(index)
     arrayFunctions.each(array, cb)
-    expect(newArray).toEqual(expected)
+    expect(newArray).toEqual([0, 1, 2])
   })
 })
 
 describe('map', () => {
   test('tests that each can modify each item', () => {
     const array = [2, 4, 6]
-    const cb = item => item + 1
-    const expected = [3, 5, 7]
+    const cb = item => item - 1
     const actual = arrayFunctions.map(array, cb)
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual([1, 3, 5])
   })
 })
 
 describe('reduce', () => {
   test('reduce works', () => {
-    const array = [1, 2, 3, 4, 5]
+    const array = [1, 2, 3, 4]
     const cb = (acc, item) => acc + item
-    const expected = 15
     const actual = arrayFunctions.reduce(array, cb)
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual(10)
   })
 })
