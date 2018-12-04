64b9fdbe69be6f21d9eedbe2bfe85598e181715e
Rahul Desai <rd825@georgetown.edu>
2018-12-04 18:24:44 -0500

    Arrays tests

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -11,7 +11,47 @@ describe('Arrays', () => {
   describe('map', () => {
     it('should be a function', () => {
       const map = arrayFunctions.map;
-      expect(typeof map).toBe('object');
+      expect(typeof map).toBe('function');
     });
   });
 });
+
+describe('each', () => {
+  test('each can access each item', () => {
+    const array = [2, 4, 6]
+    const newArray = []
+    const expected = [3, 5, 7]
+    const cb = item => newArray.push(item + 1)
+    arrayFunctions.each(array, cb)
+    expect(newArray).toEqual(expected)
+  })
+
+  test('each can access the indices', () => {
+    const array = [2, 4, 6]
+    const newArray = []
+    const expected = [0, 1, 2]
+    const cb = (_, index) => newArray.push(index)
+    arrayFunctions.each(array, cb)
+    expect(newArray).toEqual(expected)
+  })
+})
+
+describe('map', () => {
+  test('tests that each can modify each item', () => {
+    const array = [2, 4, 6]
+    const cb = item => item + 1
+    const expected = [3, 5, 7]
+    const actual = arrayFunctions.map(array, cb)
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('reduce', () => {
+  test('reduce works', () => {
+    const array = [1, 2, 3, 4, 5]
+    const cb = (acc, item) => acc + item
+    const expected = 15
+    const actual = arrayFunctions.reduce(array, cb)
+    expect(actual).toEqual(expected)
+  })
+})
