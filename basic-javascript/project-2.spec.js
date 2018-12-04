5e4f0539f02a1da7bb3be2589448295d843bdd2e
unknown <you@example.com>
2018-12-04 15:45:05 -0600

    finished up to additemtofront

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -33,3 +33,33 @@ test('fizz buzz', () => {
     const num = 20
     expect(fizzBuzz(num)).toBeTruthy()
 })
+test('is prime', () => {
+    const num = 7
+    expect(isPrime(num)).toBeTruthy()
+})
+test('return first index of array', () => {
+    const arr = ['one', 'two', 'three']
+    expect(returnFirst(arr)).toBe(arr[0])
+})
+test('return last item in an array', () => {
+    const arr = ['one', 'two', 'three']
+    expect(returnLast(arr)).toBe(arr[arr.length-1])
+})
+test('get array length', () => {
+    const arr = ['one', 'two', 'three']
+    expect(getArrayLength(arr)).toBe(arr.length)
+})
+test('increment by one', () => {
+    const arr = [1,2,3]
+    expect(incrementByOne(arr)).toEqual([2,3,4])
+})
+test('add item to array', () => {
+    const arr = ['one', 'two', 'three']
+    const item = 'four'
+    expect(addItemToArray(arr, item)).toEqual([...arr])
+})
+test('add item to array', () => {
+    const arr = ['one', 'two', 'three']
+    const item = 'zero'
+    expect(addItemToFront(arr, item)).toEqual([...arr])
+})
