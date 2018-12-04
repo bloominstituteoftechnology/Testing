529d02e6226e69735a7af011f9e5e39c787b4da2
Brian G <brian.gregorius@gmail.com>
2018-12-04 13:54:32 -0500

    cleaned code

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,184 +1,158 @@
-const {
-    multiplyByTen,
-    subtractFive,
-    areSameLength,
-    areEqual,
-    lessThanNinety,
-    greaterThanFifty,
-    add,
-    subtract,
-    divide,
-    multiply,
-    getRemainder,
-    isEven,
-    isOdd,
-    square,
-    cube,
-    raiseToPower,
-    roundNumber,
-    roundUp,
-    addExclamationPoint,
-    combineNames,
-    getGreeting,
-    getRectangleArea,
-    getTriangleArea,
-    getCircleArea,
-    getRectangularPrismVolume
-} = require('./project-1');
+const helpers = require('./project-1');
 
 // start testing!
 
 test('multiply by Ten', () => {
     const input = 5;
     const expected = 50;
-    expect(multiplyByTen(input)).toEqual(expected);
+    expect(helpers.multiplyByTen(input)).toEqual(expected);
 });
 
 test('subtract by five', () => {
     const input = 10;
     const expected = 5;
-    expect(subtractFive(input)).toEqual(expected);
+    expect(helpers.subtractFive(input)).toEqual(expected);
 });
 
 test('are by same length', () => {
     const input = 'toy';
     const expected = 'dog';
-    expect(areSameLength(input, expected)).toBeTruthy();
+    expect(helpers.areSameLength(input, expected)).toBeTruthy();
 });
 
 test('are equal', () => {
     const input = 10;
     const expected = 10;
-    expect(areEqual(input, expected)).toBeTruthy();
+    expect(helpers.areEqual(input, expected)).toBeTruthy();
 });
 
 test('less than ninety', () => {
     const input = 100;
     const expected = false;
-    expect(lessThanNinety(input, expected)).toBe(expected);
+    expect(helpers.lessThanNinety(input, expected)).toBe(expected);
 });
 
 test('greater than fifty', () => {
     const input = 100;
     const expected = true;
-    expect(greaterThanFifty(input, expected)).toBe(expected);
+    expect(helpers.greaterThanFifty(input, expected)).toBe(expected);
 });
 
 test('add', () => {
     const input = 2;
     const inputTwo = 3;
     const expected = 5;
-    expect(add(input, inputTwo)).toBe(expected);
+    expect(helpers.add(input, inputTwo)).toBe(expected);
 });
 
 test('subtract', () => {
     const input = 3;
     const inputTwo = 2;
     const expected = 1;
-    expect(subtract(input, inputTwo)).toBe(expected);
+    expect(helpers.subtract(input, inputTwo)).toBe(expected);
 });
 
 test('divide', () => {
     const input = 6;
     const inputTwo = 2;
     const expected = 3;
-    expect(divide(input, inputTwo)).toBe(expected);
+    expect(helpers.divide(input, inputTwo)).toBe(expected);
 });
 
 test('multiply', () => {
     const input = 6;
     const inputTwo = 2;
     const expected = 12;
-    expect(multiply(input, inputTwo)).toBe(expected);
+    expect(helpers.multiply(input, inputTwo)).toBe(expected);
 });
 
 test('get Remainder', () => {
     const input = 6;
     const inputTwo = 3;
     const expected = 0;
-    expect(getRemainder(input, inputTwo)).toBe(expected);
+    expect(helpers.getRemainder(input, inputTwo)).toBe(expected);
 });
 
 test('is Even', () => {
     const input = 6;
     const expected = true;
-    expect(isEven(input)).toBe(expected);
+    expect(helpers.isEven(input)).toBe(expected);
 });
 
 test('is Odd', () => {
     const input = 7;
     const expected = true;
-    expect(isOdd(input)).toBe(expected);
+    expect(helpers.isOdd(input)).toBe(expected);
 });
 
 test('square', () => {
     const input = 3;
     const expected = 9;
-    expect(square(input)).toBe(expected);
+    expect(helpers.square(input)).toBe(expected);
 });
 
 test('cube', () => {
     const input = 3;
     const expected = 27;
-    expect(cube(input)).toBe(expected);
+    expect(helpers.cube(input)).toBe(expected);
 });
 
 test('raise To Power', () => {
     const input = 3;
     const inputTwo = 3;
     const expected = 27;
-    expect(raiseToPower(input, inputTwo)).toBe(expected);
+    expect(helpers.raiseToPower(input, inputTwo)).toBe(expected);
 });
 
 test('round Number', () => {
     const input = 3.7;
     const expected = 4;
-    expect(roundNumber(input)).toBe(expected);
+    expect(helpers.roundNumber(input)).toBe(expected);
 });
 
 test('roundUp', () => {
     const input = 2.3;
     const expected = 3;
-    expect(roundUp(input)).toBe(expected);
+    expect(helpers.roundUp(input)).toBe(expected);
 });
 
 test('add Exclamation Point', () => {
     const input = 'heyo';
     const expected = 'heyo!';
-    expect(addExclamationPoint(input)).toBe(expected);
+    expect(helpers.addExclamationPoint(input)).toBe(expected);
 });
 
 test('combineNames', () => {
     const input = 'mike';
     const inputTwo = 'smith';
     const expected = 'mike smith';
-    expect(combineNames(input, inputTwo)).toBe(expected);
+    expect(helpers.combineNames(input, inputTwo)).toBe(expected);
 });
 
 test('getGreeting', () => {
     const input = 'mike';
     const expected = 'Hello mike!';
-    expect(getGreeting(input)).toBe(expected);
+    expect(helpers.getGreeting(input)).toBe(expected);
 });
 
 test('get Rectangle Area', () => {
     const input = 3;
     const inputTwo = 2;
     const expected = 6;
-    expect(getRectangleArea(input, inputTwo)).toBe(expected);
+    expect(helpers.getRectangleArea(input, inputTwo)).toBe(expected);
 });
 
 test('get Triangle Area', () => {
     const input = 3;
     const inputTwo = 2;
     const expected = 3;
-    expect(getTriangleArea(input, inputTwo)).toBe(expected);
+    expect(helpers.getTriangleArea(input, inputTwo)).toBe(expected);
 });
 
 test('get Circle Area', () => {
     const input = 4;
     const expected = 50.26548245743669;
-    expect(getCircleArea(input)).toBe(expected);
+    expect(helpers.getCircleArea(input)).toBe(expected);
 });
 
 test('get Circle Area', () => {
@@ -186,5 +160,5 @@ test('get Circle Area', () => {
     const inputTwo = 2;
     const inputThree = 3;
     const expected = 24;
-    expect(getRectangularPrismVolume(input, inputTwo, inputThree)).toBe(expected);
+    expect(helpers.getRectangularPrismVolume(input, inputTwo, inputThree)).toBe(expected);
 });
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,23 +1,4 @@
-const {
-    getBiggest,
-    greeting,
-    isTenOrFive,
-    isInRange,
-    isInteger,
-    fizzBuzz,
-    isPrime,
-    returnFirst,
-    returnLast,
-    getArrayLength,
-    incrementByOne,
-    addItemToArray,
-    addItemToFront,
-    wordsToSentence,
-    contains,
-    addNumbers,
-    averageTestScore,
-    largestNumber
-} = require('./project-2');
+const helpers = require('./project-2');
 
 test('Jest runs', () => {
 
@@ -25,103 +6,103 @@ test('Jest runs', () => {
 
 test('get Biggest', () => {
 
-    expect(getBiggest(5, 6)).toBe(6);
-    expect(getBiggest(5, 5)).toBe(5);
-    expect(getBiggest(9, 5)).toBe(9);
+    expect(helpers.getBiggest(5, 6)).toBe(6);
+    expect(helpers.getBiggest(5, 5)).toBe(5);
+    expect(helpers.getBiggest(9, 5)).toBe(9);
 });
 
 test('greeting', () => {
-    expect(greeting('German')).toBe('Guten Tag!');
-    expect(greeting('Spanish')).toBe('Hola!');
-    expect(greeting('English')).toBe('Hello!');
+    expect(helpers.greeting('German')).toBe('Guten Tag!');
+    expect(helpers.greeting('Spanish')).toBe('Hola!');
+    expect(helpers.greeting('English')).toBe('Hello!');
 });
 
 
 test('is Ten Or Five', () => {
-    expect(isTenOrFive(4)).toBe(false);
-    expect(isTenOrFive(5)).toBe(true);
-    expect(isTenOrFive(10)).toBe(true);
+    expect(helpers.isTenOrFive(4)).toBe(false);
+    expect(helpers.isTenOrFive(5)).toBe(true);
+    expect(helpers.isTenOrFive(10)).toBe(true);
 });
 
 test('is In Range', () => {
-    expect(isInRange(5)).toBe(false);
-    expect(isInRange(25)).toBe(true);
-    expect(isInRange(50)).toBe(false);
+    expect(helpers.isInRange(5)).toBe(false);
+    expect(helpers.isInRange(25)).toBe(true);
+    expect(helpers.isInRange(50)).toBe(false);
 });
 
 test('is Integer', () => {
-    expect(isInteger(5)).toBe(true);
-    expect(isInteger(5.1)).toBe(false);
+    expect(helpers.isInteger(5)).toBe(true);
+    expect(helpers.isInteger(5.1)).toBe(false);
 });
 
 test('fizzBuzz', () => {
-    expect(fizzBuzz(5)).toBe('buzz');
-    expect(fizzBuzz(3)).toBe('fizz');
-    expect(fizzBuzz(2)).toBe(2);
-    expect(fizzBuzz(15)).toBe('fizzbuzz');
+    expect(helpers.fizzBuzz(5)).toBe('buzz');
+    expect(helpers.fizzBuzz(3)).toBe('fizz');
+    expect(helpers.fizzBuzz(2)).toBe(2);
+    expect(helpers.fizzBuzz(15)).toBe('fizzbuzz');
 });
 
 test('is Prime', () => {
-    expect(isPrime(13)).toBe(true);
-    expect(isPrime(4)).toBe(false);
-    expect(isPrime(67)).toBe(true);
+    expect(helpers.isPrime(13)).toBe(true);
+    expect(helpers.isPrime(4)).toBe(false);
+    expect(helpers.isPrime(67)).toBe(true);
 });
 
 test('return First', () => {
-    expect(returnFirst([1, 2, 3, 4])).toBe(1);
-    expect(returnFirst(['cat', 'dog', 'bird'])).toBe('cat');
+    expect(helpers.returnFirst([1, 2, 3, 4])).toBe(1);
+    expect(helpers.returnFirst(['cat', 'dog', 'bird'])).toBe('cat');
 });
 
 test('return Last', () => {
-    expect(returnLast(['cat', 'dog', 'bird'])).toBe('bird');
-    expect(returnLast([1, 2, 3, 4])).toBe(4);
+    expect(helpers.returnLast(['cat', 'dog', 'bird'])).toBe('bird');
+    expect(helpers.returnLast([1, 2, 3, 4])).toBe(4);
 });
 
 test('getArray Length', () => {
     const input = [1, 2, 3, 4];
     const expected = 4;
-    expect(getArrayLength(input)).toBe(expected);
+    expect(helpers.getArrayLength(input)).toBe(expected);
 });
 
 test('increment By One', () => {
     const input = [1, 2, 3, 4];
     const expected = [2, 3, 4, 5];
-    expect(incrementByOne(input)).toEqual(expected);
+    expect(helpers.incrementByOne(input)).toEqual(expected);
 });
 
 test('add Item To Array', () => {
-    expect(addItemToArray([1, 2, 3, 4], 7)).toEqual([1, 2, 3, 4, 7]);
-    expect(addItemToArray(['cat', 'dog', 'bird'], 'fish')).toEqual(['cat', 'dog', 'bird', 'fish']);
+    expect(helpers.addItemToArray([1, 2, 3, 4], 7)).toEqual([1, 2, 3, 4, 7]);
+    expect(helpers.addItemToArray(['cat', 'dog', 'bird'], 'fish')).toEqual(['cat', 'dog', 'bird', 'fish']);
 });
 
 test('add Item To Front', () => {
-    expect(addItemToFront(['cat', 'dog', 'bird'], 'fish')).toEqual(['fish', 'cat', 'dog', 'bird']);
-    expect(addItemToFront([1, 2, 3, 4], 7)).toEqual([7, 1, 2, 3, 4]);
+    expect(helpers.addItemToFront(['cat', 'dog', 'bird'], 'fish')).toEqual(['fish', 'cat', 'dog', 'bird']);
+    expect(helpers.addItemToFront([1, 2, 3, 4], 7)).toEqual([7, 1, 2, 3, 4]);
 });
 
 test('words To Sentence', () => {
-    const input = 'the dog';
-    const expected = "t h e   d o g";
-    expect(wordsToSentence(input)).toEqual(expected);
+    const input = ['the', 'dog'];
+    const expected = "the dog";
+    expect(helpers.wordsToSentence(input)).toEqual(expected);
 });
 
 test('contains', () => {
     const input = ['cat', 'dog', 'bird'];
-    expect(contains(input, 'fish')).toEqual(false);
-    expect(contains(input, 'cat')).toEqual(true);
+    expect(helpers.contains(input, 'fish')).toEqual(false);
+    expect(helpers.contains(input, 'cat')).toEqual(true);
 });
 
 test('add numbers', () => {
-    expect(addNumbers([2, 3, 4, 5])).toEqual(14);
+    expect(helpers.addNumbers([2, 3, 4, 5])).toEqual(14);
 })
 
 test('average Test Score', () => {
     const input = [80, 50, 43, 100, 95, 89, 99, 95];
     const expected = 81.375;
-    expect(averageTestScore(input)).toEqual(expected);
+    expect(helpers.averageTestScore(input)).toEqual(expected);
 });
 
 test('largest Number', () => {
-    expect(largestNumber([80, 50, 43, 100, 95, 89, 99, 95])).toEqual(100);
-    expect(largestNumber([80, 50, 143, 100, 95, 89, 99, 95])).toEqual(143);
+    expect(helpers.largestNumber([80, 50, 43, 100, 95, 89, 99, 95])).toEqual(100);
+    expect(helpers.largestNumber([80, 50, 143, 100, 95, 89, 99, 95])).toEqual(143);
 });
