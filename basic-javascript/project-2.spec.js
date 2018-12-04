21592ebb7cbf5093dc14306b3f58a2f4d25a679f
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-04 14:06:51 -0700

    tests (fissbuzz - getARrayLength)

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -36,4 +36,34 @@ describe('project-2', () => {
             expect(funcs.isInteger(30)).toBeTruthy()
         })
     })
+
+    describe('fizzBuzz', () => {
+        it('fizzbuzz if divisible by 15, buzz divisible by 5, fizz divisible by 3', () => {
+            expect(funcs.fizzBuzz(30)).toBe('fizzbuzz')
+        })
+    })
+
+    describe('isPrime', () => {
+        it('returns true if prime', () => {
+            expect(funcs.isPrime(31)).toBeTruthy()
+        })
+    })
+
+    describe('returnFirst', () => {
+        it('returns first item in array', () => {
+            expect(funcs.returnFirst([4,3,5,7,5,6,7])).toEqual(4)
+        })
+    })
+
+    describe('returnLast', () => {
+        it('returns last item in array', () => {
+            expect(funcs.returnLast([4,3,5,7,5,6,7])).toEqual(7)
+        })
+    })
+
+    describe('getArrayLength', () => {
+        it('returns length of array', () => {
+            expect(funcs.getArrayLength([4,3,5,7,5,6,7])).toEqual(7)
+        })
+    })
 })
