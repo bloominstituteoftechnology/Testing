ed4f7830776c5485aee8a08707e76bc44d5b87a2
Gannon <gkleblanc@gmail.com>
2018-12-04 16:30:45 -0500

    doing some refactoring to project one for fun, finished project two using unit based testing only to more closely follow what we did in lecture though not as intense. Might add more intensity later.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -47,7 +47,7 @@ nums.forEach(func => test('returns a number', () => returnsNumber(func)))
 //string based functions
 const returnsString = func => {
     let result = func('test')
-    result.includes('undefined') ? result = func('infinite', 'jest') : result
+    result.includes('undefined') ? result = func('test1', 'test2') : result
     expect(typeof result === 'string').toBeTruthy()
 }
 
@@ -56,7 +56,7 @@ strs.forEach(func => test('returns a string', () => returnsString(func)))
 
 //boolean string based functions
 const returnsBooleanStr = func => {
-    let result = func('jest', 'jest')
+    let result = func('blah', 'dah')
     expect(typeof result === 'boolean').toBeTruthy()
 }
 
@@ -87,11 +87,11 @@ const validateResults = expected => {
     })
     strs.forEach(func => {
         let result = func('test')
-        result.includes('undefined') ? result = func('infinite', 'jest') : result
+        result.includes('undefined') ? result = func('test1', 'test2') : result
         actual.push(result)
     })
     boolStr.forEach(func => {
-        let result = func('jest', 'jest')
+        let result = func('blah', 'dah')
         actual.push(result)
     })
     boolNum.forEach(func => {
@@ -102,5 +102,129 @@ const validateResults = expected => {
     expect(actual).toEqual(expected)
 }
 
-let expected = [50, -4, 25, 125, 5, 5, 78.53981633974483, 7, 3, 2.5, 10, 1, 25, 10, 5, 6, 'test!', 'infinite jest', 'Hello test!', true, true, true, false, true, false]
+let expected = [50, -4, 25, 125, 5, 5, 78.53981633974483, 7, 3, 2.5, 10, 1, 25, 10, 5, 6, 'test!', 'blah dah', 'Hello test!', true, true, true, false, true, false]
 test('validate results', () => validateResults(expected))
+
+//Not sure if the above was true "unit" testing, so redoing this a different way
+
+const helpers = require("./project-1");
+
+describe("multipliesByTen", () => {
+    test("multiplies properly", () => {
+        const answer = helpers.multiplyByTen(2, 10);
+        expect(answer).toBe(20);
+    });
+});
+describe("subtractFive", () => {
+    test("subtracts by five", () => {
+        const answer = helpers.subtractFive(10);
+        expect(answer).toBe(5);
+    });
+});
+describe("areSameLength", () => {
+    test("strings are same length", () => {
+        const answer = helpers.areSameLength("asdf", "zcvv");
+        expect(answer).toBe(true);
+    });
+    test("is a string", () => {
+        expect(() => helpers.areSameLength("hello")).toThrow();
+    });
+});
+describe("areEqual", () => {
+    test("variables are equal", () => {
+        const answer = helpers.areEqual(1, 1);
+        expect(answer).toBe(true);
+    });
+});
+describe("lessThanNinety", () => {
+    test("is less than ninety", () => {
+        const answer = helpers.lessThanNinety(80);
+        expect(answer).toBe(true);
+    });
+});
+describe("greaterThanFifty", () => {
+    test("is greater than fifty", () => {
+        const answer = helpers.greaterThanFifty(51);
+        expect(answer).toBe(true);
+    });
+});
+describe("add", () => {
+    const nameOfTest = "adds two numbers";
+    const callback = () => {
+        const sum = helpers.add(25, 5);
+        expect(sum).toBe(30);
+    };
+    test(nameOfTest, callback);
+});
+describe("subrtract", () => {
+    const difference = helpers.subtract(10, 5);
+    expect(difference).toBe(5);
+});
+describe("divide", () => {
+    const dividen = helpers.divide(10, 5);
+    expect(dividen).toBe(2);
+});
+describe("multiply", () => {
+    const product = helpers.multiply(10, 5);
+    expect(product).toBe(50);
+});
+describe("getRemainder", () => {
+    const remainder = helpers.getRemainder(10, 3);
+    expect(remainder).toEqual(1);
+});
+describe("isEven", () => {
+    const even = helpers.isEven(68);
+    expect(even).toBe(true);
+});
+describe("isOdd", () => {
+    const odd = helpers.isOdd(69);
+    expect(odd).toBe(true);
+});
+describe("square", () => {
+    const square = helpers.square(12);
+    expect(square).toEqual(144);
+});
+describe("cube", () => {
+    const cube = helpers.cube(3);
+    expect(cube).toEqual(27);
+});
+describe("raiseToPower", () => {
+    const answer = helpers.raiseToPower(3, 3);
+    expect(answer).toEqual(27);
+});
+describe("roundNumber", () => {
+    const round = helpers.roundNumber(3.5);
+    expect(round).toEqual(4);
+});
+describe("roundUp", () => {
+    const roundUp = helpers.roundUp(3.3);
+    expect(roundUp).toEqual(4);
+});
+describe("addExclamationPoint", () => {
+    const resultString = helpers.addExclamationPoint("hello world");
+    expect(resultString).toEqual("hello world!");
+});
+describe("combineNames", () => {
+    const combinedNames = helpers.combineNames("Chance", "Halo");
+    expect(combinedNames).toEqual("Chance Halo");
+});
+describe("getGreeting", () => {
+    const greeting = helpers.getGreeting("Chance");
+    expect(greeting).toEqual("Hello Chance!");
+});
+describe("getRectangeArea", () => {
+    const area = helpers.getRectangleArea(2, 4);
+    expect(area).toEqual(8);
+});
+describe("getTriangleArea", () => {
+    const area = helpers.getTriangleArea(2, 4);
+    expect(area).toEqual(4);
+});
+describe("getCircleArea", () => {
+    const area = helpers.getCircleArea(2);
+    expect(area).toEqual(4 * Math.PI);
+});
+describe("getRectangularPrismVolume", () => {
+    const volume = helpers.getRectangularPrismVolume(2, 3, 4);
+    expect(volume).toBe(24);
+});
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,165 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+describe("getBiggest", () => {
+    test("gets the biggest variable", () => {
+        const biggest = funcs.getBiggest(2, 3);
+
+        expect(biggest).toBe(3);
+    });
+});
+
+describe("greeting", () => {
+    test("gets the correct greeting", () => {
+        const greeting = funcs.greeting("Spanish");
+
+        expect(greeting).toEqual("Hola!");
+    });
+});
+
+describe("isTenOrGive", () => {
+    test("tests if number is 10 or 5", () => {
+        const tenOrFive = funcs.isTenOrFive(5);
+
+        expect(tenOrFive).toBe(true);
+    });
+});
+
+describe("isInRange", () => {
+    test("tests if number is between 20 and 50", () => {
+        const result = funcs.isInRange(35);
+
+        expect(result).toBe(true);
+    });
+});
+
+describe("isInteger", () => {
+    test("is an integer", () => {
+        const int = funcs.isInteger(3);
+
+        expect(int).toBe(true);
+    });
+});
+
+describe("fizzBuzz", () => {
+    test("returns fizz if divis by 3, buzz if by 5, fizzbuzz if both", () => {
+        const fizz = funcs.fizzBuzz(6);
+        const buzz = funcs.fizzBuzz(10);
+        const fizzBuzz = funcs.fizzBuzz(15);
+
+        expect(fizz).toEqual("fizz");
+        expect(buzz).toEqual("buzz");
+        expect(fizzBuzz).toEqual("fizzbuzz");
+    });
+});
+
+describe("isPrime", () => {
+    test("is number prime", () => {
+        const prime = funcs.isPrime(7);
+
+        expect(prime).toBe(true);
+    });
+});
+
+describe("returnFirst", () => {
+    test("returns first item in an array", () => {
+        const first = funcs.returnFirst([1, 2, 3]);
+
+        expect(first).toEqual(1);
+    });
+});
+
+describe("returnLast", () => {
+    test("returns last item in an array", () => {
+        const last = funcs.returnLast([1, 2, 3]);
+
+        expect(last).toEqual(3);
+    });
+});
+
+describe("getArrayLength", () => {
+    test("returns the length of an array", () => {
+        const length = funcs.getArrayLength([1, 2, 3]);
+
+        expect(length).toEqual(3);
+    });
+});
+
+describe("incrementByOne", () => {
+    test("increments each value by 1", () => {
+        const result = funcs.incrementByOne([10, 15, 22]);
+
+        expect(result).toEqual([11, 16, 23]);
+    });
+});
+
+describe("addItemToArray", () => {
+    test("adds an item to the end of an array", () => {
+        const arr = funcs.addItemToArray([1, 2, 3], 4);
+
+        expect(arr).toEqual([1, 2, 3, 4]);
+    });
+});
+
+describe("addItemToFront", () => {
+    test("adds an item to the beginning of an array", () => {
+        const arr = funcs.addItemToFront([2, 3, 4], 1);
+
+        expect(arr).toEqual([1, 2, 3, 4]);
+    });
+});
+
+describe("wordsToSentence", () => {
+    test("adds words in an array together", () => {
+        const sentence = funcs.wordsToSentence([
+            "hello",
+            "darkness",
+            "my",
+            "old",
+            "friend"
+        ]);
+
+        expect(sentence).toEqual("hello darkness my old friend");
+    });
+});
+
+describe("contains", () => {
+    test("checks if array contains an item", () => {
+        const arr = [1, 2, 3, 4, 5];
+        const item = 5;
+        const result = funcs.contains(arr, item);
+
+        expect(result).toBe(true);
+    });
+});
+
+describe("addNumbers", () => {
+    test("adds numbers in an array", () => {
+        const numbers = [5, 10, 15, 20];
+
+        const result = funcs.addNumbers(numbers);
+
+        expect(result).toBe(50);
+    });
+});
+
+describe("averageTestScore", () => {
+    test("averages the scores in an array", () => {
+        const scores = [95, 91, 55, 45, 85];
+
+        const average = funcs.averageTestScore(scores);
+
+        expect(average).toBe((95 + 91 + 55 + 45 + 85) / scores.length);
+    });
+});
+
+describe("largestNumber", () => {
+    test("returns the largest number in an array", () => {
+        const numbers = [1, 2, 3, 4, 5, 6, 16, 7];
+
+        const largestNumber = funcs.largestNumber(numbers);
+
+        expect(largestNumber).toBe(16);
+    });
+});
