ec45757605e7d7ae3a43429e6140caade5f6c7fe
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-04 13:28:45 -0600

    test 2 complete

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,6 +1,6 @@
 const { getBiggest, greeting, isTenOrFive, isInRange, isInteger, fizzBuzz, isPrime, returnFirst, returnLast, getArrayLength, incrementByOne, addItemToArray, addItemToFront, wordsToSentence, contains, addNumbers, averageTestScore, largestNumber } = require('./project-2');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
 describe('get biggest', () => {
     test('numbers work', () => {
         const x = 'string'
@@ -12,4 +12,8 @@ describe('get biggest', () => {
         const y = {}
         expect(getBiggest(x,y)).toBe(null)
     })
+})
+test('receives greeting', () => {
+    const language = 'german'
+    expect(greeting(language)).toBeTruthy()
 })
