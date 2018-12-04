767d9dec593f5248f59e90e39a050bf59320acc4
unknown <quan.nguyen199712@gmail.com>
2018-12-03 19:53:13 -0500

    some more coming

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,24 +1,43 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
+describe("multiply by ten", () => {
+  test("multiply by ten", () => {
+    const test = 5;
+    const result = helpers.multiplyByTen(test);
+    expect(result).toBe(50);
+  });
+});
+describe("subtract by 5", () => {
+  test("subtract 5", () => {
+    const result = helpers.subtractFive(15);
+    expect(result).toBe(10);
+  });
+});
+describe("areSameLength", () => {
+  test(" areSameLength true", () => {
+    const result = helpers.areSameLength("poppy", "lucky");
+    expect(result).toBe(true);
+  });
+  test(" areSameLength false", () => {
+    const result = helpers.areSameLength("popp", "lucky");
+    expect(result).toBe(false);
+  });
+});
+describe("areEqual", () => {
+  test(" areEqualString false", () => {
+    const result = helpers.areEqual("poppy", "lucky");
+    expect(result).toBe(false);
+  });
+  test(" areEqualString true", () => {
+    const result = helpers.areEqual("poppy", "poppy");
+    expect(result).toBe(true);
+  });
+  test("areEqualIntTrue", ()=>{
+      const result= helpers.areEqual(5,5);
+      expect(result).toBe(true)
+  })
+  test("areEqualFalse", ()=>{
+      expect(helpers.areEqual(4,5)).toBe(false)
+  })
+});
 
-test('multiply by ten', ()=>{
-    const test= 5
-    const result = helpers.multiplyByTen(test)
-    expect(result).toBe(50)
-})
-test('subtract 5', ()=>{
-    const result= helpers.subtractFive(15)
-    expect(result).toBe(10)
-})
-test(' areSameLength true', ()=>{
-    const result= helpers.areSameLength('poppy', 'lucky')
-    expect(result).toBe(true)
-})
-test(' areSameLength false', ()=>{
-    const result= helpers.areSameLength('popp', 'lucky')
-    expect(result).toBe(false)
-})
-test(' areEqual false', ()=>{
-    const result= helpers.equal('poppy', 'lucky')
-    expect(result).toBe(false)
-})
 // start testing!
