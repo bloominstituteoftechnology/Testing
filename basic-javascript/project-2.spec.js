ae44906a1ab6905f6e3a099e42b27740a302dab7
Stublink <chrisrey95@gmail.com>
2018-12-04 16:23:07 -0500

    added describe boilerplate for project2 test

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,61 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+
+
+describe('getBiggest function', () => {
+    
+});
+describe('greeting function', () => {
+    
+});
+describe('isTenOrFive function', () => {
+    
+});
+describe('isInRange function', () => {
+    
+});
+describe('isInteger function', () => {
+    
+});
+describe('fizzbuzz function', () => {
+    
+});
+describe('isPrime function', () => {
+    
+});
+describe('returnFirst function', () => {
+    
+});
+describe('returnLast function', () => {
+    
+});
+
+describe(' getArrayLength function', () => {
+    
+});
+describe('incrementByOne function', () => {
+    
+});
+describe('addItemToArray function', () => {
+    
+});
+describe('addItemToFront function', () => {
+    
+});
+describe('wordsToSentence function', () => {
+    
+});
+describe('contains function', () => {
+    
+});
+describe('addNumbers function', () => {
+    
+});
+describe('averageTestScore function', () => {
+    
+});
+describe('largestNumber function', () => {
+    
+});
