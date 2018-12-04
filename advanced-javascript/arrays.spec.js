6eb8c89e63ada0a61ca5e954c21f9a525371e01b
Chiboba <briangfelix@gmail.com>
2018-12-04 15:38:53 -0800

    array stretch

--- advanced-javascript/arrays.spec.js
+++ advanced-javascript/arrays.spec.js
@@ -13,6 +13,15 @@ describe('Arrays', () => {
     return num;
   }
 
+  function findVal(find, val) {
+    if(val === find) {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+
   function reduceVal(memo, item) {
     return memo + item;
   }
@@ -23,7 +32,7 @@ describe('Arrays', () => {
       expect(typeof map).toBe('function');
     });
 
-    it('should returned the array', () => {
+    it('should return the array', () => {
       expect(arrayFunctions.map([1,2,3], giveNum)).toEqual([1,2,3])
     })
   });
@@ -35,8 +44,47 @@ describe('Arrays', () => {
       expect(typeof reduce).toBe('function');
     });
 
-    it('should returned the array', () => {
+    it('should return the acc value', () => {
       expect(arrayFunctions.reduce([1,2,3], reduceVal)).toBe(6)
+    })
+
+  });
+
+  describe('find', () => {
+
+    it('should be a function', () => {
+      const find = arrayFunctions.find;
+      expect(typeof find).toBe('function');
+    });
+
+    it('should the expected value', () => {
+      expect(arrayFunctions.find([1,2,3], findVal, 3)).toBe(3)
+    })
+
+  });
+
+  describe('filter', () => {
+
+    it('should be a function', () => {
+      const filter = arrayFunctions.filter;
+      expect(typeof filter).toBe('function');
+    });
+
+    it('should return an array with just the filtered values', () => {
+      expect(arrayFunctions.filter([1,2,3], findVal, 1)).toEqual([1])
+    })
+
+  });
+
+  describe('flatten', () => {
+
+    it('should be a function', () => {
+      const flatten = arrayFunctions.flatten;
+      expect(typeof flatten).toBe('function');
+    });
+
+    it('should return an array with just the filtered values', () => {
+      expect(arrayFunctions.flatten([1,2,3, [1,2]])).toEqual([1,2,3,1,2])
     })
 
   });
