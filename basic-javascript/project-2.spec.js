6d5d91d74b028ba6a74aefc9c559b300b0552b8c
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-04 13:53:58 -0700

    completed tests (getBiggest -isInteger)

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -5,4 +5,35 @@ test('Jest runs', () => {
 
 });
 
+describe('project-2', () => {
 
+    describe('getBiggest', () => {
+        it('return larger number', () => {
+            expect(funcs.getBiggest(10,5)).toEqual(10)
+        })
+    })
+
+    describe('greeting', () => {
+        it('return greeting in given language', () => {
+            expect(funcs.greeting('German')).toEqual('Guten Tag!')
+        })
+    })
+
+    describe('isTenOrFive', () => {
+        it('returns true if number is 10 or 5', () => {
+            expect(funcs.isTenOrFive(10)).toBeTruthy()
+        })
+    })
+
+    describe('isInRange', () => {
+        it('returns true if number is between 20 and 50', () => {
+            expect(funcs.isInRange(30)).toBeTruthy()
+        })
+    })
+
+    describe('isInteger', () => {
+        it('returns true if number is a whole number', () => {
+            expect(funcs.isInteger(30)).toBeTruthy()
+        })
+    })
+})
