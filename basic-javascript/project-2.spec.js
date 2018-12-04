d700695be427adfa2ce13ac43cd54f44509ffbec
Chad Jemmett <chad.jemmett@gmail.com>
2018-12-04 13:51:18 -0700

    Tried advanced, going back to basic :|

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,13 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+//
+
+describe('these functions on project 2', ()=> {
+  it('will get the biggest number', ()=> {
+
+  })
+
+
+
+ })
