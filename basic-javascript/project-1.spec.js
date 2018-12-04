b9059e13e401e64d70bf75703c9b4379428c4c22
txiong000 <tkx422@gmail.com>
2018-12-04 15:14:03 -0600

    finish basic proj1

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -22,7 +22,91 @@ describe('strings should equal same length', () => {
     test('string should be same length', () => {
         const a = 'hello';
         const b = 'olleh';
-        expect(helpers.areSameLength(a, b)).toBeTruthy();
+        expect(helpers.areSameLength(a, b)).toBe(true);
     })
     
+})
+
+test('param x and y should be equal', ()=> {
+    expect(helpers.areEqual(1, 1)).toBe(true);
+})
+
+test('input should be less than ninety', () => {
+    expect(helpers.lessThanNinety(4)).toBe(true);
+})
+
+test('add function', ()=> {
+    expect(helpers.add(2, 2)).toEqual(4);
+})
+
+test('subtract function', () => {
+    expect(helpers.subtract(3, 2)).toEqual(1);
+})
+
+test('divide function', ()=> {
+    expect(helpers.divide(4, 2)).toEqual(2);
+})
+
+test('multiply function', ()=> {
+    expect(helpers.multiply(5, 5)).toEqual(25);
+})
+
+test('get remainder function', ()=> {
+    expect(helpers.getRemainder(5, 3)).toEqual(2);
+})
+
+test('is even function', ()=> {
+    expect(helpers.isEven(4)).toBe(true);
+})
+
+test('is odd func', ()=> {
+    expect(helpers.isOdd(3)).toBe(true);
+})
+
+test('squared func', ()=> {
+    expect(helpers.square(5)).toEqual(25);
+})
+
+test('cubed func', ()=> {
+    expect(helpers.cube(3)).toEqual(27);
+})
+
+test('raise to power func', ()=> {
+    expect(helpers.raiseToPower(2, 3)).toEqual(8);
+})
+
+test('round number using Math.round', ()=> {
+    expect(helpers.roundNumber(3.2)).toEqual(3);
+})
+
+test('round number using Math.ceil', ()=> {
+    expect(helpers.roundUp(3.5)).toEqual(4);
+})
+
+test('add exclamation point func', ()=> {
+    expect(helpers.addExclamationPoint('hello')).toBe('hello!');
+})
+
+test('combine name func', ()=> {
+    expect(helpers.combineNames('hello', 'world')).toEqual('hello world');
+})
+
+test('greeting func', ()=> {
+    expect(helpers.getGreeting('Lambda')).toEqual('Hello Lambda!');
+})
+
+test('rectangle area', ()=> {
+    expect(helpers.getRectangleArea(2, 3)).toEqual(6);
+})
+
+test('get triangle area func', ()=> {
+    expect(helpers.getTriangleArea(4, 4)).toEqual(8);
+})
+
+test('circle area func', ()=> {
+    expect(helpers.getCircleArea(4)).toEqual(50.26548245743669);
+})
+
+test('rectangular prism volum func', ()=> {
+    expect(helpers.getRectangularPrismVolume(4, 4, 4)).toEqual(64);
 })
