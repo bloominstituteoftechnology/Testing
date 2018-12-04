0a72c5a9c12a5867948ac2042f73d5c970927f96
Axolotl <samuelmachat@gmail.com>
2018-12-04 18:12:29 -0500

    MOOOOOOOOOOOOOOOOOOOOOOAAAAAAAAAAR TEEEEESSTTSS

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -110,3 +110,136 @@ describe('fizzBuzz', () => {
     expect(fizzBuzz(7)).toBe(7)
   })
 })
+
+describe('isPrime', () => {
+  test('returns true if prime', () => {
+    expect(isPrime(7)).toBe(true)
+  })
+
+  test('returns false if not prime', () => {
+    expect(isPrime(6)).toBe(false)
+  })
+})
+
+describe('returnFirst', () => {
+  test('returns first item in array', () => {
+    expect(returnFirst([1, 2])).toBe(1)
+  })
+})
+
+describe('returnLast', () => {
+  test('returns last item in array', () => {
+    expect(returnLast([1, 2])).toBe(2)
+  })
+})
+
+describe('returnLast', () => {
+  test('returns last item in array', () => {
+    expect(returnLast([1, 2])).toBe(2)
+  })
+})
+
+describe('getArrayLength', () => {
+  test('returns array length', () => {
+    expect(getArrayLength([1, 2])).toBe(2)
+  })
+})
+
+describe('incrementByOne', () => {
+  test('returns array with each item incremented by one', () => {
+    expect(incrementByOne([1, 2])).toEqual([2, 3])
+  })
+})
+
+describe('addItemToArray', () => {
+  test('returns array with item added', () => {
+    expect(addItemToArray([1, 2], 3)).toEqual([1, 2, 3])
+  })
+})
+
+describe('addItemToFront', () => {
+  test('returns array with item added to front', () => {
+    expect(addItemToFront([1, 2], 3)).toEqual([3, 1, 2])
+  })
+})
+
+describe('wordsToSentence', () => {
+  test('returns array into sentence', () => {
+    expect(wordsToSentence(['I', 'am', 'not', 'a', 'chicken'])).toEqual(
+      'I am not a chicken'
+    )
+  })
+})
+
+describe('contains', () => {
+  test('returns true if array contains item', () => {
+    expect(contains(['I', 'am', 'not', 'a', 'chicken'], 'I')).toBe(true)
+  })
+
+  test('returns false if array does not contain item', () => {
+    expect(contains(['I', 'am', 'not', 'a', 'chicken'], 'blar')).toBe(false)
+  })
+})
+
+describe('addNumbers', () => {
+  test('returns sum of numbers', () => {
+    expect(addNumbers([2, 3])).toEqual(5)
+  })
+})
+
+describe('averageTestScore', () => {
+  test('returns average', () => {
+    expect(averageTestScore([2, 3])).toEqual(2.5)
+  })
+})
+
+describe('largestNumber', () => {
+  test('returns largest', () => {
+    expect(largestNumber([2, 3])).toEqual(3)
+  })
+
+  test('returns largest if numbers are the same', () => {
+    expect(largestNumber([3, 3])).toEqual(3)
+  })
+
+  test('returns largest if only one argument', () => {
+    expect(largestNumber([3])).toEqual(3)
+  })
+
+  test('returns largest if numbers in different order', () => {
+    expect(largestNumber([3, 2])).toEqual(3)
+  })
+
+  test('returns largest if one number is negative', () => {
+    expect(largestNumber([-3, 2])).toEqual(2)
+  })
+
+  test('returns largest if both numbers are negative', () => {
+    expect(largestNumber([-3, -2])).toEqual(-2)
+  })
+
+  test('returns largest if both numbers are really big', () => {
+    expect(
+      largestNumber([
+        7879384798527497594887498274945,
+        82705948705294870952874098724502480728948475284502482045
+      ])
+    ).toEqual(82705948705294870952874098724502480728948475284502482045)
+  })
+
+  test('throws error if one item is not a number', () => {
+    expect(() => largestNumber(['a', -2])).toThrow()
+  })
+
+  test('throws error arg is a string', () => {
+    expect(() => largestNumber('lkjlakfea')).toThrow()
+  })
+
+  test('throws error arg is an object', () => {
+    expect(() => largestNumber({})).toThrow()
+  })
+
+  test('throws error arg is a number', () => {
+    expect(() => largestNumber(8)).toThrow()
+  })
+})
