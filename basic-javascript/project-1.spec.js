48b472652fd344d4e121df44fc5895405ddaa377
unknown <quan.nguyen199712@gmail.com>
2018-12-03 19:23:23 -0500

    some tests done

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,24 @@
 const helpers = require('./project-1');
 
+test('multiply by ten', ()=>{
+    const test= 5
+    const result = helpers.multiplyByTen(test)
+    expect(result).toBe(50)
+})
+test('subtract 5', ()=>{
+    const result= helpers.subtractFive(15)
+    expect(result).toBe(10)
+})
+test(' areSameLength true', ()=>{
+    const result= helpers.areSameLength('poppy', 'lucky')
+    expect(result).toBe(true)
+})
+test(' areSameLength false', ()=>{
+    const result= helpers.areSameLength('popp', 'lucky')
+    expect(result).toBe(false)
+})
+test(' areEqual false', ()=>{
+    const result= helpers.equal('poppy', 'lucky')
+    expect(result).toBe(false)
+})
 // start testing!
