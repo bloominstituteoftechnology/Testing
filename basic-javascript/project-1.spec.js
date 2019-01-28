250fcc8510cfb94bdec25bd90983013db74f66d9
Aman <amsingh714@gmail.com>
2019-01-28 16:24:26 -0500

    added tests for multiplyby10, subtractby5, samelength

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,26 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
 
 // start testing!
+describe("Project-1", () => {
+  describe("multiplyByTen method", () => {
+    test("Multiply by 10", () => {
+      expect(helpers.multiplyByTen(1)).toEqual(10);
+    });
+  })
+
+  describe("subtractFive method", () => {
+    test("Subtract by 5", () => {
+      expect(helpers.subtractFive(10)).toEqual(5);
+    });
+  });
+
+  describe("areSameLength method", () => {
+    test("Same length", () => {
+      expect(helpers.areSameLength("hello", "three"));
+    });
+  });
+
+  
+
+
+}); // Project 1
