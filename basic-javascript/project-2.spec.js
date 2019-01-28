c04d8bbaaa2710876b5c4d70a37264d4fce51147
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-28 18:05:13 -0500

    fixed some errors and prime test completed

--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -2,7 +2,7 @@ const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
 
-// describe('the functions in the `project-2.js` file', () => {
+describe('the functions in the `project-2.js` file', () => {
 
     describe('the getBiggest function', () => {
         it('should retrun the Biggest of the 2 integers given.', () => {
@@ -15,7 +15,7 @@ const funcs = require('./project-2');
     });
 
     describe('the `greeting` function', () => {
-        describe('the greeting function', () => {
+        it('shold return the correct string for each greeting case', () => {
             expect(funcs.greeting('German')).toEqual('Guten Tag!');
             expect(funcs.greeting('Spanish')).toEqual('Hola!');
             expect(funcs.greeting('')).toEqual('Hello!');
@@ -25,40 +25,71 @@ const funcs = require('./project-2');
 
 
     describe('the is Ten or Five function', () => {
-        expect(funcs.isTenOrFive(5)).toBe(true);
-        expect(funcs.isTenOrFive(10)).toBe(true);
-        expect(funcs.isTenOrFive(7)).toBe(false);
-        expect(funcs.isTenOrFive(3)).toBe(false);
-        expect(funcs.isTenOrFive('7')).toBe(false);
-        expect(funcs.isTenOrFive(13)).toBe(false);
+        it('should retun true if equal to 5 or 10', () => {
+            expect(funcs.isTenOrFive(5)).toBe(true);
+            expect(funcs.isTenOrFive(10)).toBe(true);
+
+        });
+
+        it('should return false if not 5 0r 10', () => {
+            expect(funcs.isTenOrFive(7)).toBe(false);
+            expect(funcs.isTenOrFive(3)).toBe(false);
+            expect(funcs.isTenOrFive('7')).toBe(false);
+            expect(funcs.isTenOrFive(13)).toBe(false);
+
+        });
 
         
     });
     describe('the is in Range function', () => {
-        expect(funcs.isInRange(25)).toEqual(true);
+        it('should retutn true if within the stated range', () => {
+            expect(funcs.isInRange(25)).toEqual(true);
 
-        expect(funcs.isInRange(5)).toEqual(false);
-        expect(funcs.isInRange(125)).toEqual(false);
+        });
+        it('should return return false if outside the range', () => {
+            expect(funcs.isInRange(5)).toEqual(false);
+            expect(funcs.isInRange(125)).toEqual(false);
+
+        })
+
 
     });
 
     describe('the is Integer function', () => {
-        expect(funcs.isInteger(7)).toBe(true);
-        expect(funcs.isInteger(17)).toBe(true);
+        it('should return true if an integer', () => {
+            expect(funcs.isInteger(7)).toBe(true);
+            expect(funcs.isInteger(17)).toBe(true);
+        });
 
-        expect(funcs.isInteger(17.5)).toBe(false);
-        expect(funcs.isInteger(2.222111111111111111)).toBe(false);
+        it('should return false if  not an integer', () => {
+            expect(funcs.isInteger(17.5)).toBe(false);
+            expect(funcs.isInteger(2.222111111111111111)).toBe(false);           
+        });
+
 
     });
     describe('the fizz buzz function', () => {
-        expect(funcs.fizzBuzz(15)).toEqual('fizzbuzz');
-        expect(funcs.fizzBuzz(3)).toBe('fizz');
-        expect(funcs.fizzBuzz(5)).toEqual('buzz');
+        it('should return the correct string', () => {
+            expect(funcs.fizzBuzz(15)).toEqual('fizzbuzz');
+            expect(funcs.fizzBuzz(3)).toBe('fizz');
+            expect(funcs.fizzBuzz(5)).toEqual('buzz');
+
+        });
 
 
     });
-    describe('the is prime function', () => {
-        
+    describe('the prime function', () => {
+        it('should return true for any prinme number', () => {
+            expect(funcs.isPrime(5)).toBe(true);
+        });
+
+        it('should return false for any non-prime number', () => {
+            expect(funcs.isPrime(0)).toBe(false);
+            expect(funcs.isPrime(-5)).toBe(false);
+            expect(funcs.isPrime(1)).toBe(false);
+        });
+
+
     });
     describe('', () => {});
     // describe('', () => {});
@@ -89,4 +120,4 @@ const funcs = require('./project-2');
 
     // });
 
-// });
+});
